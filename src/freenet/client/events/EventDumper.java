182d195e16b3709946ef8a607f8411b1b758fe5c
Matthew Toseland <toad@amphibian.dyndns.org>
2005-11-25 01:07:19 +0000

    213: drop the number at the beginning of the status dumped to stderr.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@7594 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/client/events/EventDumper.java
+++ src/freenet/client/events/EventDumper.java
@@ -3,7 +3,7 @@ package freenet.client.events;
 public class EventDumper implements ClientEventListener {
 
 	public void receive(ClientEvent ce) {
-		System.err.println(ce.getCode()+":"+ce.getDescription());
+		System.err.println(ce.getDescription());
 	}
 
 }
