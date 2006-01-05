8aba5b7633077e85c81eb749e7ce47f23c470da3
Florent Daignière <nextgens@freenetproject.org>
2006-01-05 20:46:44 +0000

    Get rid of old builds
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@7764 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -20,10 +20,10 @@ public abstract class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	public static final int buildNumber = 317;
+	public static final int buildNumber = 318;
 
 	/** Oldest build of Fred we will talk to */
-	public static final int lastGoodBuild = 310;
+	public static final int lastGoodBuild = 318;
 
 	/** The highest reported build of fred */
 	public static int highestSeenBuild = buildNumber;
