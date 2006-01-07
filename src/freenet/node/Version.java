1fb05f464fcf890613f4a134288e0982a4e43861
sleon <sleon@sleon.dyndns.org>
2006-01-07 03:01:14 +0000

    to make all nodes provide correct output data for statistics
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@7789 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -20,10 +20,10 @@ public abstract class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	public static final int buildNumber = 328;
+	public static final int buildNumber = 329;
 
 	/** Oldest build of Fred we will talk to */
-	public static final int lastGoodBuild = 318;
+	public static final int lastGoodBuild = 329;
 
 	/** The highest reported build of fred */
 	public static int highestSeenBuild = buildNumber;
