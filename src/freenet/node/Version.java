41e8ea4803e968ada8f162f3ae8076b7c2e4ed82
Florent Daignière <nextgens@freenetproject.org>
2006-01-09 12:49:01 +0000

    fixes a compilation problem with eclipse
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@7825 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -20,7 +20,7 @@ public abstract class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	public static final int buildNumber = SVN_REVISION
+	public static final int buildNumber = 0 ;
 
 	/** Oldest build of Fred we will talk to */
 	public static final int lastGoodBuild = 332;
