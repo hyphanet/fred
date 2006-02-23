56284ea4986d36fc379fa04161c620528a5afe2a
Florent Daignière <nextgens@freenetproject.org>
2006-02-23 14:36:05 +0000

    476: Mandatory update
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@8131 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -20,10 +20,10 @@ public abstract class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 475;
+	private static final int buildNumber = 476;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int lastGoodBuild = 403;
+	private static final int lastGoodBuild = 475;
 
 	public static final int buildNumber() {
 		return buildNumber;
