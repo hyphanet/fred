0a434edb50a2b1c024569cbfee0ce49cf72a0642
Dave Baker <dbkr@freenetproject.org>
2006-05-07 14:56:45 +0000

    687: Tweak the peer parsing regexp to remove whitespace at the end which was causing references to be rejected because of whitespace on the end of the port number.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@8643 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/clients/http/DarknetConnectionsToadlet.java
+++ src/freenet/clients/http/DarknetConnectionsToadlet.java
@@ -260,7 +260,7 @@ public class DarknetConnectionsToadlet extends Toadlet {
 			} else if (reftext.length() > 0) {
 				// read from post data or file upload
 				// this slightly scary looking regexp chops any extra characters off the beginning or ends of lines and removes extra line breaks
-				ref = reftext.replaceAll(".*?((?:[\\w,\\.]+\\=[^\r\n]+)|(?:End)).*(?:\\r?\\n)*", "$1\n");
+				ref = reftext.replaceAll(".*?((?:[\\w,\\.]+\\=[^\r\n]+?)|(?:End))[ \\t]*(?:\\r?\\n)+", "$1\n");
 			} else {
 				this.sendErrorPage(ctx, 200, "OK", "Failed to add node: Could not detect either a node reference or a URL. Please <a href=\".\">Try again</a>.");
 				request.freeParts();
