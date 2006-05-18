bd2bd1cc3ac7c82399210f4ec1eb5a0884783721
Florent Daignière <nextgens@freenetproject.org>
2006-05-18 10:40:36 +0000

    Updated MeaningfulNodeNameUserAlert's message
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@8760 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/MeaningfulNodeNameUserAlert.java
+++ src/freenet/node/MeaningfulNodeNameUserAlert.java
@@ -15,7 +15,8 @@ public class MeaningfulNodeNameUserAlert implements UserAlert {
 		return "It seems that your node's name isn't defined. Setting "+
 		"up a node name doesn't affect your anonymity in any way but "+
 		"is useful for your peers to know who you are in case they have "+
-		"to reach you. You can change the node's name at the Configuration page.";
+		"to reach you. You can change the node's name at the Configuration page. "+
+		"Putting your e-mail address there is generally speaking a good idea.";
 	}
 
 	public short getPriorityClass() {
