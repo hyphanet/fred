fe95def26bd512fd6241d69da28a88c1aa7dde56
Matthew Toseland <toad@amphibian.dyndns.org>
2006-05-24 19:35:18 +0000

    737: Make 732 mandatory.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@8848 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -18,10 +18,10 @@ public abstract class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 736;
+	private static final int buildNumber = 737;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int lastGoodBuild = 698;
+	private static final int lastGoodBuild = 732;
 
 	public static final int buildNumber() {
 		return buildNumber;
