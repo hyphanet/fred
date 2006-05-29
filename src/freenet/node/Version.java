63563b752ff6ce79bffc40eb9fca55140a55b7fb
Matthew Toseland <toad@amphibian.dyndns.org>
2006-05-29 20:45:41 +0000

    752: Make recent changes mandatory. Network reset due to fixing a variety of bugs which caused backoff. Thanks go to Zothar, MRogers, and Nextgens.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@8934 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -18,10 +18,10 @@ public abstract class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 751;
+	private static final int buildNumber = 752;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int lastGoodBuild = 732;
+	private static final int lastGoodBuild = 752;
 
 	public static final int buildNumber() {
 		return buildNumber;
