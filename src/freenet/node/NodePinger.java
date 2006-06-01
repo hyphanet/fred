555d32d08296f62a90e2cd662ea1b202e1ced866
Matthew Toseland <toad@amphibian.dyndns.org>
2006-06-01 18:24:08 +0000

    768: Use median, not mean, of node ping times to calculate overall average ping time. Exclude backed off nodes unless all are backed off. Add some temporal averaging to prevent nodes going in and out of backoff from influencing it too much.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@8969 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/NodePinger.java
+++ src/freenet/node/NodePinger.java
@@ -1,6 +1,9 @@
 package freenet.node;
 
+import java.util.Arrays;
+
 import freenet.support.Logger;
+import freenet.support.math.TimeDecayingRunningAverage;
 
 /**
  * Track average round-trip time for each peer node, get a geometric mean.
@@ -8,9 +11,13 @@ import freenet.support.Logger;
 public class NodePinger implements Runnable {
 
 	private double meanPing = 0;
+	/** Average over time to avoid nodes flitting in and out of backoff having too much impact. */
+	private TimeDecayingRunningAverage tdra;
 	
 	NodePinger(Node n) {
 		this.node = n;
+		this.tdra = new TimeDecayingRunningAverage(0.0, 30*1000, // 30 seconds
+				0.0, Double.MAX_VALUE);
 		Thread t = new Thread(this, "Node pinger");
 		t.setDaemon(true);
 		t.start();
@@ -31,21 +38,39 @@ public class NodePinger implements Runnable {
 
 	/** Recalculate the mean ping time */
 	void recalculateMean(PeerNode[] peers) {
-		int peerCount = 0;
-		double total = 1.0;
+		if(peers.length == 0) return;
+		double d = calculateMedianPing(peers);
+		tdra.report(d);
+		meanPing = tdra.currentValue();
+		Logger.minor(this, "Reporting ping to temporal averager: "+d+" result "+meanPing);
+	}
+	
+	double calculateMedianPing(PeerNode[] peers) {
+		
+		double[] allPeers = new double[peers.length];
+		
+		/** Not backed off peers' ping times */
+		double[] nbPeers = new double[peers.length];
+		
+		/** Number of not backed off peers */
+		int nbCount = 0;
+		
 		for(int i=0;i<peers.length;i++) {
 			PeerNode peer = peers[i];
-			if(!peer.isConnected()) continue;
-			peerCount++;
-			double avgPingTime = peer.averagePingTime();
-			Logger.minor(this, "Peer: "+peer.getPeer()+", avgPingTime: "+avgPingTime);
-			total *= avgPingTime;
+			double pingTime = peer.averagePingTime();
+			if(!peer.isRoutingBackedOff()) {
+				nbPeers[nbCount++] = pingTime;
+			}
+			allPeers[i] = pingTime;
 		}
-		if(peerCount > 0) {
-			total = Math.pow(total, 1.0 / peerCount);
-			meanPing = total;
-			Logger.minor(this, "Mean ping: "+meanPing+"ms");
+		
+		if(nbCount > 0) {
+			Arrays.sort(nbPeers, 0, nbCount);
+			return nbPeers[nbCount / 2]; // round down - prefer lower
 		}
+		
+		Arrays.sort(allPeers);
+		return allPeers[peers.length / 2];
 	}
 
 	public double averagePingTime() {
