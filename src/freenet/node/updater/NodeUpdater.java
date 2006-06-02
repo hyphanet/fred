6a74cb0e3479401039c85764f14894a7c3e14f48
Matthew Toseland <toad@amphibian.dyndns.org>
2006-06-02 21:31:04 +0000

    use windows linefeeds when updating on windows.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@9009 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/updater/NodeUpdater.java
+++ src/freenet/node/updater/NodeUpdater.java
@@ -263,15 +263,15 @@ public class NodeUpdater implements ClientCallback, USKCallback {
 					boolean stillSucceeded = false;
 					while((line = br.readLine()) != null) {
 						if(line.equals("wrapper.java.classpath.1="+fRunning.getName())) {
-							bw.write("wrapper.java.classpath.1="+fNew.getName()+"\n");
+							bw.write("wrapper.java.classpath.1="+fNew.getName()+"\r\n");
 							succeeded = true;
 						} else if(line.equals("wrapper.java.classpath.2="+fRunning.getName())) {
-							bw.write("wrapper.java.classpath.2="+fNew.getName()+"\n");
+							bw.write("wrapper.java.classpath.2="+fNew.getName()+"\r\n");
 							succeeded = true;
 						} else {
 							if(line.equals("wrapper.restart.reload_configuration=TRUE"))
 								stillSucceeded = true;
-							bw.write(line+"\n");
+							bw.write(line+"\r\n");
 						}
 					}
 					bw.close();
