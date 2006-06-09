8559c83e91be1df22a94513777d97e30c31ad6b9
David Sowder <freenet@david.sowder.com>
2006-06-09 13:21:09 +0000

    Limit concurrently running ARK Fetch requests and time ARKFetcher starts in couldNotSendHandshake() like we're already doing in sentHandshake().
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@9095 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/PeerNode.java
+++ src/freenet/node/PeerNode.java
@@ -885,8 +885,18 @@ public class PeerNode implements PeerContext {
         }
         // Don't fetch ARKs for peers we have verified (through handshake) to be incompatible with us
         if(handshakeCount == MAX_HANDSHAKE_COUNT && !(verifiedIncompatibleOlderVersion || verifiedIncompatibleNewerVersion)) {
-        	Logger.normal( this, "Starting ARK Fetcher after "+handshakeCount+" failed handshakes(2) for "+getPeer()+" with identity '"+getIdentityString()+"'");
-        	arkFetcher.start();
+        	int numARKFetchers = node.getNumARKFetchers();
+        	if( numARKFetchers >= 30 ) {  // Limit concurrent ARK Fetch Requests to 30 since we UserAlert at 20 disconnected peers anyway
+				Logger.minor( this, "Not starting ARK Fetcher after "+handshakeCount+" failed handshakes for "+getPeer()+" with identity '"+getIdentityString()+"' because there are already 30 ARK Fetchers running.");
+        	} else {
+				Logger.normal( this, "Starting ARK Fetcher after "+handshakeCount+" failed handshakes for "+getPeer()+" with identity '"+getIdentityString()+"'");
+				long arkFetcherStartTime1 = System.currentTimeMillis();
+				arkFetcher.start();
+				long arkFetcherStartTime2 = System.currentTimeMillis();
+				if((arkFetcherStartTime2 - arkFetcherStartTime1) > 500) {
+					Logger.normal(this, "arkFetcherStartTime2 is more than half a second after arkFetcherStartTime1 ("+(arkFetcherStartTime2 - arkFetcherStartTime1)+") working on "+myName);
+				}
+			}
         }
     }
 
