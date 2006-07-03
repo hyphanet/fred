7d1631694948ce8a773004a23b06e40792e45de5
Florent Daignière <nextgens@freenetproject.org>
2006-07-03 20:56:17 +0000

    maybe it doesn't like the "Maxvalue" 				WrapperManager.signalStarting((int) Integer.MAX_VALUE);
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@9448 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Node.java
+++ src/freenet/node/Node.java
@@ -1408,7 +1408,7 @@ public class Node {
 				System.err.println("Could not open store: "+e);
 				e.printStackTrace();
 				System.err.println("Attempting to reconstruct...");
-				WrapperManager.signalStarting((int) Integer.MAX_VALUE);
+				WrapperManager.signalStarting(1000000);
 				tmp = new BerkeleyDBFreenetStore(storeDir.getPath()+File.separator+"store-"+portNumber, maxStoreKeys, 32768, CHKBlock.TOTAL_HEADERS_LENGTH, BerkeleyDBFreenetStore.TYPE_CHK);
 			}
 			chkDatastore = tmp;
@@ -1423,7 +1423,7 @@ public class Node {
 				System.err.println("Could not open store: "+e);
 				e.printStackTrace();
 				System.err.println("Attempting to reconstruct...");
-				WrapperManager.signalStarting((int) Integer.MAX_VALUE);
+				WrapperManager.signalStarting(1000000);
 				tmp = new BerkeleyDBFreenetStore(storeDir.getPath()+File.separator+"pubkeystore-"+portNumber, maxStoreKeys, DSAPublicKey.PADDED_SIZE, 0, BerkeleyDBFreenetStore.TYPE_PUBKEY);
 			}
 			this.pubKeyDatastore = tmp;
