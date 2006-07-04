328fe8f7702708d0d5f10f43b449aac9fcccdc74
David Sowder <freenet@david.sowder.com>
2006-07-04 02:16:07 +0000

    Sort FCP client-to-node class names in FCPMessage.create().  No functional changes.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@9462 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/fcp/FCPMessage.java
+++ src/freenet/node/fcp/FCPMessage.java
@@ -30,38 +30,38 @@ public abstract class FCPMessage {
 	 * Create a message from a SimpleFieldSet, and the message's name, if possible. 
 	 */
 	public static FCPMessage create(String name, SimpleFieldSet fs, BucketFactory bfTemp, PersistentTempBucketFactory bfPersistent) throws MessageInvalidException {
-		if(name.equals(ClientHelloMessage.name))
-			return new ClientHelloMessage(fs);
 		if(name.equals(ClientGetMessage.name))
 			return new ClientGetMessage(fs);
+		if(name.equals(ClientHelloMessage.name))
+			return new ClientHelloMessage(fs);
+		if(name.equals(ClientPutComplexDirMessage.name))
+			return new ClientPutComplexDirMessage(fs, bfTemp, bfPersistent);
+		if(name.equals(ClientPutDiskDirMessage.name))
+			return new ClientPutDiskDirMessage(fs);
 		if(name.equals(ClientPutMessage.name))
 			return new ClientPutMessage(fs);
 		if(name.equals(GenerateSSKMessage.name))
 			return new GenerateSSKMessage(fs);
+		if(name.equals(GetRequestStatusMessage.name))
+			return new GetRequestStatusMessage(fs);
 		if(name.equals(ListPeersMessage.name))
 			return new ListPeersMessage(fs);
 		if(name.equals(ListPersistentRequestsMessage.name))
 			return new ListPersistentRequestsMessage(fs);
-		if(name.equals(RemovePeer.name))
-			return new RemovePeer(fs);
-		if(name.equals(RemovePersistentRequest.name))
-			return new RemovePersistentRequest(fs);
-		if(name.equals(WatchGlobal.name))
-			return new WatchGlobal(fs);
 		if(name.equals(ModifyPeer.name))
 			return new ModifyPeer(fs);
 		if(name.equals(ModifyPersistentRequest.name))
 			return new ModifyPersistentRequest(fs);
-		if(name.equals(ClientPutDiskDirMessage.name))
-			return new ClientPutDiskDirMessage(fs);
-		if(name.equals(ClientPutComplexDirMessage.name))
-			return new ClientPutComplexDirMessage(fs, bfTemp, bfPersistent);
-		if(name.equals(SubscribeUSKMessage.name))
-			return new SubscribeUSKMessage(fs);
-		if(name.equals(GetRequestStatusMessage.name))
-			return new GetRequestStatusMessage(fs);
+		if(name.equals(RemovePeer.name))
+			return new RemovePeer(fs);
+		if(name.equals(RemovePersistentRequest.name))
+			return new RemovePersistentRequest(fs);
 		if(name.equals(ShutdownMessage.name))
 			return new ShutdownMessage();
+		if(name.equals(SubscribeUSKMessage.name))
+			return new SubscribeUSKMessage(fs);
+		if(name.equals(WatchGlobal.name))
+			return new WatchGlobal(fs);
 		if(name.equals("Void"))
 			return null;
 		throw new MessageInvalidException(ProtocolErrorMessage.INVALID_MESSAGE, "Unknown message name "+name, null);
