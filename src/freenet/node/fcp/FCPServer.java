a1e703a75427500674898bbfbff74ecc5edbd0f7
Matthew Toseland <toad@amphibian.dyndns.org>
2006-07-14 15:39:03 +0000

    trivial
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@9599 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/fcp/FCPServer.java
+++ src/freenet/node/fcp/FCPServer.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
