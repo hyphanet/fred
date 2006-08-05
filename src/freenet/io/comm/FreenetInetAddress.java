7f0d6674832dec5b7aa2bfdc8e8a7fc49d01faca
Matthew Toseland <toad@amphibian.dyndns.org>
2006-08-05 00:19:55 +0000

    remove unused import
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@9893 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/io/comm/FreenetInetAddress.java
+++ src/freenet/io/comm/FreenetInetAddress.java
@@ -2,7 +2,6 @@ package freenet.io.comm;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.EOFException;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
