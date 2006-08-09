d378c04dc945ac96c7dcce34af60486fff175e31
David Roden <bombe@pterodactylus.net>
2006-08-09 21:12:25 +0000

    fix missing decimal digit in progress percentage
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@10008 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/clients/http/QueueToadlet.java
+++ src/freenet/clients/http/QueueToadlet.java
@@ -479,9 +479,9 @@ public class QueueToadlet extends Toadlet {
 			NumberFormat nf = NumberFormat.getInstance();
 			nf.setMaximumFractionDigits(1);
 			if (finalized) {
-				progressBar.addChild("div", new String[] { "class", "title" }, new String[] { "progress_fraction_finalized", "finalized" }, (int) ((fetched / (double) min) * 100) + "%");
+				progressBar.addChild("div", new String[] { "class", "title" }, new String[] { "progress_fraction_finalized", "finalized" }, nf.format((int) ((fetched / (double) min) * 1000) / 10.0) + "%");
 			} else {
-				progressBar.addChild("div", new String[] { "class", "title" }, new String[] { "progress_fraction_not_finalized", "not finalized" }, (int) ((fetched / (double) min) * 100) + "%");
+				progressBar.addChild("div", new String[] { "class", "title" }, new String[] { "progress_fraction_not_finalized", "not finalized" }, nf.format((int) ((fetched / (double) min) * 1000) / 10.0)+ "%");
 			}
 		}
 		return progressCell;
