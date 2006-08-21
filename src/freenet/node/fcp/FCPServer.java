4e9934664b6065bc52df0572cae15d3301e6c99c
Jogy <jogy@freenetproject.org>
2006-08-21 20:11:11 +0000

    only a textual change: that's "IP" not "Ip"
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@10232 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/fcp/FCPServer.java
+++ src/freenet/node/fcp/FCPServer.java
@@ -296,7 +296,7 @@ public class FCPServer implements Runnable {
 		fcpConfig.register("enabled", true, 2, true, "Is FCP server enabled ?", "Is FCP server enabled ?", new FCPEnabledCallback(core));
 		fcpConfig.register("port", FCPServer.DEFAULT_FCP_PORT /* anagram of 1984, and 1000 up from old number */,
 				2, true, "FCP port number", "FCP port number", new FCPPortNumberCallback(core));
-		fcpConfig.register("bindTo", "127.0.0.1", 2, true, "Ip address to bind to", "Ip address to bind the FCP server to", new FCPBindtoCallback(core));
+		fcpConfig.register("bindTo", "127.0.0.1", 2, true, "IP address to bind to", "IP address to bind the FCP server to", new FCPBindtoCallback(core));
 		fcpConfig.register("allowedHosts", "127.0.0.1", 2, true, "Allowed hosts", "Hostnames or IP addresses that are allowed to connect to the FCP server. May be a comma-separated list of hostnames, single IPs and even CIDR masked IPs like 192.168.0.0/24", new FCPAllowedHostsCallback(core));
 		PersistentDownloadsEnabledCallback cb1;
 		PersistentDownloadsFileCallback cb2;
