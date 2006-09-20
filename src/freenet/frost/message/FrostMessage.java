7710214cc1cc3d52f3805586f3cb9ac37cf62535
Volodya Mozhenkov <Volodya@WhenGendarmeSleeps.org>
2006-09-20 06:20:11 +0000

    Removing 4 more headers (2 of which become wrong due to the previous revert) to be consistant.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@10491 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/frost/message/FrostMessage.java
+++ src/freenet/frost/message/FrostMessage.java
@@ -1,23 +1,3 @@
-/*
-  FrostMessageObject.java / Frost
-  Copyright (C) 2003-2006  Frost Project <jtcfrost.sourceforge.net>
-  Public Domain 2006  VolodyA! V A <volodya@whengendarmesleeps.org>
-
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License as
-  published by the Free Software Foundation; either version 2 of
-  the License, or (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-  General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
-
 package freenet.frost.message;
 
 import java.util.*;
