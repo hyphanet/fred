e3836ae2287aeefb62599b2c7e5d5322db6e6258
Matthew Toseland <toad@amphibian.dyndns.org>
2006-11-29 23:41:57 +0000

    Drop unnecessary comments
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@11127 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/pluginmanager/PluginNotFoundException.java
+++ src/freenet/pluginmanager/PluginNotFoundException.java
@@ -8,22 +8,18 @@ public class PluginNotFoundException extends Exception {
 
 	public PluginNotFoundException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public PluginNotFoundException(String arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 	public PluginNotFoundException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
 	}
 
 	public PluginNotFoundException(Throwable arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 }
