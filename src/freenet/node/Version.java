555ffdce3446fea787f0fbdbb9fa33b257c67aef
Matthew Toseland <toad@amphibian.dyndns.org>
2006-12-01 17:52:48 +0000

    1006: Another fix to the firefox 2 problem (warn the user that download-to-disk won't necessarily save them). Fix a minor bug relating to invalid chars in URLs. Add EntryPoint to the bookmarks and update Indicia's index.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@11173 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -24,7 +24,7 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1005;
+	private static final int buildNumber = 1006;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 990;
