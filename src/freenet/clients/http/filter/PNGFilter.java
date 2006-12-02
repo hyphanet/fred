459e9676e781db6909bd60fbec79195490212059
Matthew Toseland <toad@amphibian.dyndns.org>
2006-12-02 16:42:54 +0000

    Always close
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@11194 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/clients/http/filter/PNGFilter.java
+++ src/freenet/clients/http/filter/PNGFilter.java
@@ -29,18 +29,22 @@ public class PNGFilter implements ContentDataFilter {
 		InputStream is = data.getInputStream();
 		BufferedInputStream bis = new BufferedInputStream(is);
 		DataInputStream dis = new DataInputStream(bis);
-		// Check the header
-		byte[] headerCheck = new byte[pngHeader.length];
-		dis.read(headerCheck);
-		if(!Arrays.equals(headerCheck, pngHeader)) {
-			// Throw an exception
-			String message = "The file you tried to fetch is not a PNG. It does not include a valid PNG header. "+
+		try {
+			// Check the header
+			byte[] headerCheck = new byte[pngHeader.length];
+			dis.read(headerCheck);
+			if(!Arrays.equals(headerCheck, pngHeader)) {
+				// Throw an exception
+				String message = 
+					"The file you tried to fetch is not a PNG. It does not include a valid PNG header. "+
 					"It might be some other file format, and your browser may do something horrible with it, "+
 					"therefore we have blocked it."; 
-			throw new DataFilterException("Not a PNG - invalid header", "Not a PNG - invalid header",
-					"<p>"+message+"</p>", new HTMLNode("p").addChild("#", message));
+				throw new DataFilterException("Not a PNG - invalid header", "Not a PNG - invalid header",
+						"<p>"+message+"</p>", new HTMLNode("p").addChild("#", message));
+			}
+		} finally {
+			dis.close();
 		}
-		dis.close();
 		return data;
 	}
 
