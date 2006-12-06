edf47fae603f092bfb9421fec56fc4bb8907a9be
Matthew Toseland <toad@amphibian.dyndns.org>
2006-12-06 01:56:32 +0000

    Comments
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@11262 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/io/comm/UdpSocketManager.java
+++ src/freenet/io/comm/UdpSocketManager.java
@@ -674,8 +674,8 @@ public class UdpSocketManager extends Thread {
      */
     public int getMaxPacketSize() {
     	return 1472-28; // officially GRE is 1476 and PPPoE is 1492.
-    	// unofficially, PPPoE is often 1472 (seen in the wild). So use that.
-    	// Minus 28 bytes for UDP/IP header.
+    	// unofficially, PPPoE is often 1472 (seen in the wild). Also PPPoATM is sometimes 1472. 
+    	// So use that. Minus 28 bytes for UDP/IP header.
     }
 
 	/**
