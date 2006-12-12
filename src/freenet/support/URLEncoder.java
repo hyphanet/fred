5e59e8fed89c7d82e13d2add486aeddff0a8c7fc
Matthew Toseland <toad@amphibian.dyndns.org>
2006-12-12 00:25:21 +0000

    Indent.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@11348 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/support/URLEncoder.java
+++ src/freenet/support/URLEncoder.java
@@ -8,45 +8,45 @@ import java.io.UnsupportedEncodingException;
  * don't turn spaces into +'s, and we allow through non-ascii characters unless told not to.
  */
 public class URLEncoder {
-  // Moved here from FProxy by amphibian
-  final static String safeURLCharacters = "*-_./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz";
+	// Moved here from FProxy by amphibian
+	final static String safeURLCharacters = "*-_./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz";
 
-  /**
-   * Encode a string for inclusion in a URI.
-   *
-   * @param  URL  String to encode
-   * @param force List of characters (in the form of a string) which must be encoded as well as the built-in.
-   * @return      HTML-safe version of string
-   */
-  public final static String encode(String URL, String force, boolean ascii) {
-    StringBuffer enc = new StringBuffer(URL.length());
-    for (int i = 0; i < URL.length(); ++i) {
-      char c = URL.charAt(i);
-      if (((safeURLCharacters.indexOf(c) >= 0) || ((!ascii) && c >= 0200)) 
-    		  && (force == null || force.indexOf(c) < 0)) {
-        enc.append(c);
-      } else {
-    	try {
-			byte[] encoded = ("" + c).getBytes("UTF-8");
-			for(int j=0;j<encoded.length;j++) {
-				byte b = encoded[j];
-				int x = b & 0xFF;
-				if(x < 16)
-					enc.append("%0");
-				else
-					enc.append('%');
-				enc.append(Integer.toHexString(x));
+	/**
+	 * Encode a string for inclusion in a URI.
+	 *
+	 * @param  URL  String to encode
+	 * @param force List of characters (in the form of a string) which must be encoded as well as the built-in.
+	 * @return      HTML-safe version of string
+	 */
+	public final static String encode(String URL, String force, boolean ascii) {
+		StringBuffer enc = new StringBuffer(URL.length());
+		for (int i = 0; i < URL.length(); ++i) {
+			char c = URL.charAt(i);
+			if (((safeURLCharacters.indexOf(c) >= 0) || ((!ascii) && c >= 0200))
+					&& (force == null || force.indexOf(c) < 0)) {
+				enc.append(c);
+			} else {
+				try {
+					byte[] encoded = ("" + c).getBytes("UTF-8");
+					for (int j = 0; j < encoded.length; j++) {
+						byte b = encoded[j];
+						int x = b & 0xFF;
+						if (x < 16)
+							enc.append("%0");
+						else
+							enc.append('%');
+						enc.append(Integer.toHexString(x));
+					}
+				} catch (UnsupportedEncodingException e) {
+					throw new Error(e);
+				}
 			}
-		} catch (UnsupportedEncodingException e) {
-			throw new Error(e);
 		}
-      }
-    }
-    return enc.toString();
-  }
+		return enc.toString();
+	}
 
-  public static String encode(String s) {
-	return encode(s, null, false);
-  }
+	public static String encode(String s) {
+		return encode(s, null, false);
+	}
 
 }
