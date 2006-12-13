e6da05fd9d88350d426a776d70fdb645b538f19c
Matthew Toseland <toad@amphibian.dyndns.org>
2006-12-13 21:57:43 +0000

    Some member functions
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@11385 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/plugin/api/FreenetPluginManager.java
+++ src/freenet/plugin/api/FreenetPluginManager.java
@@ -1,8 +1,20 @@
 package freenet.plugin.api;
 
+import java.util.Random;
+
 /**
  * The plugin's interface to the Freenet node.
  */
 public interface FreenetPluginManager {
 
+	/** Node version */
+	public long version();
+	
+	/** Node SVN version, if known, otherwise -1 */
+	public long internalVersion();
+	
+	/** Random number generator */
+	public Random random();
+	
+	
 }
