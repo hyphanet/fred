2cd9dec933fc549c8b13ff9ab08e67a5955d38c7
Matthew Toseland <toad@amphibian.dyndns.org>
2006-12-15 20:11:16 +0000

    Handle an impossible crash.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@11418 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/support/FileLoggerHook.java
+++ src/freenet/support/FileLoggerHook.java
@@ -20,6 +20,7 @@ import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Locale;
+import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
 import java.util.TimeZone;
 import java.util.Vector;
@@ -823,6 +824,7 @@ public class FileLoggerHook extends LoggerHook {
 	private static final int LINE_OVERHEAD = 60;
 	
 	public void logString(byte[] b) {
+		int noElementCount = 0;
 		synchronized (list) {
 			int sz = list.size();
 			list.add(b);
@@ -831,7 +833,19 @@ public class FileLoggerHook extends LoggerHook {
 			if ((list.size() > MAX_LIST_SIZE) || (listBytes > MAX_LIST_BYTES)) {
 				while ((list.size() > (MAX_LIST_SIZE * 0.9F))
 					|| (listBytes > (MAX_LIST_BYTES * 0.9F))) {
-					byte[] ss = (byte[]) (list.removeFirst());
+					byte[] ss;
+					try {
+						ss = (byte[]) (list.removeFirst());
+					} catch (NoSuchElementException e) {
+						// Yes I know this is impossible but it happens with 1.6
+						noElementCount++;
+						if(noElementCount > 1000) {
+							System.err.println("Lost log line because of constant NoSuchElementException's");
+							e.printStackTrace();
+							return;
+						}
+						continue;
+					}
 					listBytes -= (ss.length + LINE_OVERHEAD);
 					x++;
 				}
