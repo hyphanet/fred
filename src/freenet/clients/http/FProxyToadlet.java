440cf761d1f6bba428e12a9f950399567ce39da3
Florent Daignière <nextgens@freenetproject.org>
2006-12-16 15:08:11 +0000

    HTMLEncode some comments in javadoc
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@11437 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/clients/http/FProxyToadlet.java
+++ src/freenet/clients/http/FProxyToadlet.java
@@ -218,10 +218,10 @@ public class FProxyToadlet extends Toadlet {
 		/**
 		 * Look for any of the following strings:
 		 * <rss
-		 * <feed
-		 * <rdf:RDF
+		 * &lt;feed
+		 * &lt;rdf:RDF
 		 * 
-		 * If they start at the beginning of the file, or are preceded by one or more <! or <? tags,
+		 * If they start at the beginning of the file, or are preceded by one or more &lt;! or &lt;? tags,
 		 * then firefox will read it as RSS. In which case we must force it to be downloaded to disk. 
 		 */
 		if(checkForString(buf, "<rss")) return true;
