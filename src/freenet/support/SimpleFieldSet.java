639485bf06ec2aed21632165efe06d7c5edf4ed9
Matthew Toseland <toad@amphibian.dyndns.org>
2007-02-10 00:09:29 +0000

    Valid no-op
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@11730 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/support/SimpleFieldSet.java
+++ src/freenet/support/SimpleFieldSet.java
@@ -224,7 +224,7 @@ public class SimpleFieldSet {
      */
 	private synchronized final boolean put(String key, String value, boolean allowMultiple, boolean overwrite) {
 		int idx;
-		if(value == null) return false;
+		if(value == null) return true; // valid no-op
 		if((idx = key.indexOf(MULTI_LEVEL_CHAR)) == -1) {
 			String x = (String) values.get(key);
 			
