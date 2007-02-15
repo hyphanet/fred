8ee4ecaf10f3fff9be8f64f3c99f03cf5f692da1
Matthew Toseland <toad@amphibian.dyndns.org>
2007-02-15 16:23:38 +0000

    Add final
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@11800 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/client/TempStoreElement.java
+++ src/freenet/client/TempStoreElement.java
@@ -9,15 +9,15 @@ import freenet.support.io.FileBucket;
 import freenet.support.io.PaddedEphemerallyEncryptedBucket;
 
 class TempStoreElement {
+	final File myFilename;
+	final PaddedEphemerallyEncryptedBucket bucket;
+	final FileBucket underBucket;
+	
 	TempStoreElement(File myFile, FileBucket fb, PaddedEphemerallyEncryptedBucket encryptedBucket) {
 		this.myFilename = myFile;
 		this.underBucket = fb;
 		this.bucket = encryptedBucket;
 	}
-	
-	File myFilename;
-	PaddedEphemerallyEncryptedBucket bucket;
-	FileBucket underBucket;
 	
 	public void close() {
 		underBucket.free();
