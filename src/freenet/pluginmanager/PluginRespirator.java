5bd36fcf71409dda64580769fc7f75516083e96b
Florent Daignière <nextgens@freenetproject.org>
2007-03-15 23:54:05 +0000

    reapply r12151: add a few additionnal method to PluginRespirator
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@12150 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/pluginmanager/PluginRespirator.java
+++ src/freenet/pluginmanager/PluginRespirator.java
@@ -3,18 +3,22 @@ package freenet.pluginmanager;
 import java.net.URISyntaxException;
 
 import freenet.client.HighLevelSimpleClient;
+import freenet.clients.http.PageMaker;
 import freenet.clients.http.filter.FilterCallback;
 import freenet.node.Node;
 import freenet.node.RequestStarter;
+import freenet.support.HTMLNode;
 import freenet.support.URIPreEncoder;
 
 public class PluginRespirator {
-	private HighLevelSimpleClient hlsc = null;
-	private Node node;
+	private final HighLevelSimpleClient hlsc;
+	private final Node node;
+	private final PageMaker pageMaker;
 	
 	public PluginRespirator(Node node, PluginManager pm) {
 		this.node = node;
 		this.hlsc = node.clientCore.makeClient(RequestStarter.INTERACTIVE_PRIORITY_CLASS);
+		this.pageMaker = new PageMaker("clean");
 	}
 	
 	//public HighLevelSimpleClient getHLSimpleClient() throws PluginSecurityException {
@@ -32,5 +36,19 @@ public class PluginRespirator {
 		} catch (URISyntaxException e) {
 			throw new Error(e);
 		}
+	}
+	
+	public PageMaker getPageMaker(){
+		return pageMaker;
+	}
+	
+	public HTMLNode addFormChild(HTMLNode parentNode, String target, String name) {
+		HTMLNode formNode =
+			parentNode.addChild("form", new String[] { "action", "method", "enctype", "id", "name", "accept-charset" }, 
+					new String[] { target, "post", "multipart/form-data", name, name, "utf-8"} );
+		formNode.addChild("input", new String[] { "type", "name", "value" }, 
+				new String[] { "hidden", "formPassword", node.clientCore.formPassword });
+		
+		return formNode;
 	}
 }
