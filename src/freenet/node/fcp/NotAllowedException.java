09ff80137d264b2b379ab62bdf162f9175d27057
Matthew Toseland <toad@amphibian.dyndns.org>
2007-03-20 21:15:35 +0000

    Missed out file
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@12239 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ src/freenet/node/fcp/NotAllowedException.java
@@ -0,0 +1,7 @@
+package freenet.node.fcp;
+
+/** Thrown when a client tries to upload a file it's not allowed to upload (or otherwise read it), or 
+ * download to a file which it's not allowed to download to (or otherwise write to). */
+public class NotAllowedException extends Exception {
+
+}
