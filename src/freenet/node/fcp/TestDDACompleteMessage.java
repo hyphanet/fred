5338bdebc9d206c1a75dd5e5e476d52e8be81b42
Florent Daignière <nextgens@freenetproject.org>
2007-04-12 12:53:45 +0000

    Rename testDDAMessages to follow naming conventions
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@12599 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ src/freenet/node/fcp/TestDDACompleteMessage.java
@@ -0,0 +1,88 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
+package freenet.node.fcp;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import freenet.node.Node;
+import freenet.node.fcp.FCPConnectionHandler.DDACheckJob;
+import freenet.support.Logger;
+import freenet.support.SimpleFieldSet;
+
+/**
+ * client -> node: DDARequest { WantRead=true, WantWrite=true, Dir=/tmp/blah }
+ * node -> client: DDAReply { Dir=/tmp/blah, ReadFilename=random1, WriteFilename=random2, ContentToWrite=random3 }
+ * client -> node: DDAResponse { Dir=/tmp/blah, ReadContent=blah }
+ * node -> client: DDAComplete { Dir=/tmp/blah, ReadAllowed=true, WriteAllowed=true }
+ * 
+ * @author Florent Daigni&egrave;re &lt;nextgens@freenetproject.org&gt;
+ *
+ */
+public class TestDDACompleteMessage extends FCPMessage {
+	public static String NAME = "TestDDAComplete";
+	public static String READ_ALLOWED = "ReadAllowed";
+	public static String WRITE_ALLOWED = "WriteAllowed";
+
+	final DDACheckJob checkJob;
+	final String readContentFromClient;
+	private final FCPConnectionHandler handler;
+	
+	public TestDDACompleteMessage(FCPConnectionHandler handler, DDACheckJob job, String readContent) {
+		this.checkJob = job;
+		this.readContentFromClient = readContent;
+		this.handler = handler;
+	}
+
+	public SimpleFieldSet getFieldSet() {
+		SimpleFieldSet sfs = new SimpleFieldSet(true);
+		
+		sfs.putSingle(TestDDARequestMessage.DIRECTORY, checkJob.directory.toString());
+		
+		boolean isReadAllowed = false; 
+		boolean isWriteAllowed = false;
+		
+		if(checkJob.readFilename != null) {
+			isReadAllowed = (readContentFromClient != null) &&  (checkJob.readContent.equals(readContentFromClient));
+			// cleanup in any case : we created it!... let's hope the client will do the same on its side.
+			checkJob.readFilename.delete();
+			sfs.putSingle(READ_ALLOWED, String.valueOf(isReadAllowed));
+		}
+		
+		if(checkJob.writeFilename != null) {
+			File maybeWrittenFile = checkJob.writeFilename;
+			if (maybeWrittenFile.exists() && maybeWrittenFile.isFile() && maybeWrittenFile.canRead()) {
+				try {
+					FileReader fr = new FileReader(maybeWrittenFile);
+					StringBuffer sb = new StringBuffer();
+					
+					int current = fr.read();					
+					while(current != -1) {
+						sb.append((char)current);
+						current = fr.read();
+					}
+					
+					fr.close();
+					isWriteAllowed = checkJob.writeContent.equals(sb.toString().trim());
+				} catch (IOException e) {
+					Logger.error(this, "Caught an IOE trying to read the file (" + maybeWrittenFile + ")! " + e.getMessage());
+				}
+			}
+			sfs.putSingle(WRITE_ALLOWED, String.valueOf(isWriteAllowed));
+		}
+		
+		handler.registerTestDDAResult(checkJob.directory.toString(), isReadAllowed, isWriteAllowed);
+		
+		return sfs;
+	}
+
+	public String getName() {
+		return NAME;
+	}
+
+	public void run(FCPConnectionHandler handler, Node node) throws MessageInvalidException {
+		throw new MessageInvalidException(ProtocolErrorMessage.INVALID_MESSAGE, NAME + " goes from server to client not the other way around", NAME, false);
+	}
+}
