c3fadbee6b84a8a857325da5f4d440452ac53223
Matthew Toseland <toad@amphibian.dyndns.org>
2007-04-14 13:48:24 +0000

    Fix the build. :|
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@12682 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/fcp/ClientHelloMessage.java
+++ src/freenet/node/fcp/ClientHelloMessage.java
@@ -41,7 +41,7 @@ public class ClientHelloMessage extends FCPMessage {
 
 	public void run(FCPConnectionHandler handler, Node node) {
 		// We know the Hello is valid.
-		FCPMessage msg = new NodeHelloMessage(node, handler);
+		FCPMessage msg = new NodeHelloMessage(node);
 		handler.outputHandler.queue(msg);
 		handler.setClientName(clientName);
 	}
