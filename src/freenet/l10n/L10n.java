1784a0173ccfd2d9f825d399708c38d4fe719375
Matthew Toseland <toad@amphibian.dyndns.org>
2007-05-02 14:35:12 +0000

    Add swedish and italian translations from Cooo and _ph00 respectively.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@13097 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/l10n/L10n.java
+++ src/freenet/l10n/L10n.java
@@ -36,7 +36,7 @@ public class L10n {
 	public static final String OVERRIDE_SUFFIX = ".override" + SUFFIX;
 	
 	// English has to remain the first one!
-	public static final String[] AVAILABLE_LANGUAGES = { "en", "fr", "pl", "unlisted" };
+	public static final String[] AVAILABLE_LANGUAGES = { "en", "fr", "pl", "it", "se", "unlisted" };
 	private final String selectedLanguage;
 	
 	private static SimpleFieldSet currentTranslation = null;
