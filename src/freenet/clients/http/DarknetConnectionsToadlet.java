74365aa7c56674b25ad259520a4f04a8f00508a8
Matthew Toseland <toad@amphibian.dyndns.org>
2007-05-04 17:16:51 +0000

    fix indenting
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@13147 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/clients/http/DarknetConnectionsToadlet.java
+++ src/freenet/clients/http/DarknetConnectionsToadlet.java
@@ -213,34 +213,34 @@ public class DarknetConnectionsToadlet extends Toadlet {
 			activityInfobox.addChild("div", "class", "infobox-header", l10n("activityTitle"));
 			HTMLNode activityInfoboxContent = activityInfobox.addChild("div", "class", "infobox-content");
 			HTMLNode activityList = drawActivity(activityInfoboxContent, node);
-				if (advancedModeEnabled) {
-					if (numARKFetchers > 0) {
-						activityList.addChild("li", "ARK\u00a0Fetch\u00a0Requests:\u00a0" + numARKFetchers);
-					}
-					long[] total = IOStatisticCollector.getTotalIO();
-					long total_output_rate = (total[0]) / nodeUptimeSeconds;
-					long total_input_rate = (total[1]) / nodeUptimeSeconds;
-					long totalPayload = node.getTotalPayloadSent();
-					long total_payload_rate = totalPayload / nodeUptimeSeconds;
-					int percent = (int) (100 * totalPayload / total[0]);
-					activityList.addChild("li", "Total Output:\u00a0" + SizeUtil.formatSize(total[0], true) + "\u00a0(" + SizeUtil.formatSize(total_output_rate, true) + "ps)");
-					activityList.addChild("li", "Payload Output:\u00a0" + SizeUtil.formatSize(totalPayload, true) + "\u00a0(" + SizeUtil.formatSize(total_payload_rate, true) + "ps) ("+percent+"%)");
-					activityList.addChild("li", "Total Input:\u00a0" + SizeUtil.formatSize(total[1], true) + "\u00a0(" + SizeUtil.formatSize(total_input_rate, true) + "ps)");
-					long[] rate = stats.getNodeIOStats();
-					long delta = (rate[5] - rate[2]) / 1000;
-					if(delta > 0) {
-						long output_rate = (rate[3] - rate[0]) / delta;
-						long input_rate = (rate[4] - rate[1]) / delta;
-						SubConfig nodeConfig = node.config.get("node");
-						int outputBandwidthLimit = nodeConfig.getInt("outputBandwidthLimit");
-						int inputBandwidthLimit = nodeConfig.getInt("inputBandwidthLimit");
-						if(inputBandwidthLimit == -1) {
-							inputBandwidthLimit = outputBandwidthLimit * 4;
-						}
-						activityList.addChild("li", "Output Rate:\u00a0" + SizeUtil.formatSize(output_rate, true) + "ps (of\u00a0"+SizeUtil.formatSize(outputBandwidthLimit, true)+"ps)");
-						activityList.addChild("li", "Input Rate:\u00a0" + SizeUtil.formatSize(input_rate, true) + "ps (of\u00a0"+SizeUtil.formatSize(inputBandwidthLimit, true)+"ps)");
-					}
+			if (advancedModeEnabled) {
+				if (numARKFetchers > 0) {
+					activityList.addChild("li", "ARK\u00a0Fetch\u00a0Requests:\u00a0" + numARKFetchers);
 				}
+				long[] total = IOStatisticCollector.getTotalIO();
+				long total_output_rate = (total[0]) / nodeUptimeSeconds;
+				long total_input_rate = (total[1]) / nodeUptimeSeconds;
+				long totalPayload = node.getTotalPayloadSent();
+				long total_payload_rate = totalPayload / nodeUptimeSeconds;
+				int percent = (int) (100 * totalPayload / total[0]);
+				activityList.addChild("li", "Total Output:\u00a0" + SizeUtil.formatSize(total[0], true) + "\u00a0(" + SizeUtil.formatSize(total_output_rate, true) + "ps)");
+				activityList.addChild("li", "Payload Output:\u00a0" + SizeUtil.formatSize(totalPayload, true) + "\u00a0(" + SizeUtil.formatSize(total_payload_rate, true) + "ps) ("+percent+"%)");
+				activityList.addChild("li", "Total Input:\u00a0" + SizeUtil.formatSize(total[1], true) + "\u00a0(" + SizeUtil.formatSize(total_input_rate, true) + "ps)");
+				long[] rate = stats.getNodeIOStats();
+				long delta = (rate[5] - rate[2]) / 1000;
+				if(delta > 0) {
+					long output_rate = (rate[3] - rate[0]) / delta;
+					long input_rate = (rate[4] - rate[1]) / delta;
+					SubConfig nodeConfig = node.config.get("node");
+					int outputBandwidthLimit = nodeConfig.getInt("outputBandwidthLimit");
+					int inputBandwidthLimit = nodeConfig.getInt("inputBandwidthLimit");
+					if(inputBandwidthLimit == -1) {
+						inputBandwidthLimit = outputBandwidthLimit * 4;
+					}
+					activityList.addChild("li", "Output Rate:\u00a0" + SizeUtil.formatSize(output_rate, true) + "ps (of\u00a0"+SizeUtil.formatSize(outputBandwidthLimit, true)+"ps)");
+					activityList.addChild("li", "Input Rate:\u00a0" + SizeUtil.formatSize(input_rate, true) + "ps (of\u00a0"+SizeUtil.formatSize(inputBandwidthLimit, true)+"ps)");
+				}
+			}
 
 			nextTableCell = advancedModeEnabled ? overviewTableRow.addChild("td") : overviewTableRow.addChild("td", "class", "last");
 
