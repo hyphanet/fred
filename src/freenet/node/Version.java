05695bb7df8b0df069a244605009f7baaa0e63a3
Matthew Toseland <toad@amphibian.dyndns.org>
2007-05-31 19:53:04 +0000

    1035: - binary blobs: fetch your favourite freesite (currently has to be a single file but will include all blocks needed for it) to a .fblob file and reinsert it on a disconnected darknet without needing the privkey - deadlock fixes in containers layer - translations: -- new norwegian (norsk) translation from Nogaso -- updated swedish translation from Cooo -- new polish translation from kolo (we had a few strings before, now hundreds) -- french fixes from batosai -- more work on the italian translation from _ph00 -- lots of string fixes (we weren't looking up the key, we were looking up the wrong key etc) -- pick up the language from the default locale -- NodeLanguage in NodeHello indicating current node language code -- specify that the l10n override file is UTF8 encoded in the MIME type - lots of work (nextgens) on FirstTimeWizardToadlet, a wizard toadlet which will ask the user a series of basic questions immediately after install: node name, store size, bandwidth, trusted networks etc. this isn't used by the installer yet though. - client layer -- fix infinite loop in freesite insertion -- fix deadlock in container fetching code -- minor locking fixes in container layer -- rename URI_PARSE_ERROR to FREENET_URI_PARSE_ERROR and update explanation string accordingly - web interface -- only show user alert summary if we have full access -- drop FreeHoo as it's pre-1010 and will soon be deprecated -- improve stats layour when no preemptive rejection reasons -- redirect from /darknet/ to /friends/ -- robots.txt is a 200 not a 400; only show it if fproxy.doRobots=true (default false). -- fix anchor typo on queue page -- "Remove" not "Delete" removing stuff from the queue on /queue/ - database -- only try loading the database if dumping it succeeded -- create the reconstructFile when we get a DbChecksumException or RunRecoveryException (so it will be recovered on next startup) -- never open the blocks sub-index with dupes allowed = true - plugins -- "pluginname#" will download the plugin to a file, load it, but not update it on startup (unlike pluginname*) - refactoring -- move AsyncMessageCallback, ByteCounter to io.comm, expose sendAsync, getBootID, getThrottle on PeerContext, remove DummyPeerContext -- refactor the code that decides whether to do an IP detection via plugins -- delete some dead code - beginnings of new bulk data transfer layer - comments - indenting - logging
    Lots of work on UP&P plugin
    - Make it display something
    - Working external IP detection
    - Port forwarding is setup on startup
    Minor work on MDNSDiscovery plugin
    Add seamonkey to the list of supported browsers for the installer browser launcher
    Bundle UP&P with the installer.
    Website: Advertise the bundle-installer (useful for ppl who can't let the standard installer download components directly)
    
    WARNING: Pre-1010 insecure SSKs will be deprecated soon. Please move your sites to new keys, and mirror unmaintained sites you care about to new keys also. _ph00 is coordinating an effort to migrate old sites on the mirrorproject frost board.
    
    
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@13418 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -24,7 +24,7 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1034;
+	private static final int buildNumber = 1035;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1029;
