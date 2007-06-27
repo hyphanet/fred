ce8d686f0214eff8a41a3c84600f97878d60ea56
Alberto Bacchelli <sback@sback.it>
2007-06-27 10:12:44 +0000

    Added better explaination on ".." SFS problem, and tests on put and get methods
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@13775 67a373e5-eb02-0410-a15c-ee090a768436

--- test/freenet/support/SimpleFieldSetTest.java
+++ test/freenet/support/SimpleFieldSetTest.java
@@ -35,6 +35,17 @@ public class SimpleFieldSetTest extends TestCase {
 		String methodKey = "foo..bar";
 		String methodValue = "foobar";
 		methodSFS.putSingle(methodKey,methodValue);
+		methodSFS.subset("foo").subset("").get("bar");	   /*it returns "foobar" */
+		
+		/*methodSFS.subset("foo").subset(null).get("bar");   /* it raises null exception
+																				* because subset(null) returns
+																				* null by default */
+		
+		methodSFS.get("foo..bar");						   /* it doesn't work
+		 													* but if I put("foo.bar.boo","bazoo") 
+		 													* and I get("foo.bar.boo") -> it returns "bazoo"
+		 													* so it should do the same for "foo..bar" 
+		 													* or it would raise an exception */
 		//assertEquals(methodSFS.get(methodKey),methodValue);
 	}
 	
@@ -49,6 +60,45 @@ public class SimpleFieldSetTest extends TestCase {
 		String methodValue = "foobar";
 		methodSFS.putAppend(methodKey,methodValue);
 		//assertEquals(methodSFS.get(methodKey),methodValue);
+	}
+	
+	/**
+	 * Test put() and get() methods
+	 * using a normal Map behaviour
+	 * and without MULTI_LEVEL_CHARs
+	 */
+	public void testSimpleFieldSetPutAndGet_NoMultiLevel(){
+		String[][] methodPairsArray = { {"A","a"},{"B","b"},{"C","c"},{"D","d"},{"E","e"},{"F","f"} };
+		putAndGetPairsTests(methodPairsArray);
+	}
+	
+	/**
+	 * Test put() and get() methods
+	 * using a normal Map behaviour
+	 * and with MULTI_LEVEL_CHARs
+	 */
+	public void testSimpleFieldSetPutAndGet_MultiLevel(){
+		String[][] methodPairsArray_DoubleLevel = 
+			{ {"A.A","aa"},{"A.B","ab"},{"A.C","ac"},{"A.D","ad"},{"A.E","ae"},{"A.F","af"} };
+		String[][] methodPairsArray_MultiLevel = 
+			{ {"A.A.A.A","aa"},{"A.B.A","ab"},{"A.C.Cc","ac"},{"A.D.F","ad"},{"A.E.G","ae"},{"A.F.J.II.UI.BOO","af"} };
+		putAndGetPairsTests(methodPairsArray_DoubleLevel);
+		putAndGetPairsTests(methodPairsArray_MultiLevel);
+	}
+	
+	
+	/**
+	 * It puts key-value pairs in a SimpleFieldSet
+	 * and verify if it can do the correspondant
+	 * get correctly.
+	 * @param aPairsArray
+	 */
+	private void putAndGetPairsTests(String[][] aPairsArray) {
+		SimpleFieldSet methodSFS = new SimpleFieldSet(true);
+		for (int i = 0; i < aPairsArray.length; i++)		//putting values
+			methodSFS.putSingle(aPairsArray[i][0], aPairsArray[i][1]);
+		for (int i = 0; i < aPairsArray.length; i++)		//getting values
+			assertEquals(methodSFS.get(aPairsArray[i][0]),aPairsArray[i][1]);
 	}
 	
 }
