14d324db66ffb851281341d59320cdcba7ea6ac4
Matthew Toseland <toad@amphibian.dyndns.org>
2007-07-14 20:38:07 +0000

    and another one
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@14126 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/clients/http/ConnectionsToadlet.java
+++ src/freenet/clients/http/ConnectionsToadlet.java
@@ -569,7 +569,6 @@ public abstract class ConnectionsToadlet extends Toadlet {
 			}
 			PeerNode pn;
 			try {
-				pn = node.createNewDarknetNode(fs);
 				if(isOpennet()) {
 					pn = node.createNewOpennetNode(fs);
 				} else {
