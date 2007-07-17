60a94eeedf1aaa55e51330e7d7d4a0086213304c
Matthew Toseland <toad@amphibian.dyndns.org>
2007-07-17 15:19:46 +0000

    1045: - opennet fixes -- fix adding opennet peers when less than the full number - we weren't adding the peer to the routing table!!!! -- always show the opennet noderef, not just in advanced mode - further FCP/opennet fixes, add ListPeer command - french translation updates Related: - Work on the spider - Work on JFK - Work on the C++ FCP library Credits: - zothar - batosai - nextgens - toad - swati - kryptos - mkolar
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@14163 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -24,17 +24,17 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1044;
+	private static final int buildNumber = 1045;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 1041;
-	private static final int newLastGoodBuild = 1044;
+	private static final int oldLastGoodBuild = 1044;
+	private static final int newLastGoodBuild = 1045;
 	static final long transitionTime;
 	
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2007, Calendar.JULY, 17, 0, 0, 0 );
+		_cal.set( 2007, Calendar.JULY, 19, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 	
