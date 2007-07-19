04cc4c76af647bf4f2dd29a5ccf66a2f140b9a3b
Matthew Toseland <toad@amphibian.dyndns.org>
2007-07-19 11:30:01 +0000

    1047: Sorry, make it mandatory 0:00 on the 21st, not the 19th.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@14192 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -24,17 +24,17 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1046;
+	private static final int buildNumber = 1047;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1045;
-	private static final int newLastGoodBuild = 1046;
+	private static final int newLastGoodBuild = 1047;
 	static final long transitionTime;
 	
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2007, Calendar.JULY, 19, 0, 0, 0 );
+		_cal.set( 2007, Calendar.JULY, 21, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 	
