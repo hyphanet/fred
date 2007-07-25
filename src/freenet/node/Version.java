01f1dba8a624a05dba7ab1c9b9b8ce545fdd0859
Matthew Toseland <toad@amphibian.dyndns.org>
2007-07-25 16:58:16 +0000

    1048: (MANDATORY 0:00 27 JULY)
    - L10n
    -- German translation (thanks saces and Michael Tänzer)
    -- Updated italian translation
    -- Minor fixes
    -- Opennet-related config description etc improvements (somebody needs to look at these)
    
    - first time wizard toadlet:
    -- first step asks the user whether they want opennet support
    -- improvements to strings: click here / cancel -> continue / cancel
    
    - opennet
    -- don't drop a connected peer unless at least 10 successful requests since last dropped a peer
    -- fixes to startup sort order - never connected peers must be dropped first, not last
    -- warn the user if they are running opennet
    -- better wording in config strings
    
    - temporary files
    -- major refactoring of freenet.support.io.*
    -- try to prevent opening streams to a bucket after it's closed
    -- reduce memory usage by not keeping the File, just keep the variable part of the filename
    -- use TempFileBucket / PersistentTempFileBucket wherever we've used FileBucket for a temp file
    -- always write the tempDir to the freenet.ini even if it hasn't changed
    
    - more work on memory usage
    -- splitfile fetchers: don't keep the key list
    -- ClientGetter: don't keep the current state after finished
    -- encrypted buckets: don't keep a Rijndael, just keep the key unless we are actually using it
    -- updater: download the update to a normal temp file, not an ArrayBucket
    -- client-level coalescing: CRS.pendingKeys can contain either a single SendableGet or an array of them
    -- client-level coalescing memory leak: remove the client:key from the pending keys list before losing the key from the segment!
    -- cut maximum unmatched message age to 10 minutes
    -- tempfile refactorings, see above
    
    - disallow node names over 128 chars (they can make the auth packets huge), dump legacy name checking code
    
    - SingleFileFetcher:
    -- handleMetadata() is synchronized. it should be because it reads and writes many of SFF's variables; when we implement resumable persistent requests, we will need this, and it's probably a good idea anyway.
    -- always call it on a separate thread even when calling it from another handleMetadata(). include ticker on FetcherContext for this purpose.
    -- wrap in wrapHandleMetadata(), which calls it and handles any exceptions (simplifies code)
    -- unset finalised size when it needs to be unset
    
    - FCP
    -- Fix Internal error: data != returnBucket (caused by an earlier bucket error resulting in returnBucket = null, then the request is restarted)
    -- Remove hard limit of 256 completed requests
    
    - better error handling in bookmark editor toadlet
    
    - Minor dev stuff
    -- refactor HTMLEncoder/HTMLDecoder to share a common table in HTMLEntities
    -- delete dead code, minor refactorings, scope reductions, comments
    -- indenting, typo fixes, comments
    -- logging, thread names (FEC Pool <number>)
    
    Installer:
    - don't enable opennet, we'll ask in the post-install
    
    SoC:
    - More work on the Echo blogging plugin, including i18n, bugfixes, javadocs
    - More work on JFK (new DoS-resistent link crypto)
    - More work on unit tests (lots of support/ stuff, including new infrastructure class UTFUtil)
    
    And some minor changes to the website.
    
    Credits:
    - kryptos
    - sback
    - nextgens
    - saces
    - fred
    - ph00
    - juiceman
    - toad
    
    
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@14339 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -24,17 +24,17 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1047;
+	private static final int buildNumber = 1048;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 1045;
-	private static final int newLastGoodBuild = 1047;
+	private static final int oldLastGoodBuild = 1047;
+	private static final int newLastGoodBuild = 1048;
 	static final long transitionTime;
 	
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2007, Calendar.JULY, 21, 0, 0, 0 );
+		_cal.set( 2007, Calendar.JULY, 27, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 	
