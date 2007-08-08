650ef572aa841636b2982159a761bf4d3ebce15c
Matthew Toseland <toad@amphibian.dyndns.org>
2007-08-08 20:01:10 +0000

    Add MutableBoolean. Sorry, needed for the previous commit
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@14537 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ src/freenet/support/MutableBoolean.java
@@ -0,0 +1,15 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
+package freenet.support;
+
+/**
+ * Freely modifiable boolean wrapper.
+ * @author toad
+ *
+ */
+public class MutableBoolean {
+
+	public boolean value;
+	
+}
