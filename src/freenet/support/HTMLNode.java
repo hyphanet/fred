75d8d0ff876da059b96a37334f2b5eaa43782d9a
Alberto Bacchelli <sback@sback.it>
2007-08-18 14:36:43 +0000

    TODO added: A meaningful exception should be raised when trying to call the generate() method for a HTMLDoctype with child number != 1
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@14784 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/support/HTMLNode.java
+++ src/freenet/support/HTMLNode.java
@@ -190,6 +190,9 @@ public class HTMLNode {
 		 */
 		public StringBuffer generate(StringBuffer tagBuffer) {
 			tagBuffer.append("<!DOCTYPE ").append(name).append(" PUBLIC \"").append(systemUri).append("\">\n");
+			//TODO A meaningful exception should be raised 
+			// when trying to call the method for a HTMLDoctype 
+			// with number of child != 1 
 			return ((HTMLNode) children.get(0)).generate(tagBuffer);
 		}
 
