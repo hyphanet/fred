22a495c32e2644843fe4d8a6c0b8ef1cf7582d96
Florent Daignière <nextgens@freenetproject.org>
2007-08-23 20:46:56 +0000

    get the node to download freenet-ext #16
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@14867 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/NodeStarter.java
+++ src/freenet/node/NodeStarter.java
@@ -36,7 +36,7 @@ public class NodeStarter
     private Node node;
 	private static LoggingConfigHandler logConfigHandler;
 	//TODO: cleanup
-	public static int RECOMMENDED_EXT_BUILD_NUMBER = 15;
+	public static int RECOMMENDED_EXT_BUILD_NUMBER = 16;
 	
 	/*
 		(File.separatorChar == '\\') &&
