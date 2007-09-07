8b46c21a47178af2034b4a8588a56f7e8a50178c
Matthew Toseland <toad@amphibian.dyndns.org>
2007-09-07 16:49:23 +0000

    Re-order and renumber the constants. More positive = more confidence in success, more negative = more confidence in failure
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@15002 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/pluginmanager/ForwardPortStatus.java
+++ src/freenet/pluginmanager/ForwardPortStatus.java
@@ -3,21 +3,21 @@ package freenet.pluginmanager;
 public class ForwardPortStatus {
 	
 	public final int status;
-	/** The port forward is in progress */
-	public static final int IN_PROGRESS = 0;
 	/** The port forward definitely succeeded. */
-	public static final int DEFINITE_SUCCESS = 1;
+	public static final int DEFINITE_SUCCESS = 3;
 	/** The port forward probably succeeded. I.e. it succeeded unless there was
 	 * for example hostile action on the part of the router. */
 	public static final int PROBABLE_SUCCESS = 2;
 	/** The port forward may have succeeded. Or it may not have. We should 
 	 * definitely try to check out of band. See UP&P: Many routers say they've
 	 * forwarded the port when they haven't. */
-	public static final int MAYBE_SUCCESS = 3;
-	/** The port forward definitely failed. */
-	public static final int DEFINITE_FAILURE = -1;
+	public static final int MAYBE_SUCCESS = 1;
+	/** The port forward is in progress */
+	public static final int IN_PROGRESS = 0;
 	/** The port forward probably failed */
-	public static final int PROBABLE_FAILURE = -2;
+	public static final int PROBABLE_FAILURE = -1;
+	/** The port forward definitely failed. */
+	public static final int DEFINITE_FAILURE = -2;
 	
 	public final String reasonString;
 	
