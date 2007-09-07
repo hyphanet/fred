ec748208d36e861a29e2eb7358a06d80e7149706
Matthew Toseland <toad@amphibian.dyndns.org>
2007-09-07 17:14:04 +0000

    Shortcut evaluation of equals()
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@15006 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/pluginmanager/ForwardPort.java
+++ src/freenet/pluginmanager/ForwardPort.java
@@ -35,6 +35,7 @@ public class ForwardPort {
 	}
 	
 	public boolean equals(Object o) {
+		if(o == this) return true;
 		if(!(o instanceof ForwardPort)) return false;
 		ForwardPort f = (ForwardPort) o;
 		return (f.name.equals(name)) && f.isIP6 == isIP6 && f.protocol == protocol && f.portNumber == portNumber;
