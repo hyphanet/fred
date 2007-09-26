25409f06b1c24e5fc6e5b48cb91ce9302186ee15
Matthew Toseland <toad@amphibian.dyndns.org>
2007-09-26 17:34:37 +0000

    add time last success to OPNStatus
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@15338 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/OpennetPeerNodeStatus.java
+++ src/freenet/node/OpennetPeerNodeStatus.java
@@ -4,6 +4,9 @@ public class OpennetPeerNodeStatus extends PeerNodeStatus {
 
 	OpennetPeerNodeStatus(PeerNode peerNode) {
 		super(peerNode);
+		timeLastSuccess = ((OpennetPeerNode)peerNode).timeLastSuccess();
 	}
 
+	public final long timeLastSuccess;
+	
 }
