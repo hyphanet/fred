5d6a73565d7b5a04b7a84350367d9066c148e9cf
Matthew Toseland <toad@amphibian.dyndns.org>
2007-09-26 12:11:50 +0000

    Was: Don't drop peer if (not enough successes since last) && (we have a node to add) && (we have too many peers) - Wrong! Now: Don't drop peer if (not enough successes since last) || (we don't have a node to add) Bug was causing more churn than should happen.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@15320 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/OpennetManager.java
+++ src/freenet/node/OpennetManager.java
@@ -280,7 +280,7 @@ public class OpennetManager {
 			} else while(peersLRU.size() > MAX_PEERS - (nodeToAddNow == null ? 0 : 1)) {
 				PeerNode toDrop;
 				// can drop peers which are over the limit
-				toDrop = peerToDrop(noDisconnect && nodeToAddNow != null && peersLRU.size() >= MAX_PEERS);
+				toDrop = peerToDrop(noDisconnect || nodeToAddNow == null);
 				if(toDrop == null) {
 					if(logMINOR)
 						Logger.minor(this, "No more peers to drop, cannot accept peer"+(nodeToAddNow == null ? "" : nodeToAddNow.toString()));
