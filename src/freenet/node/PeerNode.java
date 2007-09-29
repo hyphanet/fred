d3706991f6330a669472b01d0bd73d4d73a78ed0
Florent Daignière <nextgens@freenetproject.org>
2007-09-29 16:30:02 +0000

    Assume that testnet is disabled so that we don't have to transfer that field in compressednoderefs
    git-svn-id: file:///home/toad/git-migration/temprepository/branches/freenet-jfk@15386 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/PeerNode.java
+++ src/freenet/node/PeerNode.java
@@ -93,6 +93,7 @@ public abstract class PeerNode implements PeerContext, USKRetrieverCallback {
     //TODO: sync ?
     protected byte[] jfkKa;
     protected byte[] jfkKe;
+    protected byte[] jfkKs;
 	
     /** My low-level address for SocketManager purposes */
     private Peer detectedPeer;
@@ -1718,7 +1719,7 @@ public abstract class PeerNode implements PeerContext, USKRetrieverCallback {
      * @throws FSParseException */
     protected synchronized boolean innerProcessNewNoderef(SimpleFieldSet fs, boolean forARK) throws FSParseException {
         boolean changedAnything = false;
-        if(node.testnetEnabled != Fields.stringToBool(fs.get("testnet"), true)) {
+        if(node.testnetEnabled != Fields.stringToBool(fs.get("testnet"), false)) {
         	String err = "Preventing connection to node "+detectedPeer+" - peer.testnet="+!node.testnetEnabled+'(' +fs.get("testnet")+") but node.testnet="+node.testnetEnabled;
         	Logger.error(this, err);
         	throw new FSParseException(err);
