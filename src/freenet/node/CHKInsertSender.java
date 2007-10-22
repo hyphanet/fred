da16b5457bcf984d278fa83bc43a54ee5516afc9
Matthew Toseland <toad@amphibian.dyndns.org>
2007-10-22 18:41:04 +0000

    Detect when realRun() returns without setting status
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@15475 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/CHKInsertSender.java
+++ src/freenet/node/CHKInsertSender.java
@@ -209,6 +209,14 @@ public final class CHKInsertSender implements Runnable, AnyInsertSender, ByteCou
         node.addInsertSender(myKey, origHTL, this);
         try {
         	realRun();
+        	int myStatus;
+        	synchronized(this) {
+        		myStatus = status;
+        	}
+        	if(myStatus == NOT_FINISHED) {
+        		Logger.error(this, "realRun() returned without setting status on "+this);
+        		finish(INTERNAL_ERROR, null); // Avoid deadlock
+        	}
 		} catch (OutOfMemoryError e) {
 			OOMHandler.handleOOM(e);
             int myStatus;
