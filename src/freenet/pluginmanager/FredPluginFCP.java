e013bc72c3640a54700682e791c6b8ab15143735
saces <saces@freenetproject.org>
2007-11-17 22:37:47 +0000

    add comment
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@15805 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/pluginmanager/FredPluginFCP.java
+++ src/freenet/pluginmanager/FredPluginFCP.java
@@ -6,6 +6,19 @@ package freenet.pluginmanager;
 import freenet.node.fcp.FCPMessage;
 import freenet.support.SimpleFieldSet;
 
+/**
+ * Interface that has to be implemented for plugins that want to add fcp commands
+ * 
+ * the namesheme is plugin.class.commandname
+ * the node looks for the plugin "plugin.class" (the implementor of this interface)
+ * and ask him for the FCPCommand corresponding to "commandname".
+ * If the plugin don't know what to do with "commandname" it returns null.
+ * 
+ * see plugins.FCPHello for a simple sample.
+ * 
+ * @author saces
+ *
+ */
 public interface FredPluginFCP {
 	
 	FCPMessage create(String name, SimpleFieldSet fs);
