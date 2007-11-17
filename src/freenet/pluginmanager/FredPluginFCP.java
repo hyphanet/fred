92660628cca59cbc75367380193518390b655cb7
saces <saces@freenetproject.org>
2007-11-17 12:27:06 +0000

    new plugin type: FredPluginFCP
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@15797 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ src/freenet/pluginmanager/FredPluginFCP.java
@@ -0,0 +1,13 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
+package freenet.pluginmanager;
+
+import freenet.node.fcp.FCPMessage;
+import freenet.support.SimpleFieldSet;
+
+public interface FredPluginFCP {
+	
+	FCPMessage create(String name, SimpleFieldSet fs);
+	
+}
