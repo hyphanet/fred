b7dec18072b0e98cf789d4aaf7d3180f9d360407
saces <saces@freenetproject.org>
2007-11-21 11:05:15 +0000

    new interface that allows direct talking to FredPluginFCP implementing plugins.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@15900 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ src/freenet/pluginmanager/FredPluginTalker.java
@@ -0,0 +1,26 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
+package freenet.pluginmanager;
+
+import freenet.support.SimpleFieldSet;
+import freenet.support.api.Bucket;
+
+/**
+ * Interface that has to be implemented for plugins that wants talk to
+ * other plugins that implements FredPluginFCP
+ *  
+ * @author saces
+ *
+ */
+public interface FredPluginTalker {
+	
+	/**
+	 * @param pluginname - reply from
+	 * @param indentifier - identifer from your call
+	 * @param params parameters passed back
+	 * @param data a bucket of data passed back, can be null
+	 */
+	void onReply(String pluginname, String indentifier, SimpleFieldSet params, Bucket data);
+	
+}
