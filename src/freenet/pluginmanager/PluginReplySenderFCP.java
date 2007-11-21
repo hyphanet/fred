6ba7c81d2e1cfd581cab515889f303d31e91d730
saces <saces@freenetproject.org>
2007-11-21 13:45:28 +0000

    refactored fcp plugin handling
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@15901 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ src/freenet/pluginmanager/PluginReplySenderFCP.java
@@ -0,0 +1,29 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
+package freenet.pluginmanager;
+
+import freenet.node.fcp.FCPConnectionHandler;
+import freenet.node.fcp.FCPPluginReply;
+import freenet.support.SimpleFieldSet;
+import freenet.support.api.Bucket;
+
+/**
+ * @author saces
+ *
+ */
+public class PluginReplySenderFCP extends PluginReplySender {
+	
+	final FCPConnectionHandler handler; 
+
+	public PluginReplySenderFCP(FCPConnectionHandler handler2, String pluginname2, String identifier2) {
+		super(pluginname2, identifier2);
+		handler = handler2;
+	}
+
+	public void send(SimpleFieldSet params, Bucket bucket) {
+		FCPPluginReply reply = new FCPPluginReply(pluginname, identifier, params, bucket);
+		handler.outputHandler.queue(reply);
+	}
+
+}
