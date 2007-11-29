7bea73b3c23d8ed4e0b4aca9542e113b5e44791f
Florent Daignière <nextgens@freenetproject.org>
2007-11-29 12:43:24 +0000

    FProxyToadlet: don't leak an InputStream
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@16075 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/clients/http/FProxyToadlet.java
+++ src/freenet/clients/http/FProxyToadlet.java
@@ -223,13 +223,16 @@ public class FProxyToadlet extends Toadlet {
 	 * REDFLAG Expect future security issues! 
 	 * @throws IOException */
 	private static boolean horribleEvilHack(Bucket data) throws IOException {
-		int sz = (int) Math.min(data.size(), 512);
-		if(sz == 0) return false;
-		InputStream is = data.getInputStream();
-		byte[] buf = new byte[sz];
-		// FIXME Fortunately firefox doesn't detect RSS in UTF16 etc ... yet
-		is.read(buf);
-		/**
+		InputStream is = null;
+		try {
+			int sz = (int) Math.min(data.size(), 512);
+			if(sz == 0)
+				return false;
+			is = data.getInputStream();
+			byte[] buf = new byte[sz];
+			// FIXME Fortunately firefox doesn't detect RSS in UTF16 etc ... yet
+			is.read(buf);
+			/**
 		 * Look for any of the following strings:
 		 * <rss
 		 * &lt;feed
@@ -238,10 +241,16 @@ public class FProxyToadlet extends Toadlet {
 		 * If they start at the beginning of the file, or are preceded by one or more &lt;! or &lt;? tags,
 		 * then firefox will read it as RSS. In which case we must force it to be downloaded to disk. 
 		 */
-		if(checkForString(buf, "<rss")) return true;
-		if(checkForString(buf, "<feed")) return true;
-		if(checkForString(buf, "<rdf:RDF")) return true;
-		is.close();
+			if(checkForString(buf, "<rss"))
+				return true;
+			if(checkForString(buf, "<feed"))
+				return true;
+			if(checkForString(buf, "<rdf:RDF"))
+				return true;
+		}
+		finally {
+			is.close();
+		}
 		return false;
 	}
 
