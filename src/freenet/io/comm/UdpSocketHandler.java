a0dcd8eb45c6bcf03e80491be9dcae7a6f80b8da
Matthew Toseland <toad@amphibian.dyndns.org>
2007-11-29 18:50:58 +0000

    better thread name
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@16097 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/io/comm/UdpSocketHandler.java
+++ src/freenet/io/comm/UdpSocketHandler.java
@@ -43,7 +43,7 @@ public class UdpSocketHandler extends Thread implements PacketSocketHandler, Por
 	private final String title;
 	
 	public UdpSocketHandler(int listenPort, InetAddress bindto, Node node, long startupTime, String title) throws SocketException {
-		super("UdpSocketHandler packet receiver thread on port " + listenPort);
+		super("UdpSocketHandler packet receiver thread for "+title);
 		this.node = node;
 		this.title = title;
 		_bindTo = bindto;
