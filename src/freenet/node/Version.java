9cd339907f0af3c049d19274616bfccfbd06219c
Matthew Toseland <toad@amphibian.dyndns.org>
2007-11-30 15:22:10 +0000

    1082: (mandatory 2 dec) - Fix the bandwidth goes to infinity, payload goes to zero bug. Problem was we were continually sending handshakes once we started to rekey! - Fix various places where we closed streams more than once causing it to always throw and not save the data. - Fix various places where we were too tolerant of throw in close() - we need to close() them explicitly in the main path so we know if they throw, not Closer.close() them. FCP: - Fix CompletionTime. - Fix downloads/uploads serialisation.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@16135 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -24,11 +24,11 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1081;
+	private static final int buildNumber = 1082;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1074;
-	private static final int newLastGoodBuild = 1081;
+	private static final int newLastGoodBuild = 1082;
 	static final long transitionTime;
 	
 	static {
