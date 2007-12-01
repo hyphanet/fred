c9cffdd955d924bf0e1c0e6178f8d3c304e082e9
Matthew Toseland <toad@amphibian.dyndns.org>
2007-12-01 22:58:26 +0000

    1084: Link level connectivity: - Rekeying bug: we weren't checking for disconnection if the node is rekeying. - Rekeying bug: we weren't killing the connection after a failed rekey if it was due to the data transfer threshold rather than the once-per-hour threshold. - Rekeying bug: Once we started rekeying, we wouldn't send data packets, and PacketSender wouldn't recognise the rekey had succeeded because of PeerNode.hasLiveHandshake() being broken. - Fix PeerNode.hasLiveHandshake(). - Drop support for STS. Delete the old code. - Simplify rekeying logic. Dev stuff: - Delete dead code. - Logging. - Comments. L10n: - "minimum tunnel lifetime" not "minimum tunnel length" Bookmarks: - Fix update alerts Requests: - If run out of HTL in a RequestSender, surely it is a DNF, not an RNF? - Try the next peer if one is disconnected in RequestSender. - Minor refactoring in RequestSender to make code clearer and not miss exceptions like that one. Opennet: - Announcement. Not tested yet but back end is implemented. Will wire up a test harness soon, and then work on the rest - anonymous connect to seednodes, seednodes harvesting, etc. FCP: - Disallow parallel TestDDA's for the same directory. - Optimise TestDDA random number generation slightly (use node.fastWeakRandom instead of making a new one). - PersistentGet: add MaxSize and BinaryBlob.
    Credits:
    bombe
    nextgens
    toad
    
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@16200 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -24,17 +24,17 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1083;
+	private static final int buildNumber = 1084;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1074;
-	private static final int newLastGoodBuild = 1082;
+	private static final int newLastGoodBuild = 1084;
 	static final long transitionTime;
 	
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2007, Calendar.DECEMBER, 2, 0, 0, 0 );
+		_cal.set( 2007, Calendar.DECEMBER, 3, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 	
