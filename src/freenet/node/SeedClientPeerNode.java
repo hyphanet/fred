f4c0eca4e2c556ebc643af3fd420aaea2503bdfd
Matthew Toseland <toad@amphibian.dyndns.org>
2007-12-05 17:30:34 +0000

    Javadocs
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@16336 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/SeedClientPeerNode.java
+++ src/freenet/node/SeedClientPeerNode.java
@@ -1,9 +1,16 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
 package freenet.node;
 
 import freenet.io.comm.PeerParseException;
 import freenet.io.comm.ReferenceSignatureVerificationException;
 import freenet.support.SimpleFieldSet;
 
+/**
+ * Seed node's representation of a client node connecting in order to announce.
+ * @author toad
+ */
 public class SeedClientPeerNode extends PeerNode {
 
 	public SeedClientPeerNode(SimpleFieldSet fs, Node node2, NodeCrypto crypto, PeerManager peers, boolean fromLocal, OutgoingPacketMangler mangler) throws FSParseException, PeerParseException, ReferenceSignatureVerificationException {
--- src/freenet/node/SeedServerPeerNode.java
+++ src/freenet/node/SeedServerPeerNode.java
@@ -1,9 +1,16 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
 package freenet.node;
 
 import freenet.io.comm.PeerParseException;
 import freenet.io.comm.ReferenceSignatureVerificationException;
 import freenet.support.SimpleFieldSet;
 
+/**
+ * Sender's representation of a seed node.
+ * @author toad
+ */
 public class SeedServerPeerNode extends PeerNode {
 
 	public SeedServerPeerNode(SimpleFieldSet fs, Node node2, NodeCrypto crypto, PeerManager peers, boolean fromLocal, OutgoingPacketMangler mangler) throws FSParseException, PeerParseException, ReferenceSignatureVerificationException {
