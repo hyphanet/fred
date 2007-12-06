200c2500f52925331e4b3f7d1ec2e4ef2ac7d908
Matthew Toseland <toad@amphibian.dyndns.org>
2007-12-06 17:22:08 +0000

    imports
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@16366 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/config/FilePersistentConfig.java
+++ src/freenet/config/FilePersistentConfig.java
@@ -4,14 +4,12 @@
 package freenet.config;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedWriter;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 
 import freenet.support.Logger;
 import freenet.support.SimpleFieldSet;
