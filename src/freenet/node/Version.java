18cda18cd2ebbd693d9a083ad72a3f003a07a71a
Matthew Toseland <toad@amphibian.dyndns.org>
2007-12-06 16:11:41 +0000

    1089: (mandatory 0:00 8 dec) Opennet announcements: general fixes - Decrement the HTL when we try a node and move on to another one. - Prevent looping while the announce is in progress. Opennet announcements: seednode connections - seednodes.fref contains list of seed nodes to connect to. - Not bundled yet, we'll bundle it when we've done some testing. - If we have no peers, Announcer reads this and connects to peernodes and announces until we have 10 opennet peers. - New peer types SeedClientPeerNode and SeedServerPeerNode. Not directly visible on friends/strangers pages, don't contribute to status summaries. Can have a seed client node and a real peer node connected to the same node. Anonymous initiator link crypto: - Needed for opennet (seednode connection). May also be used for various forms of invites later on. - Initiator knows responder but responder doesn't know initiator. - Outer encryption wrapper based only on the responder's identity. - One extra control byte to indicate the type of connection (e.g. seednode). - Standard darknet connections don't send the initiator identity etc in JFK (this is just a matter of leaving out fields we already know the value of; the bits we do send, we send as specified). This protocol sends the initiator identity etc, although we still leave out the responder identity (except the hash in packet 1). - Can send oversize (fragmented) packets for packet 3 and 4. Not easily avoidable at present. Link crypto: - Fix the unknown exponential bug. Disconnected darknet support: - Option to disable routing on a peer. If enabled, we won't route to it, and it won't route to us. Useful to keep a disconnected darknet up to date from a single darknet connection without passing any routing traffic. Dev stuff: - Logging. - Tidying code up - remove useless casts, rename variables, double ;, etc. - Javadocs, comments. - Minor refactoring. Misc stuff: - Remove the NPTL warning. - Warn users if they are not using the wrapper. - Warn users if they are not running a Sun VM.
    Echo
    - Refactoring.
    - Insert as USK.
    - Get some stuff from the pluginrespirator not in constructor.
    - Bugfixes.
    
    Website:
    - Added some more people to the People page.
    
    nextgens
    toad
    
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@16364 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -24,17 +24,17 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1088;
+	private static final int buildNumber = 1089;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 1085;
-	private static final int newLastGoodBuild = 1088;
+	private static final int oldLastGoodBuild = 1088;
+	private static final int newLastGoodBuild = 1089;
 	static final long transitionTime;
 	
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2007, Calendar.DECEMBER, 6, 0, 0, 0 );
+		_cal.set( 2007, Calendar.DECEMBER, 8, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 	
