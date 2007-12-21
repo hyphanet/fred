89605fc53492b6114621d6f625a7b5a5131f56c1
Matthew Toseland <toad@amphibian.dyndns.org>
2007-12-21 23:43:53 +0000

    1094: (Mandatory Sunday) Versioning: - Recommend freenet-ext.jar #18. - Fix an NPE. Big packets: - Send JFK(4)'s with only one noderef for anon-initiator. Unfortunately we need to send the old ones for normal connection setup, so that UOM works. Low-level CHK inserts: - Send the request asynchronously. If we don't get a response in 10 seconds we timeout anyway, so there's no reason for it. - Don't use BlockTransmitter.failedByOverload to decide whether the transfer failed. It's never set. Use the retval of send(). - Use an async filter to wait for the completion message, saves one thread per node-we-sent-the-data-to. We still have the overall completion waiter. - InsertHandler.finish(): Wait for the incoming transfer before committing and waiting for completion. - InsertHandler: Don't commit twice. - InsertHandler: Fix race condition, set the receiveStarted flag slightly earlier. - InsertHandler: Send transfers completed notification if we are the last node, or couldn't send it anywhere. Prevents timeout. - CHKInsertSender: Trivial refactoring. Catch and handle DisconnectedException instead of falling through to Throwable handler. Web interface: - Queue toadlet: show unknown sizes as "unknown". - Fix the stats page - don't show seednodes etc locations, don't break when nodes don't have a location. L10n: - German update from NEOatNHNG. Startup: - Don't log an error if we're just creating new node* for the first time. Dev stuff: - Indenting. - Logging. - Shorten some thread names. - Comments. - Remove dead code.
    Freenet-ext #18:
    - Update [Oracle] Berkeley DB Java Edition to 3.2.68. This is used by the datastore.
    
    
    NEOatNHNG
    nextgens
    robert
    toad
    
    
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@16772 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -24,17 +24,17 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1093;
+	private static final int buildNumber = 1094;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 1092;
-	private static final int newLastGoodBuild = 1093;
+	private static final int oldLastGoodBuild = 1093;
+	private static final int newLastGoodBuild = 1094;
 	static final long transitionTime;
 	
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2007, Calendar.DECEMBER, 21, 0, 0, 0 );
+		_cal.set( 2007, Calendar.DECEMBER, 23, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 	
