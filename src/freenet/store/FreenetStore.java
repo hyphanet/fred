4fc98dfa5b063a57a6a9dd3d2b9668a4efa49450
Matthew Toseland <toad@amphibian.dyndns.org>
2008-01-05 21:24:34 +0000

    Delete old redundant methods for fetching/storing SSK/CHK/pubkeys
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@16922 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/store/FreenetStore.java
+++ src/freenet/store/FreenetStore.java
@@ -4,12 +4,6 @@ import java.io.IOException;
 
 import com.sleepycat.je.DatabaseException;
 
-import freenet.crypt.DSAPublicKey;
-import freenet.keys.CHKBlock;
-import freenet.keys.NodeCHK;
-import freenet.keys.NodeSSK;
-import freenet.keys.SSKBlock;
-
 /**
  * Datastore interface
  */
@@ -24,25 +18,6 @@ public interface FreenetStore {
 	 */
 	StorableBlock fetch(byte[] routingKey, byte[] fullKey, boolean dontPromote) throws IOException;
 	
-    /**
-     * Retrieve a block.
-     * @param dontPromote If true, don't promote data if fetched.
-     * @return null if there is no such block stored, otherwise the block.
-     */
-    public CHKBlock fetch(NodeCHK key, boolean dontPromote) throws IOException;
-
-    /**
-     * Retrieve a block.
-     * @param dontPromote If true, don't promote data if fetched.
-     * @return null if there is no such block stored, otherwise the block.
-     */
-    public SSKBlock fetch(NodeSSK key, boolean dontPromote) throws IOException;
-
-    /**
-     * Fetch a public key.
-     */
-    public DSAPublicKey fetchPubKey(byte[] hash, boolean dontPromote) throws IOException;
-    
     /** Store a block.
      * @throws KeyCollisionException If the key already exists but has different contents.
      * @param ignoreAndOverwrite If true, overwrite old content rather than throwing a KeyCollisionException.
@@ -50,23 +25,6 @@ public interface FreenetStore {
     public void put(StorableBlock block, byte[] routingkey, byte[] fullKey, byte[] data, byte[] header, 
     		boolean overwrite) throws IOException, KeyCollisionException;
     
-    /**
-     * Store a block.
-     * @throws KeyCollisionException If the key already exists but has different contents.
-     * @param ignoreAndOverwrite If true, overwrite old content rather than throwing a KeyCollisionException. 
-     */
-    public void put(SSKBlock block, boolean ignoreAndOverwrite) throws IOException, KeyCollisionException;
-
-    /**
-     * Store a block.
-     */
-    public void put(CHKBlock block) throws IOException;
-    
-    /**
-     * Store a public key.
-     */
-    public void put(byte[] hash, DSAPublicKey key) throws IOException;
-
     /**
      * Change the store size.
      * @param maxStoreKeys The maximum number of keys to be cached.
