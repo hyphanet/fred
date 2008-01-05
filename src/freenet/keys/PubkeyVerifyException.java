663fe62994c87261b774f816fb433fe1f345085a
Matthew Toseland <toad@amphibian.dyndns.org>
2008-01-05 19:49:47 +0000

    Add a serial version to stop eclipse complaining
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@16916 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/keys/PubkeyVerifyException.java
+++ src/freenet/keys/PubkeyVerifyException.java
@@ -4,6 +4,8 @@ import freenet.crypt.CryptFormatException;
 
 public class PubkeyVerifyException extends KeyVerifyException {
 
+	private static final long serialVersionUID = 1L;
+
 	public PubkeyVerifyException(CryptFormatException e) {
 		super(e);
 	}
