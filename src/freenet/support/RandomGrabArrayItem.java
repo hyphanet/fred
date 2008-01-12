cf25a545fc3276ace4c4b779ced778a6d2d32263
Matthew Toseland <toad@amphibian.dyndns.org>
2008-01-12 21:00:56 +0000

    locking note
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@17032 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/support/RandomGrabArrayItem.java
+++ src/freenet/support/RandomGrabArrayItem.java
@@ -5,7 +5,8 @@ public interface RandomGrabArrayItem {
 	/** If true, will be automatically removed from the RGA, and not returned.
 	 * True indicates that the item is no longer needed for some reason - in a request,
 	 * usually that it has either been explicitly cancelled or that it is not needed
-	 * because other queued blocks have been sufficient. */
+	 * because other queued blocks have been sufficient. LOCKING: Should hold as few 
+	 * locks as possible as this needs to be called while holding the RGA lock(s). */
 	public boolean isCancelled();
 	
 	/** Can this item be removed from the queue after it has been handled?
