f8858653ad358aa54b50e5f95718a1ed2f08903e
Matthew Toseland <toad@amphibian.dyndns.org>
2008-01-24 00:01:53 +0000

    1103: (mandatory tomorrow, sorry folks, but look below!) Request layer: - Un-break CHK request data transfers! - Inserts: Start the data transfer at the beginning, without waiting for a response (revert 16727). Client layer: - Fix a deadlock / avoid nested locking. Data transfer: - Don't propagate repeated packets. - Sanity check. - Actually send the allSent message. IIRC we don't use this atm. Make it BULK_DATA so it is sent after the blocks it refers to. - Don't re-add a block to the resend queue if it's already there. Dev stuff: - Logging. - Thread names (use executor with a name).
    robert
    toad
    
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@17217 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -24,11 +24,11 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1102;
+	private static final int buildNumber = 1103;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1101;
-	private static final int newLastGoodBuild = 1102;
+	private static final int newLastGoodBuild = 1103;
 	static final long transitionTime;
 	
 	static {
