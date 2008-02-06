162b1c584da181b8a15b485c3862079795fa1035
Matthew Toseland <toad@amphibian.dyndns.org>
2008-02-06 01:02:02 +0000

    One too many htl decrements here too
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@17583 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/AnnounceSender.java
+++ src/freenet/node/AnnounceSender.java
@@ -99,16 +99,12 @@ public class AnnounceSender implements Runnable, ByteCounter {
         	if(source != null)
         		htl = node.decrementHTL(source, htl);
         }
-        boolean firstHop = false;
         
 		// Now route it.
 		
         HashSet nodesRoutedTo = new HashSet();
         HashSet nodesNotIgnored = new HashSet();
         while(true) {
-        	if(!firstHop)
-        		htl = node.decrementHTL(source, htl);
-        	firstHop = true;
             if(logMINOR) Logger.minor(this, "htl="+htl);
             if(htl == 0) {
             	// No more nodes.
