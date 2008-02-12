0fd13e1a84d7b491e1875f645a19d1c1ce6266d5
Matthew Toseland <toad@amphibian.dyndns.org>
2008-02-12 23:44:10 +0000

    Hopefully fix
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@17847 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/simulator/RealNodeRequestInsertTest.java
+++ src/freenet/node/simulator/RealNodeRequestInsertTest.java
@@ -55,7 +55,7 @@ public class RealNodeRequestInsertTest extends RealNodeRoutingTest {
         wd.mkdir();
         //NOTE: globalTestInit returns in ignored random source
         //NodeStarter.globalTestInit(name, false, Logger.ERROR, "freenet.node.Location:normal,freenet.node.simulator.RealNode:minor,freenet.node.Insert:MINOR,freenet.node.Request:MINOR,freenet.node.Node:MINOR");
-        NodeStarter.globalTestInit(name, false, Logger.ERROR, "");
+        NodeStarter.globalTestInit(name, false, Logger.ERROR, "freenet.node.Location:MINOR,freenet.io.comm:MINOR,freenet.node.Node:MINOR");
         System.out.println("Insert/retrieve test");
         System.out.println();
         DummyRandomSource random = new DummyRandomSource();
