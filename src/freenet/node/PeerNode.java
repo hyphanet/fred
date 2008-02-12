b21d418c38c547d9d83e75b7f8355c00f79eb66a
Matthew Toseland <toad@amphibian.dyndns.org>
2008-02-12 02:01:55 +0000

    Much shorter averaging period for PeerNode ping time
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@17806 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/PeerNode.java
+++ src/freenet/node/PeerNode.java
@@ -553,7 +553,8 @@ public abstract class PeerNode implements PeerContext, USKRetrieverCallback {
 		// A SimpleRunningAverage would be a bad choice because it would cause oscillations.
 		// So go for a filter.
 		pingAverage =
-			new TimeDecayingRunningAverage(1, 240 * 1000 /* should be significantly longer than a typical transfer */, 0, NodePinger.CRAZY_MAX_PING_TIME, node);
+			// Short average otherwise we will reject for a *REALLY* long time after any spike.
+			new TimeDecayingRunningAverage(1, 30, 0, NodePinger.CRAZY_MAX_PING_TIME, node);
 
 		// TDRA for probability of rejection
 		pRejected =
