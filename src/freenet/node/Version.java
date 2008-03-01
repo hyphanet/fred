d18a1afc20ea7d54516e3b577407a78b2011fa7e
Matthew Toseland <toad@amphibian.dyndns.org>
2008-03-01 02:14:55 +0000

    1118: Link layer: - Fix various bugs causing nodes to not exchange locations (not to use the FNPLocChangeNotification, not to send it, etc) and therefore causing connections to new nodes to fail. - The criteria to be on the connected peers list is not that it be routable, it's now that it is a real connection and is connected. This appears to be necessary for new connections (as above) to work. - Don't send JFK(4)'s with bothNoderefs, even if we are doing a normal darknet/opennet connection. Remove the code to send them. - Keep the code to parse bothNoderefs JFK(4)'s, and fix a bug breaking one-noderef JFK(4)'s if pn.jfkMyRef hasn't been set (it should be, but it's not essential). - Don't include physical.udp, ark.number and ark.pubURI in for-setup noderefs, they will be passed across later via an N2NTM. Congestion control: - Report the correct number of bytes (1024, not 1137) to the master throttle, for both block and bulk transfers. - Token buckets: Don't allow force-grabbing a negative number of tokens. Log an error if this happens. Dev stuff: - PeerNode.isLocalAddress(): trivial simplification. - Trivial clarifications. - Logging.
    toad
    zothar
    
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@18282 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -24,11 +24,11 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1117;
+	private static final int buildNumber = 1118;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1113;
-	private static final int newLastGoodBuild = 1117;
+	private static final int newLastGoodBuild = 1118;
 	static final long transitionTime;
 	
 	static {
