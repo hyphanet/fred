306a22c0760b8e4391baafc5ad7fbd512885e163
Matthew Toseland <toad@amphibian.dyndns.org>
2008-03-04 16:52:55 +0000

    Fix warning: add generated serialVersion
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@18347 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/clients/http/PermanentRedirectException.java
+++ src/freenet/clients/http/PermanentRedirectException.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 public class PermanentRedirectException extends Exception {
 
+	private static final long serialVersionUID = -166786248237623796L;
 	URI newuri;
 	
 	public PermanentRedirectException() {
