89a497b3649e56a72db8cc8bbb99113c589e1286
Matthew Toseland <toad@amphibian.dyndns.org>
2008-03-10 14:37:57 +0000

    1122: (Mandatory 12 March) Throttled packet sends (link layer): - Add a timeout for sendThrottledPacket(). Only wait this long to get clearance to send the packet, if it takes longer, throw an exception, because it would have already timed out on the other side. - Notify the throttle in BlockTransmitter when we disconnect. This shouldn't be necessary, but somewhere there is a bug and somehow it's not getting notified. Between these two changes the bug should have no real effect any more (it was having a major effect), and we should get an ERROR logged when a bulk transmit hangs and times out because it wasn't notified. - Move getThrottle() to PeerNode. Fixes NPEs as well as more logical. HTL: - Reduce HTL to 10. Now we have 10 hops random at 10, 9 hops countdown, 4 hops random terminal. Fproxy: - Inline prefetch support (i.e. only frames and images). Configurable, off by default. Might help with connection-limited browsers but testing wasn't positive, so it's off at the moment. - FoundURICallback: foundURI(uri, boolean isInline). - Don't show activelinks if the useragent includes Safari. It does bad things such as not rendering the page at all until all its got all the alinks. Startup etc: - Only run one splitfile start per priority level at any given time. We had 60+ running and they were causing timeouts because the node dispatcher thread was unable to access the datastore in a reasonable time (during request restarting just after startup; the actual number depended on how many restartable requests you have, but is now a maximum of 7, and practically not often that many). - Also tweak the priorities - run immediate decodes at normal, updates etc at minimum, everything in the middle at low. - Run the startup serialisation at low. Bookmarks: - Update editions. - Add Freenet Activelink Index. Both the static and dynamic versions. - Reorder the bookmarks. Stats: - Don't doublecount SSK payload in RequestHandler. - Track bytes used by EVERY message sent. Display this info on the stats page, totals by category. Also some messages were in displayed categories already but weren't being reported to them. - Track exactly how many bytes are used by a specific packet, and account them to the various packets being sent (onSent()'s). - Track the bandwidth used by ack-only packets. Probe requests: - Fix an NPE. Freenet URIs: - FreenetURI: An unrecognised keyType is a malformed freenet URI. - Remove any leading slashes for more user friendly error handling. Thanks Ratchet. Native thread libraries: - Fix loading criterion (was slightly broken). - Load the OS/X native threads library. **********FIXME this makes things worse doesn't it? Updater: - Try the easy way only on *nix (bad condition). Dev stuff: - Comments. - Logging. - Thread names.
    Installer:
    - Fix a freebsd bug by preferring IPv4 over IPv6.
    - Change the default nice level to 10.
    
    jSite:
    - Fix insert (and tell the user) when a default file was specified and then removed in a later edition.
    
    Thingamablog:
    - Option to publish an activelink.
    
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@18438 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -24,17 +24,17 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1121;
+	private static final int buildNumber = 1122;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 1113;
-	private static final int newLastGoodBuild = 1121;
+	private static final int oldLastGoodBuild = 1121;
+	private static final int newLastGoodBuild = 1122;
 	static final long transitionTime;
 	
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2008, Calendar.MARCH, 6, 0, 0, 0 );
+		_cal.set( 2008, Calendar.MARCH, 12, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 	
