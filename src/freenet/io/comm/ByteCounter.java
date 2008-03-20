837ea3f4e78e5ad4bbc2ba61d9c7119812c71ac9
Matthew Toseland <toad@amphibian.dyndns.org>
2008-03-20 17:03:45 +0000

    Clarify javadocs
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@18647 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/io/comm/ByteCounter.java
+++ src/freenet/io/comm/ByteCounter.java
@@ -8,11 +8,12 @@ package freenet.io.comm;
  */
 public interface ByteCounter {
 	
+	/** Sent some bytes. *Includes* any bytes flagged as already-reported-to-throttle. */
 	public void sentBytes(int x);
 	
 	public void receivedBytes(int x);
 	
-	/** Sent payload - only include the number of bytes of actual payload i.e. data from the user's point of view, as opposed to overhead */
+	/** Sent payload - only include the number of bytes of actual payload i.e. data from the user's point of view, as opposed to overhead. */
 	public void sentPayload(int x);
 
 }
