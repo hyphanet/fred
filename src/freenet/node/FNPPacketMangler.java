596418a41952d6a7ce01a1fa13f3c8448813494c
Matthew Toseland <toad@amphibian.dyndns.org>
2008-03-20 16:42:11 +0000

    indenting
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@18642 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/FNPPacketMangler.java
+++ src/freenet/node/FNPPacketMangler.java
@@ -1682,12 +1682,12 @@ public class FNPPacketMangler implements OutgoingPacketMangler, IncomingPacketFi
 		if(pn != null)
 			pn.reportOutgoingPacket(data, 0, data.length, System.currentTimeMillis());
 		if(PeerNode.shouldThrottle(replyTo, node)) {
-		int reportableBytes = data.length - alreadyReportedBytes;
-		if(reportableBytes < 0) {
-			Logger.error(this, "alreadyReportedBytes ("+alreadyReportedBytes+")> data.length ("+data.length+")");
-			reportableBytes = 0;
-		}
-		node.outputThrottle.forceGrab(reportableBytes);
+			int reportableBytes = data.length - alreadyReportedBytes;
+			if(reportableBytes < 0) {
+				Logger.error(this, "alreadyReportedBytes ("+alreadyReportedBytes+")> data.length ("+data.length+")");
+				reportableBytes = 0;
+			}
+			node.outputThrottle.forceGrab(reportableBytes);
 		}
 	}
 
