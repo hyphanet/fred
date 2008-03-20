7c6c8b998ff22874edf67419f2368721b267638f
Matthew Toseland <toad@amphibian.dyndns.org>
2008-03-20 20:17:14 +0000

    1126: (mandatory saturday) Stats: - Reinstate reporting payload bytes for SSK data replies. - Report payload more accurately in SSKInsertSender. - Only report bytes to the global throttle if the connection is subject to throttling. - Call sentPayload() from sendThrottledPacket(). - Fix calling sentPayload() twice in RequestHandler. Requests: - Stop the insert handler if we lose the connection to the source. SSK split messages: - Send the new SSK messages on an SSK insert collision as well as FNPSSKDataFound. - Handle the new messages for an SSK insert collision. - Handle FNPSSKDataFoundNew when sent a GetOfferedKey in RequestSender (we had the code but we weren't listening for the message!). - Now that code is used, fix a bug: we were waiting for a pubkey even though we didn't need one and therefore hadn't asked for one, causing a timeout. - Don't send an old-format SSK in response to GetOfferedKey unless the flag to send old-format SSK messages is set. Simulations: - ULPR test: Hook into Dispatcher and use this to determine which nodes have been visited by requests for the key. Dev stuff: - Logging. Log transfer failures and disconnects at normal. We have stats about them and they don't need to spam the ERROR-level logs. - Indenting. - Comments, javadocs. - Minor refactoring, delete dead code.
    Installer:
    - It's firefox -P <profile>, not firefox -p <profile>. Both linux and windows.
    
    toad
    robert
    
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@18662 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -24,11 +24,11 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1125;
+	private static final int buildNumber = 1126;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1124;
-	private static final int newLastGoodBuild = 1125;
+	private static final int newLastGoodBuild = 1126;
 	static final long transitionTime;
 	
 	static {
