afb8175f279ff47bf4fa2fefa291227bf3311813
Matthew Toseland <toad@amphibian.dyndns.org>
2008-03-21 22:48:42 +0000

    comments
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@18704 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/KeysFetchingLocally.java
+++ src/freenet/node/KeysFetchingLocally.java
@@ -6,6 +6,7 @@ public interface KeysFetchingLocally {
 	
 	/**
 	 * Is this key currently being fetched locally?
+	 * LOCKING: This should be safe just about anywhere, the lock protecting it is always taken last.
 	 */
 	public boolean hasKey(Key key);
 
