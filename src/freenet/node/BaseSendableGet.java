36b22136a95a7288db878c82c8dc2c50059f97d6
Matthew Toseland <toad@amphibian.dyndns.org>
2008-03-22 00:09:02 +0000

    Missing class
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@18716 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ src/freenet/node/BaseSendableGet.java
@@ -0,0 +1,12 @@
+package freenet.node;
+
+import freenet.keys.Key;
+
+public abstract class BaseSendableGet extends SendableRequest {
+	
+	/** Get a numbered key to fetch. */
+	public abstract Key getNodeKey(Object token);
+	
+	public abstract boolean hasValidKeys(KeysFetchingLocally fetching);
+
+}
