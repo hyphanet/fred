4483b50de06af1308a7ee0120c3208c02693f907
Matthew Toseland <toad@amphibian.dyndns.org>
2008-03-24 16:03:00 +0000

    More rigorous: only apply if we have had packets.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@18735 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/PeerNode.java
+++ src/freenet/node/PeerNode.java
@@ -2409,8 +2409,8 @@ public abstract class PeerNode implements PeerContext, USKRetrieverCallback {
 				} catch(WouldBlockException e) {
 					Logger.error(this, "Impossible: " + e, e);
 				}
-			if(t > -1 && now - tracker.timeLastDecodedPacket() > 60*1000 && cur != null && 
-					(now - cur.timeLastDecodedPacket() < 30*1000) && 
+			if(t > -1 && tracker.timeLastDecodedPacket() > 0 && (now - tracker.timeLastDecodedPacket()) > 60*1000 && 
+					cur != null && cur.timeLastDecodedPacket() > 0 && (now - cur.timeLastDecodedPacket() < 30*1000) && 
 					(tracker.countAckRequests() > 0 || tracker.countResendRequests() > 0)) {
 				Logger.error(this, "No packets decoded on "+tracker+" for 60 seconds, deprecating in favour of cur: "+cur);
 				prev.completelyDeprecated(cur);
