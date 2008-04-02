f777d6557d18774301ec58939732fc433fb48573
Dave Baker <dbkr@freenetproject.org>
2008-04-02 22:01:06 +0000

    Fix logging confusion (assuming ERROR was intended, change if not)
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@18922 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/client/async/USKManager.java
+++ src/freenet/client/async/USKManager.java
@@ -212,7 +212,7 @@ public class USKManager {
 			USK clear = origUSK.clearCopy();
 			USKCallback[] callbacks = (USKCallback[]) subscribersByClearUSK.get(clear);
 			if(callbacks == null){ // maybe we should throw something ? shall we allow multiple unsubscriptions ?
-				if(Logger.shouldLog(Logger.MINOR, this)){
+				if(Logger.shouldLog(Logger.ERROR, this)){
 					Logger.error(this, "The callback is null! it has been already unsubscribed, hasn't it?");
 					new NullPointerException("The callback is null! it has been already unsubscribed, hasn't it?").printStackTrace();
 				}
