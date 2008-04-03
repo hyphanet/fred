aad313d51fbc2bb7e226043d6e5f210bf500ca1c
Matthew Toseland <toad@amphibian.dyndns.org>
2008-04-03 18:31:56 +0000

    1134: ANOTHER deadlock fix. This one was probably introduced in 1131.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@18965 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -24,11 +24,11 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1133;
+	private static final int buildNumber = 1134;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1131;
-	private static final int newLastGoodBuild = 1133;
+	private static final int newLastGoodBuild = 1134;
 	static final long transitionTime;
 	
 	static {
