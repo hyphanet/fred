630909e638f35cec2f0b751ee367187956104063
Florent Daignière <nextgens@freenetproject.org>
2008-04-04 05:21:19 +0000

    indent
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@18967 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/support/math/RunningAverage.java
+++ src/freenet/support/math/RunningAverage.java
@@ -6,21 +6,25 @@ package freenet.support.math;
 import java.io.Serializable;
 
 public interface RunningAverage extends Serializable, Cloneable {
-    
-    public Object clone();
-    
+
+	public Object clone();
+
 	public double currentValue();
+
 	public void report(double d);
+
 	public void report(long d);
-    /**
-     * Get what currentValue() would be if we reported some given value
-     * @param r the value to mimic reporting
-     * @return the output of currentValue() if we were to report r
-     */
-    public double valueIfReported(double r);
-    /**
-     * @return the total number of reports on this RunningAverage so far.
-     * Used for weighted averages, confidence/newbieness estimation etc.
-     */
-    public long countReports();
+
+	/**
+	 * Get what currentValue() would be if we reported some given value
+	 * @param r the value to mimic reporting
+	 * @return the output of currentValue() if we were to report r
+	 */
+	public double valueIfReported(double r);
+
+	/**
+	 * @return the total number of reports on this RunningAverage so far.
+	 * Used for weighted averages, confidence/newbieness estimation etc.
+	 */
+	public long countReports();
 }
