d108fde66af7e9c3bc9fcce6be6ae7122e8012a4
Matthew Toseland <toad@amphibian.dyndns.org>
2008-04-05 14:59:33 +0000

    Fix #2206: we were sending the unescaped URI over an ISO-8859-1 connection (HTTP), and of course wierd chars were turned into ?'s.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@19033 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/clients/http/FProxyToadlet.java
+++ src/freenet/clients/http/FProxyToadlet.java
@@ -432,7 +432,7 @@ public final class FProxyToadlet extends Toadlet {
 			if(Logger.shouldLog(Logger.MINOR, this))
 				Logger.minor(this, "Failed to fetch "+uri+" : "+e);
 			if(e.newURI != null) {
-				Toadlet.writePermanentRedirect(ctx, msg, '/' +e.newURI.toString() + override);
+				Toadlet.writePermanentRedirect(ctx, msg, '/' +e.newURI.toACIIString() + override);
 			} else if(e.mode == FetchException.TOO_BIG) {
 				HTMLNode pageNode = ctx.getPageMaker().getPageNode(l10n("fileInformationTitle"), ctx);
 				HTMLNode contentNode = ctx.getPageMaker().getContentNode(pageNode);
