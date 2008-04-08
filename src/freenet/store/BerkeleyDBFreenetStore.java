53ffaf356aa02695063c19c3ace0d492bf4829d1
Matthew Toseland <toad@amphibian.dyndns.org>
2008-04-08 18:09:32 +0000

    Revert 19085 (keysRAF won't be null unless callback.storeFullKeys()).
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@19088 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/store/BerkeleyDBFreenetStore.java
+++ src/freenet/store/BerkeleyDBFreenetStore.java
@@ -1151,7 +1151,7 @@ public class BerkeleyDBFreenetStore implements FreenetStore {
 					Logger.normal(this, "Block "+l+" : LRU "+lruVal);
 				}
 				boolean readKey = false;
-				if(callback.storeFullKeys() && keysRAF != null && keysRAFLength > (l+1)*keyLength) {
+				if(keysRAF != null && keysRAFLength > (l+1)*keyLength) {
 					try {
 						keysRAF.readFully(keyBuf);
 						readKey = true;
