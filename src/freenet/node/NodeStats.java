0fe29fe418dd597f5a4608dc5d0906eb32a0ecdf
Matthew Toseland <toad@amphibian.dyndns.org>
2008-04-10 18:27:11 +0000

    Revert 18846. Doing it by the fraction causes nasty problems - the node won't recover. Will fix the problems leading up to 18846 a different way...
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@19150 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/NodeStats.java
+++ src/freenet/node/NodeStats.java
@@ -488,10 +488,7 @@ public class NodeStats implements Persistable {
 			successfulChkOfferReplyBytesSentAverage.currentValue() * numCHKOfferReplies +
 			successfulSskOfferReplyBytesSentAverage.currentValue() * numSSKOfferReplies;
 		double bandwidthAvailableOutput =
-			// It's safer to use the fraction. If for some reason our output has been over the limit
-			// for a while, (which we cannot entirely eliminate with the current code), doing it the
-			// other way could produce a very low or even negative number.
-			(node.getOutputBandwidthLimit() * overheadFraction) * 90; // 90 seconds at full power; we have to leave some time for the search as well
+			(node.getOutputBandwidthLimit() - sentOverheadPerSecond) * 90; // 90 seconds at full power; we have to leave some time for the search as well
 		if(bandwidthLiabilityOutput > bandwidthAvailableOutput) {
 			pInstantRejectIncoming.report(1.0);
 			rejected("Output bandwidth liability", isLocal);
