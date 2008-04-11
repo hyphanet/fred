601d5a62403e3c6c1718110f3f02406fc62d6ea2
Daniel Cheng (鄭郁邦) <j16sdiz+freenet@gmail.com>
2008-04-11 07:57:20 +0000

    findbugs: PartiallyReceivedBulk.recv is never written --> always null
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@19166 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/io/xfer/PartiallyReceivedBulk.java
+++ src/freenet/io/xfer/PartiallyReceivedBulk.java
@@ -30,8 +30,6 @@ public class PartiallyReceivedBulk {
 	final int blocks;
 	private BulkTransmitter[] transmitters;
 	final MessageCore usm;
-	/** The one and only BulkReceiver */
-	private BulkReceiver recv;
 	private int blocksReceivedCount;
 	// Abort status
 	boolean _aborted;
@@ -132,21 +130,17 @@ public class PartiallyReceivedBulk {
 		if(Logger.shouldLog(Logger.NORMAL, this))
 			Logger.normal(this, "Aborting "+this+": "+errCode+" : "+why, new Exception("debug"));
 		BulkTransmitter[] notifyBTs;
-		BulkReceiver notifyBR;
 		synchronized(this) {
 			_aborted = true;
 			_abortReason = errCode;
 			_abortDescription = why;
 			notifyBTs = transmitters;
-			notifyBR = recv;
 		}
 		if(notifyBTs != null) {
 			for(int i=0;i<notifyBTs.length;i++) {
 				notifyBTs[i].onAborted();
 			}
 		}
-		if(notifyBR != null)
-			notifyBR.onAborted();
 		raf.close();
 	}
 
