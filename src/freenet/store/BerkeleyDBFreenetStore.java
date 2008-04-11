31e75aa595846a9330be80c70534a0fd83278ba3
Matthew Toseland <toad@amphibian.dyndns.org>
2008-04-11 15:18:35 +0000

    Allow a bit more time for store reconstruction.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@19187 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/store/BerkeleyDBFreenetStore.java
+++ src/freenet/store/BerkeleyDBFreenetStore.java
@@ -885,7 +885,7 @@ public class BerkeleyDBFreenetStore implements FreenetStore {
 			throw new IllegalStateException("Store must be empty before reconstruction!");
 		System.err.println("Reconstructing store index from store file: callback="+callback);
 		Logger.error(this, "Reconstructing store index from store file: callback="+callback);
-		WrapperManager.signalStarting((int)(Math.min(Integer.MAX_VALUE, 5*60*1000+(storeRAF.length()/(dataBlockSize+headerBlockSize))*100)));
+		WrapperManager.signalStarting((int)(Math.min(Integer.MAX_VALUE, 5*60*1000+(storeRAF.length()/(dataBlockSize+headerBlockSize))*1000)));
 		// Reusing the buffer is safe, provided we don't do anything with the resulting StoreBlock.
 		byte[] header = new byte[headerBlockSize];
 		byte[] data = new byte[dataBlockSize];
