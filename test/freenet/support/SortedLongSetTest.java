691e5ca3220cb65817bb5d803e5be8c30c50a5a2
Daniel Cheng (鄭郁邦) <j16sdiz+freenet@gmail.com>
2008-04-12 14:01:07 +0000

    make SortedLongSet accept Long.MAX_VALUE
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@19230 67a373e5-eb02-0410-a15c-ee090a768436

--- test/freenet/support/SortedLongSetTest.java
+++ test/freenet/support/SortedLongSetTest.java
@@ -8,13 +8,7 @@ import junit.framework.TestCase;
  * @author sdiz
  */
 public class SortedLongSetTest extends TestCase {
-	/*
-	 * FIXME use Long.MAX_VALUE , not MAX_VALUE - 1
-	 */
-	private final static long[] testArray  =  {
-		10, 8, 6, 2, 0, 1, 11,
-	        Long.MAX_VALUE - 1, 4, 7, 5, 3, Long.MIN_VALUE 
-	};
+	private final static long[] testArray = { 10, 8, 6, 2, 0, 1, 11, Long.MAX_VALUE, 4, 7, 5, 3, Long.MIN_VALUE };
 
 	protected SortedLongSet perpare(long[] array) {
 		SortedLongSet set = new SortedLongSet();
@@ -60,7 +54,7 @@ public class SortedLongSetTest extends TestCase {
 		// Contain
 		assertTrue(set.contains(0L));
 		assertTrue(set.contains(3L));
-		assertTrue(set.contains(Long.MAX_VALUE - 1));
+		assertTrue(set.contains(Long.MAX_VALUE));
 		assertTrue(set.contains(Long.MIN_VALUE));
 
 		// Not contain
