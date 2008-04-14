52232195b62b03162e77da49758a1714305b11ec
Florent Daignière <nextgens@freenetproject.org>
2008-04-14 02:48:48 +0000

    fix the build
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@19293 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/keys/FreenetURI.java
+++ src/freenet/keys/FreenetURI.java
@@ -643,10 +643,12 @@ public class FreenetURI implements Cloneable{
 			dis.readFully(cryptoKey);
 			// Number of bytes of extra depends on key type
 			int extraLen;
-			if((type == CHK) || (type == SSK))
+			if((type == CHK) || (type == SSK)) {
 				extraLen = ClientCHK.EXTRA_LENGTH;
-			extra = new byte[extraLen];
-			dis.readFully(extra);
+				extra = new byte[extraLen];
+				dis.readFully(extra);
+			} else
+				extra = new byte[0];
 		}
 		String docName = null;
 		if(type != CHK)
