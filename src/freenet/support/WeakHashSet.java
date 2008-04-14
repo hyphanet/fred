221a60b4ebf65133121d20b4b8568f1846a028f8
Daniel Cheng (鄭郁邦) <j16sdiz+freenet@gmail.com>
2008-04-14 08:24:36 +0000

    use java.util.AbstractSet
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@19308 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/support/WeakHashSet.java
+++ src/freenet/support/WeakHashSet.java
@@ -1,12 +1,11 @@
 package freenet.support;
 
+import java.util.AbstractSet;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.Set;
 import java.util.WeakHashMap;
 
-public class WeakHashSet implements Set {
-	
+public class WeakHashSet extends AbstractSet {
 	private final WeakHashMap map;
 	
 	public WeakHashSet() {
@@ -15,14 +14,6 @@ public class WeakHashSet implements Set {
 
 	public boolean add(Object key) {
 		return map.put(key, null) == null;
-	}
-
-	public boolean addAll(Collection arg0) {
-		boolean changed = false;
-		for(Iterator i=arg0.iterator();i.hasNext();) {
-			changed |= add(i.next());
-		}
-		return changed;
 	}
 
 	public void clear() {
@@ -34,10 +25,7 @@ public class WeakHashSet implements Set {
 	}
 
 	public boolean containsAll(Collection arg0) {
-		for(Iterator i=arg0.iterator();i.hasNext();) {
-			if(!map.containsKey(i.next())) return false;
-		}
-		return true;
+		return map.keySet().containsAll(arg0);
 	}
 
 	public boolean isEmpty() {
@@ -52,19 +40,6 @@ public class WeakHashSet implements Set {
 		return map.remove(key) != null;
 	}
 
-	public boolean removeAll(Collection arg0) {
-		boolean changed = false;
-		for(Iterator i=arg0.iterator();i.hasNext();) {
-			changed |= remove(i.next());
-		}
-		return changed;
-	}
-
-	public boolean retainAll(Collection arg0) {
-		// FIXME
-		throw new UnsupportedOperationException();
-	}
-
 	public int size() {
 		return map.size();
 	}
@@ -76,5 +51,4 @@ public class WeakHashSet implements Set {
 	public Object[] toArray(Object[] arg0) {
 		return map.keySet().toArray(arg0);
 	}
-
 }
