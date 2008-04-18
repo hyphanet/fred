hyphanet/fred

commit 07aebee93178ee8abb4a1ea8a4dc68af33fbc639

remove duplication in longHashCode(byte[]) to reuse longHashCode(byte[], offset, length)
git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@19396 67a373e5-eb02-0410-a15c-ee090a768436

src/freenet/support/Fields.java
@@ -431,13 +431,8 @@ public abstract class Fields {
 	 * Long version of above Not believed to be secure in any sense of the word :)
 	 */
 	public static final long longHashCode(byte[] b) {
-		long h = 0;
-		for (int i = b.length - 1; i >= 0; --i) {
-			int x = b[i] & 0xff;
-			h ^= ((long) x) << ((i & 7) << 3);
-		}
-		return h;
-	}
+		return longHashCode(b, 0, 0);
+	}	
 
 	/**
 	 * Long version of above Not believed to be secure in any sense of the word :)

commit 32363a624efb4f2c7d136113332969d0d7023c03

corrected refactoring bug introduced in R19396 and added unit test for Fields.longToHashCode(byte[]) to demonstrate the fix :/
git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@19401 67a373e5-eb02-0410-a15c-ee090a768436

src/freenet/support/Fields.java
@@ -431,7 +431,7 @@ public abstract class Fields {
 	 * Long version of above Not believed to be secure in any sense of the word :)
 	 */
 	public static final long longHashCode(byte[] b) {
-		return longHashCode(b, 0, 0);
+		return longHashCode(b, 0, b.length);
 	}	
 
 	/**

test/freenet/support/FieldsTest.java
@@ -4,6 +4,11 @@
 
 package freenet.support;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.omg.CORBA.LongHolder;
+
 import junit.framework.TestCase;
 
 /**
@@ -220,4 +225,19 @@ public class FieldsTest extends TestCase {
 		
 		assertEquals(0, Fields.hashCode(input));
 	}
+	
+	public void testLongHashcode() {
+		
+		byte[] b1 = new byte[] { 1, 1, 2, 2, 3, 3 };
+		byte[] b2 = new byte[] { 2, 2, 3, 3, 4, 4 };
+		byte[] b3 = new byte[] { 1, 1, 2, 2, 3, 3 };
+		
+		Long l1 = new Long(Fields.longHashCode(b1));
+		Long l2 = new Long(Fields.longHashCode(b2));
+		Long l3 = new Long(Fields.longHashCode(b3));
+
+		assertFalse(l1.equals(l2));
+		assertFalse(l2.equals(l3));
+		assertTrue(l3.equals(l1)); // should be same due to Fields.longHashcode
+	}	
 }