fab7ede94aa0c99e28724b645ec8cbf6d1938f72
Matthew Toseland <toad@amphibian.dyndns.org>
2008-04-19 17:28:52 +0000

    We want to make nanosPerTick bigger for the grabbedBytesLimiter, not smaller. So DoubleTokenBucket was limiting forceGrab()'s to ~ 125% of the limit, not 80%.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@19440 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/support/DoubleTokenBucket.java
+++ src/freenet/support/DoubleTokenBucket.java
@@ -15,7 +15,7 @@ public class DoubleTokenBucket extends TokenBucket {
 		super(max, nanosPerTick, initialValue);
 		if(forceGrabLimit > 1.0) throw new IllegalArgumentException();
 		logMINOR = Logger.shouldLog(Logger.MINOR, this);
-		grabbedBytesLimiter = new TokenBucket((long)(max * forceGrabLimit), (long)(nanosPerTick * forceGrabLimit), (long)(initialValue * forceGrabLimit));
+		grabbedBytesLimiter = new TokenBucket((long)(max * forceGrabLimit), (long)(nanosPerTick / forceGrabLimit), (long)(initialValue * forceGrabLimit));
 	}
 	
 	public synchronized void forceGrab(long tokens) {
