f53e1319112f268fea41810130bba4d9778fc51d
Matthew Toseland <toad@amphibian.dyndns.org>
2008-04-22 15:38:56 +0000

    Fix HTML useralerts
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@19500 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/useralerts/SimpleHTMLUserAlert.java
+++ src/freenet/node/useralerts/SimpleHTMLUserAlert.java
@@ -6,7 +6,7 @@ import freenet.support.HTMLNode;
 public class SimpleHTMLUserAlert extends AbstractUserAlert {
 
 	public SimpleHTMLUserAlert(boolean canDismiss, String title, String shortText, HTMLNode content, short type) {
-		super(canDismiss, title, shortText, content.getContent(), content, type, true, L10n.getString("UserAlert.hide"), true, null);
+		super(canDismiss, title, content.getContent(), shortText, content, type, true, L10n.getString("UserAlert.hide"), true, null);
 	}
 
 	public void isValid(boolean validity) {
