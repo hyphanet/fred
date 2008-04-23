63f4c9f0357373990bc299aa532bce0d9ac586d0
Florent Daignière <nextgens@freenetproject.org>
2008-04-23 04:56:11 +0000

    bookmarks update notifications didn't have working links :/
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@19515 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/clients/http/bookmark/BookmarkItem.java
+++ src/freenet/clients/http/bookmark/BookmarkItem.java
@@ -81,7 +81,7 @@ public class BookmarkItem extends Bookmark {
         public HTMLNode getHTMLText() {
             HTMLNode n = new HTMLNode("div");
             L10n.addL10nSubstitution(n, "BookmarkItem.bookmarkUpdatedWithLink", new String[]{"link", "/link", "name", "edition"},
-                    new String[]{"<a href=\"" + key.toString() + "\">", "</a>", HTMLEncoder.encode(name), Long.toString(key.getSuggestedEdition())});
+                    new String[]{"<a href=\"/" + key.toString() + "\">", "</a>", HTMLEncoder.encode(name), Long.toString(key.getSuggestedEdition())});
             return n;
         }
 
