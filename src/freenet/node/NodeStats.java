0ba82867d80ca38ea0100082aca3649ef2c06b85
Matthew Toseland <toad@amphibian.dyndns.org>
2008-04-28 17:10:36 +0000

    More logging
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@19598 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/NodeStats.java
+++ src/freenet/node/NodeStats.java
@@ -587,7 +587,7 @@ public class NodeStats implements Persistable {
 		double bandwidthAvailableInput =
 			node.getInputBandwidthLimit() * 90; // 90 seconds at full power
 		if(bandwidthAvailableInput < 0){
-			Logger.error(this, "Negative available bandwidth: "+bandwidthAvailableInput);
+			Logger.error(this, "Negative available bandwidth: "+bandwidthAvailableInput+" node.ibwlimit="+node.getInputBandwidthLimit()+" node.obwlimit="+node.getOutputBandwidthLimit()+" node.inputLimitDefault="+node.inputLimitDefault);
 		}
 		if(bandwidthLiabilityInput > bandwidthAvailableInput) {
 			pInstantRejectIncoming.report(1.0);
