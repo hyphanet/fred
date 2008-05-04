5bd656387d04fdf838cc3b0d420703d273104862
Daniel Cheng (鄭郁邦) <j16sdiz+freenet@gmail.com>
2008-05-04 09:42:08 +0000

    ident
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@19723 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/fcp/ClientPut.java
+++ src/freenet/node/fcp/ClientPut.java
@@ -228,25 +228,27 @@ public class ClientPut extends ClientPutBase {
 			MessageDigest md = SHA256.getMessageDigest();
 			byte[] foundHash;
 			try {
-			try {
-				md.update(salt.getBytes("UTF-8"));
-			} catch (UnsupportedEncodingException e) {}
-			try {
-				InputStream is = data.getInputStream();
-				SHA256.hash(is, md);
-				is.close();
-			} catch (IOException e) {
-				SHA256.returnMessageDigest(md);
-				Logger.error(this, "Got IOE: " +e.getMessage(), e);
-				throw new MessageInvalidException(ProtocolErrorMessage.COULD_NOT_READ_FILE, "Unable to access file: "+e, identifier, global);
-			}
-			foundHash = md.digest();
+				try {
+					md.update(salt.getBytes("UTF-8"));
+				} catch (UnsupportedEncodingException e) {
+				}
+				try {
+					InputStream is = data.getInputStream();
+					SHA256.hash(is, md);
+					is.close();
+				} catch (IOException e) {
+					SHA256.returnMessageDigest(md);
+					Logger.error(this, "Got IOE: " + e.getMessage(), e);
+					throw new MessageInvalidException(ProtocolErrorMessage.COULD_NOT_READ_FILE,
+					        "Unable to access file: " + e, identifier, global);
+				}
+				foundHash = md.digest();
 			} finally {
 				SHA256.returnMessageDigest(md);
 			}
-			
+
 			if(logMINOR) Logger.minor(this, "FileHash result : we found " + Base64.encode(foundHash) + " and were given " + Base64.encode(saltedHash) + '.');
-			
+
 			if(!Arrays.equals(saltedHash, foundHash))
 				throw new MessageInvalidException(ProtocolErrorMessage.DIRECT_DISK_ACCESS_DENIED, "The hash doesn't match! (salt used : \""+salt+"\")", identifier, global);
 		}
