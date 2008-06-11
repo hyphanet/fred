3043e04fcd63aa74df1298928702a8efe98a1904
Florent Daignière <nextgens@freenetproject.org>
2008-06-11 04:37:18 +0000

    1152: mandatory on June the 15th
    "If we are run on a 1.4 JVM, tell the user that this won't be supported for much longer, and temporarily (session-only) turn off auto-update deployment."
    
    If you are still using freenet with a 1.4 JVM it's time to consider upgrading *NOW*
    
    fproxy:
    	- implement a "bulk fetch box" (#2362)
    	- most toadlets have a mode-selection array to switch from simple to advanced mode easily (#2358)
    	- fix #2389: Removing lots of notifications breaks
    	- allow content type override when using a parameter to specify the URI to get
    	- return 404 on "file not in archive", needed for git-over-fproxy
    	- fix 'unknown alert level' when all alert are invalid
    
    l10n:
    	- use full names instead of the ISO code (#1333)
    	- update to the italian translation by luke771
    
    client layer:
    	- Fix too many splitfile blocks by raising the limit a bit
    	- Wait until we have 3 peers before starting requests
    
    FCP:
    	- use TargetFilename to find a MIME type before trying Identifier
    
    Misc:
    	- optimize the way peers are written to disk
    
    And of course some work on logging and some dead-code removal...
    
    authors:
    	- bombe
    	- nextgens
    	- sdiz
    	- toad
    
    
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@20268 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -24,17 +24,17 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1151;
+	private static final int buildNumber = 1152;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1145;
-	private static final int newLastGoodBuild = 1146;
+	private static final int newLastGoodBuild = 1152;
 	static final long transitionTime;
 	
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2008, Calendar.MAY, 8, 0, 0, 0 );
+		_cal.set( 2008, Calendar.JUNE, 15, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 	
