02139d08fd92fea83c12279b7830b6f9878823a0
Matthew Toseland <toad@amphibian.dyndns.org>
2008-06-19 14:52:59 +0000

    imports
    git-svn-id: file:///home/toad/git-migration/temprepository/branches/db4o/freenet@20482 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/fcp/FCPClient.java
+++ src/freenet/node/fcp/FCPClient.java
@@ -6,7 +6,6 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.Vector;
 
 import com.db4o.ObjectContainer;
