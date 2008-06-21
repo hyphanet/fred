e8168a0900c071ff8a3ddebedd34ae88de74d6b7
Florent Daignière <nextgens@freenetproject.org>
2008-06-21 14:05:42 +0000

    indent
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@20568 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/clients/http/StartupToadlet.java
+++ src/freenet/clients/http/StartupToadlet.java
@@ -11,50 +11,50 @@ import freenet.support.api.HTTPRequest;
  */
 public class StartupToadlet extends Toadlet {
 
-    private StaticToadlet staticToadlet;
-    private volatile boolean isPRNGReady = false;
-	
+	private StaticToadlet staticToadlet;
+	private volatile boolean isPRNGReady = false;
+
 	public StartupToadlet(StaticToadlet staticToadlet) {
 		super(null);
 		this.staticToadlet = staticToadlet;
 	}
-	
-    public void handleGet(URI uri, HTTPRequest req, ToadletContext ctx) throws ToadletContextClosedException, IOException, RedirectException {
-    // If we don't disconnect we will have pipelining issues
-    ctx.forceDisconnect();
 
-        String path = uri.getPath();
-        if (path.startsWith(StaticToadlet.ROOT_URL) && staticToadlet != null) {
-            staticToadlet.handleGet(uri, req, ctx);
-        } else {
-            String desc = "Freenet is starting up";
-            HTMLNode pageNode = ctx.getPageMaker().getPageNode(desc, false, ctx);
-            HTMLNode headNode = ctx.getPageMaker().getHeadNode(pageNode);
-            headNode.addChild("meta", new String[]{"http-equiv", "content"}, new String[]{"refresh", "20; url="});
-            HTMLNode contentNode = ctx.getPageMaker().getContentNode(pageNode);
-	    
-		if(!isPRNGReady) {
-			HTMLNode prngInfobox = contentNode.addChild(ctx.getPageMaker().getInfobox("infobox-error", desc));
-			HTMLNode prngInfoboxContent = ctx.getPageMaker().getContentNode(prngInfobox);
-			prngInfoboxContent.addChild("#", "There isn't enough entropy available on your system... Freenet won't start until it can gather enough.");
+	public void handleGet(URI uri, HTTPRequest req, ToadletContext ctx) throws ToadletContextClosedException, IOException, RedirectException {
+		// If we don't disconnect we will have pipelining issues
+		ctx.forceDisconnect();
+
+		String path = uri.getPath();
+		if(path.startsWith(StaticToadlet.ROOT_URL) && staticToadlet != null)
+			staticToadlet.handleGet(uri, req, ctx);
+		else {
+			String desc = "Freenet is starting up";
+			HTMLNode pageNode = ctx.getPageMaker().getPageNode(desc, false, ctx);
+			HTMLNode headNode = ctx.getPageMaker().getHeadNode(pageNode);
+			headNode.addChild("meta", new String[]{"http-equiv", "content"}, new String[]{"refresh", "20; url="});
+			HTMLNode contentNode = ctx.getPageMaker().getContentNode(pageNode);
+
+			if(!isPRNGReady) {
+				HTMLNode prngInfobox = contentNode.addChild(ctx.getPageMaker().getInfobox("infobox-error", desc));
+				HTMLNode prngInfoboxContent = ctx.getPageMaker().getContentNode(prngInfobox);
+				prngInfoboxContent.addChild("#", "There isn't enough entropy available on your system... Freenet won't start until it can gather enough.");
+			}
+
+			HTMLNode infobox = contentNode.addChild(ctx.getPageMaker().getInfobox("infobox-error", desc));
+			HTMLNode infoboxContent = ctx.getPageMaker().getContentNode(infobox);
+			infoboxContent.addChild("#", "Your freenet node is starting up, please hold on.");
+
+			WelcomeToadlet.maybeDisplayWrapperLogfile(ctx, contentNode);
+
+			//TODO: send a Retry-After header ?
+			writeHTMLReply(ctx, 503, desc, pageNode.generate());
 		}
-	
-	HTMLNode infobox = contentNode.addChild(ctx.getPageMaker().getInfobox("infobox-error", desc));
-	HTMLNode infoboxContent = ctx.getPageMaker().getContentNode(infobox);
-	infoboxContent.addChild("#", "Your freenet node is starting up, please hold on.");
-            
-            WelcomeToadlet.maybeDisplayWrapperLogfile(ctx, contentNode);
-	    
-            //TODO: send a Retry-After header ?
-            writeHTMLReply(ctx, 503, desc, pageNode.generate());
-        }
-}
-    
-    public String supportedMethods() {
-        return "GET";
-    }
-    
-    public void setIsPRNGReady() {
-	    isPRNGReady = true;
-    }
+	}
+
+	public String supportedMethods() {
+		return "GET";
+	}
+
+	public void setIsPRNGReady() {
+		isPRNGReady = true;
+	}
 }
