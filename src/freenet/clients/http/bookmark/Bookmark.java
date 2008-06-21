5fc9aa8596f2728a7588f06bd77a3b1d949a2973
Florent Daignière <nextgens@freenetproject.org>
2008-06-21 09:36:20 +0000

    indent
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@20549 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/clients/http/bookmark/Bookmark.java
+++ src/freenet/clients/http/bookmark/Bookmark.java
@@ -4,33 +4,31 @@ import freenet.support.SimpleFieldSet;
 
 public abstract class Bookmark {
 
-    protected String name;
+	protected String name;
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    protected void setName(String s) {
-        name = s;
-    }
+	protected void setName(String s) {
+		name = s;
+	}
 
-    public boolean equals(Object o) {
-        if (o == this)
-		return true;
-        if (o instanceof Bookmark) {
-            Bookmark b = (Bookmark) o;
-            if (!b.name.equals(name)) {
-                return false;
-            }
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
-    public int hashCode() {
-    	return name.hashCode();
-    }
-    
-    public abstract SimpleFieldSet getSimpleFieldSet();
+	public boolean equals(Object o) {
+		if(o == this)
+			return true;
+		if(o instanceof Bookmark) {
+			Bookmark b = (Bookmark) o;
+			if(!b.name.equals(name))
+				return false;
+			return true;
+		} else
+			return false;
+	}
+
+	public int hashCode() {
+		return name.hashCode();
+	}
+
+	public abstract SimpleFieldSet getSimpleFieldSet();
 }
