2c5757f7cafdc17e23e71984776bb1dfc12187aa
Florent Daignière <nextgens@freenetproject.org>
2008-06-22 04:50:43 +0000

    second part of bug #229: More entropy issues
    Now the error message can be translated
    
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@20618 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/clients/http/StartupToadlet.java
+++ src/freenet/clients/http/StartupToadlet.java
@@ -1,5 +1,6 @@
 package freenet.clients.http;
 
+import freenet.l10n.L10n;
 import java.io.IOException;
 import java.net.URI;
 
@@ -27,21 +28,21 @@ public class StartupToadlet extends Toadlet {
 		if(path.startsWith(StaticToadlet.ROOT_URL) && staticToadlet != null)
 			staticToadlet.handleGet(uri, req, ctx);
 		else {
-			String desc = "Freenet is starting up";
+			String desc = L10n.getString("StartupToadlet.title");
 			HTMLNode pageNode = ctx.getPageMaker().getPageNode(desc, false, ctx);
 			HTMLNode headNode = ctx.getPageMaker().getHeadNode(pageNode);
 			headNode.addChild("meta", new String[]{"http-equiv", "content"}, new String[]{"refresh", "20; url="});
 			HTMLNode contentNode = ctx.getPageMaker().getContentNode(pageNode);
 
 			if(!isPRNGReady) {
-				HTMLNode prngInfobox = contentNode.addChild(ctx.getPageMaker().getInfobox("infobox-error", desc));
+				HTMLNode prngInfobox = contentNode.addChild(ctx.getPageMaker().getInfobox("infobox-error", L10n.getString("StartupToadlet.entropyErrorTitle")));
 				HTMLNode prngInfoboxContent = ctx.getPageMaker().getContentNode(prngInfobox);
-				prngInfoboxContent.addChild("#", "There isn't enough entropy available on your system... Freenet won't start until it can gather enough.");
+				prngInfoboxContent.addChild("#", L10n.getString("StartupToadlet.entropyErrorContent"));
 			}
 
 			HTMLNode infobox = contentNode.addChild(ctx.getPageMaker().getInfobox("infobox-error", desc));
 			HTMLNode infoboxContent = ctx.getPageMaker().getContentNode(infobox);
-			infoboxContent.addChild("#", "Your freenet node is starting up, please hold on.");
+			infoboxContent.addChild("#", L10n.getString("StartupToadlet.isStartingUp"));
 
 			WelcomeToadlet.maybeDisplayWrapperLogfile(ctx, contentNode);
 
