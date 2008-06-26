33c85d94ebdf9e9c04e40ec86b959bf3b9a93da9
Matthew Toseland <toad@amphibian.dyndns.org>
2008-06-26 21:43:41 +0000

    Should be safe to check for non-persistent requests.
    git-svn-id: file:///home/toad/git-migration/temprepository/branches/db4o/freenet@20786 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/RequestStarter.java
+++ src/freenet/node/RequestStarter.java
@@ -224,6 +224,7 @@ public class RequestStarter implements Runnable, RandomGrabArrayItemExclusionLis
 	}
 
 	private boolean startRequest(ChosenRequest req, boolean logMINOR) {
+		if((!req.isPersistent()) && req.request.isCancelled(null)) return false;
 		if(logMINOR) Logger.minor(this, "Running request "+req+" priority "+req.prio);
 		core.getExecutor().execute(new SenderThread(req, req.key), "RequestStarter$SenderThread for "+req);
 		return true;
