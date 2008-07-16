0f2047d5f60819eee7f2793e0492ba8b1df1e3d4
Florent Daignière <nextgens@freenetproject.org>
2008-07-16 10:12:24 +0000

    revert r21141 (commited too much)
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@21142 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/l10n/L10n.java
+++ src/freenet/l10n/L10n.java
@@ -94,7 +94,6 @@ public class L10n {
 	* @throws MissingResourceException
 	*/
 	public static void setLanguage(String selectedLanguage) throws MissingResourceException {
-		selectedLanguage = mapLanguageNameToLongName(selectedLanguage);
 		synchronized (sync) {
 			Logger.normal(CLASS_NAME, "Changing the current language to : " + selectedLanguage);
 			currentClass = new L10n(selectedLanguage);
@@ -366,24 +365,6 @@ public class L10n {
 			
 			if(currentShortCode.equalsIgnoreCase(name) || currentLongName.equalsIgnoreCase(name) ||	currentCountryCodeName.equalsIgnoreCase(name))
 				return currentShortCode;
-		}
-		return null;
-	}
-
-	/**
-	 * Map a language identifier to its corresponding long name
-	 * 
-	 * @param The name to look for
-	 * @return The full text language name OR null if not found
-	 */
-	public static String mapLanguageNameToLongName(String name) {
-		for(int i=0; i<AVAILABLE_LANGUAGES.length; i++) {
-			String currentShortCode = AVAILABLE_LANGUAGES[i][0];
-			String currentLongName = AVAILABLE_LANGUAGES[i][1];
-			String currentCountryCodeName = AVAILABLE_LANGUAGES[i][2];
-			
-			if(currentShortCode.equalsIgnoreCase(name) || currentLongName.equalsIgnoreCase(name) ||	currentCountryCodeName.equalsIgnoreCase(name))
-				return currentLongName;
 		}
 		return null;
 	}
