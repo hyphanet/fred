ddebfb5bfda54e1ab74b22248d84a2aa8fdea8e6
Matthew Toseland <toad@amphibian.dyndns.org>
2008-07-19 18:46:37 +0000

    Commented out useful method for some debugging scenarios
    git-svn-id: file:///home/toad/git-migration/temprepository/branches/db4o/freenet@21266 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/support/io/DelayedFreeBucket.java
+++ src/freenet/support/io/DelayedFreeBucket.java
@@ -129,4 +129,8 @@ public class DelayedFreeBucket implements Bucket, SerializableToFieldSetBucket {
 		// Cascading activation of dependancies
 		container.activate(bucket, 1);
 	}
+	
+//	public void objectOnDeactivate(ObjectContainer container) {
+//		if(Logger.shouldLog(Logger.MINOR, this)) Logger.minor(this, "Deactivating "+super.toString()+" : "+bucket, new Exception("debug"));
+//	}
 }
