8b6456a5f0a16ed57568e9f6fb16924ba2dd4a36
Matthew Toseland <toad@amphibian.dyndns.org>
2008-07-23 16:39:35 +0000

    Paranoia - seen an NPE which suggests keys here have been deactivated despite being registered as nonpersistent
    git-svn-id: file:///home/toad/git-migration/temprepository/branches/db4o/freenet@21347 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/client/async/ClientRequestScheduler.java
+++ src/freenet/client/async/ClientRequestScheduler.java
@@ -263,6 +263,10 @@ public class ClientRequestScheduler implements RequestScheduler {
 				short prio = listener.getPriorityClass(selectorContainer);
 				final Key[] keys = listener.listKeys(selectorContainer);
 				final boolean dontCache = listener.dontCache(null);
+				for(int i=0;i<keys.length;i++) {
+					if(keys[i].getRoutingKey() == null)
+						throw new NullPointerException();
+				}
 				datastoreCheckerExecutor.execute(new Runnable() {
 
 					public void run() {
