9f28b635d58ccc35551e545efa2983f4ab14d6af
Matthew Toseland <toad@amphibian.dyndns.org>
2008-07-30 22:11:26 +0000

    Use passed-in value of persistent
    git-svn-id: file:///home/toad/git-migration/temprepository/branches/db4o/freenet@21512 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/client/async/SingleBlockInserter.java
+++ src/freenet/client/async/SingleBlockInserter.java
@@ -56,7 +56,7 @@ public class SingleBlockInserter extends SendableInsert implements ClientPutStat
 	private int consecutiveRNFs;
 	
 	public SingleBlockInserter(BaseClientPutter parent, Bucket data, short compressionCodec, FreenetURI uri, InsertContext ctx, PutCompletionCallback cb, boolean isMetadata, int sourceLength, int token, boolean getCHKOnly, boolean addToParent, boolean dontSendEncoded, Object tokenObject, ObjectContainer container, ClientContext context, boolean persistent) {
-		super(parent.persistent());
+		super(persistent);
 		assert(persistent == parent.persistent());
 		this.consecutiveRNFs = 0;
 		this.tokenObject = tokenObject;
