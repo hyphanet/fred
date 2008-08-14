7a4ceb8df92b1cd765978e7529826871aba66654
Florent Daignière <nextgens@freenetproject.org>
2008-08-14 19:58:24 +0000

    Update SizeUtil ... We might need to get a value without any space
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@21863 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/support/SizeUtil.java
+++ src/freenet/support/SizeUtil.java
@@ -10,7 +10,17 @@ public class SizeUtil {
 		return formatSize(sz, false);
 	}
 
+	public static String formatSize(boolean withoutSpace, long sz) {
+		String[] result = _formatSize(sz);
+		return result[0].concat(result[1]);
+	}
+	
 	public static String formatSize(long sz, boolean useNonBreakingSpace) {
+		String[] result = _formatSize(sz);
+		return result[0].concat((useNonBreakingSpace ? "\u00a0" : " ")).concat(result[1]);
+	}
+	
+	public static String[] _formatSize(long sz) {
 		long s = 1;
 		int i;
 		for(i=0;i<SizeUtil.suffixes.length;i++) {
@@ -24,23 +34,19 @@ public class SizeUtil {
 		s /= 1024; // we use the previous unit
 		if (s == 1)  // Bytes? Then we don't need real numbers with a comma
 		{
-			return sz + " " + SizeUtil.suffixes[0];
+			return new String[] { String.valueOf(sz), SizeUtil.suffixes[0] };
 		}
 		else
 		{
 			double mantissa = (double)sz / (double)s;
-			String o = Double.toString(mantissa);
+			String o = String.valueOf(mantissa);
 			if(o.indexOf('.') == 3)
 				o = o.substring(0, 3);
 			else if((o.indexOf('.') > -1) && (o.indexOf('E') == -1) && (o.length() > 4))
 				o = o.substring(0, 4);
 			if(i < SizeUtil.suffixes.length) // handle the case where the mantissa is Infinity
-				if(useNonBreakingSpace) {
-					o += '\u00a0' + SizeUtil.suffixes[i];
-				} else {
-					o += ' ' + SizeUtil.suffixes[i];
-				}
-			return o;
+				return new String[] { o , SizeUtil.suffixes[i] };
+			return new String[] { o , "" };
 		}
 	}
 }
