79ccbcd5799cc20174e8646fe6be4dc36c945adc
Florent Daignière <nextgens@freenetproject.org>
2008-08-17 18:35:16 +0000

    even better that way
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@21978 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/support/io/LineReadingInputStream.java
+++ src/freenet/support/io/LineReadingInputStream.java
@@ -56,7 +56,7 @@ public class LineReadingInputStream extends FilterInputStream implements LineRea
 				if(ctr >= maxLength)
 					throw new TooLongException("We reached maxLength="+maxLength+ " parsing\n "+HexUtil.bytesToHex(buf, 0, ctr) + "\n" + new String(buf, 0, ctr, utf ? "UTF-8" : "ISO-8859-1"));
 			}
-			byte[] newBuf = new byte[Math.min(buf.length * 2, maxLength)];
+			byte[] newBuf = new byte[buf.length * 2];
 			System.arraycopy(buf, 0, newBuf, 0, buf.length);
 			buf = newBuf;
 		}
