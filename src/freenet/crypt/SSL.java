affcd33fdc349f07a8dbb2ed84a24c6a593f8626
Matthew Toseland <toad@amphibian.dyndns.org>
2008-08-20 18:54:43 +0000

    Port 21994 from trunk, essential for new freenet-ext.jar
    git-svn-id: file:///home/toad/git-migration/temprepository/branches/db4o/freenet@22064 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/crypt/SSL.java
+++ src/freenet/crypt/SSL.java
@@ -15,7 +15,6 @@
  */
 package freenet.crypt;
 
-import com.sleepycat.je.utilint.NotImplementedYetException;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -216,7 +215,7 @@ public class SSL {
 				fis = new FileInputStream(keyStore);
 				keystore.load(fis, keyStorePass.toCharArray());
 			} catch(FileNotFoundException fnfe) {
-				throw new NotImplementedYetException();
+				throw new UnsupportedOperationException("Not implemented yet!");
 //				//If keystore not exist, create keystore and server certificat
 //				keystore.load(null, keyStorePass.toCharArray());
 //				CertAndKeyGen keypair = new CertAndKeyGen("DSA", "SHA1WithDSA");
