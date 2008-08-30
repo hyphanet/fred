588496819b6b4a1d112e5ad942553eb23c253b5a
Matthew Toseland <toad@amphibian.dyndns.org>
2008-08-30 22:02:18 +0000

    Logging.
    git-svn-id: file:///home/toad/git-migration/temprepository/branches/db4o/freenet@22262 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/client/async/ClientRequestScheduler.java
+++ src/freenet/client/async/ClientRequestScheduler.java
@@ -644,7 +644,7 @@ public class ClientRequestScheduler implements RequestScheduler {
 			if(logMINOR) Logger.minor(this, "Queue size: "+length+" SSK="+isSSKScheduler+" insert="+isInsertScheduler);
 			if(length >= MAX_STARTER_QUEUE_SIZE) {
 				if(length >= WARNING_STARTER_QUEUE_SIZE)
-					Logger.error(this, "Queue already full: "+starterQueue.size());
+					Logger.error(this, "Queue already full: "+length);
 				return;
 			}
 			if(length > MAX_STARTER_QUEUE_SIZE * 3 / 4) {
