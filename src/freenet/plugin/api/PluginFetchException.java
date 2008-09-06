8455f1b3d1da21a7be15dc174ca4de5f76c81963
Daniel Cheng (鄭郁邦) <j16sdiz+freenet@gmail.com>
2008-09-06 06:35:12 +0000

    warning hunt: freenet.plugin.api
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@22487 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/plugin/api/PluginFetchException.java
+++ src/freenet/plugin/api/PluginFetchException.java
@@ -6,6 +6,7 @@ package freenet.plugin.api;
 /**
  * Base class of exceptions thrown when a plugin fetch fails.
  */
+@SuppressWarnings("serial")
 public abstract class PluginFetchException extends Exception {
 
 }
