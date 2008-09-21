ea7ded6188e0138c07d56246b6f5395cd1e3dd29
Daniel Cheng (鄭郁邦) <j16sdiz+freenet@gmail.com>
2008-09-21 07:53:48 +0000

    lock forked filter on merging
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@22728 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/support/BloomFilter.java
+++ src/freenet/support/BloomFilter.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.util.Random;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -119,14 +120,20 @@ public abstract class BloomFilter {
 			if (forkedFilter == null)
 				return;
 
-			filter.position(0);
-			forkedFilter.filter.position(0);
+			Lock forkedLock = forkedFilter.lock.writeLock();
+			forkedLock.lock();
+			try {
+				filter.position(0);
+				forkedFilter.filter.position(0);
 
-			filter.put(forkedFilter.filter);
+				filter.put(forkedFilter.filter);
 
-			filter.position(0);
-			forkedFilter.finalize();
-			forkedFilter = null;
+				filter.position(0);
+				forkedFilter.finalize();
+				forkedFilter = null;
+			} finally {
+				forkedLock.unlock();
+			}
 		} finally {
 			lock.writeLock().unlock();
 		}
