d06e843ebc58937723e261ded1eddd7f82b39068
Matthew Toseland <toad@amphibian.dyndns.org>
2008-09-27 18:24:13 +0000

    Maybe fix an NPE?
    git-svn-id: file:///home/toad/git-migration/temprepository/branches/db4o/freenet@22867 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/client/async/SplitFileFetcherSegment.java
+++ src/freenet/client/async/SplitFileFetcherSegment.java
@@ -379,7 +379,6 @@ public class SplitFileFetcherSegment implements FECCallback {
 			for(int i=0;i<dataBuckets.length;i++) {
 				if(logMINOR) Logger.minor(this, "Copying data from block "+i);
 				SplitfileBlock status = dataBuckets[i];
-				if(persistent) container.activate(status, 1);
 				if(status == null) throw new NullPointerException();
 				Bucket data = status.getData();
 				if(data == null) throw new NullPointerException();
