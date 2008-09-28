5602dad50efdfec1b6ea2ba5baab6d0f9e492453
Florent Daignière <nextgens@freenetproject.org>
2008-09-28 13:51:13 +0000

    indent
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@22880 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/support/compress/Compressor.java
+++ src/freenet/support/compress/Compressor.java
@@ -12,7 +12,7 @@ import freenet.support.api.BucketFactory;
  */
 public abstract class Compressor {
 
-    public static final Compressor GZIP = new GzipCompressor();
+	public static final Compressor GZIP = new GzipCompressor();
 
 	public abstract Bucket compress(Bucket data, BucketFactory bf, long maxLength) throws IOException, CompressionOutputSizeException;
 
@@ -39,18 +39,18 @@ public abstract class Compressor {
 
 	public static Compressor getCompressionAlgorithmByDifficulty(int i) {
 		if(i == 0)
-            return GZIP;
+			return GZIP;
 		// FIXME when we get more compression algos, put them here.
 		return null;
 	}
 
 	public static Compressor getCompressionAlgorithmByMetadataID(short algo) {
 		if(algo == Metadata.COMPRESS_GZIP)
-            return GZIP;
+			return GZIP;
 		// FIXME when we get more compression algos, put them here.
 		return null;
 	}
-	
+
 	/** Decompress in RAM only.
 	 * @param dbuf Input buffer.
 	 * @param i Offset to start reading from.
@@ -61,5 +61,4 @@ public abstract class Compressor {
 	 * @returns The number of bytes actually written.
 	 */
 	public abstract int decompress(byte[] dbuf, int i, int j, byte[] output) throws CompressionOutputSizeException;
-
 }
