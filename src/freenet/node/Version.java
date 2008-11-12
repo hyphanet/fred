a6a8a7790d316fdab5a1c0be2fed1ef271d6e0dd
Matthew Toseland <toad@amphibian.dyndns.org>
2008-11-12 13:23:26 +0000

    1174: Compression: - Enable >100 char filenames in tar containers. L10n: - French update Plugins: - Fix loading plugins from disk on Windows (bug in getting plugin name). Connection setup: - Detect and ignore replayed JFK(4)'s. This solves a possible local DoS, and also may help with the "Packet X sent Yms ago and not acked" errors, which seem to be related to losing KeyTracker's. - If the HMAC is correct, there is no need to find another KeyTracker to decode the message with. Logging: - Fix a bug (accidental breakage) introduced recently. Bookmarks: - Update editions. - Add wAnnA? for now, since it's been updated again, as a stop-gap until we have proper XMLLibrarian integration. Dev stuff: - Remove MockRandomSource, already have DummyRandomSource. Only used by unit tests. - LRUQueue: option to use as a fixed size queue. - Logging.
    batosai
    saces
    j16sdiz
    
    Meanwhile... (INCOMPLETE)
    
    db4o: (toad)
    - Work around duplicate objects bug in db4o: http://tracker.db4o.com/browse/COR-1447
    - SegmentedChainBucket: Run segment store jobs asynchronously.
    - Logging.
    
    Freetalk plugin: (xor)
    - Rename from FMS.
    - Major db4o-related refactoring etc. Use db4o for everything.
    - Comments.
    
    WoT: (xor, batosai)
    - Comments.
    - Refactoring and APIs to support Freetalk plugin.
    
    XMLLibrarian:
    - Update default to new index from wAnnA?
    
    
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@23514 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -24,11 +24,11 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1173;
+	private static final int buildNumber = 1174;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1173;
-	private static final int newLastGoodBuild = 1173;
+	private static final int newLastGoodBuild = 1174;
 	static final long transitionTime;
 	
 	static {
