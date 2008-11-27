25aeb3fbb089fff33f191d200b8f5f081d9f37d5
Matthew Toseland <toad@amphibian.dyndns.org>
2008-11-27 20:38:41 +0000

    1185: (MANDATORY MONDAY) Maybe fix bandwidth collapsing bug: - On blocking, and on disconnect, we were losing messages: not requeueing them, and not calling the callbacks either. - This seems to be at least part of the cause of request handler IDs not being removed, which seems to be the cause of the bandwidth usage collapsing bug. Salted hash datastore: - Missed a file in useralert. Misc: - Make the node fetch freenet-ext.jar edition 26. - Fix an NPE on loading the homepage when the plugins haven't been loaded yet. Dev stuff: - Logging.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@23920 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -24,17 +24,17 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1184;
+	private static final int buildNumber = 1185;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 1180;
-	private static final int newLastGoodBuild = 1181;
+	private static final int oldLastGoodBuild = 1181;
+	private static final int newLastGoodBuild = 1185;
 	static final long transitionTime;
 	
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2008, Calendar.NOVEMBER, 23, 0, 0, 0 );
+		_cal.set( 2008, Calendar.DECEMBER, 1, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 	
