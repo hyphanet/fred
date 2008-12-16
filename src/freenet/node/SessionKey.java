200d8ce36e43a1a4f3386e45b4acd5edb5942ed2
Matthew Toseland <toad@amphibian.dyndns.org>
2008-12-16 23:50:24 +0000

    Doh, missing file from 24396
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@24399 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ src/freenet/node/SessionKey.java
@@ -0,0 +1,41 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
+package freenet.node;
+
+import freenet.crypt.BlockCipher;
+
+/**
+ * Class representing a single session key.
+ * @author Matthew Toseland <toad@amphibian.dyndns.org> (0xE43DA450)
+ */
+public class SessionKey {
+	
+	/** A PacketTracker may have more than one SessionKey, but a SessionKey 
+	 * may only have one PacketTracker. In other words, in some cases it is
+	 * possible to change the session key without invalidating the packet
+	 * sequence, but it is never possible to invalidate the packet sequence
+	 * without changing the session key. */
+	final PacketTracker packets;
+	
+	private static boolean logMINOR;
+	/** Parent PeerNode */
+	public final PeerNode pn;
+	/** Cipher to both encrypt outgoing packets with and decrypt
+	 * incoming ones. */
+	public final BlockCipher sessionCipher;
+	/** Key for above cipher, so far for debugging */
+	public final byte[] sessionKey;
+
+	SessionKey(PeerNode parent, PacketTracker tracker, BlockCipher cipher, byte[] sessionKey) {
+		this.pn = parent;
+		this.packets = tracker;
+		this.sessionCipher = cipher;
+		this.sessionKey = sessionKey;
+	}
+	
+	public String toString() {
+		return super.toString()+":"+packets.toString();
+	}
+
+}
