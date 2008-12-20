04ef8911ae8887291672d015e3edc3318c243d7c
Daniel Cheng (鄭郁邦) <j16sdiz+freenet@gmail.com>
2008-12-20 15:53:40 +0000

    yet another leak that prevent plugin unload
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@24672 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/client/async/SplitFileFetcher.java
+++ src/freenet/client/async/SplitFileFetcher.java
@@ -5,6 +5,7 @@ package freenet.client.async;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.ref.WeakReference;
 import java.util.LinkedList;
 
 import freenet.client.ArchiveContext;
@@ -33,7 +34,7 @@ public class SplitFileFetcher implements ClientGetState {
 	final ArchiveContext archiveContext;
 	final LinkedList decompressors;
 	final ClientMetadata clientMetadata;
-	final ClientRequester parent;
+	final WeakReference<ClientRequester> parent;
 	final GetCompletionCallback cb;
 	final int recursionLevel;
 	/** The splitfile type. See the SPLITFILE_ constants on Metadata. */
@@ -68,7 +69,7 @@ public class SplitFileFetcher implements ClientGetState {
 		this.clientMetadata = clientMetadata;
 		this.cb = rcb;
 		this.recursionLevel = recursionLevel + 1;
-		this.parent = parent2;
+		this.parent = new WeakReference<ClientRequester>(parent2);
 		if(parent2.isCancelled())
 			throw new FetchException(FetchException.CANCELLED);
 		overrideLength = metadata.dataLength();
@@ -166,8 +167,9 @@ public class SplitFileFetcher implements ClientGetState {
 				throw new FetchException(FetchException.INVALID_METADATA, "Unable to allocate all check blocks to segments - buggy or malicious inserter");
 		}
 		this.token = token2;
-		parent.addBlocks(splitfileDataBlocks.length + splitfileCheckBlocks.length);
-		parent.addMustSucceedBlocks(splitfileDataBlocks.length);
+				
+		parent2.addBlocks(splitfileDataBlocks.length + splitfileCheckBlocks.length);
+		parent2.addMustSucceedBlocks(splitfileDataBlocks.length);
 	}
 
 	/** Return the final status of the fetch. Throws an exception, or returns a 
@@ -302,7 +304,11 @@ public class SplitFileFetcher implements ClientGetState {
 	}
 
 	public void scheduleOffThread() {
-		fetchContext.slowSerialExecutor[parent.priorityClass].execute(new Runnable() {
+		ClientRequester p = parent.get();
+		if (p == null)
+			return;
+
+		fetchContext.slowSerialExecutor[p.priorityClass].execute(new Runnable() {
 			public void run() {
 				schedule();
 			}
