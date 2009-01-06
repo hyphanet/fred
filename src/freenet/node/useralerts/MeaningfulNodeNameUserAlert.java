475602037d6df9710beff3400cb3ec82ecec1790
Matthew Toseland <toad@amphibian.dyndns.org>
2009-01-06 21:46:25 +0000

    Fix the build
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@24943 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/useralerts/MeaningfulNodeNameUserAlert.java
+++ src/freenet/node/useralerts/MeaningfulNodeNameUserAlert.java
@@ -51,9 +51,9 @@ public class MeaningfulNodeNameUserAlert extends AbstractUserAlert {
 		HTMLNode itemNode = listNode.addChild("li");
 		itemNode.addChild("span", new String[]{ "class", "title", "style" },
 				new String[]{ "configshortdesc", L10n.getString("ConfigToadlet.defaultIs", new String[] { "default" }, new String[] { o.getDefault() }), 
-				"cursor: help;" }).addChild(L10n.getHTMLNode(o.getShortDesc(), fixer));
+				"cursor: help;" }).addChild(L10n.getHTMLNode(o.getShortDesc(), freenet.clients.http.NullLinkFixer.instance));
 		itemNode.addChild("input", new String[] { "type", "class", "alt", "name", "value" }, new String[] { "text", "config", o.getShortDesc(), "node.name", o.getValueString() });
-		itemNode.addChild("span", "class", "configlongdesc").addChild(L10n.getHTMLNode(o.getLongDesc(), fixer));
+		itemNode.addChild("span", "class", "configlongdesc").addChild(L10n.getHTMLNode(o.getLongDesc(), freenet.clients.http.NullLinkFixer.instance));
 		formNode.addChild("input", new String[] { "type", "value" }, new String[] { "submit", L10n.getString("UserAlert.apply") });
 		formNode.addChild("input", new String[] { "type", "value" }, new String[] { "reset", L10n.getString("UserAlert.reset") });
 
