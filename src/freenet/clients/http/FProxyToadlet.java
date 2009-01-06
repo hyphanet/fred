bc1fce3f1bfedbb2e060efe7e4231ba0a3e1f30f
Matthew Toseland <toad@amphibian.dyndns.org>
2009-01-06 21:22:31 +0000

    Fix some links in generated content.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@24939 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/clients/http/FProxyToadlet.java
+++ src/freenet/clients/http/FProxyToadlet.java
@@ -457,7 +457,7 @@ public final class FProxyToadlet extends Toadlet {
 			if(Logger.shouldLog(Logger.MINOR, this))
 				Logger.minor(this, "Failed to fetch "+uri+" : "+e);
 			if(e.newURI != null) {
-				Toadlet.writePermanentRedirect(ctx, msg, '/' +e.newURI.toASCIIString() + override);
+				Toadlet.writePermanentRedirect(ctx, msg, ctx.fixLink('/' +e.newURI.toASCIIString() + override));
 			} else if(e.mode == FetchException.TOO_BIG) {
 				HTMLNode pageNode = ctx.getPageMaker().getPageNode(l10n("fileInformationTitle"), ctx);
 				HTMLNode contentNode = ctx.getPageMaker().getContentNode(pageNode);
