f6244461a38f6faae336d0ab10465272c915c5b2
Matthew Toseland <toad@amphibian.dyndns.org>
2009-01-09 23:18:22 +0000

    Pad the last block even if we don't freeData!
    git-svn-id: file:///home/toad/git-migration/temprepository/branches/db4o/freenet@24996 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/client/async/SplitFileInserter.java
+++ src/freenet/client/async/SplitFileInserter.java
@@ -104,6 +104,7 @@ public class SplitFileInserter implements ClientPutState {
 			if(freeData) {
 				// BucketTools.split will free it but not removeFrom().
 				data.removeFrom(container);
+			}
 				if(dataBuckets[dataBuckets.length-1].size() < CHKBlock.DATA_LENGTH) {
 					Bucket oldData = dataBuckets[dataBuckets.length-1];
 					dataBuckets[dataBuckets.length-1] = BucketTools.pad(oldData, CHKBlock.DATA_LENGTH, context.persistentBucketFactory, (int) oldData.size());
@@ -111,7 +112,6 @@ public class SplitFileInserter implements ClientPutState {
 					oldData.free();
 					if(persistent) oldData.removeFrom(container);
 				}
-			}
 			if(logMINOR)
 				Logger.minor(this, "Data size "+data.size()+" buckets "+dataBuckets.length);
 		} catch (IOException e) {
