36fd60383eadffedc4c8f5a091fceb2f4536acb2
Matthew Toseland <toad@amphibian.dyndns.org>
2009-01-13 18:20:25 +0000

    Missing file, fix build.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@25050 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ src/freenet/clients/http/NullToadletContainer.java
@@ -0,0 +1,85 @@
+package freenet.clients.http;
+
+import java.net.InetAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import freenet.clients.http.PageMaker.THEME;
+import freenet.support.HTMLNode;
+import freenet.support.URLEncodedFormatException;
+import freenet.support.api.BucketFactory;
+
+public class NullToadletContainer implements ToadletContainer {
+	
+	public static NullToadletContainer instance = new NullToadletContainer();
+
+	public HTMLNode addFormChild(HTMLNode parentNode, String target, String id) {
+		HTMLNode formNode =
+			parentNode.addChild("form", new String[] { "action", "method", "enctype", "id",  "accept-charset" }, 
+					new String[] { target, "post", "multipart/form-data", id, "utf-8"} ).addChild("div");
+		
+		return formNode;
+	}
+
+	public boolean allowPosts() {
+		return false;
+	}
+
+	public boolean doRobots() {
+		return false;
+	}
+
+	public boolean enableInlinePrefetch() {
+		return false;
+	}
+
+	public boolean enablePersistentConnections() {
+		return false;
+	}
+
+	public Toadlet findToadlet(URI uri) throws PermanentRedirectException {
+		return null;
+	}
+
+	public String generateSID(String realPath) throws URLEncodedFormatException {
+		return null;
+	}
+
+	public BucketFactory getBucketFactory() {
+		return null;
+	}
+
+	public String getFormPassword() {
+		return null;
+	}
+
+	public THEME getTheme() {
+		return null;
+	}
+
+	public boolean isAllowedFullAccess(InetAddress remoteAddr) {
+		return false;
+	}
+
+	public boolean isSecureIDCheckingDisabled() {
+		return true;
+	}
+
+	public boolean publicGatewayMode() {
+		return false;
+	}
+
+	public void register(Toadlet t, String urlPrefix, boolean atFront,
+			boolean fullAccessOnly) {
+		throw new UnsupportedOperationException();
+	}
+
+	public String fixLink(String orig) {
+		return orig;
+	}
+
+	public URI fixLink(URI uri) throws URISyntaxException {
+		return uri;
+	}
+
+}
