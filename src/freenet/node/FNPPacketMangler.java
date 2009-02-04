d2d05be8688b48f90b319896f0b58b7f5b054401
Matthew Toseland <toad@amphibian.dyndns.org>
2009-02-04 14:39:21 +0000

    Logging. REQUEST BACKPORT.
    git-svn-id: file:///home/toad/git-migration/temprepository/branches/db4o/freenet@25540 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/node/FNPPacketMangler.java
+++ src/freenet/node/FNPPacketMangler.java
@@ -572,7 +572,7 @@ public class FNPPacketMangler implements OutgoingPacketMangler, IncomingPacketFi
 			// Phase 3
 			processJFKMessage3(payload, 4, null, replyTo, false, true, setupType, negType);
 		} else {
-			Logger.error(this, "Invalid phase "+packetType+" for anonymous-initiator (we are the responder)");
+			Logger.error(this, "Invalid phase "+packetType+" for anonymous-initiator (we are the responder) from "+replyTo);
 		}
 	}
 
@@ -619,7 +619,7 @@ public class FNPPacketMangler implements OutgoingPacketMangler, IncomingPacketFi
 			// Phase 4
 			processJFKMessage4(payload, 4, pn, replyTo, false, true, setupType, negType);
 		} else {
-			Logger.error(this, "Invalid phase "+packetType+" for anonymous-initiator (we are the responder)");
+			Logger.error(this, "Invalid phase "+packetType+" for anonymous-initiator (we are the initiator) from "+replyTo);
 		}
 	}
 
