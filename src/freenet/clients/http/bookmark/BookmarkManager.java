f49eda92f4294adf1982ef909f1a32157d756d08
Daniel Cheng (鄭郁邦) <j16sdiz+freenet@gmail.com>
2009-02-10 03:42:16 +0000

    make "Default Bookmarks" translatable (fix bug #2540)
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@25583 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/clients/http/bookmark/BookmarkManager.java
+++ src/freenet/clients/http/bookmark/BookmarkManager.java
@@ -85,7 +85,7 @@ public class BookmarkManager {
 	}
 
 	public void reAddDefaultBookmarks() {
-		BookmarkCategory bc = new BookmarkCategory("Default bookmarks - " + new Date());
+		BookmarkCategory bc = new BookmarkCategory(l10n("defaultBookmarks") + " - " + new Date());
 		addBookmark("/", bc);
 		_innerReadBookmarks("/", bc, DEFAULT_BOOKMARKS);
 	}
