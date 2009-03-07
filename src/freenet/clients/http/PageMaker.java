562f027e9dc6de4b99e89dd676166a448d201c79
xor <xor@freenetproject.org>
2009-03-07 13:27:09 +0000

    Add a function for removing all navigation links.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@25925 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/clients/http/PageMaker.java
+++ src/freenet/clients/http/PageMaker.java
@@ -126,6 +126,13 @@ public final class PageMaker {
 		navigationLinks.remove(name);
 	}
 	
+	public void removeAllNavigationLinks() {
+		navigationLinkTexts.clear();
+		navigationLinkTextsNonFull.clear();
+		navigationLinkTitles.clear();
+		navigationLinks.clear();
+	}
+	
 	public HTMLNode createBackLink(ToadletContext toadletContext, String name) {
 		String referer = toadletContext.getHeaders().get("referer");
 		if (referer != null) {
