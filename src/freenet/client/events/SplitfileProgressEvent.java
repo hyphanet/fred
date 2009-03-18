282e3760d723e52c937bc6f136cd21db0fdf27b4
Daniel Cheng (鄭郁邦) <j16sdiz+freenet@gmail.com>
2009-03-18 07:57:37 +0000

    SplitfileProgressEvent: say "Succeed", not "Fetched"
    SplitfileProgressEvent is used for Putter too
    
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@26088 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/client/events/SplitfileProgressEvent.java
+++ src/freenet/client/events/SplitfileProgressEvent.java
@@ -10,42 +10,42 @@ public class SplitfileProgressEvent implements ClientEvent {
 	public static int code = 0x07;
 	
 	public final int totalBlocks;
-	public final int fetchedBlocks;
+	public final int succeedBlocks;
 	public final int failedBlocks;
 	public final int fatallyFailedBlocks;
 	public int minSuccessfulBlocks;
 	public final boolean finalizedTotal;
 	
-	public SplitfileProgressEvent(int totalBlocks, int fetchedBlocks, int failedBlocks, 
+	public SplitfileProgressEvent(int totalBlocks, int succeedBlocks, int failedBlocks, 
 			int fatallyFailedBlocks, int minSuccessfulBlocks, boolean finalizedTotal) {
 		this.totalBlocks = totalBlocks;
-		this.fetchedBlocks = fetchedBlocks;
+		this.succeedBlocks = succeedBlocks;
 		this.failedBlocks = failedBlocks;
 		this.fatallyFailedBlocks = fatallyFailedBlocks;
 		this.minSuccessfulBlocks = minSuccessfulBlocks;
 		this.finalizedTotal = finalizedTotal;
 		if(Logger.shouldLog(Logger.MINOR, this))
-			Logger.minor(this, "Created SplitfileProgressEvent: total="+totalBlocks+" fetched="+fetchedBlocks+" failed="+failedBlocks+" fatally="+fatallyFailedBlocks+" min success="+minSuccessfulBlocks+" finalized="+finalizedTotal);
+			Logger.minor(this, "Created SplitfileProgressEvent: total="+totalBlocks+" succeed="+succeedBlocks+" failed="+failedBlocks+" fatally="+fatallyFailedBlocks+" min success="+minSuccessfulBlocks+" finalized="+finalizedTotal);
 	}
 
 	public String getDescription() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("Completed ");
-		if((minSuccessfulBlocks == 0) && (fetchedBlocks == 0))
+		if((minSuccessfulBlocks == 0) && (succeedBlocks == 0))
 			minSuccessfulBlocks = 1;
 		if(minSuccessfulBlocks == 0) {
 			if(Logger.globalGetThreshold() > Logger.MINOR)
-				Logger.error(this, "minSuccessfulBlocks=0, fetchedBlocks="+fetchedBlocks+", totalBlocks="+totalBlocks+
+				Logger.error(this, "minSuccessfulBlocks=0, succeedBlocks="+succeedBlocks+", totalBlocks="+totalBlocks+
 						", failedBlocks="+failedBlocks+", fatallyFailedBlocks="+fatallyFailedBlocks+", finalizedTotal="+finalizedTotal);
 			else
-				Logger.error(this, "minSuccessfulBlocks=0, fetchedBlocks="+fetchedBlocks+", totalBlocks="+totalBlocks+
+				Logger.error(this, "minSuccessfulBlocks=0, succeedBlocks="+succeedBlocks+", totalBlocks="+totalBlocks+
 						", failedBlocks="+failedBlocks+", fatallyFailedBlocks="+fatallyFailedBlocks+", finalizedTotal="+finalizedTotal, new Exception("debug"));
 		} else {
-			sb.append((100*(fetchedBlocks)/minSuccessfulBlocks));
+			sb.append((100*(succeedBlocks)/minSuccessfulBlocks));
 			sb.append('%');
 		}
 		sb.append(' ');
-		sb.append(fetchedBlocks);
+		sb.append(succeedBlocks);
 		sb.append('/');
 		sb.append(minSuccessfulBlocks);
 		sb.append(" (failed ");
