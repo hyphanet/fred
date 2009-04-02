0aebf4672a5cc034c11ed6bb093623e24d2af992
Matthew Toseland <toad@amphibian.dyndns.org>
2009-04-02 17:38:52 +0000

    Non-public
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@26370 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/client/ArchiveStoreContext.java
+++ src/freenet/client/ArchiveStoreContext.java
@@ -16,7 +16,7 @@ import freenet.support.Logger;
  * 
  * Always take the lock on ArchiveStoreContext before the lock on ArchiveManager, NOT the other way around.
  */
-public class ArchiveStoreContext {
+class ArchiveStoreContext {
 
 	private FreenetURI key;
 	private final ArchiveManager.ARCHIVE_TYPE archiveType;
