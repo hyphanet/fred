ddd1d7817fc831fb16cb77e599422394da7d522e
Daniel Cheng (鄭郁邦) <j16sdiz+freenet@gmail.com>
2009-04-14 04:41:29 +0000

    Logging (BaseFileBucket.java)
    Profiling show this contribute to 5% of all shouldLog() calls
    
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@26764 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/support/io/BaseFileBucket.java
+++ src/freenet/support/io/BaseFileBucket.java
@@ -408,7 +408,7 @@ public abstract class BaseFileBucket implements Bucket, SerializableToFieldSetBu
 	
 	public void free(boolean forceFree) {
 		Object[] toClose;
-		if(Logger.shouldLog(Logger.MINOR, this))
+		if(logMINOR)
 			Logger.minor(this, "Freeing "+this, new Exception("debug"));
 		synchronized(this) {
 			if(freed) return;
