0aff125a336ddb22f796907f596f71ef660d92ce
saces <saces@freenetproject.org>
2009-04-22 14:54:48 +0000

    remove unused imports
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@27213 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/client/async/BaseManifestPutter.java
+++ src/freenet/client/async/BaseManifestPutter.java
@@ -7,8 +7,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 import java.util.Vector;
