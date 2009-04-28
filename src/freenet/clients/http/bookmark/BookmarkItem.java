6e46317c1abb42b2557c5fdd0f0561a4f41b9aa2
Matthew Toseland <toad@amphibian.dyndns.org>
2009-04-28 20:55:50 +0100

    Remove unused var, organise imports

--- src/freenet/clients/http/bookmark/BookmarkItem.java
+++ src/freenet/clients/http/bookmark/BookmarkItem.java
@@ -3,7 +3,8 @@
  * http://www.gnu.org/ for further details of the GPL. */
 package freenet.clients.http.bookmark;
 
-import freenet.client.async.USKRetriever;
+import java.net.MalformedURLException;
+
 import freenet.keys.FreenetURI;
 import freenet.keys.USK;
 import freenet.l10n.L10n;
@@ -15,9 +16,7 @@ import freenet.node.useralerts.UserAlertManager;
 import freenet.support.Fields;
 import freenet.support.HTMLEncoder;
 import freenet.support.HTMLNode;
-
 import freenet.support.SimpleFieldSet;
-import java.net.MalformedURLException;
 
 public class BookmarkItem extends Bookmark {
     public static final String NAME = "Bookmark";
@@ -27,7 +26,6 @@ public class BookmarkItem extends Bookmark {
     private final BookmarkUpdatedUserAlert alert;
     private final UserAlertManager alerts;
     protected String desc;
-	USKRetriever retriever;
 
     public BookmarkItem(FreenetURI k, String n, String d, boolean hasAnActivelink, UserAlertManager uam)
             throws MalformedURLException {
