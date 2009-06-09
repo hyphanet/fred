437ed078b6db3de6733214a7ee9863df4527ea06
Matthew Toseland <toad@amphibian.dyndns.org>
2009-06-09 15:19:41 +0100

    Link to the FAFS pages on Frost and FMS

--- src/freenet/clients/http/ChatForumsToadlet.java
+++ src/freenet/clients/http/ChatForumsToadlet.java
@@ -34,10 +34,10 @@ public class ChatForumsToadlet extends Toadlet implements LinkEnabledCallback {
 		contentBox.addChild("p", l10n("content1"));
 		HTMLNode ul = contentBox.addChild("ul");
 		HTMLNode li = ul.addChild("li");
-		L10n.addL10nSubstitution(li, "ChatForumsToadlet.frost", new String[] { "frost-freenet", "frost-web", "/link" }, 
-				new String[] { "<a href=\"/freenet:USK@QRZAI1nSm~dAY2hTdzVWXmEhkaI~dso0OadnppBR7kE,wq5rHGBI7kpChBe4yRmgBChIGDug7Xa5SG9vYGXdxR0,AQACAAE/frost/9/\">", "<a href=\"/?_CHECKED_HTTP_=http://jtcfrost.sourceforge.net/\">", "</a>" });
+		L10n.addL10nSubstitution(li, "ChatForumsToadlet.frost", new String[] { "frost-freenet", "frost-web", "frost-help", "/link" }, 
+				new String[] { "<a href=\"/freenet:USK@QRZAI1nSm~dAY2hTdzVWXmEhkaI~dso0OadnppBR7kE,wq5rHGBI7kpChBe4yRmgBChIGDug7Xa5SG9vYGXdxR0,AQACAAE/frost/9/\">", "<a href=\"/?_CHECKED_HTTP_=http://jtcfrost.sourceforge.net/\">", "<a href=\"/SSK@ugb~uuscsidMI-Ze8laZe~o3BUIb3S50i25RIwDH99M,9T20t3xoG-dQfMO94LGOl9AxRTkaz~TykFY-voqaTQI,AQACAAE/FAFS-49/files/frost.htm\">", "</a>" });
 		li = ul.addChild("li");
-		L10n.addL10nSubstitution(li, "ChatForumsToadlet.fms", new String[] { "fms", "/link" }, new String[] { "<a href=\"/USK@0npnMrqZNKRCRoGojZV93UNHCMN-6UU3rRSAmP6jNLE,~BG-edFtdCC1cSH4O3BWdeIYa8Sw5DfyrSV-TKdO5ec,AQACAAE/fms/101/\">", "</a>" });
+		L10n.addL10nSubstitution(li, "ChatForumsToadlet.fms", new String[] { "fms", "fms-help", "/link" }, new String[] { "<a href=\"/USK@0npnMrqZNKRCRoGojZV93UNHCMN-6UU3rRSAmP6jNLE,~BG-edFtdCC1cSH4O3BWdeIYa8Sw5DfyrSV-TKdO5ec,AQACAAE/fms/101/\">", "<a href=\"/SSK@ugb~uuscsidMI-Ze8laZe~o3BUIb3S50i25RIwDH99M,9T20t3xoG-dQfMO94LGOl9AxRTkaz~TykFY-voqaTQI,AQACAAE/FAFS-49/files/fms.htm\">", "</a>" });
 		contentBox.addChild("p", l10n("content2"));
 		
 		this.writeHTMLReply(ctx, 200, "OK", pageNode.generate());
