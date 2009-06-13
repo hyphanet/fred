f81e0d4210c562c3afa13487a97ee4924898ae23
kurmiashish <kurmiashish@gmail.com>
2009-06-14 02:48:14 +0530

    made isVoidElement more efficient

--- src/freenet/clients/http/filter/ElementInfo.java
+++ src/freenet/clients/http/filter/ElementInfo.java
@@ -1,20 +1,34 @@
 package freenet.clients.http.filter;
-
+import java.util.HashSet;
 public class ElementInfo {
 
-	private static final String[] VOID_ELEMENTS = { "area", "base", "basefont",
-		"bgsound", "br", "col", "command", "embed", "event-source",
-		"frame", "hr", "img", "input", "keygen", "link", "meta", "param",
-		"source", "spacer", "wbr"};
-	
+	private static HashSet<String> VOID_ELEMENTS=new HashSet<String>();
+	static {
+		VOID_ELEMENTS.add("area");
+		VOID_ELEMENTS.add("base");
+		VOID_ELEMENTS.add("basefont");
+		VOID_ELEMENTS.add("bgsound");
+		VOID_ELEMENTS.add("br");
+		VOID_ELEMENTS.add("col");
+		VOID_ELEMENTS.add("command");
+		VOID_ELEMENTS.add("embed");
+		VOID_ELEMENTS.add("event-source");
+		VOID_ELEMENTS.add("frame");
+		VOID_ELEMENTS.add("hr");
+		VOID_ELEMENTS.add("img");
+		VOID_ELEMENTS.add("input");
+		VOID_ELEMENTS.add("keygen");
+		VOID_ELEMENTS.add("link");
+		VOID_ELEMENTS.add("meta");
+		VOID_ELEMENTS.add("param");
+		VOID_ELEMENTS.add("source");
+		VOID_ELEMENTS.add("spacer");
+		VOID_ELEMENTS.add("wbr");
+		
+	}
 	public static boolean isVoidElement(String element) {
 		
-		for(int i=0;i<VOID_ELEMENTS.length;i++)
-		{
-			if(VOID_ELEMENTS[i].equals(element))
-				return true;
-		}
-		return false;
+		return VOID_ELEMENTS.contains(element);
 	}
 
 }
