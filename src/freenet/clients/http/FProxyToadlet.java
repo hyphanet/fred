a698ca740d0b451ca107e029a3a88589888e9564
Matthew Toseland <toad@amphibian.dyndns.org>
2009-08-01 18:01:46 +0100

    document

--- src/freenet/clients/http/FProxyToadlet.java
+++ src/freenet/clients/http/FProxyToadlet.java
@@ -221,6 +221,8 @@ public final class FProxyToadlet extends Toadlet implements RequestClient {
 				MultiValueTable<String, String> headers = new MultiValueTable<String, String>();
 				headers.put("Content-Disposition", "attachment; filename=\"" + key.getPreferredFilename() + '"');
 				// was application/x-msdownload, but some unix browsers offer to open that in Wine as default!
+				// it is important that this type not be understandable, but application/octet-stream doesn't work.
+				// see http://onjava.com/pub/a/onjava/excerpt/jebp_3/index3.html
 				context.sendReplyHeaders(200, "OK", headers, "application/x-download", data.size());
 				context.writeData(data);
 			} else {
