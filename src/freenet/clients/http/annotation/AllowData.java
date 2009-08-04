1ef338ea6200a848cca3ceaa14582d15646ce976
saces <saces@freenetproject.org>
2009-08-04 13:12:10 +0200

    default value 'false'

--- src/freenet/clients/http/annotation/AllowData.java
+++ src/freenet/clients/http/annotation/AllowData.java
@@ -26,5 +26,5 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention( RUNTIME )
 @Documented
 public @interface AllowData {
-	boolean value();
+	boolean value() default false;
 }
