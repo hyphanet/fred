3beec9cd77922b57d60f6bf57a4e56c73ac6f966
Ximin Luo <xl269@cam.ac.uk>
2009-08-17 17:57:16 +0100

    make the Executor extend java.util.concurrent.Executor

--- src/freenet/support/Executor.java
+++ src/freenet/support/Executor.java
@@ -1,14 +1,19 @@
 /**
  * @author toad
- * To the extent that this is copyrightable, it's part of Freenet and licensed 
+ * To the extent that this is copyrightable, it's part of Freenet and licensed
  * under GPL2 or later. However, it's a trivial interface taken from Sun JDK 1.5,
  * and we will use that when we migrate to 1.5.
  */
 package freenet.support;
 
-public interface Executor {
-	
+/**
+** Note that unlike {@link java.util.concurrent.Executor}, none of these run
+** methods throw {@link java.util.concurrent.RejectedExecutionException}.
+*/
+public interface Executor extends java.util.concurrent.Executor {
+
 	/** Execute a job. */
+	public void execute(Runnable job);
 	public void execute(Runnable job, String jobName);
 	public void execute(Runnable job, String jobName, boolean fromTicker);
 
