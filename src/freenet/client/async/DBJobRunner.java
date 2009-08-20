8a8699a99203c5c00e95e817cba43f1fe211dadb
Matthew Toseland <toad@amphibian.dyndns.org>
2009-08-20 20:22:53 +0100

    Javadocs

--- src/freenet/client/async/DBJobRunner.java
+++ src/freenet/client/async/DBJobRunner.java
@@ -6,11 +6,22 @@ package freenet.client.async;
 import com.db4o.ObjectContainer;
 
 /**
- * Interface for an object which queues and runs DBJob's.
+ * Interface for an object which queues and runs DBJob's. These are, apart from during startup,
+ * the only way to interact with the database.
  * @author toad
  */
 public interface DBJobRunner {
 	
+	/**
+	 * Queue a database job. This will be run on the database thread, once it comes off the
+	 * queue.
+	 * @param job The job to run.
+	 * @param priority The priority of the job. This uses NativeThread priorities, but only
+	 * to determine which job to run first.
+	 * @param checkDupes If true, check whether there is a job which is equals() to this job
+	 * which is already queued to run, and if so don't queue the new job.
+	 * @throws DatabaseDisabledException
+	 */
 	public void queue(DBJob job, int priority, boolean checkDupes) throws DatabaseDisabledException;
 	
 	/** Run this database job blocking. If we are already on the database thread, 
@@ -18,8 +29,10 @@ public interface DBJobRunner {
 	 * it to finish. */
 	public void runBlocking(DBJob job, int priority) throws DatabaseDisabledException;
 
+	/** Is the caller running on the database thread? */
 	public boolean onDatabaseThread();
 
+	/** How many jobs are queued at a given priority? */
 	public int getQueueSize(int priority);
 	
 	/** Queue a database job to be executed just after restart.
@@ -31,6 +44,7 @@ public interface DBJobRunner {
 	/** Remove a queued on-restart database job. */
 	public void removeRestartJob(DBJob job, int priority, ObjectContainer container) throws DatabaseDisabledException;
 
+	/** Has the database been killed? */
 	public boolean killedDatabase();
 	
 	/** Tell the job runner that this transaction needs to be committed, even if the
