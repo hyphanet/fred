82bf2ba0f395ff5f081abbee14c17fe539b45332
Matthew Toseland <toad@amphibian.dyndns.org>
2009-08-21 13:21:00 +0100

    Fix unit test.

--- test/freenet/store/RAMSaltMigrationTest.java
+++ test/freenet/store/RAMSaltMigrationTest.java
@@ -84,6 +84,7 @@ public class RAMSaltMigrationTest extends TestCase {
 		
 		CHKStore newStore = new CHKStore();
 		SaltedHashFreenetStore saltStore = SaltedHashFreenetStore.construct(new File(tempDir, "saltstore"), "teststore", newStore, weakPRNG, 10, 0, false, new SemiOrderedShutdownHook(), true, true, ticker, null);
+		saltStore.start(null);
 		
 		ramStore.migrateTo(newStore, false);
 		
@@ -113,6 +114,7 @@ public class RAMSaltMigrationTest extends TestCase {
 		
 		CHKStore newStore = new CHKStore();
 		SaltedHashFreenetStore saltStore = SaltedHashFreenetStore.construct(new File(tempDir, "saltstore"), "teststore", newStore, weakPRNG, 10, 0, false, new SemiOrderedShutdownHook(), true, true, ticker, storeKey);
+		saltStore.start(null);
 		
 		ramStore.migrateTo(newStore, false);
 		
