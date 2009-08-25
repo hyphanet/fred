7a33c9cb40638617a13b1d5d54b30bbad08b1bc2
Evan Daniel <evand@pobox.com>
2009-08-25 10:56:42 -0400

    SizeUtil support for large / negative numbers.

--- src/freenet/support/SizeUtil.java
+++ src/freenet/support/SizeUtil.java
@@ -23,18 +23,24 @@ public class SizeUtil {
 	public static String[] _formatSize(long sz) {
 		long s = 1;
 		int i;
+		boolean negative = (sz < 0);
+		if (negative) sz *= -1;
+
 		for(i=0;i<SizeUtil.suffixes.length;i++) {
-			s *= 1024;
-			if(s > sz) {
+			if (s > Long.MAX_VALUE / 1024) {
+				// Largest supported size
 				break;
-				// Smaller than multiplier [i] - use the previous one
 			}
+			if(s * 1024 > sz) {
+				// Smaller than multiplier [i] - use the previous one
+				break;
+			}
+			s *= 1024;
 		}
 		
-		s /= 1024; // we use the previous unit
 		if (s == 1)  // Bytes? Then we don't need real numbers with a comma
 		{
-			return new String[] { String.valueOf(sz), SizeUtil.suffixes[0] };
+			return new String[] { (negative ? "-" : "") + String.valueOf(sz), SizeUtil.suffixes[0] };
 		}
 		else
 		{
@@ -44,6 +50,7 @@ public class SizeUtil {
 				o = o.substring(0, 3);
 			else if((o.indexOf('.') > -1) && (o.indexOf('E') == -1) && (o.length() > 4))
 				o = o.substring(0, 4);
+			if (negative) o = "-" + o;
 			if(i < SizeUtil.suffixes.length) // handle the case where the mantissa is Infinity
 				return new String[] { o , SizeUtil.suffixes[i] };
 			return new String[] { o , "" };
