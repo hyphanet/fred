131fe0429783b3970b0dab530fb065b2ff38adf3
Matthew Toseland <toad@amphibian.dyndns.org>
2009-09-18 23:06:21 +0100

    Only offer once per peer, cleanup slightly

--- src/freenet/node/FailureTableEntry.java
+++ src/freenet/node/FailureTableEntry.java
@@ -322,7 +322,7 @@ class FailureTableEntry implements TimedOutNodesList {
 			PeerNode pn = ref.get();
 			if(pn == null) continue;
 			if(pn.getBootID() != requestedBootIDs[i]) continue;
-			if(set.contains(pn)) continue;
+			if(!set.add(pn)) continue;
 			pn.offer(key);
 		}
 	}
@@ -436,7 +436,7 @@ class FailureTableEntry implements TimedOutNodesList {
 				if(requestedTimeoutHTLs[i] >= htl) {
 					long thisTimeout = requestedTimeouts[i];
 					if(thisTimeout > timeout && thisTimeout > now)
-						timeout = requestedTimeouts[i];
+						timeout = thisTimeout;
 				}
 			}
 		}
