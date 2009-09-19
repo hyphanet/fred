dea6aa47d0372d6ed1a96c5110e5065470dc8aa5
Matthew Toseland <toad@amphibian.dyndns.org>
2009-09-19 19:33:11 +0100

    1236 (1234 mandatory now, 1236 mandatory Tuesday)

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -48,11 +48,11 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1235;
+	private static final int buildNumber = 1236;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 1231;
-	private static final int newLastGoodBuild = 1234;
+	private static final int oldLastGoodBuild = 1234;
+	private static final int newLastGoodBuild = 1236;
 	static final long transitionTime;
 
 	static {
