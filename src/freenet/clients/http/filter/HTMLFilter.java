47f7e07d3593e4932eea648340762cc5fba32295
Evan Daniel <evand@pobox.com>
2009-10-11 19:56:53 -0400

    Add XHTML 1.1 doctype to filter.

--- src/freenet/clients/http/filter/HTMLFilter.java
+++ src/freenet/clients/http/filter/HTMLFilter.java
@@ -1960,6 +1960,9 @@ public class HTMLFilter implements ContentDataFilter, CharsetExtractor {
 				"-//W3C//DTD XHTML 1.0 Frameset//EN",
 				"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd");
 			DTDs.put(
+				"-//W3C//DTD XHTML 1.1//EN",
+				"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd");
+			DTDs.put(
 				"-//W3C//DTD HTML 4.01//EN",
 				"http://www.w3.org/TR/html4/strict.dtd");
 			DTDs.put(
