f2b9ad81951bbcc344fb10635daf31317db1d5c8
Matthew Toseland <toad@amphibian.dyndns.org>
2009-11-04 21:52:57 +0000

    1239: Mandatory Monday 9 November.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -48,17 +48,17 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1238;
+	private static final int buildNumber = 1239;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 1236;
-	private static final int newLastGoodBuild = 1237;
+	private static final int oldLastGoodBuild = 1237;
+	private static final int newLastGoodBuild = 1239;
 	static final long transitionTime;
 
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2009, Calendar.OCTOBER, 22, 0, 0, 0 );
+		_cal.set( 2009, Calendar.NOVEMBER, 9, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 
