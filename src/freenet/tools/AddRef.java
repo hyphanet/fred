b49f6fb628152ade14db853ac8f44a0868f45ca3
Juiceman <juiceman69@gmail.com>
2009-12-09 23:35:10 -0500

    Test NetBeans source formatting template.  Indents, braces, spacing, blank lines.

--- src/freenet/tools/AddRef.java
+++ src/freenet/tools/AddRef.java
@@ -25,27 +25,27 @@ public class AddRef {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		if(args.length < 1){
+		if(args.length < 1) {
 			System.err.println("Please provide a file name as the first argument.");
 			System.exit(-1);
 		}
 
 		final File reference = new File(args[0]);
-		if((reference == null) || !(reference.isFile()) || !(reference.canRead())){
+		if((reference == null) || !(reference.isFile()) || !(reference.canRead())) {
 			System.err.println("Please provide a file name as the first argument.");
-			System.exit(-1);	
+			System.exit(-1);
 		}
 
 		new AddRef(reference);
 	}
 
-	AddRef(File reference){
+	AddRef(File reference) {
 		Socket fcpSocket = null;
 
 		FCPMessage fcpm;
 		SimpleFieldSet sfs = new SimpleFieldSet(true);
 
-		try{
+		try {
 			fcpSocket = new Socket("127.0.0.1", FCPServer.DEFAULT_FCP_PORT);
 			fcpSocket.setSoTimeout(2000);
 
@@ -53,7 +53,7 @@ public class AddRef {
 			LineReadingInputStream lis = new LineReadingInputStream(is);
 			OutputStream os = fcpSocket.getOutputStream();
 
-			try{
+			try {
 				sfs.putSingle("Name", "AddRef");
 				sfs.putSingle("ExpectedVersion", "2.0");
 				fcpm = FCPMessage.create("ClientHello", sfs);
@@ -63,15 +63,15 @@ public class AddRef {
 				String messageName = lis.readLine(128, 128, true);
 				sfs = getMessage(lis);
 				fcpm = FCPMessage.create(messageName, sfs);
-				if((fcpm == null) || !(fcpm instanceof NodeHelloMessage)){
+				if((fcpm == null) || !(fcpm instanceof NodeHelloMessage)) {
 					System.err.println("Not a valid FRED node!");
 					System.exit(1);
 				}
-			} catch(MessageInvalidException me){
+			} catch (MessageInvalidException me) {
 				me.printStackTrace();
 			}
-			
-			try{
+
+			try {
 				sfs = SimpleFieldSet.readFrom(reference, false, true);
 				fcpm = FCPMessage.create(AddPeer.NAME, sfs);
 				fcpm.send(os);
@@ -79,8 +79,8 @@ public class AddRef {
 
 				// TODO: We ought to do stricter checking!
 				// FIXME: some checks even
-			} catch(MessageInvalidException me){
-				System.err.println("Invalid reference file!"+me);
+			} catch (MessageInvalidException me) {
+				System.err.println("Invalid reference file!" + me);
 				me.printStackTrace();
 			}
 
@@ -89,39 +89,43 @@ public class AddRef {
 			os.close();
 			fcpSocket.close();
 			System.out.println("That reference has been added");
-		}catch (SocketException se){
+		} catch (SocketException se) {
 			System.err.println(se);
 			se.printStackTrace();
 			System.exit(1);
-		}catch (IOException ioe){
+		} catch (IOException ioe) {
 			System.err.println(ioe);
 			ioe.printStackTrace();
 			System.exit(2);
-		}finally {
-			try{
+		} finally {
+			try {
 				Thread.sleep(3000);
-			}catch (InterruptedException e) {}
+			} catch (InterruptedException e) {
+			}
 		}
 	}
 
-        /**
-         *
-         * @param lis
-         * @return
-         */
-        protected SimpleFieldSet getMessage(LineReadingInputStream lis){
+	/**
+	 *
+	 * @param lis
+	 * @return
+	 */
+	protected SimpleFieldSet getMessage(LineReadingInputStream lis) {
 		SimpleFieldSet sfs = new SimpleFieldSet(true);
 		try {
-			while(lis.available()>0){
+			while (lis.available() > 0) {
 				String line = lis.readLine(128, 128, true);
 				int index = line.indexOf('=');
-				if(index == -1 || line.startsWith("End")) return sfs;
-				sfs.putSingle(line.substring(0, index), line.substring(index+1));
+				if(index == -1 || line.startsWith("End")) {
+					return sfs;
+				}
+				sfs.putSingle(line.substring(0, index), line.substring(index + 1));
 			}
-		}catch(IOException e){
+		} catch (IOException e) {
 			return sfs;
 		}
 
 		return sfs;
 	}
+
 }
