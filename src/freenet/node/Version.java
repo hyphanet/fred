8bb4abe5ecc1c44035f3b5123593cf6d86f7198a
Matthew Toseland <toad@amphibian.dyndns.org>
2010-02-04 13:50:11 +0000

    1240. Changelog will follow in tag.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -48,17 +48,17 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1239;
+	private static final int buildNumber = 1240;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 1237;
-	private static final int newLastGoodBuild = 1239;
+	private static final int oldLastGoodBuild = 1239;
+	private static final int newLastGoodBuild = 1240;
 	static final long transitionTime;
 
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2009, Calendar.NOVEMBER, 9, 0, 0, 0 );
+		_cal.set( 2010, Calendar.FEBRUARY, 10, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 
