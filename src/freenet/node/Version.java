0aff49d35a7616178d83e5846fde6565980a7a09
Matthew Toseland <toad@amphibian.dyndns.org>
2010-03-16 14:54:09 +0000

    Build 1243. Mandatory on 20th.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -48,11 +48,11 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1242;
+	private static final int buildNumber = 1243;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1241;
-	private static final int newLastGoodBuild = 1242;
+	private static final int newLastGoodBuild = 1243;
 	static final long transitionTime;
 
 	static {
