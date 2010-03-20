a807732a0b6106ee79a322edb4225a9064e50330
Martin Nyhus <martin.nyhus@gmx.com>
2010-03-20 02:26:51 +0100

    Slightly simplify the code
    Keep the dead store to size in case we add something else later

--- src/freenet/node/PeerNode.java
+++ src/freenet/node/PeerNode.java
@@ -4254,17 +4254,11 @@ public abstract class PeerNode implements PeerContext, USKRetrieverCallback {
 			
 			if(mustSend) {
 				int size = minSize;
-				boolean gotEnough = false;
 				size = messageQueue.addUrgentMessages(size, now, minSize, maxSize, messages);
-				if(size < 0) {
-					gotEnough = true;
-					size = -size;
-				}
 
 				// Now the not-so-urgent messages.
-				if(!gotEnough) {
+				if(size >= 0) {
 					size = messageQueue.addNonUrgentMessages(size, now, minSize, maxSize, messages);
-					if(size < 0) size = -size;
 				}
 			}
 			
