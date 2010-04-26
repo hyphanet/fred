df0d0514c770638cfc9213240c1c2d4300b108bf
Matthew Toseland <toad@amphibian.dyndns.org>
2010-04-26 23:08:33 +0100

    Document and assert

--- generator/js/src/freenet/client/tools/QueryParameter.java
+++ generator/js/src/freenet/client/tools/QueryParameter.java
@@ -2,14 +2,20 @@ package freenet.client.tools;
 
 /** This class represents a query parameter that has a name and value */
 public class QueryParameter {
-	/** The name of the parameter */
+	/** The name of the parameter. Must be URL-safe. */
 	private String	name;
-	/** The value of the parameter */
+	/** The value of the parameter. Must be URL-safe. */
 	private String	value;
 
+	/** Create a query parameter.
+	 * @param name The name of the parameter. Must be URL-safe already.
+	 * @param value The value of the parameter. Must be URL-safe already.
+	 */
 	public QueryParameter(String name, String value) {
 		this.name = name;
 		this.value = value;
+		assert(name.indexOf('&') == -1);
+		assert(value.indexOf('&') == -1);
 	}
 
 	public String getName() {
