5a36fa169cc962b2bfb8dfaed89ad578c4113c00
Matthew Toseland <toad@amphibian.dyndns.org>
2010-05-15 13:53:10 +0100

    Make it extend AbstractCollection and SortedSet

--- src/freenet/support/SortedIntSet.java
+++ src/freenet/support/SortedIntSet.java
@@ -1,10 +1,17 @@
 package freenet.support;
 
+import java.util.AbstractCollection;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.SortedSet;
+
 
 /**
  * Sorted array of long's.
  */
-public class SortedIntSet {
+public class SortedIntSet extends AbstractCollection<Integer> implements SortedSet<Integer> {
 
 	private int[] data;
 	private int length;
@@ -20,6 +27,16 @@ public class SortedIntSet {
 		length = 0;
 	}
 	
+	public SortedIntSet(int[] input) {
+		this.data = input;
+		length = input.length;
+		verify();
+	}
+	
+	public int size() {
+		return length;
+	}
+
 	/**
 	 * Get the smallest item on this set
 	 * 
@@ -28,6 +45,16 @@ public class SortedIntSet {
 	public synchronized int getFirst() {
 		if(length == 0) return -1;
 		return data[0];
+	}
+
+	/**
+	 * Get the largest item on this set
+	 * 
+	 * @return the largest item
+	 */
+	public synchronized int getLast() {
+		if(length == 0) return -1;
+		return data[length-1];
 	}
 
 	/**
@@ -60,12 +87,14 @@ public class SortedIntSet {
 	 * @param item
 	 *            the item to be removed
 	 */
-	public synchronized void remove(int item) {
+	public synchronized boolean remove(int item) {
+		boolean ret = false;
 		int x = binarySearch(item);
 		if(x >= 0) {
 			if(x < length-1)
 				System.arraycopy(data, x+1, data, x, length-x-1);
 			data[--length] = Integer.MAX_VALUE;
+			ret = true;
 		}
 		if((length*4 < data.length) && (length > MIN_SIZE)) {
 			int[] newData = new int[Math.max(data.length/2, MIN_SIZE)];
@@ -76,6 +105,7 @@ public class SortedIntSet {
 		}
 		
 		assert(verify());
+		return ret;
 	}
 
 	/**
@@ -174,13 +204,103 @@ public class SortedIntSet {
 	 * 
 	 * @return sorted array of all items
 	 */
-	public synchronized long[] toArray() {
-		long[] output = new long[length];
+	public synchronized int[] toIntArray() {
+		int[] output = new int[length];
 		System.arraycopy(data, 0, output, 0, length);
 		return output;
+	}
+
+	/**
+	 * Get a sorted array of all items
+	 * 
+	 * @return sorted array of all items
+	 */
+	public synchronized int[] toArrayRaw() {
+		if(length == data.length) return data;
+		return toIntArray();
 	}
 
 	private int binarySearch(int key) {
 		return Fields.binarySearch(data, key, 0, length-1);
 	}
+
+	public Comparator<? super Integer> comparator() {
+		return null;
+	}
+
+	public Integer first() {
+		return getFirst();
+	}
+
+	public SortedSet<Integer> headSet(Integer arg0) {
+		throw new UnsupportedOperationException(); // FIXME
+	}
+
+	public Integer last() {
+		return getLast();
+	}
+
+	public SortedSet<Integer> subSet(Integer arg0, Integer arg1) {
+		throw new UnsupportedOperationException(); // FIXME
+	}
+
+	public SortedSet<Integer> tailSet(Integer arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean add(Integer arg0) {
+		return push(arg0.intValue());
+	}
+
+	public boolean contains(Object arg0) {
+		if(arg0 instanceof Integer) {
+			int x = (Integer)arg0;
+			return contains(x);
+		}
+		return false;
+	}
+
+	public Iterator<Integer> iterator() {
+		return new Iterator<Integer>() {
+			
+			int x = 0;
+			int last = -1;
+			boolean hasLast = false;
+
+			public boolean hasNext() {
+				return x < length;
+			}
+
+			public Integer next() {
+				if(x >= length) throw new NoSuchElementException();
+				hasLast = true;
+				last = data[x++];
+				return last;
+			}
+
+			public void remove() {
+				if(!hasLast)
+					throw new IllegalStateException();
+				SortedIntSet.this.remove(last);
+				x--;
+			}
+			
+		};
+	}
+
+	public boolean remove(Object arg0) {
+		if(arg0 instanceof Integer) {
+			return remove(((Integer)arg0).intValue());
+		}
+		return false;
+	}
+
+	public boolean removeAll(Collection<?> arg0) {
+		throw new UnsupportedOperationException(); // FIXME
+	}
+
+	public boolean retainAll(Collection<?> arg0) {
+		throw new UnsupportedOperationException(); // FIXME
+	}
+
 }
