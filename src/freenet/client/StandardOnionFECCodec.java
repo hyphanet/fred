6957bf690e012136e90359cc4bccc344b0be46f7
Matthew Toseland <toad@amphibian.dyndns.org>
2010-06-12 00:34:13 +0100

    More logging

--- src/freenet/client/StandardOnionFECCodec.java
+++ src/freenet/client/StandardOnionFECCodec.java
@@ -80,6 +80,7 @@ public class StandardOnionFECCodec extends FECCodec {
 		if(k >= n) throw new IllegalArgumentException("n must be >k: n = "+n+" k = "+k);
 		if(k > 256 || n > 256) Logger.error(this, "Wierd FEC parameters? k = "+k+" n = "+n);
 		if((!noNative) && k <= 256 && n <= 256) { // native code segfaults if k < 256 and n > 256
+			System.out.println("Creating native FEC: n="+n+" k="+k);
 			try {
 				fec2 = new Native8Code(k,n);
 				Logger.minor(this, "Loaded native FEC.");
