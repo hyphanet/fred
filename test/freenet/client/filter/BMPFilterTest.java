cafe818826d4f2d049e9ffb339fbca386b71d678
Spencer Jackson <spencerandrewjackson@gmail.com>
2010-06-11 00:45:00 -0400

    Dump tested BMPs to file, if they fail to match in unit test

--- test/freenet/client/filter/BMPFilterTest.java
+++ test/freenet/client/filter/BMPFilterTest.java
@@ -1,16 +1,19 @@
 package freenet.client.filter;
 
 import junit.framework.TestCase;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.FileNotFoundException;
 import java.io.OutputStream;
+import java.util.Arrays;
 
 import freenet.l10n.NodeL10n;
 import freenet.support.api.Bucket;
 import freenet.support.io.ArrayBucket;
 import freenet.support.io.BucketTools;
 import freenet.support.io.Closer;
+import freenet.support.io.FileBucket;
 import freenet.support.io.NullBucket;
 
 
@@ -58,6 +61,15 @@ public class BMPFilterTest extends TestCase {
 				objBMPFilter.readFilter(inputStream, outputStream, "", null, null);
 				inputStream.close();
 				outputStream.close();
+				if(ib.size() != output.size() || !Arrays.equals(BucketTools.toByteArray(ib), BucketTools.toByteArray(output))) {
+					File inputDump = new File("BMPInputDump");
+					File outputDump = new File("BMPOutputDump");
+					System.out.println("Buckets don't match! Dumping to "+inputDump.getCanonicalPath()+" and "+ outputDump.getCanonicalPath());
+					FileBucket inputDumpBucket = new FileBucket(inputDump, false, false, false, false, false);
+					FileBucket outputDumpBucket = new FileBucket(outputDump, false, false, false, false, false);
+					BucketTools.copy(ib, inputDumpBucket);
+					BucketTools.copy(output, outputDumpBucket);
+				}
 				assertEquals(filename + " should be valid", expectedresult,0);
 				assertEquals("Input and output should be the same length", ib.size(), output.size());
 				assertEquals("Input and output should be identical", BucketTools.toByteArray(ib), BucketTools.toByteArray(output));
