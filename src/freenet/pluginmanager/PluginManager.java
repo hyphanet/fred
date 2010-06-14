bf21c31afd4453142a7868b70b8779b48dc21996
Matthew Toseland <toad@amphibian.dyndns.org>
2010-06-14 21:20:50 +0100

    Require Library v12 (new format index charset fix).

--- src/freenet/pluginmanager/PluginManager.java
+++ src/freenet/pluginmanager/PluginManager.java
@@ -1024,7 +1024,7 @@ public class PluginManager {
 		addOfficialPlugin("XMLLibrarian", false, 25, true, new FreenetURI("CHK@PzdgNIKIzYKet2x6rk2i9TMA8R3RTKf7~H7NBB-D1m4,8rfAK29Z8LkAcmwfVgF0RBGtTxaZZBmc7qcX5AoQUEo,AAIC--8/XMLLibrarian.jar"));
 		addOfficialPlugin("XMLSpider", false, 46, true, new FreenetURI("CHK@2FZYDhLSbhGQXeVfJNptV1~V3dNmS4zI59PpNaBlRqo,bvvqS-mMgeWHgLpEJzzYc18xY2urQSGYPmq4nmmJV6k,AAIC--8/XMLSpider.jar"));
 		addOfficialPlugin("Freereader", false, 4, true, new FreenetURI("CHK@4PuSjXk4Z0Hdu04JLhdPHLyOVLljj8qVbjRn3rHVzvg,bDGYnuYj67Q4uzroPBEWAYWRk26bPzf-iQ4~Uo3S7mg,AAIC--8/Freereader.jar"));
-		addOfficialPlugin("Library", false, 11, true, new FreenetURI("CHK@Gz~arBWM6SPqhyURc0GM~NX4QkrIVvYXFWrqP~VKkeM,o1Atj4wm59uNjzhVJAvoovhViocEzUzBjxkb94CKSFU,AAIC--8/Library.jar"));
+		addOfficialPlugin("Library", false, 12, true, new FreenetURI("CHK@AXlkDi2x7gnmxKg238a1LLfpYIJrlQu5BvF2Y7cQXBY,iECQrxPnvo63kjWfEs7w7HK8h-ZvLwb1-CT37dNmPco,AAIC--8/Library.jar"));
 		addOfficialPlugin("Spider", false, 47, false, new FreenetURI("CHK@avlS677YAH4q40rxTX9RiL87tNCer-KFkEH4ihf5Too,Rf8YkLYfNuZ4EshXgDjkMwE9dQJ8JuFHfhi6bu986mY,AAIC--8/Spider.jar"));
 		} catch (MalformedURLException e) {
 			throw new Error("Malformed hardcoded URL: "+e, e);
