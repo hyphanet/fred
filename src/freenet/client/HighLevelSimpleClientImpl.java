4175a4aee897e551888480a47b7bba4735149874
Matthew Toseland <toad@amphibian.dyndns.org>
2010-06-17 20:38:34 +0100

    Allow 136.

--- src/freenet/client/HighLevelSimpleClientImpl.java
+++ src/freenet/client/HighLevelSimpleClientImpl.java
@@ -81,9 +81,9 @@ public class HighLevelSimpleClientImpl implements HighLevelSimpleClient, Request
 	static final int MAX_SPLITFILE_CHECK_BLOCKS_PER_SEGMENT = 1536;
 	// For scaling purposes, 128 data 128 check blocks i.e. one check block per data block.
 	public static final int SPLITFILE_SCALING_BLOCKS_PER_SEGMENT = 128;
-	/* We can go down to 131 data 125 check if it avoids creating a new segment.
+	/* The number of data blocks in a segment depends on how many segments there are.
 	 * FECCodec.standardOnionCheckBlocks will automatically reduce check blocks to compensate for more than half data blocks. */
-	public static final int SPLITFILE_BLOCKS_PER_SEGMENT = 131;
+	public static final int SPLITFILE_BLOCKS_PER_SEGMENT = 136;
 	public static final int SPLITFILE_CHECK_BLOCKS_PER_SEGMENT = 128;
 	public static final int EXTRA_INSERTS_SINGLE_BLOCK = 0;
 	public static final int EXTRA_INSERTS_SPLITFILE_HEADER = 2;
