6cc8fe7df7dec0ad0d1e4a08fda336b80418cf3a
Spencer Jackson <spencerandrewjackson@gmail.com>
2010-06-19 02:12:42 -0400

    Document threaded decompressors

--- src/freenet/support/compress/DecompressorThreadManager.java
+++ src/freenet/support/compress/DecompressorThreadManager.java
@@ -11,6 +11,13 @@ import java.util.LinkedList;
 
 import freenet.support.io.Closer;
 
+/** Creates and manages decompressor threads. This class
+ * is given all decompressors which should be applied to an
+ * InputStream via addDecompressor. The decompressors will be
+ * strung together and executed when the execute method is called.
+ * This class also stores any errors which may arise.
+ * @author sajack
+*/
 public class DecompressorThreadManager {
 
 	final LinkedList<DecompressorThread> threads;
@@ -20,12 +27,22 @@ public class DecompressorThreadManager {
 	private boolean finished = false;
 	private Exception error = null;
 
+	/** Creates a new DecompressorThreadManager
+	 * 
+	 * @param input The stream that will be decompressed, if compressed
+	 * @param maxLen, the maximum number of bytes to extract
+	 */
 	public DecompressorThreadManager(InputStream input, long maxLen) {
 		threads = new LinkedList<DecompressorThread>();
 		this.maxLen = maxLen;
 		this.input = input;
 	}
 
+	/**Queues a decompressor to be ran against the input stream
+	 * 
+	 * @param compressor The object whose decompress method will be executed
+	 * @throws IOException
+	 */
 	public synchronized void addDecompressor(Compressor compressor)  throws IOException {
 		DecompressorThread thread = new DecompressorThread(compressor, this, input, output, maxLen);
 		threads.add(thread);
@@ -33,6 +50,11 @@ public class DecompressorThreadManager {
 		output = new PipedOutputStream();
 	}
 
+	/** Creates and executes a new thread for each decompressor,
+	 * chaining the output of the previous to the next.
+	 * 
+	 * @return An InputStream from which uncompressed data may be read from
+	 */
 	public synchronized InputStream execute() {
 		if(threads.isEmpty()) {
 			onFinish();
@@ -55,15 +77,26 @@ public class DecompressorThreadManager {
 		
 	}
 
+	/** Informs the manager that a nonrecoverable exception has occured in the
+	 * decompression threads
+	 * 
+	 * @param e The thrown exception
+	 */
 	public synchronized void onFailure(Exception e) {
 		error = e;
 	}
 
+	/** Marks that the decompression of the stream has finished and wakes
+	 * threads blocking on completion
+	 */
 	public synchronized void onFinish() {
 		finished = true;
 		notifyAll();
 	}
 
+	/** Blocks until all threads have finished executing and cleaning up.
+	 * 
+	 */
 	public synchronized void waitFinished() {
 		while(!finished) {
 			try {
@@ -74,6 +107,10 @@ public class DecompressorThreadManager {
 		}
 	}
 
+	/**
+	 * 
+	 * @return Returns an exception which was caught during the decompression
+	 */
 	public synchronized Exception getError() {
 		return error;
 	}
