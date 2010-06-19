c3a61fb52970c140f76bf7bbbfc1b34da61e5af0
Matthew Toseland <toad@amphibian.dyndns.org>
2010-06-19 13:30:26 +0100

    Doh, lost changes in this file.

--- src/freenet/crypt/HashType.java
+++ src/freenet/crypt/HashType.java
@@ -6,12 +6,17 @@ package freenet.crypt;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import org.bitpedia.collider.core.Ed2Handler;
+import org.bitpedia.util.TigerTree;
+
 public enum HashType {
 	SHA1(1),
 	MD5(2),
 	SHA256(4, "SHA-256"),
 	SHA384(8, "SHA-384"),
-	SHA512(16, "SHA-512");
+	SHA512(16, "SHA-512"),
+	ED2K(32, null),
+	TTH(64, null);
 	
 	/** Bitmask for aggregation. */
 	public final int bitmask;
@@ -29,6 +34,12 @@ public enum HashType {
 	}
 	
 	public MessageDigest get() throws NoSuchAlgorithmException {
+		if(javaName == null) {
+			if(this.name().equals("ED2K"))
+				return new Ed2Handler();
+			if(this.name().equals("TTH"))
+				return new TigerTree();
+		}
 		if(this.equals(SHA256)) {
 			// User the pool
 			return freenet.crypt.SHA256.getMessageDigest();
