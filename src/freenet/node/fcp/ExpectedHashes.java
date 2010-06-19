a7af8491218eb9cafe22660e25e738e8b2c56764
Matthew Toseland <toad@amphibian.dyndns.org>
2010-06-19 20:18:31 +0100

    Missing files.

---
+++ src/freenet/node/fcp/ExpectedHashes.java
@@ -0,0 +1,53 @@
+package freenet.node.fcp;
+
+import com.db4o.ObjectContainer;
+
+import freenet.client.events.ExpectedHashesEvent;
+import freenet.crypt.HashResult;
+import freenet.node.Node;
+import freenet.support.HexUtil;
+import freenet.support.SimpleFieldSet;
+
+public class ExpectedHashes extends FCPMessage {
+
+	final HashResult[] hashes;
+	final String identifier;
+	final boolean global;
+	
+	public ExpectedHashes(ExpectedHashesEvent event, String identifier, boolean global) {
+		this.identifier = identifier;
+		this.global = global;
+		this.hashes = event.hashes;
+	}
+
+	@Override
+	public SimpleFieldSet getFieldSet() {
+		SimpleFieldSet fs = new SimpleFieldSet(false);
+		SimpleFieldSet values = new SimpleFieldSet(false);
+		fs.put("Hashes", values);
+		for(HashResult hash : hashes)
+			values.putOverwrite(hash.type.name(), HexUtil.bytesToHex(hash.result));
+		fs.putOverwrite("Identifier", identifier);
+		fs.put("Global", global);
+		return fs;
+	}
+
+	@Override
+	public String getName() {
+		return "ExpectedHashes";
+	}
+
+	@Override
+	public void removeFrom(ObjectContainer container) {
+		for(HashResult res : hashes)
+			res.removeFrom(container);
+		container.delete(this);
+	}
+
+	@Override
+	public void run(FCPConnectionHandler handler, Node node)
+			throws MessageInvalidException {
+		throw new UnsupportedOperationException();
+	}
+
+}
