d90254603cd859381f2336592501efb34f434010
Spencer Jackson <spencerandrewjackson@gmail.com>
2010-06-21 19:11:25 -0400

    Place DecompressorThread into its manager class' file

--- src/freenet/support/compress/DecompressorThreadManager.java
+++ src/freenet/support/compress/DecompressorThreadManager.java
@@ -122,4 +122,55 @@ public class DecompressorThreadManager {
 		return error;
 	}
 
+	/**Represents a thread which invokes a decompressor upon an
+	 * input stream. These threads should be instantiated by a
+	 * <code>DecompressorThreadManager</code>
+	 * @author sajack
+	 */
+	class DecompressorThread implements Runnable {
+
+		/**The compressor whose decompress method will be invoked*/
+		final Compressor compressor;
+		/**The stream compressed data will be read from*/
+		final InputStream input;
+		/**The stream decompressed data will be written*/
+		final PipedOutputStream output;
+		/**A upper limit to how much data may be decompressed. This is passed to the decompressor*/
+		final long maxLen;
+		/**The manager which created the thread*/
+		final DecompressorThreadManager manager;
+		/**Whether or not this thread should signal the manager that decompression has finished*/
+		boolean isLast = false;
+
+		public DecompressorThread(Compressor compressor, DecompressorThreadManager manager, InputStream input, PipedOutputStream output, long maxLen) {
+			this.compressor = compressor;
+			this.input = input;
+			this.output = output;
+			this.maxLen = maxLen;
+			this.manager = manager;
+		}
+
+		/**Begins the decompression */
+		public void run() {
+			try {
+				if(manager.getError() == null) {
+					compressor.decompress(input, output, maxLen, maxLen * 4);
+					input.close();
+					output.close();
+					if(isLast) manager.onFinish();
+				}
+			} catch (Exception e) {
+				manager.onFailure(e);
+			} finally {
+				Closer.close(input);
+				Closer.close(output);
+			}
+		}
+
+		/** Should be called before executing the thread when there 
+		 * are no further decompressors pending*/
+		public void setLast() {
+			isLast = true;
+		}
+	}
 }
