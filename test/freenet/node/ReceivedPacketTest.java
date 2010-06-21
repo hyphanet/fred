84b39f2af5e07fe7f2f45b675dc363d9ce382b8b
Martin Nyhus <martin.nyhus@gmx.com>
2010-06-21 16:03:08 +0200

    Add test case for parsing received packet

---
+++ test/freenet/node/ReceivedPacketTest.java
@@ -0,0 +1,16 @@
+package freenet.node;
+
+import junit.framework.TestCase;
+
+public class ReceivedPacketTest extends TestCase {
+	public void testEmptyPacket() {
+		//Sequence number 0 and 0 acks
+		byte[] packet = new byte[] {0x00, 0x00, 0x00, 0x00, 0x00};
+		ReceivedPacket r = ReceivedPacket.create(packet);
+
+		assertTrue(r.getSequenceNumber() == 0);
+		assertTrue(r.getAcks().size() == 0);
+		assertTrue(r.getFragments().size() == 0);
+		assertFalse(r.getError());
+	}
+}
