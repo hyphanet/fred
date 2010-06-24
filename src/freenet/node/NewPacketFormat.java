hyphanet/fred

commit caf20bbb4f2872d6d4468dd3b051dbfe80f8a252

Ack at the very end, in case we hit errors

src/freenet/node/NewPacketFormat.java
@@ -41,13 +41,6 @@ public class NewPacketFormat implements PacketFormat {
 		// TODO: Check HMAC
 		// TODO: Decrypt
 
-		//Ack received packet
-		long sequenceNumber = (buf[offset] << 24) | (buf[offset + 1] << 16) | (buf[offset + 2] << 8) | buf[offset + 3];
-		offset += 4;
-		synchronized(acks) {
-			acks.addLast(sequenceNumber);
-		}
-
 		//Process received acks
 		int numAcks = buf[offset];
 		if(numAcks > 0) {
@@ -74,6 +67,14 @@ public class NewPacketFormat implements PacketFormat {
 		}
 
 		// TODO: Handle received message fragments
+
+		//Ack received packet
+		long sequenceNumber = (buf[offset] << 24) | (buf[offset + 1] << 16) | (buf[offset + 2] << 8) | buf[offset + 3];
+		offset += 4;
+		synchronized(acks) {
+			acks.addLast(sequenceNumber);
+		}
+
 		throw new UnsupportedOperationException();
 
 	}

commit b412e2b7841fd827e42583b88221679371d7243d

Fix reading sequence number
caf20bbb broke reading the sequence number since offset is different
after we have read the fragments. Use packetStart instead of 0 since we
want to get rid of the initial copy of the packet.

src/freenet/node/NewPacketFormat.java
@@ -50,6 +50,8 @@ public class NewPacketFormat implements PacketFormat {
 		buf = temp;
 		offset = 0;
 
+		int packetStart = offset;
+
 		//Check the hash
 		//TODO: Decrypt hash first
 		byte[] packetHash = new byte[HMAC_LENGTH];
@@ -156,7 +158,7 @@ public class NewPacketFormat implements PacketFormat {
 		}
 
 		//Ack received packet
-		long sequenceNumber = (buf[offset] << 24) | (buf[offset + 1] << 16) | (buf[offset + 2] << 8) | buf[offset + 3];
+		long sequenceNumber = (buf[packetStart] << 24) | (buf[packetStart + 1] << 16) | (buf[packetStart + 2] << 8) | buf[packetStart + 3];
 		offset += 4;
 		synchronized(acks) {
 			acks.addLast(sequenceNumber);