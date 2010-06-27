92350f556277d9bf4db7fa75337af669d94398da
Martin Nyhus <martin.nyhus@gmx.com>
2010-06-27 22:45:54 +0200

    Return null if fragmentLength would be negative

--- src/freenet/node/MessageWrapper.java
+++ src/freenet/node/MessageWrapper.java
@@ -283,6 +283,8 @@ public class MessageWrapper {
 
 		//Copy start to end into dest
 		if(item.buf.length - start < 0) return null;
+		if(maxLength - 9 < 0) return null;
+		if(end - start < 0) return null;
 		int fragmentLength = Math.min((end - start), (maxLength - 9));
 		fragmentLength = Math.min(fragmentLength, item.buf.length - start);
 
