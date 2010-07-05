b6f558d0c24e7be6321ecbc02f50096aff36954f
Spencer Jackson <spencerandrewjackson@gmail.com>
2010-07-05 16:59:33 -0400

    Use magic number from VorbisBitstreamFilter

--- src/freenet/client/filter/OggBitstreamFilter.java
+++ src/freenet/client/filter/OggBitstreamFilter.java
@@ -7,7 +7,6 @@ package freenet.client.filter;
 import java.io.IOException;
 
 public abstract class OggBitstreamFilter {
-	public static final byte[] VORBIS_MAGIC_NUMBER = new byte[] {0x76, 0x6f, 0x72, 0x62, 0x69, 0x73};
 	int lastPageSequenceNumber;
 	final int serialNumber;
 
@@ -19,9 +18,9 @@ public abstract class OggBitstreamFilter {
 	abstract boolean parse(OggPage page) throws IOException;
 
 	public static OggBitstreamFilter getBitstreamFilter(OggPage page) {
-		for(int i = 0; i <= VORBIS_MAGIC_NUMBER.length; i++) {
-			if(i == VORBIS_MAGIC_NUMBER.length) return new VorbisBitstreamFilter(page);
-			if(page.getPayload()[i+1] != VORBIS_MAGIC_NUMBER[i]) break;
+		for(int i = 0; i <= VorbisBitstreamFilter.magicNumber.length; i++) {
+			if(i == VorbisBitstreamFilter.magicNumber.length) return new VorbisBitstreamFilter(page);
+			if(page.getPayload()[i+1] != VorbisBitstreamFilter.magicNumber[i]) break;
 		}
 		return null;
 	}
