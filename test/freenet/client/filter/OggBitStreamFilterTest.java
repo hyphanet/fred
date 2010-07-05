71884bbb4467ca1068376f771be5d2b026ee1a10
Spencer Jackson <spencerandrewjackson@gmail.com>
2010-07-05 18:05:54 -0400

    Test the obtaining of ogg bitstream filters

---
+++ test/freenet/client/filter/OggBitStreamFilterTest.java
@@ -0,0 +1,31 @@
+package freenet.client.filter;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+
+public class OggBitStreamFilterTest extends TestCase{
+	public void testGetVorbisBitstreamFilter() throws IOException {
+		InputStream input = getClass().getResourceAsStream("./ogg/vorbis_header.ogg");
+		DataInputStream dis = new DataInputStream(input);
+		OggPage page = OggPage.readPage(dis);
+		Assert.assertEquals(VorbisBitstreamFilter.class, getFilterClass(page));
+	}
+
+	public void testGetFilterForInvalidFormat() throws IOException {
+		InputStream input = getClass().getResourceAsStream("./ogg/invalid_header.ogg");
+		DataInputStream dis = new DataInputStream(input);
+		OggPage page = OggPage.readPage(dis);
+		Assert.assertEquals(null, getFilterClass(page));
+	}
+
+	private Class<? extends OggBitstreamFilter> getFilterClass(OggPage page) {
+		OggBitstreamFilter filter = OggBitstreamFilter.getBitstreamFilter(page);
+		if(filter != null) return filter.getClass();
+		return null;
+	}
+}
