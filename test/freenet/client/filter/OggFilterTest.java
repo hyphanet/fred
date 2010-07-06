9d47f61d62470f4fcfa0eb5019ab4b7a1906ab3a
Spencer Jackson <spencerandrewjackson@gmail.com>
2010-07-06 13:07:44 -0400

    Add unit tests on obtaining and validating ogg pages

---
+++ test/freenet/client/filter/OggFilterTest.java
@@ -0,0 +1,66 @@
+package freenet.client.filter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import freenet.l10n.NodeL10n;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class OggFilterTest extends TestCase {
+	public void testStripNonsenseInterruption() throws IOException {
+		InputStream badData = getClass().getResourceAsStream("./ogg/nonsensical_interruption.ogg");
+		ByteArrayOutputStream filteredDataStream = new ByteArrayOutputStream();
+		DataOutputStream output = new DataOutputStream(filteredDataStream);
+		DataInputStream input = new DataInputStream(badData);
+		OggPage page = OggPage.readPage(input);
+		if(page.headerValid()) output.write(page.array());
+		page = OggPage.readPage(input);
+		if(page.headerValid()) output.write(page.array());
+		byte[] filteredData = filteredDataStream.toByteArray();
+		output.close();
+		input.close();
+
+		InputStream goodData = getClass().getResourceAsStream("./ogg/nonsensical_interruption_filtered.ogg");
+		input = new DataInputStream(goodData);
+		ByteArrayOutputStream expectedDataStream = new ByteArrayOutputStream();
+		output = new DataOutputStream(expectedDataStream);
+		page = OggPage.readPage(input);
+		if(page.headerValid()) output.write(page.array());
+		page = OggPage.readPage(input);
+		if(page.headerValid()) output.write(page.array());
+		byte[] expectedData = expectedDataStream.toByteArray();
+		output.close();
+		input.close();
+
+		Assert.assertTrue(Arrays.equals(filteredData, expectedData));
+	}
+
+	public void testChecksum() throws IOException {
+		DataInputStream input = new DataInputStream(getClass().getResourceAsStream("./ogg/valid_checksum.ogg"));
+		OggPage page = OggPage.readPage(input);
+		Assert.assertTrue(page.headerValid());
+		input.close();
+	}
+
+	public void testInvalidChecksumInvalidates() throws IOException {
+		DataInputStream input = new DataInputStream(getClass().getResourceAsStream("./ogg/invalid_checksum.ogg"));
+		OggPage page = OggPage.readPage(input);
+		Assert.assertFalse(page.headerValid());
+		input.close();
+	}
+
+	public void testValidSubPageCausesFailure() throws IOException {
+		DataInputStream input = new DataInputStream(getClass().getResourceAsStream("./ogg/contains_subpages.ogg"));
+		try {
+			@SuppressWarnings("unused")
+			OggPage page = OggPage.readPage(input);
+			Assert.fail("Expected exception not caught");
+		} catch(DataFilterException e) {}
+	}
+}
