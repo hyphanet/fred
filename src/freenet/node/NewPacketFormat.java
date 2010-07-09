db125ae0fb6f64ca7acf7f3b365b1f70adfd5fa3
Martin Nyhus <martin.nyhus@gmx.com>
2010-07-09 16:57:10 +0200

    Do resend before creating packets, not when receiving

--- src/freenet/node/NewPacketFormat.java
+++ src/freenet/node/NewPacketFormat.java
@@ -145,22 +145,6 @@ public class NewPacketFormat implements PacketFormat {
 			highestAckedSeqNum = packet.getSequenceNumber();
 		}
 
-		synchronized(sentPackets) {
-			int avgRtt = 0;
-			for(int rtt : lastRtts) {
-				avgRtt += rtt;
-			}
-			avgRtt = avgRtt / lastRtts.length;
-			long curTime = System.currentTimeMillis();
-
-			Iterator<SentPacket> it = sentPackets.values().iterator();
-			while(it.hasNext()) {
-				SentPacket s = it.next();
-				if(s.getSentTime() < (curTime - NUM_RTTS_TO_LOOSE * avgRtt)) {
-					it.remove();
-				}
-			}
-		}
 
 		return fullyReceived;
 	}
@@ -191,6 +175,25 @@ public class NewPacketFormat implements PacketFormat {
 
 	public boolean maybeSendPacket(long now, Vector<ResendPacketItem> rpiTemp, int[] rpiIntTemp)
 	                throws BlockedTooLongException {
+		//Mark packets as lost
+		synchronized(sentPackets) {
+			int avgRtt = 0;
+			for(int rtt : lastRtts) {
+				avgRtt += rtt;
+			}
+			avgRtt = avgRtt / lastRtts.length;
+			long curTime = System.currentTimeMillis();
+
+			Iterator<SentPacket> it = sentPackets.values().iterator();
+			while(it.hasNext()) {
+				SentPacket s = it.next();
+				if(s.getSentTime() < (curTime - NUM_RTTS_TO_LOOSE * avgRtt)) {
+					s.lost();
+					it.remove();
+				}
+			}
+		}
+
 		int maxPacketSize = pn.crypto.socket.getMaxPacketSize();
 		NPFPacket packet = createPacket(maxPacketSize, pn.getMessageQueue());
 		if(packet == null) return false;
