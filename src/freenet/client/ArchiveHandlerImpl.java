f38493830a9640a41dfb754330e48b05a1210a4c
Matthew Toseland <toad@amphibian.dyndns.org>
2010-07-22 17:03:01 +0100

    Doh, fix build.

--- src/freenet/client/ArchiveHandlerImpl.java
+++ src/freenet/client/ArchiveHandlerImpl.java
@@ -227,7 +227,7 @@ class ArchiveHandlerImpl implements ArchiveHandler {
 		});
 		while(set.hasNext()) {
 			ArchiveExtractTag tag = set.next();
-			if(tag.checkBroken(container)) continue;
+			if(tag.checkBroken(container, context)) continue;
 			tag.activateForExecution(container);
 			runPersistentOffThread(tag, context, context.archiveManager, context.persistentBucketFactory);
 		}
