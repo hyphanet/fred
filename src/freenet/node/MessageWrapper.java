8d916883063e8ab9e35e3c11eb788839f22f1c87
Martin Nyhus <martin.nyhus@gmx.com>
2010-07-23 15:44:25 +0200

    Remove unused methods

--- src/freenet/node/MessageWrapper.java
+++ src/freenet/node/MessageWrapper.java
@@ -18,59 +18,6 @@ public class MessageWrapper {
 		this.messageID = messageID;
 	}
 
-	/**
-	 * Copies up to <code>length</code> bytes of data into an array of bytes. The first byte of data is stored into
-	 * element <code>dest[offset]</code>, and the copied bytes are marked as sent.
-	 *
-	 * @param dest the destination array
-	 * @param offset the first index in <code>dest</code> that is written to
-	 * @param length the maximum number of bytes to copy
-	 * @return the number of bytes copied into the array at index 0, and the offset of the first copied byte at
-	 *         index 1
-	 */
-	public int[] getData(byte[] dest, int offset, int length) {
-		int start = 0;
-		int end = Integer.MAX_VALUE;
-		
-		synchronized(sent) {
-	                for(int[] range : sent) {
-	                	if(range[0] == start) {
-	                		start = range[1] + 1;
-	                	} else if (range[0] - start > 0) {
-	                		end = range[0] - 1;
-	                	}
-	                }
-                }
-		
-		if(start >= item.buf.length) {
-			return new int[] {0, 0};
-		}
-
-		//Copy start to end into dest
-		int realLength = Math.min((end - start), length);
-		realLength = Math.min(realLength, item.buf.length - start);
-
-		System.arraycopy(item.buf, start, dest, offset, realLength);
-		
-		sent.add(start, start + realLength - 1);
-		return new int[] {realLength, start};
-	}
-
-	/**
-	 * Copies the bytes between <code>start</code> and <code>end</code> into <code>dest</code>, starting at
-	 * <code>offset</code>. Bytes that are copied are marked as sent, unless they have already been marked as
-	 * received.
-	 *
-	 * @param dest the destination array
-	 * @param offset the first index in <code>dest</code> that is written to
-	 * @param start the first byte that is copied
-	 * @param end the last byte that is copied
-	 */
-	public void getData(byte[] dest, int offset, int start, int end) {
-		System.arraycopy(item.buf, start, dest, offset, end - start);
-		sent.add(start, end);
-	}
-
 	private boolean alreadyAcked = false;
 	/**
 	 * Mark the given range as received.
@@ -117,10 +64,6 @@ public class MessageWrapper {
 
 	public int getLength() {
 		return item.buf.length;
-	}
-
-	public boolean isShortMessage() {
-		return isShortMessage;
 	}
 
 	public boolean isFragmented(int length) {
