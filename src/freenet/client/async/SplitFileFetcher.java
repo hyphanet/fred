11d14bf205f193df9eb253c5a7917ce5e8b33d4d
Matthew Toseland <toad@amphibian.dyndns.org>
2010-07-30 12:14:58 +0100

    Another bugfix.

--- src/freenet/client/async/SplitFileFetcher.java
+++ src/freenet/client/async/SplitFileFetcher.java
@@ -331,7 +331,6 @@ public class SplitFileFetcher implements ClientGetState, HasKeyListener {
 			if(persistent) {
 				container.store(segments[i]);
 				segments[i].deactivateKeys(container);
-				container.deactivate(segments[i], 1);
 			}
 		}
 		int totalCrossCheckBlocks = segments.length * crossCheckBlocks;
