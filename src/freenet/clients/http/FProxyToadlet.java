7d3f460a2eedbb792e8308ded537e53344d98d0b
Spencer Jackson <spencerandrewjackson@gmail.com>
2010-07-30 16:35:09 -0400

    Allow the progress page to be disabled via a HTTP GET parameter

--- src/freenet/clients/http/FProxyToadlet.java
+++ src/freenet/clients/http/FProxyToadlet.java
@@ -572,7 +572,7 @@ public final class FProxyToadlet extends Toadlet implements RequestClient {
 		String accept = headers.get("accept");
 		FProxyFetchResult fr = null;
 		if(logMINOR) Logger.minor(this, "UA = "+ua+" accept = "+accept);
-		if(isBrowser(ua) && !ctx.disableProgressPage() && (accept == null || accept.indexOf("text/html") > -1) && !httprequest.isParameterSet("forcedownload")) {
+		if(isBrowser(ua) && !ctx.disableProgressPage() && (accept == null || accept.indexOf("text/html") > -1) && !httprequest.isParameterSet("forcedownload") && !httprequest.isParameterSet("noprogress")) {
 			FProxyFetchWaiter fetch = null;
 			try {
 				fetch = fetchTracker.makeFetcher(key, maxSize, fctx);
