7a440a205bd5fce02a2e7b20b6f4f70f6efa9393
Matthew Toseland <toad@amphibian.dyndns.org>
2010-08-04 00:39:52 +0100

    USK auxiliary inserts are allowed to fail

--- src/freenet/client/async/ClientPutter.java
+++ src/freenet/client/async/ClientPutter.java
@@ -292,9 +292,12 @@ public class ClientPutter extends BaseClientPutter implements PutCompletionCallb
 		if(state != null && state != oldState && persistent())
 			state.removeFrom(container, context);
 		if(super.failedBlocks > 0 || super.fatallyFailedBlocks > 0 || super.successfulBlocks < super.totalBlocks) {
-			Logger.error(this, "Failed blocks: "+failedBlocks+", Fatally failed blocks: "+fatallyFailedBlocks+
-					", Successful blocks: "+successfulBlocks+", Total blocks: "+totalBlocks+" but success?! on "+this+" from "+state,
-					new Exception("debug"));
+			if(persistent()) container.activate(uri, 1);
+			// USK auxiliary inserts are allowed to fail.
+			if(!uri.isUSK())
+				Logger.error(this, "Failed blocks: "+failedBlocks+", Fatally failed blocks: "+fatallyFailedBlocks+
+						", Successful blocks: "+successfulBlocks+", Total blocks: "+totalBlocks+" but success?! on "+this+" from "+state,
+						new Exception("debug"));
 		}
 		if(persistent())
 			container.store(this);
