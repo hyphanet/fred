d37e9d85b1590ae8356ca6dbaa301e93a6a4eace
Matthew Toseland <toad@amphibian.dyndns.org>
2010-08-20 11:02:28 +0100

    1272: Critical OOM bugfix, make 1271 mandatory, make self mandatory on 26th.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -49,17 +49,17 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1271;
+	private static final int buildNumber = 1272;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 1260;
-	private static final int newLastGoodBuild = 1271;
+	private static final int oldLastGoodBuild = 1271;
+	private static final int newLastGoodBuild = 1272;
 	static final long transitionTime;
 
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2010, Calendar.AUGUST, 23, 0, 0, 0 );
+		_cal.set( 2010, Calendar.AUGUST, 26, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 
