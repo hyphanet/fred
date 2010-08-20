4da79106659ee7dd812048dda131ce9504177d9c
Martin Nyhus <martin.nyhus@gmx.com>
2010-08-20 15:28:04 +0200

    Better defaults for a few variables

--- src/freenet/node/NewPacketFormat.java
+++ src/freenet/node/NewPacketFormat.java
@@ -49,19 +49,19 @@ public class NewPacketFormat implements PacketFormat {
 	private int nextSequenceNumber = 0;
 	private final ArrayList<HashMap<Integer, MessageWrapper>> startedByPrio;
 	private int nextMessageID = 0;
-	private int messageWindowPtrAcked = 0; //The first message id that hasn't been acked by the receiver
+	private int messageWindowPtrAcked = nextMessageID; //The first message id that hasn't been acked by the receiver
 	private final SparseBitmap ackedMessages = new SparseBitmap();
 
 	private int highestReceivedSequenceNumber = -1;
 	private final HashMap<Integer, PartiallyReceivedBuffer> receiveBuffers = new HashMap<Integer, PartiallyReceivedBuffer>();
 	private final HashMap<Integer, SparseBitmap> receiveMaps = new HashMap<Integer, SparseBitmap>();
-	private int messageWindowPtrReceived = 0; //The first message id that hasn't been fully received
+	private int messageWindowPtrReceived = nextMessageID; //The first message id that hasn't been fully received
 	private final SparseBitmap receivedMessages= new SparseBitmap();
 
 	private SessionKey watchListKey;
 	private byte[][] seqNumWatchList;
-	private int watchListPointer; //Index of the packet with the lowest sequence number
-	private int watchListOffset; //Sequence number of the packet at seqNumWatchList[watchListPointer]
+	private int watchListPointer = 0; //Index of the packet with the lowest sequence number
+	private int watchListOffset = nextSequenceNumber; //Sequence number of the packet at seqNumWatchList[watchListPointer]
 
 	private volatile int highestReceivedAck = -1;
 
