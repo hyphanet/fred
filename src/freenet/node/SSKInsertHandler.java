b540d5f2e5b0dc064fa4a5478224d92ec0d5fece
Matthew Toseland <toad@amphibian.dyndns.org>
2010-09-17 18:23:45 +0100

    Similarly here: HTL is never 0, we decrement only in Sender.

--- src/freenet/node/SSKInsertHandler.java
+++ src/freenet/node/SSKInsertHandler.java
@@ -210,18 +210,6 @@ public class SSKInsertHandler implements PrioRunnable, ByteCounter {
 		
 		if(logMINOR) Logger.minor(this, "Got block for "+key+" for "+uid);
 		
-        if(htl == 0) {
-        	Message msg = DMT.createFNPInsertReply(uid);
-        	try {
-				source.sendSync(msg, this);
-			} catch (NotConnectedException e) {
-				// Ignore
-			}
-			canCommit = true;
-            finish(SSKInsertSender.SUCCESS);
-            return;
-        }
-        
         if(htl > 0)
             sender = node.makeInsertSender(block, htl, uid, source, false, false, canWriteDatastore, forkOnCacheable, preferInsert, ignoreLowBackoff);
         
