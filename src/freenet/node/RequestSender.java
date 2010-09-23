1876e873bd45d40fc8740cd59db478e25597db01
Matthew Toseland <toad@amphibian.dyndns.org>
2010-09-23 20:20:39 +0100

    Only set sskData if we are expecting an SSK.

--- src/freenet/node/RequestSender.java
+++ src/freenet/node/RequestSender.java
@@ -855,14 +855,14 @@ acceptWaiterLoop:
             		
             		if(logMINOR) Logger.minor(this, "Got data on "+uid);
             		
-                	sskData = ((ShortBuffer)msg.getObject(DMT.DATA)).getData();
-                	
             		if(!(key instanceof NodeSSK)) {
             			Logger.error(this, "Got "+msg+" but expected a different key type from "+next);
                 		node.failureTable.onFailed(key, next, htl, (int) (System.currentTimeMillis() - timeSentRequest));
             			break;
             		}
             		
+                	sskData = ((ShortBuffer)msg.getObject(DMT.DATA)).getData();
+                	
                 	if(pubKey != null && headers != null) {
                 		finishSSK(next);
                 		return;
