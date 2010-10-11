5eb47b332087e0fd2ee3101a83911ab668701b6b
Matthew Toseland <toad@amphibian.dyndns.org>
2010-10-11 23:45:26 +0100

    1289: More work on auto-fixing/telling the user what is happening after the RequestClient-null-bug.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,7 +50,7 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1288;
+	private static final int buildNumber = 1289;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1283;
