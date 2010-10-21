c2ae35301d8ebc2c20bb5db3c52328744be9e479
Matthew Toseland <toad@amphibian.dyndns.org>
2010-10-21 17:37:13 +0100

    1294. Mandatory Monday (as was 1293).

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,11 +50,11 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1293;
+	private static final int buildNumber = 1294;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1285;
-	private static final int newLastGoodBuild = 1293;
+	private static final int newLastGoodBuild = 1294;
 	static final long transitionTime;
 
 	static {
