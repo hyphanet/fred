4dfa65c4ca3b75bd69a917a3bc53df7642adf7fa
Martin Nyhus <martin.nyhus@gmx.com>
2010-10-27 18:42:44 +0200

    Don't set the sequence number explicitly during unit tests
    The sequence numbers are assigned during createPacket() since commit
    9952ed2f, but the tests weren't updated.

--- test/freenet/node/NewPacketFormatTest.java
+++ test/freenet/node/NewPacketFormatTest.java
@@ -22,7 +22,6 @@ public class NewPacketFormatTest extends TestCase {
 
 		//Packet that should be acked
 		p = new NPFPacket();
-		p.setSequenceNumber(0);
 		p.addMessageFragment(new MessageFragment(true, false, true, 0, 8, 8, 0, new byte[] {(byte) 0x01,
 		                (byte) 0x23, (byte) 0x45, (byte) 0x67, (byte) 0x89, (byte) 0xAB, (byte) 0xCD,
 		                (byte) 0xEF }, null));
@@ -41,22 +40,18 @@ public class NewPacketFormatTest extends TestCase {
 		senderQueue.queueAndEstimateSize(new MessageItem(new byte[1024], null, false, null, (short) 0));
 
 		NPFPacket fragment1 = sender.createPacket(512, senderQueue);
-		fragment1.setSequenceNumber(0);
 		assertEquals(1, fragment1.getFragments().size());
 		receiver.handleDecryptedPacket(fragment1);
 
 		NPFPacket fragment2 = sender.createPacket(512, senderQueue);
-		fragment2.setSequenceNumber(1);
 		assertEquals(1, fragment2.getFragments().size());
 		receiver.handleDecryptedPacket(fragment2);
 
 		NPFPacket ack1 = receiver.createPacket(512, receiverQueue);
-		ack1.setSequenceNumber(0);
 		assertEquals(2, ack1.getAcks().size());
 		sender.handleDecryptedPacket(ack1);
 
 		NPFPacket fragment3 = sender.createPacket(512, senderQueue);
-		fragment3.setSequenceNumber(2);
 		assertEquals(1, fragment3.getFragments().size());
 		receiver.handleDecryptedPacket(fragment3);
 		receiver.createPacket(512, senderQueue); //Sent, but lost
@@ -67,12 +62,10 @@ public class NewPacketFormatTest extends TestCase {
 
 		NPFPacket resend1 = sender.createPacket(512, senderQueue);
 		if(resend1 == null) fail("No packet to resend");
-		resend1.setSequenceNumber(3);
 		assertEquals(0, receiver.handleDecryptedPacket(resend1).size());
 
 		//Make sure an ack is sent
 		NPFPacket ack2 = receiver.createPacket(512, receiverQueue);
-		ack2.setSequenceNumber(2);
 		assertNotNull(ack2);
 		assertEquals(1, ack2.getAcks().size());
 		assertEquals(0, ack2.getFragments().size());
@@ -89,15 +82,12 @@ public class NewPacketFormatTest extends TestCase {
 		senderQueue.queueAndEstimateSize(new MessageItem(new byte[1024], null, false, null, (short) 0));
 
 		NPFPacket fragment1 = sender.createPacket(512, senderQueue);
-		fragment1.setSequenceNumber(1);
 		assertEquals(1, fragment1.getFragments().size());
 
 		NPFPacket fragment2 = sender.createPacket(512, senderQueue);
-		fragment2.setSequenceNumber(2);
 		assertEquals(1, fragment2.getFragments().size());
 
 		NPFPacket fragment3 = sender.createPacket(512, senderQueue);
-		fragment3.setSequenceNumber(3);
 		assertEquals(1, fragment3.getFragments().size());
 
 		receiver.handleDecryptedPacket(fragment1);
@@ -116,13 +106,10 @@ public class NewPacketFormatTest extends TestCase {
 		senderQueue.queueAndEstimateSize(new MessageItem(new byte[1024], null, false, null, (short) 0));
 
 		NPFPacket fragment1 = sender.createPacket(512, senderQueue);
-		fragment1.setSequenceNumber(1);
 		assertEquals(1, fragment1.getFragments().size());
 		NPFPacket fragment2 = sender.createPacket(512, senderQueue);
-		fragment2.setSequenceNumber(2);
 		assertEquals(1, fragment2.getFragments().size());
 		NPFPacket fragment3 = sender.createPacket(512, senderQueue);
-		fragment3.setSequenceNumber(3);
 		assertEquals(1, fragment3.getFragments().size());
 
 		receiver.handleDecryptedPacket(fragment3);
@@ -138,14 +125,12 @@ public class NewPacketFormatTest extends TestCase {
 		senderQueue.queueAndEstimateSize(new MessageItem(new byte[128], null, false, null, (short) 0));
 
 		NPFPacket packet1 = sender.createPacket(512, senderQueue);
-		packet1.setSequenceNumber(0);
 		assertEquals(1, receiver.handleDecryptedPacket(packet1).size());
 
 		//Receiving the same packet twice should work
 		assertEquals(0, receiver.handleDecryptedPacket(packet1).size());
 
 		//Same message, new sequence number ie. resend
-		packet1.setSequenceNumber(1);
 		assertEquals(0, receiver.handleDecryptedPacket(packet1).size());
 	}
 
@@ -153,7 +138,6 @@ public class NewPacketFormatTest extends TestCase {
 	                throws BlockedTooLongException {
 		senderQueue.queueAndEstimateSize(new MessageItem(new byte[1], null, false, null, (short) 0));
 		NPFPacket s = sender.createPacket(512, senderQueue);
-		s.setSequenceNumber(0);
 		receiver.handleDecryptedPacket(s);
 		try {
 			Thread.sleep(delay);
@@ -161,7 +145,6 @@ public class NewPacketFormatTest extends TestCase {
 
 		}
 		NPFPacket r = receiver.createPacket(512, receiverQueue);
-		r.setSequenceNumber(0);
 		sender.handleDecryptedPacket(r);
 	}
 }
