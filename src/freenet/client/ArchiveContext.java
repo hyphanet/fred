f79d16d5f5583fe1526fa047edb8f4d3c51ba8e8
Matthew Toseland <toad@amphibian.dyndns.org>
2010-11-01 18:54:38 +0000

    Fix NPE in ArchiveContext on removing requests from the queue.

--- src/freenet/client/ArchiveContext.java
+++ src/freenet/client/ArchiveContext.java
@@ -54,10 +54,12 @@ public class ArchiveContext {
 	}
 
 	public void removeFrom(ObjectContainer container) {
-		for(Object uri : soFar) {
-			((FreenetURI)uri).removeFrom(container);
+		if(soFar != null) {
+			for(Object uri : soFar) {
+				((FreenetURI)uri).removeFrom(container);
+			}
+			container.delete(soFar);
 		}
-		container.delete(soFar);
 		container.delete(this);
 	}
 }
