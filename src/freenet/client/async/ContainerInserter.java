482837c660849e815858c94e309b00723d524dd6
Matthew Toseland <toad@amphibian.dyndns.org>
2010-11-03 12:09:52 +0000

    Move close and open outside create*Bucket.

--- src/freenet/client/async/ContainerInserter.java
+++ src/freenet/client/async/ContainerInserter.java
@@ -168,11 +168,18 @@ public class ContainerInserter implements ClientPutState {
 		}
 		
 		InsertBlock block;
+		OutputStream os = null;
 		try {
 			Bucket outputBucket = context.getBucketFactory(persistent).makeBucket(-1);
+			os = new BufferedOutputStream(outputBucket.getOutputStream());
 			String mimeType = (archiveType == ARCHIVE_TYPE.TAR ?
-				createTarBucket(outputBucket, container) :
-				createZipBucket(outputBucket, container));
+				createTarBucket(os, container) :
+				createZipBucket(os, container));
+			os.flush();
+			os.close();
+			os = null;
+			if(logMINOR)
+				Logger.minor(this, "Archive size is "+outputBucket.size());
 			
 			if(logMINOR) Logger.minor(this, "We are using "+archiveType);
 			
@@ -185,6 +192,8 @@ public class ContainerInserter implements ClientPutState {
 		} catch (IOException e) {
 			fail(new InsertException(InsertException.BUCKET_ERROR, e, null), container, context);
 			return;
+		} finally {
+			Closer.close(os);
 		}
 		
 		boolean dc = dontCompress;
@@ -290,70 +299,49 @@ public class ContainerInserter implements ClientPutState {
 	
 
 	
-	private String createTarBucket(Bucket outputBucket, @SuppressWarnings("unused") ObjectContainer container) throws IOException {
+	private String createTarBucket(OutputStream os, @SuppressWarnings("unused") ObjectContainer container) throws IOException {
 		if(logMINOR) Logger.minor(this, "Create a TAR Bucket");
 		
-		OutputStream os = new BufferedOutputStream(outputBucket.getOutputStream());
 		TarOutputStream tarOS = new TarOutputStream(os);
 		tarOS.setLongFileMode(TarOutputStream.LONGFILE_GNU);
 		TarEntry ze;
 
-		try {
-			
-			for(ContainerElement ph : containerItems) {
-				if(logMINOR)
-					Logger.minor(this, "Putting into tar: "+ph+" data length "+ph.data.size()+" name "+ph.targetInArchive);
-				ze = new TarEntry(ph.targetInArchive);
-				ze.setModTime(0);
-				long size = ph.data.size();
-				ze.setSize(size);
-				tarOS.putNextEntry(ze);
-				BucketTools.copyTo(ph.data, tarOS, size);
-				tarOS.closeEntry();
-			}
-			
+		for(ContainerElement ph : containerItems) {
+			if(logMINOR)
+				Logger.minor(this, "Putting into tar: "+ph+" data length "+ph.data.size()+" name "+ph.targetInArchive);
+			ze = new TarEntry(ph.targetInArchive);
+			ze.setModTime(0);
+			long size = ph.data.size();
+			ze.setSize(size);
+			tarOS.putNextEntry(ze);
+			BucketTools.copyTo(ph.data, tarOS, size);
 			tarOS.closeEntry();
-			// Both finish() and close() are necessary.
-			tarOS.finish();
-			tarOS.flush();
-			tarOS.close();
-			tarOS = null;
-		} finally {
-			Closer.close(tarOS);
 		}
 		
-		if(logMINOR)
-			Logger.minor(this, "Archive size is "+outputBucket.size());
+		tarOS.closeEntry();
+		// Both finish() and close() are necessary.
+		tarOS.finish();
 		
 		return ARCHIVE_TYPE.TAR.mimeTypes[0];
 	}
 	
-	private String createZipBucket(Bucket outputBucket, @SuppressWarnings("unused") ObjectContainer container) throws IOException {
+	private String createZipBucket(OutputStream os, @SuppressWarnings("unused") ObjectContainer container) throws IOException {
 		if(logMINOR) Logger.minor(this, "Create a ZIP Bucket");
 		
-		OutputStream os = new BufferedOutputStream(outputBucket.getOutputStream());
 		ZipOutputStream zos = new ZipOutputStream(os);
 		ZipEntry ze;
 
-		try {
-			
-			for(ContainerElement ph: containerItems) {
-				ze = new ZipEntry(ph.targetInArchive);
-				ze.setTime(0);
-				zos.putNextEntry(ze);
-				BucketTools.copyTo(ph.data, zos, ph.data.size());
-				zos.closeEntry();
-			}
-			
+		for(ContainerElement ph: containerItems) {
+			ze = new ZipEntry(ph.targetInArchive);
+			ze.setTime(0);
+			zos.putNextEntry(ze);
+			BucketTools.copyTo(ph.data, zos, ph.data.size());
 			zos.closeEntry();
-			// Both finish() and close() are necessary.
-			zos.finish();
-			zos.flush();
-			zos.close();
-			zos = null;
-		} finally {
-			Closer.close(zos);
 		}
+		
+		zos.closeEntry();
+		// Both finish() and close() are necessary.
+		zos.finish();
 		
 		return ARCHIVE_TYPE.ZIP.mimeTypes[0];
 	}
