5f193b30308b1385e128d39c304727b3b45f6467
Matthew Toseland <toad@amphibian.dyndns.org>
2010-11-08 15:27:44 +0000

    Fix a bit that slid through from new load management: RejectIsSoft causes waiting, we don't support that as we don't wait.

--- src/freenet/node/RequestSender.java
+++ src/freenet/node/RequestSender.java
@@ -740,19 +740,20 @@ loadWaiterLoop:
     				
     				if(logMINOR) Logger.minor(this, "Is local");
     				
-    				if(msg.getSubMessage(DMT.FNPRejectIsSoft) != null) {
-    					if(logMINOR) Logger.minor(this, "Soft rejection, waiting to resend");
-    					nodesRoutedTo.remove(next);
-    					origTag.removeRoutingTo(next);
-    					return DO.WAIT;
-    				} else {
+    				// FIXME new load management introduces soft rejects and waiting.
+//    				if(msg.getSubMessage(DMT.FNPRejectIsSoft) != null) {
+//    					if(logMINOR) Logger.minor(this, "Soft rejection, waiting to resend");
+//    					nodesRoutedTo.remove(next);
+//    					origTag.removeRoutingTo(next);
+//    					return DO.WAIT;
+//    				} else {
     					next.localRejectedOverload("ForwardRejectedOverload");
     					node.failureTable.onFailed(key, next, htl, (int) (System.currentTimeMillis() - timeSentRequest));
     					if(logMINOR) Logger.minor(this, "Local RejectedOverload, moving on to next peer");
     					// Give up on this one, try another
     					next.noLongerRoutingTo(origTag, false);
     					return DO.NEXT_PEER;
-    				}
+//    				}
     			}
     			//Could be a previous rejection, the timeout to incur another ACCEPTED_TIMEOUT is minimal...
     			continue;
