9d89bc793e30f77146515038fcaf2edaf2de9eef
Matthew Toseland <toad@amphibian.dyndns.org>
2010-11-10 14:05:24 +0000

    Remove noMetadata, not used

--- src/freenet/store/BerkeleyDBFreenetStore.java
+++ src/freenet/store/BerkeleyDBFreenetStore.java
@@ -1168,7 +1168,6 @@ public class BerkeleyDBFreenetStore<T extends StorableBlock> implements FreenetS
 	public T fetch(byte[] routingkey, byte[] fullKey, boolean dontPromote,
 			boolean canReadClientCache, boolean canReadSlashdotCache, BlockMetadata meta) throws IOException {
 		T retval = fetch(routingkey, fullKey, dontPromote, canReadClientCache, canReadSlashdotCache, (DSAPublicKey)null);
-		if(meta != null) meta.noMetadata = true;
 		return retval;
 	}
 	
