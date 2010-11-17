85f746483447687eb30f82dd57a06816df14111c
Matthew Toseland <toad@amphibian.dyndns.org>
2010-11-17 14:44:01 +0000

    Comparing exact milliseconds between two Calendar's where one is GMT and the other one isn't - no wonder it's not working!!

--- src/freenet/node/simulator/LongTermManySingleBlocksTest.java
+++ src/freenet/node/simulator/LongTermManySingleBlocksTest.java
@@ -321,7 +321,7 @@ public class LongTermManySingleBlocksTest {
 				//System.out.println("LINE: "+line);
 				String[] split = line.split("!");
 				Date date = dateFormat.parse(split[0]);
-				GregorianCalendar calendar = new GregorianCalendar();
+				GregorianCalendar calendar = new GregorianCalendar(TimeZone.getTimeZone("GMT"));
 				calendar.setTime(date);
 				System.out.println("Date: "+dateFormat.format(calendar.getTime()));
 				calendar.set(Calendar.HOUR_OF_DAY, 0);
