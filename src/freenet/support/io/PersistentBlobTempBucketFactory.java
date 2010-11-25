hyphanet/fred

commit 6fe45b4ff3ddce65004f49d5c98ff463efe51cab

Add to almostFreeSlots to avoid a corner case.

src/freenet/support/io/PersistentBlobTempBucketFactory.java
@@ -973,6 +973,8 @@ outer:				while(true) {
 		container.store(lastTag);
 		container.store(lastBucket);
 		synchronized(this) {
+			// Ensure that even in wierd cases it won't be reused before commit and therefore won't cause problems.
+			almostFreeSlots.put(lastTag.index, lastTag);
 			if(freeBlocksCache != null && lastTag.index < Integer.MAX_VALUE)
 				freeBlocksCache.setBit((int)lastTag.index, false);
 		}

commit c2add67e3df8d6f8e2ac90c787a4ae12b8c146bf

Fix bug caused by 6fe45b4ff3ddce65004f49d5c98ff463efe51cab : We were adding the slots we clear at the end of the file to the free blocks list. This is very counterproductive if the file is sparse.

src/freenet/support/io/PersistentBlobTempBucketFactory.java
@@ -79,6 +79,8 @@ public class PersistentBlobTempBucketFactory {
 	 * Similar to notCommittedBlobs. */
 	private transient TreeMap<Long,PersistentBlobTempBucketTag> almostFreeSlots;
 	
+	private transient long lastMovedFrom = Long.MAX_VALUE;
+	
 	private transient TreeMap<Long,PersistentBlobTempBucket> shadows;
 	
 	private transient DBJobRunner jobRunner;
@@ -779,7 +781,10 @@ outer:				while(true) {
 									if(!innerDefrag(lastBucket, shadow, lastTag, newTag, container)) return false;
 								}
 							}
-						} else break;
+						} else {
+							if(logMINOR) Logger.minor(this, "First available slot "+firstSlot+" is after slot to move "+lastCommitted);
+							break;
+						}
 						if(deactivateLastBucket)
 							container.deactivate(lastBucket, 1);
 						if(blocksMoved < MOVE_BLOCKS_PER_MINUTE) {
@@ -933,6 +938,7 @@ outer:				while(true) {
 		container.store(lastTag);
 		container.store(lastBucket);
 		synchronized(this) {
+			lastMovedFrom = Math.min(lastMovedFrom, lastTag.index);
 			// Ensure that even in wierd cases it won't be reused before commit and therefore won't cause problems.
 			almostFreeSlots.put(lastTag.index, lastTag);
 			freeBlocksCache.setBit((int)lastTag.index, false);
@@ -995,11 +1001,13 @@ outer:				while(true) {
 				Map.Entry<Long,PersistentBlobTempBucketTag> entry = it.next();
 				Long slot = entry.getKey();
 				if(slot >= blocks) continue;
+				if(slot >= lastMovedFrom) continue;
 				if(entry.getValue() != null)
 					freeSlots.put(entry.getKey(), entry.getValue());
 				freeNow++;
 			}
 		}
+		lastMovedFrom = Long.MAX_VALUE;
 		almostFreeSlots.clear();
 	}
 	