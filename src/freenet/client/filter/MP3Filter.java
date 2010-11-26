3dd9bd311e627a71d2f3c566582f3b52d9a14fc2
Matthew Toseland <toad@amphibian.dyndns.org>
2010-11-26 14:56:02 +0000

    Document

--- src/freenet/client/filter/MP3Filter.java
+++ src/freenet/client/filter/MP3Filter.java
@@ -61,6 +61,7 @@ public class MP3Filter implements ContentDataFilter {
 				//Populate header details
 				byte version = (byte) ((frameHeader & 0x00180000) >>> 19); //2 bits
 				byte layer = (byte) ((frameHeader & 0x00060000) >>> 17); //2 bits
+				// WARNING: layer is encoded! 1 = layer 3, 2 = layer 2, 3 = layer 1!
 				boolean protectionBit = ((frameHeader & 0x00010000) >>> 16) == 1 ? true : false; //1 bit
 				byte bitrateIndex = (byte) ((frameHeader & 0x0000f000) >>> 12); //4 bits
 				byte samplerateIndex = (byte) ((frameHeader & 0x0000c0000) >>> 10); //2 bits
