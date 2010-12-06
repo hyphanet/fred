hyphanet/fred

commit b9b7b077377ebb8eae87dadddd6493c065940432

Fix not being able to download without downloading all over again: Handle *all* requests, including force-download, through the main loop, hence use the cache.

src/freenet/clients/http/FProxyToadlet.java
@@ -583,7 +583,10 @@ public final class FProxyToadlet extends Toadlet implements RequestClient {
 		String accept = headers.get("accept");
 		FProxyFetchResult fr = null;
 		if(logMINOR) Logger.minor(this, "UA = "+ua+" accept = "+accept);
-		if(isBrowser(ua) && !ctx.disableProgressPage() && (accept == null || accept.indexOf("text/html") > -1) && !httprequest.isParameterSet("forcedownload")) {
+		
+		boolean canSendProgress = 
+			isBrowser(ua) && !ctx.disableProgressPage() && (accept == null || accept.indexOf("text/html") > -1) && !httprequest.isParameterSet("forcedownload");
+		
 			FProxyFetchWaiter fetch = null;
 			try {
 				fetch = fetchTracker.makeFetcher(key, maxSize, fctx, ctx.getReFilterPolicy());
@@ -619,7 +622,7 @@ public final class FProxyToadlet extends Toadlet implements RequestClient {
 				fe = fr.failed;
 				fetch.close(); // Not waiting any more, but still locked the results until sent
 				break;
-			} else {
+			} else if(canSendProgress) {
 				if(logMINOR) Logger.minor(this, "Still in progress");
 				// Still in progress
 				boolean isJsEnabled=ctx.getContainer().isFProxyJavascriptEnabled() && ua != null && !ua.contains("AppleWebKit/");
@@ -678,7 +681,6 @@ public final class FProxyToadlet extends Toadlet implements RequestClient {
 				return;
 			}
 			}
-		}
 		
 		try {
 			if(logMINOR)

commit f8caeaec7f8eeccc63845a65e5ebfa340583312a

Fix major leak introduced by b9b7b077377ebb8eae87dadddd6493c065940432

src/freenet/clients/http/FProxyFetchWaiter.java
@@ -16,16 +16,31 @@ public class FProxyFetchWaiter {
 	private boolean awoken;
 	
 	public FProxyFetchResult getResult() {
+		return getResult(false);
+	}
+	
+	public FProxyFetchResult getResult(boolean waitForever) {
 		boolean waited;
 		synchronized(this) {
-			if(!(finished || hasWaited || awoken)) {
-				awoken = false;
-				try {
-					wait(5000);
-				} catch (InterruptedException e) { 
-					// Not likely
-				};
-				hasWaited = true;
+			if(waitForever) {
+				while(!finished) {
+					try {
+						wait();
+						hasWaited = true;
+					} catch (InterruptedException e) {
+						// Ignore
+					}
+				}
+			} else {
+				if(!(finished || hasWaited || awoken)) {
+					awoken = false;
+					try {
+						wait(5000);
+					} catch (InterruptedException e) { 
+						// Not likely
+					};
+					hasWaited = true;
+				}
 			}
 			waited = hasWaited;
 		}

src/freenet/clients/http/FProxyToadlet.java
@@ -595,7 +595,7 @@ public final class FProxyToadlet extends Toadlet implements RequestClient {
 			}
 			if(fetch != null)
 			while(true) {
-			fr = fetch.getResult();
+			fr = fetch.getResult(!canSendProgress);
 			if(fr.hasData()) {
 				
 				if(fr.getFetchCount() > 1 && !fr.hasWaited() && fr.getFetchCount() > 1 && key.isUSK() && context.uskManager.lookupKnownGood(USK.create(key)) > key.getSuggestedEdition()) {
@@ -679,7 +679,8 @@ public final class FProxyToadlet extends Toadlet implements RequestClient {
 				fr.close();
 				fetch.close();
 				return;
-			}
+			} else if(fr != null)
+				fr.close();
 			}
 		
 		try {
@@ -695,7 +696,7 @@ public final class FProxyToadlet extends Toadlet implements RequestClient {
 					FProxyFetchResult result=null;
 					try{
 						waiter=progress.getWaiter();
-						result=waiter.getResult();
+						result=waiter.getResult(false);
 						if(result.failed==null && result.data!=null){
 							mimeType=result.mimeType;
 							data=result.data;