29ca82d081b44b24c0e31f0c79dd0d79515660c5
Matthew Toseland <toad@amphibian.dyndns.org>
2010-12-20 14:24:14 +0000

    Always dump the queue (and therefore don't schedule the reconnect-check job).

--- src/freenet/node/SeedClientPeerNode.java
+++ src/freenet/node/SeedClientPeerNode.java
@@ -89,7 +89,7 @@ public class SeedClientPeerNode extends PeerNode {
 
 	@Override
 	public boolean disconnected(boolean dumpMessageQueue, boolean dumpTrackers) {
-		boolean ret = super.disconnected(dumpMessageQueue, dumpTrackers);
+		boolean ret = super.disconnected(dumpMessageQueue, true);
 		node.peers.disconnect(this, false, false, false);
 		return ret;
 	}
