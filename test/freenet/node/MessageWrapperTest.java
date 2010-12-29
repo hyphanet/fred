11e24a195f7dc45eb01c1d4f86b8e527c9d1f2cb
Martin Nyhus <martin.nyhus@gmx.com>
2010-12-29 19:03:50 +0100

    Test toads off by one

--- test/freenet/node/MessageWrapperTest.java
+++ test/freenet/node/MessageWrapperTest.java
@@ -47,4 +47,57 @@ public class MessageWrapperTest extends TestCase {
 		assertFalse(frag.shortMessage);
 		assertSame(wrapper, frag.wrapper);
 	}
+	
+	public void testGetFragmentWithLoss() {
+		MessageItem item = new MessageItem(new byte[363], null, false, null, (short) 0);
+		MessageWrapper wrapper = new MessageWrapper(item, 0);
+
+		MessageFragment frag1 = wrapper.getMessageFragment(128);
+		assertNotNull(frag1);
+		assertTrue(frag1.firstFragment);
+		assertEquals(0, frag1.fragmentOffset);
+		assertTrue(frag1.fragmentLength == 121);
+		assertTrue(frag1.isFragmented);
+		assertEquals(0, frag1.messageID);
+		assertEquals(363, frag1.messageLength);
+		assertFalse(frag1.shortMessage);
+		assertSame(wrapper, frag1.wrapper);
+
+		MessageFragment frag2 = wrapper.getMessageFragment(128);
+		assertNotNull(frag2);
+		assertFalse(frag2.firstFragment);
+		assertEquals(121, frag2.fragmentOffset);
+		assertTrue(frag2.fragmentLength == 121);
+		assertTrue(frag2.isFragmented);
+		assertEquals(0, frag2.messageID);
+		assertEquals(363, frag2.messageLength);
+		assertFalse(frag2.shortMessage);
+		assertSame(wrapper, frag2.wrapper);
+
+		MessageFragment frag3 = wrapper.getMessageFragment(128);
+		assertNotNull(frag3);
+		assertFalse(frag3.firstFragment);
+		assertEquals(242, frag3.fragmentOffset);
+		assertTrue(frag3.fragmentLength == 121);
+		assertTrue(frag3.isFragmented);
+		assertEquals(0, frag3.messageID);
+		assertEquals(363, frag3.messageLength);
+		assertFalse(frag3.shortMessage);
+		assertSame(wrapper, frag3.wrapper);
+		
+		wrapper.ack(0, 120); // frag1
+		wrapper.ack(242, 262); // frag3
+		wrapper.lost(121, 241); // frag 2
+		
+		MessageFragment frag = wrapper.getMessageFragment(128);
+		assertNotNull(frag);
+		assertFalse(frag.firstFragment);
+		assertEquals(121, frag.fragmentOffset);
+		assertEquals(121, frag.fragmentLength);
+		assertTrue(frag.isFragmented);
+		assertEquals(0, frag.messageID);
+		assertEquals(363, frag.messageLength);
+		assertFalse(frag.shortMessage);
+		assertSame(wrapper, frag.wrapper);
+	}
 }
