1824c7a97513b223f9f69c6d6947f4d735c372ab
Matthew Toseland <toad@amphibian.dyndns.org>
2011-01-02 02:14:34 +0000

    Build 1317: Critical bug fix for new packet transport.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,11 +50,11 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1316;
+	private static final int buildNumber = 1317;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1312;
-	private static final int newLastGoodBuild = 1316;
+	private static final int newLastGoodBuild = 1317;
 	static final long transitionTime;
 
 	static {
