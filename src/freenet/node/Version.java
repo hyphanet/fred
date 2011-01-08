381948ecdf93a3392ae5ac540ddb9343f7e97746
Matthew Toseland <toad@amphibian.dyndns.org>
2011-01-08 02:54:33 +0000

    1323.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,11 +50,11 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1322;
+	private static final int buildNumber = 1323;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1319;
-	private static final int newLastGoodBuild = 1322;
+	private static final int newLastGoodBuild = 1323;
 	static final long transitionTime;
 
 	static {
