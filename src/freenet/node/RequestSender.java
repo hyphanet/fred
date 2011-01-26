9c96648c0f347b42129de9a84bc88d6b42768378
Matthew Toseland <toad@amphibian.dyndns.org>
2011-01-26 00:18:12 +0000

    Always unlock next peer immediately after setting waitingForOpennet. This fixes the error message and is the right thing anyway.

--- src/freenet/node/RequestSender.java
+++ src/freenet/node/RequestSender.java
@@ -1684,8 +1684,9 @@ public final class RequestSender implements PrioRunnable, ByteCounter {
         	}
             doOpennet = code == SUCCESS && !(fromOfferedKey || isSSK);
        		if(doOpennet)
-       			origTag.waitingForOpennet(next);
-       		else if(next != null) next.noLongerRoutingTo(origTag, fromOfferedKey);
+       			origTag.waitingForOpennet(next); // Call this first so we don't unlock.
+       		if(next != null) next.noLongerRoutingTo(origTag, fromOfferedKey);
+       		// After calling both, THEN tell handler.
             status = code;
             if(status == SUCCESS)
             	successFrom = next;
