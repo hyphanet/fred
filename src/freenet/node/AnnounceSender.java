ffd1472ecf1e4e79c41e99b3638ecca19295b137
Matthew Toseland <toad@amphibian.dyndns.org>
2011-01-29 12:54:42 +0000

    Increase log level.

--- src/freenet/node/AnnounceSender.java
+++ src/freenet/node/AnnounceSender.java
@@ -490,15 +490,15 @@ public class AnnounceSender implements PrioRunnable, ByteCounter {
 				// Okay, just route it.
 			}
 		} catch (FSParseException e) {
-			if(logMINOR) Logger.minor(this, "Rejecting noderef: "+e, e);
+			Logger.warning(this, "Rejecting noderef: "+e, e);
 			OpennetManager.rejectRef(uid, source, DMT.NODEREF_REJECTED_INVALID, this);
 			return false;
 		} catch (PeerParseException e) {
-			if(logMINOR) Logger.minor(this, "Rejecting noderef: "+e, e);
+			Logger.warning(this, "Rejecting noderef: "+e, e);
 			OpennetManager.rejectRef(uid, source, DMT.NODEREF_REJECTED_INVALID, this);
 			return false;
 		} catch (ReferenceSignatureVerificationException e) {
-			if(logMINOR) Logger.minor(this, "Rejecting noderef: "+e, e);
+			Logger.warning(this, "Rejecting noderef: "+e, e);
 			OpennetManager.rejectRef(uid, source, DMT.NODEREF_REJECTED_INVALID, this);
 			return false;
 		} catch (NotConnectedException e) {
