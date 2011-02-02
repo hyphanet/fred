c14f12e60113f27f83920c19a659674a3615cd38
Matthew Toseland <toad@amphibian.dyndns.org>
2011-02-02 22:12:21 +0000

    Fix unit tests.

--- test/freenet/node/NullBasePeerNode.java
+++ test/freenet/node/NullBasePeerNode.java
@@ -222,4 +222,13 @@ public class NullBasePeerNode implements BasePeerNode {
 		return false;
 	}
 
+	public double averagePingTimeCorrected() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void backoffOnResend() {
+		// Ignore
+	}
+
 }
