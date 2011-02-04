f158cd892862e9470e7ae86e7a98bf34c4ae77b8
Matthew Toseland <toad@amphibian.dyndns.org>
2011-02-04 02:07:24 +0000

    Build 1342: Hopefully fix the realtime requests SENDER_DIED (once widely deployed).

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,11 +50,11 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1341;
+	private static final int buildNumber = 1342;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1335;
-	private static final int newLastGoodBuild = 1341;
+	private static final int newLastGoodBuild = 1342;
 	static final long transitionTime;
 
 	static {
