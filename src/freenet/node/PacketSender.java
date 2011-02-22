hyphanet/fred

commit 7bb3fe4cced0042686af8d35928b7f4782f7a2bd

Disconnect properly with sending the message after no acks but have seen incoming messages. Dump the message queue immediately since stuff is not being acked. Javadocs.

src/freenet/node/PacketSender.java
@@ -193,8 +193,10 @@ public class PacketSender implements Runnable {
 					continue;
 				} else if(now - pn.lastReceivedAckTime() > pn.maxTimeBetweenReceivedAcks()) {
 					Logger.normal(this, "Disconnecting from " + pn + " - haven't received acks recently");
-					// Hopefully this is a transient network glitch, but stuff will have already started to timeout, so lets dump the pending messages.
-					pn.disconnected(true, false);
+					// Do it properly.
+					// There appears to be connectivity from them to us but not from us to them.
+					// So it is helpful for them to know that we are disconnecting.
+					node.peers.disconnect(pn, true, true, false, true);
 					continue;
 				} else if(pn.isRoutable() && pn.noLongerRoutable()) {
 					/*

src/freenet/node/PeerManager.java
@@ -531,18 +531,22 @@ public class PeerManager {
 		}
 		addPeer(pn);
 	}
+	
+	public void disconnect(final PeerNode pn, boolean sendDisconnectMessage, final boolean waitForAck, boolean purge) {
+		disconnect(pn, sendDisconnectMessage, waitForAck, purge, false);
+	}
 
 	/**
 	 * Disconnect from a specified node
 	 */
-	public void disconnect(final PeerNode pn, boolean sendDisconnectMessage, final boolean waitForAck, boolean purge) {
+	public void disconnect(final PeerNode pn, boolean sendDisconnectMessage, final boolean waitForAck, boolean purge, boolean dumpMessagesNow) {
 		if(logMINOR)
 			Logger.minor(this, "Disconnecting " + pn.shortToString());
 		synchronized(this) {
 			if(!havePeer(pn))
 				return;
 		}
-		pn.notifyDisconnecting();
+		pn.notifyDisconnecting(dumpMessagesNow);
 		if(sendDisconnectMessage) {
 			Message msg = DMT.createFNPDisconnect(true, purge, -1, new ShortBuffer(new byte[0]));
 			try {

src/freenet/node/PeerNode.java
@@ -1230,7 +1230,7 @@ public abstract class PeerNode implements USKRetrieverCallback, BasePeerNode {
 	public synchronized long timeSinceAddedOrRestarted() {
 		return System.currentTimeMillis() - timeAddedOrRestarted;
 	}
-
+	
 	/**
 	* Disconnected e.g. due to not receiving a packet for ages.
 	* @param dumpMessageQueue If true, clear the messages-to-send queue.
@@ -3935,14 +3935,33 @@ public abstract class PeerNode implements USKRetrieverCallback, BasePeerNode {
 	/** Called when a request or insert succeeds. Used by opennet. */
 	public abstract void onSuccess(boolean insert, boolean ssk);
 
-	/** Called when a delayed disconnect is occurring. Tell the node that it is being disconnected, but
-	 * that the process may take a while. */
-	public void notifyDisconnecting() {
+	/** Called when a delayed disconnect is occurring. Tell the node that it is being 
+	 * disconnected, but that the process may take a while. After this point, requests
+	 * will not be accepted from the peer nor routed to it. 
+	 * @param dumpMessagesNow If true, immediately dump the message queue, since we are
+	 * closing the connection due to some low level trouble e.g. not acknowledging. 
+	 * We will continue to try to send anything already in flight, and it is possible to
+	 * send more messages after this point, for instance the message telling it we are
+	 * disconnecting, but see above - no requests will be routed across this connection. */
+	public void notifyDisconnecting(boolean dumpMessageQueue) {
+		MessageItem[] messagesTellDisconnected = null;
 		synchronized(this) {
 			disconnecting = true;
 			jfkNoncesSent.clear();
+			if(dumpMessageQueue) {
+				// Reset the boot ID so that we get different trackers next time.
+				myBootID = node.fastWeakRandom.nextLong();
+				messagesTellDisconnected = grabQueuedMessageItems();
+			}
 		}
 		setPeerNodeStatus(System.currentTimeMillis());
+		if(messagesTellDisconnected != null) {
+			if(logMINOR)
+				Logger.minor(this, "Messages to dump: "+messagesTellDisconnected.length);
+			for(MessageItem mi : messagesTellDisconnected) {
+				mi.onDisconnect();
+			}
+		}
 	}
 
 	/** Called to cancel a delayed disconnect. Always succeeds even if the node was not being

commit 138ba5e14f26be411998f2ec6974c16d7384f2d3

Fix critical darknet bug introduced in 7bb3fe4cced0042686af8d35928b7f4782f7a2bd: Do not remove peers when disconnecting due to timeout!!!

src/freenet/node/PacketSender.java
@@ -196,7 +196,7 @@ public class PacketSender implements Runnable {
 					// Do it properly.
 					// There appears to be connectivity from them to us but not from us to them.
 					// So it is helpful for them to know that we are disconnecting.
-					node.peers.disconnect(pn, true, true, false, true);
+					node.peers.disconnect(pn, true, true, false, true, false);
 					continue;
 				} else if(pn.isRoutable() && pn.noLongerRoutable()) {
 					/*

src/freenet/node/PeerManager.java
@@ -533,13 +533,20 @@ public class PeerManager {
 	}
 	
 	public void disconnect(final PeerNode pn, boolean sendDisconnectMessage, final boolean waitForAck, boolean purge) {
-		disconnect(pn, sendDisconnectMessage, waitForAck, purge, false);
+		disconnect(pn, sendDisconnectMessage, waitForAck, purge, false, true);
 	}
 
 	/**
 	 * Disconnect from a specified node
+	 * @param sendDisconnectMessage If false, don't send the FNPDisconnected message.
+	 * @param waitForAck If false, don't wait for the ack for the FNPDisconnected message.
+	 * @param purge If true, set the purge flag on the disconnect, causing the other peer
+	 * to purge this node from e.g. its old opennet peers list.
+	 * @param dumpMessagesNow If true, dump queued messages immediately, before the 
+	 * disconnect completes.
+	 * @param remove If true, remove the node from the routing table and tell the peer to do so.
 	 */
-	public void disconnect(final PeerNode pn, boolean sendDisconnectMessage, final boolean waitForAck, boolean purge, boolean dumpMessagesNow) {
+	public void disconnect(final PeerNode pn, boolean sendDisconnectMessage, final boolean waitForAck, boolean purge, boolean dumpMessagesNow, final boolean remove) {
 		if(logMINOR)
 			Logger.minor(this, "Disconnecting " + pn.shortToString());
 		synchronized(this) {
@@ -552,7 +559,7 @@ public class PeerManager {
 			return;
 		}
 		if(sendDisconnectMessage) {
-			Message msg = DMT.createFNPDisconnect(true, purge, -1, new ShortBuffer(new byte[0]));
+			Message msg = DMT.createFNPDisconnect(remove, purge, -1, new ShortBuffer(new byte[0]));
 			try {
 				pn.sendAsync(msg, new AsyncMessageCallback() {
 
@@ -581,13 +588,17 @@ public class PeerManager {
 								return;
 							done = true;
 						}
-						if(removePeer(pn) && !pn.isSeed())
-							writePeers();
+						if(remove) {
+							if(removePeer(pn) && !pn.isSeed())
+								writePeers();
+						}
 					}
 				}, ctrDisconn);
 			} catch(NotConnectedException e) {
-				if(pn.isDisconnecting() && removePeer(pn) && !pn.isSeed())
-					writePeers();
+				if(remove) {
+					if(pn.isDisconnecting() && removePeer(pn) && !pn.isSeed())
+						writePeers();
+				}
 				return;
 			}
                         if(!pn.isSeed()) {
@@ -595,9 +606,11 @@ public class PeerManager {
 
                                 public void run() {
                                     if(pn.isDisconnecting()) {
-                                    	if(removePeer(pn)) {
-                                    		if(!pn.isSeed()) {
-                                    			writePeers();
+                                    	if(remove) {
+                                    		if(removePeer(pn)) {
+                                    			if(!pn.isSeed()) {
+                                    				writePeers();
+                                    			}
                                     		}
                                     	}
                                     	pn.disconnected(true, true);
@@ -605,9 +618,12 @@ public class PeerManager {
                                 }
                             }, Node.MAX_PEER_INACTIVITY);
                         }
-		} else
-			if(removePeer(pn) && !pn.isSeed())
-				writePeers();
+		} else {
+			if(remove) {
+				if(removePeer(pn) && !pn.isSeed())
+					writePeers();
+			}
+		}
 	}
 	final ByteCounter ctrDisconn = new ByteCounter() {
 