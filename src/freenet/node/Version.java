8c4dbe1f53d4c29a90297df4761e2ba5a7a7582f
Matthew Toseland <toad@amphibian.dyndns.org>
2011-02-25 00:43:57 +0000

    Version 4. Logging changes etc.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,7 +50,7 @@ public class Version {
 	public static final String protocolVersion = "TESTNET 1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 3;
+	private static final int buildNumber = 4;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1;
