5dd8db5281196ca186487f8dd11cdd56af567328
Matthew Toseland <toad@amphibian.dyndns.org>
2011-02-25 23:25:11 +0000

    Build 5. Mandatory.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,11 +50,11 @@ public class Version {
 	public static final String protocolVersion = "TESTNET 1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 4;
+	private static final int buildNumber = 5;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1;
-	private static final int newLastGoodBuild = 1;
+	private static final int newLastGoodBuild = 5;
 	static final long transitionTime;
 
 	static {
