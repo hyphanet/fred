11623d6940a08893142eb3bde34b22cecfa742a3
Matthew Toseland <toad@amphibian.dyndns.org>
2011-02-26 00:41:16 +0000

    Build 6: All commands now go down the connection the testnet nodes open to the controller. Ping works, other commands implemented but not tested yet.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,11 +50,11 @@ public class Version {
 	public static final String protocolVersion = "TESTNET 1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 5;
+	private static final int buildNumber = 6;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 1;
-	private static final int newLastGoodBuild = 5;
+	private static final int oldLastGoodBuild = 5;
+	private static final int newLastGoodBuild = 6;
 	static final long transitionTime;
 
 	static {
