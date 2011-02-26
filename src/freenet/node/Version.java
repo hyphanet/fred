30bb4e4a153208d1ed2471ddf619ac8edde71bd1
Matthew Toseland <toad@amphibian.dyndns.org>
2011-02-26 17:43:10 +0000

    Version 9: Fetching logs works, various related fixes, much of it in the logger.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,11 +50,11 @@ public class Version {
 	public static final String protocolVersion = "TESTNET 1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 8;
+	private static final int buildNumber = 9;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 5;
-	private static final int newLastGoodBuild = 8;
+	private static final int newLastGoodBuild = 9;
 	static final long transitionTime;
 
 	static {
