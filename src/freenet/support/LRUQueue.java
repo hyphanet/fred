hyphanet/fred

commit 5b82d0c83fd0cf2f13846a7e2d30d336772f455b

Allow reconnection in some nasty high packet loss cases where the two sides don't agree on whether they are connected.

src/freenet/support/LRUQueue.java
@@ -4,6 +4,8 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
+import freenet.node.OpennetPeerNode;
+
 /**
  * LRU Queue
  * 
@@ -186,6 +188,12 @@ public class LRUQueue<T> {
 	public synchronized void clear() {
 		list.clear();
 		hash.clear();
+	}
+
+	public synchronized T get(T obj) {
+		QItem<T> val = hash.get(obj);
+		if(val == null) return null;
+		return val.obj;
 	}
 }
 

src/freenet/node/AnnounceSender.java
@@ -520,7 +520,8 @@ public class AnnounceSender implements PrioRunnable, ByteCounter {
 		}
 		// If we want it, add it and send it.
 		try {
-			if(om.addNewOpennetNode(fs, ConnectionType.ANNOUNCE) != null) {
+			// Allow reconnection - sometimes one side has the ref and the other side doesn't.
+			if(om.addNewOpennetNode(fs, ConnectionType.ANNOUNCE, true) != null) {
 				sendOurRef(source, om.crypto.myCompressedFullRef());
 			} else {
 				if(logMINOR)

src/freenet/node/Node.java
@@ -5609,7 +5609,7 @@ public class Node implements TimeSkewDetectorCallback {
 	public OpennetPeerNode addNewOpennetNode(SimpleFieldSet fs, ConnectionType connectionType) throws FSParseException, PeerParseException, ReferenceSignatureVerificationException {
 		// FIXME: perhaps this should throw OpennetDisabledExcemption rather than returing false?
 		if(opennet == null) return null;
-		return opennet.addNewOpennetNode(fs, connectionType);
+		return opennet.addNewOpennetNode(fs, connectionType, false);
 	}
 
 	public byte[] getOpennetIdentity() {

src/freenet/node/OpennetManager.java
@@ -326,7 +326,7 @@ public class OpennetManager {
 		return stopping;
 	}
 
-	public OpennetPeerNode addNewOpennetNode(SimpleFieldSet fs, ConnectionType connectionType) throws FSParseException, PeerParseException, ReferenceSignatureVerificationException {
+	public OpennetPeerNode addNewOpennetNode(SimpleFieldSet fs, ConnectionType connectionType, boolean allowExisting) throws FSParseException, PeerParseException, ReferenceSignatureVerificationException {
 		try {
 		OpennetPeerNode pn = new OpennetPeerNode(fs, node, crypto, this, node.peers, false, crypto.packetMangler);
 		if(Arrays.equals(pn.getIdentity(), crypto.myIdentity)) {
@@ -335,7 +335,12 @@ public class OpennetManager {
 		}
 		if(peersLRU.contains(pn)) {
 			if(logMINOR) Logger.minor(this, "Not adding "+pn.userToString()+" to opennet list as already there");
-			return null;
+			if(allowExisting) {
+				// However, we can reconnect.
+				return (OpennetPeerNode) peersLRU.get(pn);
+			} else {
+				return null;
+			}
 		}
 		if(node.isSeednode() && pn.isUnroutableOlderVersion()) {
 			// We can't send the UOM to it, so we should not accept it.

commit b7f8f4e546adc36285eb6a9280f4314054cd2787

Fix import breakage in 5b82d0c83fd0cf2f13846a7e2d30d336772f455b

src/freenet/support/LRUQueue.java
@@ -4,8 +4,6 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
-import freenet.node.OpennetPeerNode;
-
 /**
  * LRU Queue
  * 