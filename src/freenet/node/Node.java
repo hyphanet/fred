25850a1646a75ec35553cc0b422f7192317e7bff
Matthew Toseland <toad@amphibian.dyndns.org>
2011-03-09 17:49:32 +0000

    Logging

--- src/freenet/node/Node.java
+++ src/freenet/node/Node.java
@@ -6063,7 +6063,7 @@ public class Node implements TimeSkewDetectorCallback {
     	    	synchronized(this) {
     	    		completeInsertsTotal++;
     	    		if(wouldHaveStored) {
-    	    			Logger.error(this, "Would have stored but haven't stored");
+    	    			Logger.normal(this, "Would have stored but haven't stored");
     	    			completeInsertsNotStoredWouldHaveStored++;
     	    		}
     	    	}
