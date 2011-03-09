54811a0fd322b242e4c30416b3844939d5a0850a
Matthew Toseland <toad@amphibian.dyndns.org>
2011-03-09 18:24:00 +0000

    Doh, version 23: fix nasty SSK insert stall bug in 22.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,11 +50,11 @@ public class Version {
 	public static final String protocolVersion = "TESTNET 1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 22;
+	private static final int buildNumber = 23;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 20;
-	private static final int newLastGoodBuild = 22;
+	private static final int newLastGoodBuild = 23;
 	static final long transitionTime;
 
 	static {
