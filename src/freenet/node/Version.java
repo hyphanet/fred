d6f06ca267c189bd3f31df79b816a8fecda31818
Matthew Toseland <toad@amphibian.dyndns.org>
2011-03-11 13:39:20 +0000

    Build 25, mandatory tomorrow.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,17 +50,17 @@ public class Version {
 	public static final String protocolVersion = "TESTNET 1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 24;
+	private static final int buildNumber = 25;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 23;
-	private static final int newLastGoodBuild = 24;
+	private static final int oldLastGoodBuild = 24;
+	private static final int newLastGoodBuild = 25;
 	static final long transitionTime;
 
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2011, Calendar.MARCH, 11, 0, 0, 0 );
+		_cal.set( 2011, Calendar.MARCH, 12, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 
