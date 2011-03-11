3a5724ca4020a53abeeb9ee78087dff2533d12bd
Matthew Toseland <toad@amphibian.dyndns.org>
2011-03-11 16:22:02 +0000

    Fix merge, missed one line.

--- src/freenet/node/PeerMessageQueue.java
+++ src/freenet/node/PeerMessageQueue.java
@@ -203,7 +203,6 @@ public class PeerMessageQueue {
 								}
 							} else {
 								assert(list.getParent() == nonEmptyItemsWithID);
-								list.addLast(item);
 							}
 							if(logMINOR) checkOrder();
 						}
