0389a85d60b83eddf70f4059ea3184b80adf675a
Matthew Toseland <toad@amphibian.dyndns.org>
2011-03-15 19:46:06 +0000

    Testnet build 27. Mandatory tomorrow.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,11 +50,11 @@ public class Version {
 	public static final String protocolVersion = "TESTNET 1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 26;
+	private static final int buildNumber = 27;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 25;
-	private static final int newLastGoodBuild = 26;
+	private static final int newLastGoodBuild = 27;
 	static final long transitionTime;
 
 	static {
