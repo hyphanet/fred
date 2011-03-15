95fa46ad104e1cc23386e754bc7edf95510a091b
Matthew Toseland <toad@amphibian.dyndns.org>
2011-03-15 21:19:55 +0000

    Missing file for testnet.

---
+++ src/freenet/node/TestnetCommandType.java
@@ -0,0 +1,16 @@
+package freenet.node;
+
+public enum TestnetCommandType {
+	
+	Close,
+	Ping,
+	GetMyReference,
+	GetConfig,
+	GetConnectionStatusCounts,
+	GetFCPStatsSummary,
+	GetConnections,
+	GetLogsList,
+	GetLog,
+	GetLogFiltered
+
+}
