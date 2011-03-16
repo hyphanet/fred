b79888ad9f9e6d74abdc551680e28613984dca56
Matthew Toseland <toad@amphibian.dyndns.org>
2011-03-16 15:06:51 +0000

    Document.

--- test/freenet/node/NewPacketFormatTest.java
+++ test/freenet/node/NewPacketFormatTest.java
@@ -13,6 +13,8 @@ import junit.framework.TestCase;
 public class NewPacketFormatTest extends TestCase {
 	
 	public void setUp() {
+		// Because we don't call maybeSendPacket, the packet sent times are not updated,
+		// so lets turn off the keepalives.
 		NewPacketFormat.DO_KEEPALIVES = false;
 	}
 	
