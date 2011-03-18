8909f790e3306f82940d700e1a7bf0707e21ff9f
Matthew Toseland <toad@amphibian.dyndns.org>
2011-03-18 00:43:22 +0000

    Build 31. Mandatory on the 19th.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,17 +50,17 @@ public class Version {
 	public static final String protocolVersion = "TESTNET 1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 30;
+	private static final int buildNumber = 31;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 30;
-	private static final int newLastGoodBuild = 30;
+	private static final int newLastGoodBuild = 31;
 	static final long transitionTime;
 
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2011, Calendar.MARCH, 18, 0, 0, 0 );
+		_cal.set( 2011, Calendar.MARCH, 19, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 
