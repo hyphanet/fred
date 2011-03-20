fdffe2c3cac310084c23ebbaaeaf1cb93fc70f2d
Juiceman <juiceman69@gmail.com>
2011-03-20 02:15:24 -0400

    Resolve https://bugs.freenetproject.org/view.php?id=4420 Added some checks for "Oracle " in the places we looked for "Sun " and edited the I10n files to change "Sun" into "Sun/Oracle"  Changing straight over to Oracle instead of Sun/Oracle is bound to confuse folks.

--- src/freenet/support/HexUtil.java
+++ src/freenet/support/HexUtil.java
@@ -226,7 +226,7 @@ public class HexUtil {
 
     /**
      * Turn a BigInteger into a hex string.
-     * BigInteger.toString(16) NPEs on Sun JDK 1.4.2_05. :<
+     * BigInteger.toString(16) NPEs on Sun/Oracle JDK 1.4.2_05. :<
      * The bugs in their Big* are getting seriously irritating...
      */
     public static String biToHex(BigInteger bi) {
