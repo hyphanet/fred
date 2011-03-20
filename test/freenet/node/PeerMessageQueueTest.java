1cb90f76cfe3f8a04de84bf4b0ec7a73e95ddde5
Martin Nyhus <martin.nyhus@gmx.com>
2011-03-20 22:05:48 +0100

    Rewrite test to cover a more general case
    Rewrite the regression test to cover the general case where two elements
    are added in the "wrong" order, ie. the least urgent first.

--- test/freenet/node/PeerMessageQueueTest.java
+++ test/freenet/node/PeerMessageQueueTest.java
@@ -25,40 +25,35 @@ public class PeerMessageQueueTest extends TestCase {
 		}
 	}
 
-	/* Regression test for the bug that was fixed in commit 88d2c14a8958b19b6144eaa0deeb3b0ecbd51957:
-	 *
-	 * getUrgentTime() would ignore all but the first item in the nonEmptyItemsWithID queue, which
-	 * meant that after getting a message from the queue and putting it back again, eg. because we
-	 * couldn't send it, getUrgentTime would return a value as if the message wasn't on the queue
-	 * (unless it was the only message).
-	 */
-	public void testUrgentTimeAfterRequeuingMessage() {
+	/* Test that getNextUrgentTime() returns the correct value, even when the items on the queue
+	 * aren't ordered by their timeout value, eg. when an item was readded because we couldn't send
+	 * it. */
+	public void testUrgentTimeQueuedWrong() {
 		PeerMessageQueue pmq = new PeerMessageQueue(null);
 
-		MessageItem item1 = new MessageItem(new byte[1024], null, false, null, (short) 0, false, false);
-		pmq.queueAndEstimateSize(item1, 1024);
+		//Constructor might take some time, so grab a range
+		long start = System.currentTimeMillis();
+		MessageItem itemUrgent = new MessageItem(new byte[1024], null, false, null, (short) 0, false, false);
+		long end = System.currentTimeMillis();
 
-		//Sleep for a little while to make sure the next item will be less urgent, just in case the
-		//above took 0ms
+		//Sleep for a little while to get a later timeout
 		try {
 			Thread.sleep(1);
 		} catch (InterruptedException e) {
 
 		}
 
-		MessageItem item2 = new MessageItem(new byte[1024], null, false, null, (short) 0, false, false);
-		pmq.queueAndEstimateSize(item2, 1024);
+		MessageItem itemNonUrgent = new MessageItem(new byte[1024], null, false, null, (short) 0, false, false);
 
-		//The urgent time before reordering the queue, this is the value it should return afterwards
+		//Queue the least urgent item first to get the wrong order
+		pmq.queueAndEstimateSize(itemNonUrgent, 1024);
+		pmq.queueAndEstimateSize(itemUrgent, 1024);
+
+		//getNextUrgentTime() should return the timeout of itemUrgent, which is within (start + 100)
+		//and (end + 100)
 		long urgentTime = pmq.getNextUrgentTime(Long.MAX_VALUE, System.currentTimeMillis());
-
-		//Reorder the queue to put the most urgent item at the end
-		MessageItem item = pmq.grabQueuedMessageItem(0);
-		pmq.queueAndEstimateSize(item, 1024);
-
-		long urgentTimeAfter = pmq.getNextUrgentTime(Long.MAX_VALUE, System.currentTimeMillis());
-
-		//Urgent time after reordering should be the same as it was before
-		assertEquals(urgentTime, urgentTimeAfter);
+		if(!((urgentTime >= (start + 100)) && (urgentTime <= (end + 100)))) {
+			fail("Timeout not in expected range. Expected: " + (start + 100) + "->" + (end + 100) + ", actual: " + urgentTime);
+		}
 	}
 }
