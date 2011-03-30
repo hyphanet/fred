f0fcaa2725e3635f3409b69fbb937a23cdc18e74
Matthew Toseland <toad@amphibian.dyndns.org>
2011-03-30 16:22:25 +0100

    Testnet Build 35.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,17 +50,17 @@ public class Version {
 	public static final String protocolVersion = "TESTNET 1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 34;
+	private static final int buildNumber = 35;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 33;
-	private static final int newLastGoodBuild = 34;
+	private static final int oldLastGoodBuild = 34;
+	private static final int newLastGoodBuild = 35;
 	static final long transitionTime;
 
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2011, Calendar.MARCH, 31, 0, 0, 0 );
+		_cal.set( 2011, Calendar.APRIL, 1, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 
