a7b3f87f2163230e4c520072a5de13765838c278
Matthew Toseland <toad@amphibian.dyndns.org>
2011-04-05 13:02:11 +0100

    Build 1364, from master. Mandatory in a week.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,17 +50,17 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1363;
+	private static final int buildNumber = 1364;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 1356;
-	private static final int newLastGoodBuild = 1358;
+	private static final int oldLastGoodBuild = 1358;
+	private static final int newLastGoodBuild = 1364;
 	static final long transitionTime;
 
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2011, Calendar.MARCH, 21, 0, 0, 0 );
+		_cal.set( 2011, Calendar.APRIL, 11, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 
