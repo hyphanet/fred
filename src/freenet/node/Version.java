ba82a7bc5a77f13ec04b3f443ba0c71bad53a036
Matthew Toseland <toad@amphibian.dyndns.org>
2011-04-20 20:16:32 +0100

    Build 43, mandatory tomorrow. More RecentlyFailed fixes merged from recentlyfailed branch on fred. These may need to be deployed soon as they may be exploitable; in any case RecentlyFailed is good.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,11 +50,11 @@ public class Version {
 	public static final String protocolVersion = "TESTNET 1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 42;
+	private static final int buildNumber = 43;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 41;
-	private static final int newLastGoodBuild = 42;
+	private static final int newLastGoodBuild = 43;
 	static final long transitionTime;
 
 	static {
