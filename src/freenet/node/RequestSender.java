c0306875b4a6f7a155ef7d675563eeecc12c2949
Matthew Toseland <toad@amphibian.dyndns.org>
2011-04-20 19:25:01 +0100

    RecentlyFailed fix: Don't allow timeLeft to be less than 0.

--- src/freenet/node/RequestSender.java
+++ src/freenet/node/RequestSender.java
@@ -1461,6 +1461,8 @@ loadWaiterLoop:
 		// Subtract 1% for good measure / to compensate for dodgy clocks
 		timeLeft -= origTimeLeft / 100;
 		
+		if(timeLeft < 0) timeLeft = 0;
+		
 		//Store the timeleft so that the requestHandler can get at it.
 		synchronized(this) {
 			recentlyFailedTimeLeft = timeLeft;
