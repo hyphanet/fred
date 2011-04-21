9652292e280de8208ed74de22db0c09ebf2cfae9
Matthew Toseland <toad@amphibian.dyndns.org>
2011-04-21 16:33:30 +0100

    Testnet build 44. Mandatory tomorrow.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,17 +50,17 @@ public class Version {
 	public static final String protocolVersion = "TESTNET 1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 43;
+	private static final int buildNumber = 44;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 41;
-	private static final int newLastGoodBuild = 43;
+	private static final int oldLastGoodBuild = 43;
+	private static final int newLastGoodBuild = 44;
 	static final long transitionTime;
 
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2011, Calendar.APRIL, 21, 0, 0, 0 );
+		_cal.set( 2011, Calendar.APRIL, 22, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 
