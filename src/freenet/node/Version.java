22a2f843c6a4600032d6a396ecfc1c5a548252db
Matthew Toseland <toad@amphibian.dyndns.org>
2011-04-22 20:39:03 +0100

    1366. Mandatory 28th April. Short changelog: configurable USK polling frequency/number of fetches; minor database defrag changes; russian update; messages fixes; very small RecentlyFailed/ULPR-related fixes; hint-fetch-with-callback method for plugins; minor web interface and bookmarks fixes; fix nasty infinite loop in LineReadingInputStream

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,17 +50,17 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1365;
+	private static final int buildNumber = 1366;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 1358;
-	private static final int newLastGoodBuild = 1364;
+	private static final int oldLastGoodBuild = 1364;
+	private static final int newLastGoodBuild = 1366;
 	static final long transitionTime;
 
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2011, Calendar.APRIL, 11, 0, 0, 0 );
+		_cal.set( 2011, Calendar.APRIL, 28, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 
