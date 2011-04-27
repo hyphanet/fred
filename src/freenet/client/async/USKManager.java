bd0be3c63bee4631e5407d9168d9289b1a30dcb7
Matthew Toseland <toad@amphibian.dyndns.org>
2011-04-27 20:21:44 +0100

    Might as well accept USKs here too.

--- src/freenet/client/async/USKManager.java
+++ src/freenet/client/async/USKManager.java
@@ -719,16 +719,21 @@ public class USKManager {
 			ObjectContainer container, boolean isMetadata) {
 		try {
 			if(persistent) container.activate(uri, 5);
+			FreenetURI uu;
 			if(uri.isSSK() && uri.isSSKForUSK()) {
-				FreenetURI uu = uri.setMetaString(null).uskForSSK();
-				USK usk = USK.create(uu);
-				if(!isMetadata)
-					context.uskManager.updateKnownGood(usk, uu.getSuggestedEdition(), context);
-				else
-					// We don't know whether the metadata is fetchable.
-					// FIXME add a callback so if the rest of the request completes we updateKnownGood().
-					context.uskManager.updateSlot(usk, uu.getSuggestedEdition(), context);
+				uu = uri.setMetaString(null).uskForSSK();
+			} else if(uri.isUSK()) {
+				uu = uri;
+			} else {
+				return;
 			}
+			USK usk = USK.create(uu);
+			if(!isMetadata)
+				context.uskManager.updateKnownGood(usk, uu.getSuggestedEdition(), context);
+			else
+				// We don't know whether the metadata is fetchable.
+				// FIXME add a callback so if the rest of the request completes we updateKnownGood().
+				context.uskManager.updateSlot(usk, uu.getSuggestedEdition(), context);
 		} catch (MalformedURLException e) {
 			Logger.error(this, "Caught "+e, e);
 		} catch (Throwable t) {
