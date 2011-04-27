2cf6d077302a7e2bf856a144cf9aa16e620c87be
Matthew Toseland <toad@amphibian.dyndns.org>
2011-04-27 22:52:37 +0100

    Logging, check the flag.

--- src/freenet/io/comm/Message.java
+++ src/freenet/io/comm/Message.java
@@ -137,11 +137,11 @@ public class Message {
 		    }
 		} catch (EOFException e) {
 			String msg = peer.getPeer()+" sent a message packet that ends prematurely while deserialising "+mspec.getName();
-			if(inSubMessage)
-				Logger.minor(Message.class, msg+" in sub-message", e);
-			else if(mspec.getName().startsWith("FNPPeerLoadStatus"))
-				Logger.minor(Message.class, msg, e);
-			else if(mspec.getName().startsWith("FNPPeerLoadStatus"))
+			if(inSubMessage) {
+				if(logMINOR) Logger.minor(Message.class, msg+" in sub-message", e);
+			} else if(mspec.getName().startsWith("FNPPeerLoadStatus")) {
+				if(logMINOR) Logger.minor(Message.class, msg, e);
+			} else if(mspec.getName().startsWith("FNPPeerLoadStatus"))
 				Logger.warning(Message.class, msg, e); // FIXME remove this after all the old builds have gone away
 			else
 				Logger.error(Message.class, msg, e);
