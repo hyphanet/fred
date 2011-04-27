c8ca2ff3cf6270a408e5a8bd9da2983f0310ada4
Matthew Toseland <toad@amphibian.dyndns.org>
2011-04-27 22:49:48 +0100

    Remove obsolete back compat code.

--- src/freenet/io/comm/Message.java
+++ src/freenet/io/comm/Message.java
@@ -139,7 +139,7 @@ public class Message {
 			String msg = peer.getPeer()+" sent a message packet that ends prematurely while deserialising "+mspec.getName();
 			if(inSubMessage ||
 					// FIXME remove this part of the condition
-					(mspec.getName().startsWith("FNPPeerLoadStatus") && Version.lastGoodBuild() < 1365))
+					(mspec.getName().startsWith("FNPPeerLoadStatus")))
 				Logger.minor(Message.class, msg+" in sub-message", e);
 			else
 				Logger.error(Message.class, msg, e);
