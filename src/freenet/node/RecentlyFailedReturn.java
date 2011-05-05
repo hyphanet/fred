02b78bfca52ec43a6d92d989bc58162c2ff476ae
Matthew Toseland <toad@amphibian.dyndns.org>
2011-05-05 15:52:45 +0100

    Logging and logging infrastructure.

--- src/freenet/node/RecentlyFailedReturn.java
+++ src/freenet/node/RecentlyFailedReturn.java
@@ -1,13 +1,22 @@
 package freenet.node;
 
+import freenet.support.Logger;
+import freenet.support.TimeUtil;
+
 /** Contains information on why we can't route a request. Initially just a flag
  * and a time. */
 public class RecentlyFailedReturn {
+	
+    private static volatile boolean logMINOR;
+    static {
+        Logger.registerClass(PeerManager.class);
+    }
 	
 	private boolean recentlyFailed;
 	private long wakeup;
 	private int countWaiting;
 	public synchronized void fail(int countWaiting, long wakeupTime) {
+ 		if(logMINOR) Logger.minor(this, "RecentlyFailed until "+TimeUtil.formatTime(wakeupTime - System.currentTimeMillis()));
 		this.countWaiting = countWaiting;
 		this.wakeup = wakeupTime;
 		this.recentlyFailed = true;
