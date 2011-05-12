06194f5e6d6061752a325cdb71c8c9419d3d3a9b
Matthew Toseland <toad@amphibian.dyndns.org>
2011-05-12 19:39:45 +0100

    1374. Mandatory on the 17th.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,11 +50,11 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1373;
+	private static final int buildNumber = 1374;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1372;
-	private static final int newLastGoodBuild = 1373;
+	private static final int newLastGoodBuild = 1374;
 	static final long transitionTime;
 
 	static {
