2ebd4ae236c9011330a595135452f16c2667dd56
Matthew Toseland <toad@amphibian.dyndns.org>
2011-05-13 15:57:29 +0100

    Remove redundant && condition

--- src/freenet/node/RequestSender.java
+++ src/freenet/node/RequestSender.java
@@ -2480,7 +2480,7 @@ loadWaiterLoop:
 			synchronized(this) {
 				status = this.status;
 			}
-			if (status!=NOT_FINISHED && sentFinished)
+			if (status!=NOT_FINISHED)
 				l.onRequestSenderFinished(status, sentFinishedFromOfferedKey);
 		}
 	}
