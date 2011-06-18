da871e0bed293966774302b9264542e7213a7bcf
Matthew Toseland <toad@amphibian.dyndns.org>
2011-06-18 23:01:29 +0100

    Fix build: This import was needed after all.

--- src/freenet/node/updater/RevocationChecker.java
+++ src/freenet/node/updater/RevocationChecker.java
@@ -23,6 +23,7 @@ import freenet.support.io.ArrayBucket;
 import freenet.support.io.BucketTools;
 import freenet.support.io.ByteArrayRandomAccessThing;
 import freenet.support.io.FileBucket;
+import freenet.support.io.FileUtil;
 import freenet.support.io.RandomAccessFileWrapper;
 import freenet.support.io.RandomAccessThing;
 
