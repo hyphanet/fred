hyphanet/fred

commit b860e68647fc333c95a02f9d20bd8d2782aa1537

Always put Global on FCP messages even if false, simplify code: use put not putSingle. [PATCH 03/22] Cosmetics: putSingle(Integer.toString()) ==> put()
Pure cosmetics:
s/putSingle\(("[^"]*",\s*\)\(Integer\|Boolean\|Short\|Long\|Float\|Double\)\.toString(\([^)]*\)));/put\1\3);/g

Potentially user-visible-changes:
s/if(global)\s*\(\S.*\.put\).*/\1("Global", global);/g
This will add Global=false for some FCP messages that was lacking it before.

src/freenet/node/fcp/NodeHelloMessage.java
@@ -43,7 +43,7 @@ public class NodeHelloMessage extends FCPMessage {
 		sfs.putSingle("Revision", Version.cvsRevision());
 		sfs.put("ExtBuild", NodeStarter.extBuildNumber);
 		sfs.putSingle("ExtRevision", NodeStarter.extRevisionNumber);
-		sfs.put("Testnet", node.isTestnetEnabled());
+		sfs.put("Testnet", node == null ? false : node.isTestnetEnabled());
 		sfs.putSingle("CompressionCodecs", Compressor.COMPRESSOR_TYPE.getHelloCompressorDescriptor());
 		sfs.putSingle("ConnectionIdentifier", id);
 		sfs.putSingle("NodeLanguage", NodeL10n.getBase().getSelectedLanguage().toString());

src/freenet/node/NodeARKInserter.java
@@ -233,7 +233,7 @@ public class NodeARKInserter implements ClientPutCallback, RequestClient {
 				node.writeNodeFile();
 			// We'll broadcast the new ARK edition to our connected peers via a differential node reference
 			SimpleFieldSet fs = new SimpleFieldSet(true);
-			fs.putSingle("ark.number", Long.toString(crypto.myARKNumber));
+			fs.put("ark.number", crypto.myARKNumber);
 			node.peers.locallyBroadcastDiffNodeRef(fs, !crypto.isOpennet, crypto.isOpennet);
 		}
 	}

src/freenet/node/NodeStats.java
@@ -1809,12 +1809,12 @@ public class NodeStats implements Persistable, BlockTimeCallback {
 		double swaps = node.getSwaps();
 		double noSwaps = node.getNoSwaps();
 		double numberOfRemotePeerLocationsSeenInSwaps = node.getNumberOfRemotePeerLocationsSeenInSwaps();
-		fs.putSingle("numberOfRemotePeerLocationsSeenInSwaps", Double.toString(numberOfRemotePeerLocationsSeenInSwaps));
+		fs.put("numberOfRemotePeerLocationsSeenInSwaps", numberOfRemotePeerLocationsSeenInSwaps);
 		double avgConnectedPeersPerNode = 0.0;
 		if ((numberOfRemotePeerLocationsSeenInSwaps > 0.0) && ((swaps > 0.0) || (noSwaps > 0.0))) {
 			avgConnectedPeersPerNode = numberOfRemotePeerLocationsSeenInSwaps/(swaps+noSwaps);
 		}
-		fs.putSingle("avgConnectedPeersPerNode", Double.toString(avgConnectedPeersPerNode));
+		fs.put("avgConnectedPeersPerNode", avgConnectedPeersPerNode);
 
 		int startedSwaps = node.getStartedSwaps();
 		int swapsRejectedAlreadyLocked = node.getSwapsRejectedAlreadyLocked();

src/freenet/node/PeerNode.java
@@ -3029,21 +3029,19 @@ public abstract class PeerNode implements USKRetrieverCallback, BasePeerNode {
 		if(detectedPeer != null)
 			fs.putSingle("detected.udp", detectedPeer.toStringPrefNumeric());
 		if(lastReceivedPacketTime() > 0)
-			fs.putSingle("timeLastReceivedPacket", Long.toString(timeLastReceivedPacket));
-		if(lastReceivedAckTime() > 0)
-			fs.putSingle("timeLastReceivedAck", Long.toString(timeLastReceivedAck));
+			fs.put("timeLastReceivedPacket", timeLastReceivedPacket);
 		if(timeLastConnected() > 0)
-			fs.putSingle("timeLastConnected", Long.toString(timeLastConnected));
+			fs.put("timeLastConnected", timeLastConnected);
 		if(timeLastRoutable() > 0)
-			fs.putSingle("timeLastRoutable", Long.toString(timeLastRoutable));
+			fs.put("timeLastRoutable", timeLastRoutable);
 		if(getPeerAddedTime() > 0 && shouldExportPeerAddedTime())
-			fs.putSingle("peerAddedTime", Long.toString(peerAddedTime));
+			fs.put("peerAddedTime", peerAddedTime);
 		if(neverConnected)
 			fs.putSingle("neverConnected", "true");
 		if(hadRoutableConnectionCount > 0)
-			fs.putSingle("hadRoutableConnectionCount", Long.toString(hadRoutableConnectionCount));
+			fs.put("hadRoutableConnectionCount", hadRoutableConnectionCount);
 		if(routableConnectionCheckCount > 0)
-			fs.putSingle("routableConnectionCheckCount", Long.toString(routableConnectionCheckCount));
+			fs.put("routableConnectionCheckCount", routableConnectionCheckCount);
 		if(currentPeersLocation != null)
 			fs.put("peersLocation", currentPeersLocation);
 		return fs;
@@ -3059,22 +3057,22 @@ public abstract class PeerNode implements USKRetrieverCallback, BasePeerNode {
 		SimpleFieldSet fs = new SimpleFieldSet(true);
 		long now = System.currentTimeMillis();
 		synchronized(this) {
-			fs.putSingle("averagePingTime", Double.toString(averagePingTime()));
+			fs.put("averagePingTime", averagePingTime());
 			long idle = now - lastReceivedPacketTime();
 			if(idle > (60 * 1000) && -1 != lastReceivedPacketTime())  // 1 minute
 
-				fs.putSingle("idle", Long.toString(idle));
+				fs.put("idle", idle);
 			if(peerAddedTime > 1)
-				fs.putSingle("peerAddedTime", Long.toString(peerAddedTime));
+				fs.put("peerAddedTime", peerAddedTime);
 			fs.putSingle("lastRoutingBackoffReasonRT", lastRoutingBackoffReasonRT);
 			fs.putSingle("lastRoutingBackoffReasonBulk", lastRoutingBackoffReasonBulk);
-			fs.putSingle("routingBackoffPercent", Double.toString(backedOffPercent.currentValue() * 100));
-			fs.putSingle("routingBackoffRT", Long.toString((Math.max(Math.max(routingBackedOffUntilRT, transferBackedOffUntilRT) - now, 0))));
-			fs.putSingle("routingBackoffBulk", Long.toString((Math.max(Math.max(routingBackedOffUntilBulk, transferBackedOffUntilBulk) - now, 0))));
-			fs.putSingle("routingBackoffLengthRT", Integer.toString(routingBackoffLengthRT));
-			fs.putSingle("routingBackoffLengthBulk", Integer.toString(routingBackoffLengthBulk));
-			fs.putSingle("overloadProbability", Double.toString(getPRejected() * 100));
-			fs.putSingle("percentTimeRoutableConnection", Double.toString(getPercentTimeRoutableConnection() * 100));
+			fs.put("routingBackoffPercent", backedOffPercent.currentValue() * 100);
+			fs.put("routingBackoffRT", Math.max(Math.max(routingBackedOffUntilRT, transferBackedOffUntilRT) - now, 0));
+			fs.put("routingBackoffBulk", Math.max(Math.max(routingBackedOffUntilBulk, transferBackedOffUntilBulk) - now, 0));
+			fs.put("routingBackoffLengthRT", routingBackoffLengthRT);
+			fs.put("routingBackoffLengthBulk", routingBackoffLengthBulk);
+			fs.put("overloadProbability", getPRejected() * 100);
+			fs.put("percentTimeRoutableConnection", getPercentTimeRoutableConnection() * 100);
 		}
 		fs.putSingle("status", getPeerNodeStatusString());
 		return fs;
@@ -3091,8 +3089,8 @@ public abstract class PeerNode implements USKRetrieverCallback, BasePeerNode {
 			fs.putAppend("physical.udp", nominalPeer.get(i).toString());
 		fs.put("auth.negTypes", negTypes);
 		fs.putSingle("identity", getIdentityString());
-		fs.putSingle("location", Double.toString(currentLocation));
-		fs.putSingle("testnet", Boolean.toString(testnetEnabled));
+		fs.put("location", currentLocation);
+		fs.put("testnet", testnetEnabled);
 		fs.putSingle("version", version);
 		if(peerCryptoGroup != null)
 			fs.put("dsaGroup", peerCryptoGroup.asFieldSet());
@@ -3100,7 +3098,7 @@ public abstract class PeerNode implements USKRetrieverCallback, BasePeerNode {
 			fs.put("dsaPubKey", peerPubKey.asFieldSet());
 		if(myARK != null) {
 			// Decrement it because we keep the number we would like to fetch, not the last one fetched.
-			fs.putSingle("ark.number", Long.toString(myARK.suggestedEdition - 1));
+			fs.put("ark.number", myARK.suggestedEdition - 1);
 			fs.putSingle("ark.pubURI", myARK.getBaseSSK().toString(false, false));
 		}
 		fs.put("opennet", isOpennet());

src/freenet/node/fcp/AllDataMessage.java
@@ -42,7 +42,7 @@ public class AllDataMessage extends DataCarryingMessage {
 		SimpleFieldSet fs = new SimpleFieldSet(true);
 		fs.put("DataLength", dataLength);
 		fs.putSingle("Identifier", identifier);
-		if(global) fs.putSingle("Global", "true");
+		fs.put("Global", global);
 		fs.put("StartupTime", startupTime);
 		fs.put("CompletionTime", completionTime);
 		if(mimeType!=null) fs.putSingle("Metadata.ContentType", mimeType);

src/freenet/node/fcp/ClientGetMessage.java
@@ -220,16 +220,16 @@ public class ClientGetMessage extends FCPMessage {
 	@Override
 	public SimpleFieldSet getFieldSet() {
 		SimpleFieldSet fs = new SimpleFieldSet(true);
-		fs.putSingle("IgnoreDS", Boolean.toString(ignoreDS));
+		fs.put("IgnoreDS", ignoreDS);
 		fs.putSingle("URI", uri.toString(false, false));
-		fs.putSingle("FilterData", Boolean.toString(filterData));
+		fs.put("FilterData", filterData);
 		fs.putSingle("Charset", charset);
 		fs.putSingle("Identifier", identifier);
-		fs.putSingle("Verbosity", Integer.toString(verbosity));
+		fs.put("Verbosity", verbosity);
 		fs.putSingle("ReturnType", getReturnTypeString());
-		fs.putSingle("MaxSize", Long.toString(maxSize));
-		fs.putSingle("MaxTempSize", Long.toString(maxTempSize));
-		fs.putSingle("MaxRetries", Integer.toString(maxRetries));
+		fs.put("MaxSize", maxSize);
+		fs.put("MaxTempSize", maxTempSize);
+		fs.put("MaxRetries", maxRetries);
 		fs.put("BinaryBlob", binaryBlob);
 		return fs;
 	}

src/freenet/node/fcp/ClientPutDirMessage.java
@@ -174,16 +174,16 @@ public abstract class ClientPutDirMessage extends BaseDataCarryingMessage {
 		SimpleFieldSet sfs = new SimpleFieldSet(true);
 		sfs.putSingle("URI", uri.toString());
 		sfs.putSingle("Identifier", identifier);
-		sfs.putSingle("Verbosity", Integer.toString(verbosity));
-		sfs.putSingle("MaxRetries", Integer.toString(maxRetries));
+		sfs.put("Verbosity", verbosity);
+		sfs.put("MaxRetries", maxRetries);
 		sfs.putSingle("ClientToken", clientToken);
-		sfs.putSingle("GetCHKOnly", Boolean.toString(getCHKOnly));
-		sfs.putSingle("PriorityClass", Short.toString(priorityClass));
+		sfs.put("GetCHKOnly", getCHKOnly);
+		sfs.put("PriorityClass", priorityClass);
 		sfs.putSingle("PersistenceType", ClientRequest.persistenceTypeString(persistenceType));
-		sfs.putSingle("DontCompress", Boolean.toString(dontCompress));
+		sfs.put("DontCompress", dontCompress);
 		if (compressorDescriptor != null)
 			sfs.putSingle("Codecs", compressorDescriptor);
-		sfs.putSingle("Global", Boolean.toString(global));
+		sfs.put("Global", global);
 		sfs.putSingle("DefaultName", defaultName);
 		return sfs;
 	}

src/freenet/node/fcp/ClientPutMessage.java
@@ -280,28 +280,28 @@ public class ClientPutMessage extends DataCarryingMessage {
 		SimpleFieldSet sfs = new SimpleFieldSet(true);
 		sfs.putSingle("URI", uri.toString());
 		sfs.putSingle("Identifier", identifier);
-		sfs.putSingle("Verbosity", Integer.toString(verbosity));
-		sfs.putSingle("MaxRetries", Integer.toString(maxRetries));
+		sfs.put("Verbosity", verbosity);
+		sfs.put("MaxRetries", maxRetries);
 		sfs.putSingle("Metadata.ContentType", contentType);
 		sfs.putSingle("ClientToken", clientToken);
 		if(uploadFromType == UPLOAD_FROM_DIRECT) {
 			sfs.putSingle("UploadFrom", "direct");
-			sfs.putSingle("DataLength", Long.toString(dataLength));
+			sfs.put("DataLength", dataLength);
 		} else if(uploadFromType == UPLOAD_FROM_DISK) {
 			sfs.putSingle("UploadFrom", "disk");
 			sfs.putSingle("Filename", origFilename.getAbsolutePath());
-			sfs.putSingle("DataLength", Long.toString(dataLength));
+			sfs.put("DataLength", dataLength);
 		} else if(uploadFromType == UPLOAD_FROM_REDIRECT) {
 			sfs.putSingle("UploadFrom", "redirect");
 			sfs.putSingle("TargetURI", redirectTarget.toString());
 		}
-		sfs.putSingle("GetCHKOnly", Boolean.toString(getCHKOnly));
-		sfs.putSingle("PriorityClass", Short.toString(priorityClass));
+		sfs.put("GetCHKOnly", getCHKOnly);
+		sfs.put("PriorityClass", priorityClass);
 		sfs.putSingle("PersistenceType", ClientRequest.persistenceTypeString(persistenceType));
-		sfs.putSingle("DontCompress", Boolean.toString(dontCompress));
+		sfs.put("DontCompress", dontCompress);
 		if (compressorDescriptor != null)
 			sfs.putSingle("Codecs", compressorDescriptor);
-		sfs.putSingle("Global", Boolean.toString(global));
+		sfs.put("Global", global);
 		sfs.put("BinaryBlob", binaryBlob);
 		return sfs;
 	}

commit 3cd272b8c6992d2c0d018329640f1dd8bd0c50a2

Not safe to assume not testnet if node not exist. Fix this bug introduced in b860e68647fc333c95a02f9d20bd8d2782aa1537.

src/freenet/node/fcp/NodeHelloMessage.java
@@ -43,7 +43,7 @@ public class NodeHelloMessage extends FCPMessage {
 		sfs.putSingle("Revision", Version.cvsRevision());
 		sfs.put("ExtBuild", NodeStarter.extBuildNumber);
 		sfs.putSingle("ExtRevision", NodeStarter.extRevisionNumber);
-		sfs.put("Testnet", node == null ? false : node.isTestnetEnabled());
+		sfs.put("Testnet", node.isTestnetEnabled());
 		sfs.putSingle("CompressionCodecs", Compressor.COMPRESSOR_TYPE.getHelloCompressorDescriptor());
 		sfs.putSingle("ConnectionIdentifier", id);
 		sfs.putSingle("NodeLanguage", NodeL10n.getBase().getSelectedLanguage().toString());