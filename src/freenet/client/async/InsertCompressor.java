hyphanet/fred

commit 75a8a34764b9195e1e43a6469c1011351231d34d

[PATCH 04/22] Fix not computing hashes when first compressor throw'n We no longer need first compressor to fully read source now, so drop MaxSize assignment.

src/freenet/client/async/InsertCompressor.java
@@ -174,9 +174,7 @@ public class InsertCompressor implements CompressJob {
 						if(first && generateHashes != 0) {
 							if(logMINOR) Logger.minor(this, "Generating hashes: "+generateHashes);
 							is = hasher = new MultiHashInputStream(is, generateHashes);
-							maxOutputSize = Long.MAX_VALUE; // Want to run it to the end anyway to get hashes. Fortunately the first hasher is always the fastest.
 						}
-						first = false;
 						try {
 							comp.compress(is, os, origSize, maxOutputSize);
 						} catch (RuntimeException e) {
@@ -185,12 +183,16 @@ public class InsertCompressor implements CompressJob {
 							// Try the next one
 							continue;
 						}
+						if(hasher != null) {
+							is.skip(Long.MAX_VALUE);
+							hashes = hasher.getResults();
+							first = false;
+						}
 					} finally {
 						Closer.close(is);
 						Closer.close(os);
 					}
 					long resultSize = result.size();
-					if(hasher != null) hashes = hasher.getResults();
 					// minSize is {SSKBlock,CHKBlock}.MAX_COMPRESSED_DATA_LENGTH
 					if(resultSize <= minSize) {
 						if(logMINOR)

commit fb1b4d1ac59791ea3f8bf86b9e93224ca4f37a35

Fix not generating hashes on uncompressible data, bug was introduced in 75a8a34764b9195e1e43a6469c1011351231d34d.

src/freenet/client/async/InsertCompressor.java
@@ -181,10 +181,17 @@ public class InsertCompressor implements CompressJob {
 							// ArithmeticException has been seen in bzip2 codec.
 							Logger.error(this, "Compression failed with codec "+comp+" : "+e, e);
 							// Try the next one
+							// RuntimeException is iffy, so lets not try the hasher.
 							continue;
+						} catch (CompressionOutputSizeException e) {
+							if(hasher != null) {
+								is.skip(Long.MAX_VALUE);
+								hashes = hasher.getResults();
+								first = false;
+							}
+							continue; // try next compressor type
 						}
 						if(hasher != null) {
-							is.skip(Long.MAX_VALUE);
 							hashes = hasher.getResults();
 							first = false;
 						}
@@ -217,8 +224,6 @@ public class InsertCompressor implements CompressJob {
 						bestCodec = comp;
 						shouldFreeOnFinally = false;
 					}
-				} catch(CompressionOutputSizeException e) {
-					continue;       // try next compressor type
 				} catch (DatabaseDisabledException e) {
 					Logger.error(this, "Database disabled compressing data", new Exception("error"));
 					shouldFreeOnFinally = true;