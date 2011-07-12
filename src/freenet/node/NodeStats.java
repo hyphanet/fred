16a2f598d76567c27dd88c9e61ee19655e52be7f
Matthew Toseland <toad@amphibian.dyndns.org>
2011-07-12 13:25:59 +0100

    Cosmetic improvement to new stats.

--- src/freenet/node/NodeStats.java
+++ src/freenet/node/NodeStats.java
@@ -3552,13 +3552,12 @@ public class NodeStats implements Persistable, BlockTimeCallback {
 		synchronized(slotTimeoutsSync) {
 			if(fatalTimeoutsInWaitLocal + fatalTimeoutsInWaitRemote + 
 					allocatedSlotLocal + allocatedSlotRemote > 0) {
+				content.addChild("#", l10n("timeoutFractions"));
 				table = content.addChild("table", "border", "0");
 				header = table.addChild("tr");
-				header.addChild("th", l10n("timeoutFractions"));
 				header.addChild("th", l10n("localHeader"));
 				header.addChild("th", l10n("remoteHeader"));
 				row = table.addChild("tr");
-				row.addChild("td", "");
 				row.addChild("td", this.fix3p3pct.format(((double)fatalTimeoutsInWaitLocal)/((double)(fatalTimeoutsInWaitLocal + allocatedSlotLocal))));
 				row.addChild("td", this.fix3p3pct.format(((double)fatalTimeoutsInWaitRemote)/((double)(fatalTimeoutsInWaitRemote + allocatedSlotRemote))));
 			}
