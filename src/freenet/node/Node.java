03ff9f7bc7cc12a97496dd07582a1a9d76ff961f
Matthew Toseland <toad@amphibian.dyndns.org>
2011-07-18 13:42:17 +0100

    It's probably safer to set the default to true rather than try to detect whether the user had turned bloom off.

--- src/freenet/node/Node.java
+++ src/freenet/node/Node.java
@@ -2041,12 +2041,7 @@ public class Node implements TimeSkewDetectorCallback {
 
 		maxTotalKeys = maxTotalDatastoreSize / sizePerKey;
 		
-		boolean hadBloomFilter = true;
-		s = nodeConfig.getRawOption("storeBloomFilterSize");
-		if(s != null && s.equals("0"))
-			hadBloomFilter = false;
-		
-		nodeConfig.register("storeUseSlotFilters", hadBloomFilter, sortOrder++, true, false, "Node.storeUseSlotFilters", "Node.storeUseSlotFiltersLong", new BooleanCallback() {
+		nodeConfig.register("storeUseSlotFilters", true, sortOrder++, true, false, "Node.storeUseSlotFilters", "Node.storeUseSlotFiltersLong", new BooleanCallback() {
 
 			public Boolean get() {
 				synchronized(Node.this) {
