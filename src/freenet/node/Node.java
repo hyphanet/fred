f8b1095ea586682e008fc01e439925e030e0361a
Matthew Toseland <toad@amphibian.dyndns.org>
2011-07-19 15:07:12 +0100

    Document ordering.

--- src/freenet/node/Node.java
+++ src/freenet/node/Node.java
@@ -1723,6 +1723,7 @@ public class Node implements TimeSkewDetectorCallback {
 
 		nodeStats = new NodeStats(this, sortOrder, new SubConfig("node.load", config), obwLimit, ibwLimit, lastVersion);
 
+		// clientCore needs new load management and other settings from stats.
 		clientCore = new NodeClientCore(this, config, nodeConfig, installConfig, getDarknetPortNumber(), sortOrder, oldConfig, fproxyConfig, toadlets, nodeDBHandle, db);
 
 		if(showFriendsVisibilityAlert)
