257af3ec9fde4f08659f5946d23331e7105e39f2
Matthew Toseland <toad@amphibian.dyndns.org>
2011-07-28 08:29:41 +0100

    Add FIXME

--- src/freenet/node/CHKInsertHandler.java
+++ src/freenet/node/CHKInsertHandler.java
@@ -330,6 +330,7 @@ public class CHKInsertHandler implements PrioRunnable, ByteCounter {
     				// Okay, great.
     				// Either we got a DataInsert, in which case the transfer was aborted above, or we got a DataInsertRejected, which means it never started.
     				// FIXME arguably we should wait until we have the message before sending the transfer cancel in case the message gets lost? Or maybe not?
+    				// FIXME unlock here rather than in finally block in realRun??? Unlocking in the finally block is safe (won't cause rejects), unlocking here might be more accurate ...
     			}
 
     			@Override
