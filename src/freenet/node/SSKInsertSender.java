8695d27e62339d9aa9f40a666caf99351e61da60
Matthew Toseland <toad@amphibian.dyndns.org>
2011-08-03 00:30:09 +0100

    Add new closerPeer() which always excludes mandatory backoff. Use it when routing requests.

--- src/freenet/node/SSKInsertSender.java
+++ src/freenet/node/SSKInsertSender.java
@@ -207,7 +207,7 @@ public class SSKInsertSender extends BaseSender implements PrioRunnable, AnyInse
             
             // Route it
             next = node.peers.closerPeer(forkedRequestTag == null ? source : null, nodesRoutedTo, target, true, node.isAdvancedModeEnabled(), -1, null,
-			        null, htl, ignoreLowBackoff ? Node.LOW_BACKOFF : 0, source == null, realTimeFlag, newLoadManagement);
+			        null, htl, ignoreLowBackoff ? Node.LOW_BACKOFF : 0, source == null, realTimeFlag);
             
             if(next == null) {
                 // Backtrack
