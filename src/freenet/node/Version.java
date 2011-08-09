7296d260c6f446ea2214201d32d99ac3b30b7900
Matthew Toseland <toad@amphibian.dyndns.org>
2011-08-09 15:40:08 +0100

    Build 1394.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,17 +50,17 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1393;
+	private static final int buildNumber = 1394;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1392;
-	private static final int newLastGoodBuild = 1393;
+	private static final int newLastGoodBuild = 1394;
 	static final long transitionTime;
 
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2011, Calendar.AUGUST, 10, 0, 0, 0 );
+		_cal.set( 2011, Calendar.AUGUST, 11, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 
