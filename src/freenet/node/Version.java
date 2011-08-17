69f3b9a6d6577f438a586c4aaa0da455e8fc6011
Matthew Toseland <toad@amphibian.dyndns.org>
2011-08-17 18:49:18 +0100

    Build 1297. Mandatory 12:00 tomorrow.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,17 +50,17 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1396;
+	private static final int buildNumber = 1397;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 1395;
-	private static final int newLastGoodBuild = 1396;
+	private static final int oldLastGoodBuild = 1396;
+	private static final int newLastGoodBuild = 1397;
 	static final long transitionTime;
 
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2011, Calendar.AUGUST, 16, 0, 0, 0 );
+		_cal.set( 2011, Calendar.AUGUST, 18, 12, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 
