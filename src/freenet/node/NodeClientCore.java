hyphanet/fred

commit cc85808173f329d8b91d0d7e36bd5c5bf8ae41c0

Use asyncGet() for SendableGet's, and handle completion asynchronously, fixing a bug where we would count offered key fetches as completed immediately.

src/freenet/node/NodeClientCore.java
@@ -857,7 +857,7 @@ public class NodeClientCore implements Persistable, DBJobRunner, OOMHook, Execut
 		}
 	}
 
-	public void asyncGet(final Key key, boolean offersOnly, final RequestCompletionListener listener, boolean canReadClientCache, boolean canWriteClientCache, final boolean realTimeFlag) {
+	public void asyncGet(final Key key, boolean offersOnly, final RequestCompletionListener listener, boolean canReadClientCache, boolean canWriteClientCache, final boolean realTimeFlag, boolean localOnly, boolean ignoreStore) {
 		final long uid = makeUID();
 		final boolean isSSK = key instanceof NodeSSK;
 		final RequestTag tag = new RequestTag(isSSK, RequestTag.START.ASYNC_GET, null, realTimeFlag, uid, node);
@@ -990,7 +990,7 @@ public class NodeClientCore implements Persistable, DBJobRunner, OOMHook, Execut
 			public void onAbortDownstreamTransfers(int reason, String desc) {
 				// Ignore, onRequestSenderFinished will also be called.
 			}
-		}, tag, canReadClientCache, canWriteClientCache, htl, realTimeFlag);
+		}, tag, canReadClientCache, canWriteClientCache, htl, realTimeFlag, localOnly, ignoreStore);
 	}
 
 	/**
@@ -999,9 +999,9 @@ public class NodeClientCore implements Persistable, DBJobRunner, OOMHook, Execut
 	 * anything and will run asynchronously. Caller is responsible for unlocking the UID.
 	 * @param key
 	 */
-	void asyncGet(Key key, boolean isSSK, boolean offersOnly, long uid, RequestSenderListener listener, RequestTag tag, boolean canReadClientCache, boolean canWriteClientCache, short htl, boolean realTimeFlag) {
+	void asyncGet(Key key, boolean isSSK, boolean offersOnly, long uid, RequestSenderListener listener, RequestTag tag, boolean canReadClientCache, boolean canWriteClientCache, short htl, boolean realTimeFlag, boolean ignoreStore, boolean localOnly) {
 		try {
-			Object o = node.makeRequestSender(key, htl, uid, tag, null, false, false, offersOnly, canReadClientCache, canWriteClientCache, realTimeFlag);
+			Object o = node.makeRequestSender(key, htl, uid, tag, null, localOnly, ignoreStore, offersOnly, canReadClientCache, canWriteClientCache, realTimeFlag);
 			if(o instanceof KeyBlock) {
 				tag.servedFromDatastore = true;
 				tag.unlockHandler();

src/freenet/client/async/ChosenBlock.java
@@ -60,10 +60,14 @@ public abstract class ChosenBlock {
 	public abstract void onFetchSuccess(ClientContext context);
 
 	public abstract short getPriority();
+	
+	private boolean sendIsBlocking;
 
 	public boolean send(NodeClientCore core, RequestScheduler sched) {
 		ClientContext context = sched.getContext();
-		return getSender(context).send(core, sched, context, this);
+		SendableRequestSender sender = getSender(context);
+		sendIsBlocking = sender.sendIsBlocking();
+		return sender.send(core, sched, context, this);
 	}
 	
 	public abstract SendableRequestSender getSender(ClientContext context);
@@ -78,5 +82,10 @@ public abstract class ChosenBlock {
 	
 	public void setGeneratedKey(ClientKey key) {
 		generatedKey = key;
+	}
+
+	/** Call this after send() */
+	public boolean sendIsBlocking() {
+		return sendIsBlocking;
 	}
 }

src/freenet/client/async/OfferedKeysList.java
@@ -20,6 +20,7 @@ import freenet.node.RequestCompletionListener;
 import freenet.node.RequestScheduler;
 import freenet.node.RequestSender;
 import freenet.node.RequestSenderListener;
+import freenet.node.SendableInsert;
 import freenet.node.SendableRequestItem;
 import freenet.node.SendableRequestSender;
 import freenet.node.NodeClientCore.SimpleRequestSenderCompletionListener;
@@ -164,8 +165,8 @@ public class OfferedKeysList extends BaseSendableGet implements RequestClient {
 		return new SendableRequestSender() {
 
 			@Override
-			public boolean send(NodeClientCore core, RequestScheduler sched, ClientContext context, ChosenBlock req) {
-				Key key = ((MySendableRequestItem) req.token).key;
+			public boolean send(NodeClientCore core, final RequestScheduler sched, ClientContext context, ChosenBlock req) {
+				final Key key = ((MySendableRequestItem) req.token).key;
 				// Have to cache it in order to propagate it; FIXME
 				// Don't let a node force us to start a real request for a specific key.
 				// We check the datastore, take up offers if any (on a short timeout), and then quit if we still haven't fetched the data.
@@ -174,17 +175,28 @@ public class OfferedKeysList extends BaseSendableGet implements RequestClient {
 
 					@Override
 					public void onSucceeded() {
-						// Ignore.
+						sched.removeFetchingKey(key);
+						// Something might be waiting for a request to complete (e.g. if we have two requests for the same key), 
+						// so wake the starter thread.
+						sched.wakeStarter();
 					}
 
 					@Override
 					public void onFailed(LowLevelGetException e) {
-						// Ignore.
+						sched.removeFetchingKey(key);
+						// Something might be waiting for a request to complete (e.g. if we have two requests for the same key), 
+						// so wake the starter thread.
+						sched.wakeStarter();
 					}
 
-				}, true, false, realTimeFlag);
+				}, true, false, realTimeFlag, false, false);
 				// FIXME reconsider canWriteClientCache=false parameter.
 				return true;
+			}
+
+			@Override
+			public boolean sendIsBlocking() {
+				return false;
 			}
 			
 		};

src/freenet/client/async/SingleBlockInserter.java
@@ -624,6 +624,11 @@ public class SingleBlockInserter extends SendableInsert implements ClientPutStat
 			req.onInsertSuccess(context);
 			return true;
 		}
+
+		@Override
+		public boolean sendIsBlocking() {
+			return true;
+		}
 	}
 
 	

src/freenet/client/async/SplitFileInserterSegment.java
@@ -1256,6 +1256,11 @@ public class SplitFileInserterSegment extends SendableInsert implements FECCallb
 				req.onInsertSuccess(context);
 				return true;
 			}
+		
+		@Override
+		public boolean sendIsBlocking() {
+			return true;
+		}
 
 
 	}

src/freenet/node/RequestStarter.java
@@ -265,12 +265,14 @@ public class RequestStarter implements Runnable, RandomGrabArrayItemExclusionLis
 			if(logMINOR) 
 				Logger.minor(this, "Finished "+req);
 			} finally {
-				if(key != null) sched.removeFetchingKey(key);
-				else if((!req.isPersistent()) && ((TransientChosenBlock)req).request instanceof SendableInsert)
-					sched.removeTransientInsertFetching((SendableInsert)(((TransientChosenBlock)req).request), req.token);
-				// Something might be waiting for a request to complete (e.g. if we have two requests for the same key), 
-				// so wake the starter thread.
-				wakeUp();
+				if(req.sendIsBlocking()) {
+					if(key != null) sched.removeFetchingKey(key);
+					else if((!req.isPersistent()) && ((TransientChosenBlock)req).request instanceof SendableInsert)
+						sched.removeTransientInsertFetching((SendableInsert)(((TransientChosenBlock)req).request), req.token);
+					// Something might be waiting for a request to complete (e.g. if we have two requests for the same key), 
+					// so wake the starter thread.
+					wakeUp();
+				}
 			}
 		}
 		

src/freenet/node/SendableGetRequestSender.java
@@ -2,7 +2,9 @@ package freenet.node;
 
 import freenet.client.async.ChosenBlock;
 import freenet.client.async.ClientContext;
+import freenet.client.async.TransientChosenBlock;
 import freenet.keys.ClientKey;
+import freenet.keys.Key;
 import freenet.support.LogThresholdCallback;
 import freenet.support.Logger;
 import freenet.support.Logger.LogLevel;
@@ -21,14 +23,18 @@ public class SendableGetRequestSender implements SendableRequestSender {
 		});
 	}
 	
+	public boolean sendIsBlocking() {
+		return false;
+	}
+	
 	/** Do the request, blocking. Called by RequestStarter. 
 	 * Also responsible for deleting it.
 	 * @return True if a request was executed. False if caller should try to find another request, and remove
 	 * this one from the queue. */
 	@Override
-	public boolean send(NodeClientCore core, final RequestScheduler sched, ClientContext context, ChosenBlock req) {
+	public boolean send(NodeClientCore core, final RequestScheduler sched, final ClientContext context, final ChosenBlock req) {
 		Object keyNum = req.token;
-		ClientKey key = req.ckey;
+		final ClientKey key = req.ckey;
 		if(key == null) {
 			Logger.error(SendableGet.class, "Key is null in send(): keyNum = "+keyNum+" for "+req);
 			return false;
@@ -42,6 +48,38 @@ public class SendableGetRequestSender implements SendableRequestSender {
 		}
 		try {
 			try {
+				final Key k = key.getNodeKey();
+				core.asyncGet(k, false, new RequestCompletionListener() {
+
+					@Override
+					public void onSucceeded() {
+						try {
+							req.onFetchSuccess(context);
+						} finally {
+							if(key != null) sched.removeFetchingKey(k);
+							else if((!req.isPersistent()) && ((TransientChosenBlock)req).request instanceof SendableInsert)
+								sched.removeTransientInsertFetching((SendableInsert)(((TransientChosenBlock)req).request), req.token);
+							// Something might be waiting for a request to complete (e.g. if we have two requests for the same key), 
+							// so wake the starter thread.
+							sched.wakeStarter();
+						}
+					}
+
+					@Override
+					public void onFailed(LowLevelGetException e) {
+						try {
+							req.onFailure(e, context);
+						} finally {
+							if(key != null) sched.removeFetchingKey(k);
+							else if((!req.isPersistent()) && ((TransientChosenBlock)req).request instanceof SendableInsert)
+								sched.removeTransientInsertFetching((SendableInsert)(((TransientChosenBlock)req).request), req.token);
+							// Something might be waiting for a request to complete (e.g. if we have two requests for the same key), 
+							// so wake the starter thread.
+							sched.wakeStarter();
+						}
+					}
+					
+				}, true, req.canWriteClientCache, req.realTimeFlag, req.localRequestOnly, req.ignoreStore);
 				core.realGetKey(key, req.localRequestOnly, req.ignoreStore, req.canWriteClientCache, req.realTimeFlag);
 			} catch (final LowLevelGetException e) {
 				req.onFailure(e, context);

src/freenet/node/SendableRequestSender.java
@@ -21,5 +21,7 @@ public interface SendableRequestSender {
 	 * @return True if a request was sent, false otherwise (in which case the request will
 	 * be removed if it hasn't already been). */
 	public abstract boolean send(NodeClientCore node, RequestScheduler sched, ClientContext context, ChosenBlock request);
+
+	public abstract boolean sendIsBlocking();
 	
 }

commit 7e65464df9c74b71eeabe7bc482fd8d96c96de59

[PATCH] Fix broken USK subscriptions
... and not only due to swapped localOnly and ignoreStore parameters.
Regression in cc85808173f329d8b91d0d7e36bd5c5bf8ae41c0/build01399.

Toad: 10^99 x thanks Eleriseth!

src/freenet/node/NodeClientCore.java
@@ -1034,7 +1034,7 @@ public class NodeClientCore implements Persistable, DBJobRunner, OOMHook, Execut
 	 * anything and will run asynchronously. Caller is responsible for unlocking the UID.
 	 * @param key
 	 */
-	void asyncGet(Key key, boolean isSSK, boolean offersOnly, long uid, RequestSenderListener listener, RequestTag tag, boolean canReadClientCache, boolean canWriteClientCache, short htl, boolean realTimeFlag, boolean ignoreStore, boolean localOnly) {
+	void asyncGet(Key key, boolean isSSK, boolean offersOnly, long uid, RequestSenderListener listener, RequestTag tag, boolean canReadClientCache, boolean canWriteClientCache, short htl, boolean realTimeFlag, boolean localOnly, boolean ignoreStore) {
 		try {
 			Object o = node.makeRequestSender(key, htl, uid, tag, null, localOnly, ignoreStore, offersOnly, canReadClientCache, canWriteClientCache, realTimeFlag);
 			if(o instanceof KeyBlock) {