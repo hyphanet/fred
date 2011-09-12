b49f882481504a56cd9f7685156dee57ff3b0aa7
Matthew Toseland <toad@amphibian.dyndns.org>
2011-09-12 11:57:49 +0100

    Build 1404. Mandatory Friday.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,17 +50,17 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1403;
+	private static final int buildNumber = 1404;
 
 	/** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1401;
-	private static final int newLastGoodBuild = 1402;
+	private static final int newLastGoodBuild = 1404;
 	static final long transitionTime;
 
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2011, Calendar.SEPTEMBER, 13, 0, 0, 0 );
+		_cal.set( 2011, Calendar.SEPTEMBER, 16, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 
