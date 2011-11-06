1f50399af2e1473a7f8d4161f96588affd9854f3
Matthew Toseland <toad@amphibian.dyndns.org>
2011-11-06 15:27:36 +0000

    Don't use NO_LOAD_STATS status if NLM is off.

--- src/freenet/node/PeerNode.java
+++ src/freenet/node/PeerNode.java
@@ -3883,6 +3883,9 @@ public abstract class PeerNode implements USKRetrieverCallback, BasePeerNode {
 		int oldPeerNodeStatus;
 		long threshold = maxPeerPingTime();
 		boolean noLoadStats = noLoadStats();
+		if(node.enableNewLoadManagement(true) || node.enableNewLoadManagement(false))
+			// New load management off -> don't care whether they have load stats or not.
+			noLoadStats = false;
 		synchronized(this) {
 			oldPeerNodeStatus = peerNodeStatus;
 			peerNodeStatus = getPeerNodeStatus(now, localRoutingBackedOffUntilRT, localRoutingBackedOffUntilBulk, averagePingTime() > threshold, noLoadStats);
