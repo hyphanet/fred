hyphanet/fred

commit bdb6e3b5f482cee97731ced06f2ae19e6d465e96

cache the results for 60 secs
git-svn-id: file:///home/toad/git-migration/temprepository/trunk/freenet@25317 67a373e5-eb02-0410-a15c-ee090a768436

src/freenet/node/NodeStats.java
@@ -172,6 +172,8 @@ public class NodeStats implements Persistable {
 	
 	// ThreadCounting stuffs
 	public final ThreadGroup rootThreadGroup;
+	private int[] activeThreadsByPriorities = new int[NativeThread.JAVA_PRIORITY_RANGE];
+	private int[] waitingThreadsByPriorities = new int[NativeThread.JAVA_PRIORITY_RANGE];
 	private int threadLimit;
 	
 	final NodePinger nodePinger;
@@ -202,6 +204,8 @@ public class NodeStats implements Persistable {
 		ThreadGroup tg = Thread.currentThread().getThreadGroup();
 		while(tg.getParent() != null) tg = tg.getParent();
 		this.rootThreadGroup = tg;
+		this.activeThreadsByPriorities = new int[NativeThread.JAVA_PRIORITY_RANGE];
+		this.waitingThreadsByPriorities = new int[NativeThread.JAVA_PRIORITY_RANGE];
 		throttledPacketSendAverage =
 			new TimeDecayingRunningAverage(1, 10*60*1000 /* should be significantly longer than a typical transfer */, 0, Long.MAX_VALUE, node);
 		nodePinger = new NodePinger(node);
@@ -423,6 +427,10 @@ public class NodeStats implements Persistable {
 					long end = System.currentTimeMillis();
 					if(logMINOR)
 						Logger.minor(this, "Throttle check took "+TimeUtil.formatTime(end-now,2,true));
+
+					// Doesn't belong here... but anyway, should do the job.
+					activeThreadsByPriorities = node.executor.runningThreads();
+					waitingThreadsByPriorities = node.executor.waitingThreads();
 				}
 			}
 	};
@@ -1016,11 +1024,11 @@ public class NodeStats implements Persistable {
 	}
 	
 	public int[] getActiveThreadsByPriority() {
-		return node.executor.runningThreads();
+		return activeThreadsByPriorities;
 	}
 	
 	public int[] getWaitingThreadsByPriority() {
-		return node.executor.waitingThreads();
+		return waitingThreadsByPriorities;
 	}
 
 	public int getThreadLimit() {

commit a3e913d1d94c58de13137384723b4158214e04fb

fix the pooled threads display in StatisticsToadlet
bdb6e3b5 introduced thread count caching, then 884c4cbd inadvertently
broke it. This commit restores the original behavior.

src/freenet/node/NodeStats.java
@@ -244,8 +244,6 @@ public class NodeStats implements Persistable, BlockTimeCallback {
 
 	// ThreadCounting stuffs
 	public final ThreadGroup rootThreadGroup;
-	private int[] activeThreadsByPriorities = new int[NativeThread.JAVA_PRIORITY_RANGE];
-	private int[] waitingThreadsByPriorities = new int[NativeThread.JAVA_PRIORITY_RANGE];
 	private int threadLimit;
 
 	final NodePinger nodePinger;
@@ -307,8 +305,6 @@ public class NodeStats implements Persistable, BlockTimeCallback {
 		ThreadGroup tg = Thread.currentThread().getThreadGroup();
 		while(tg.getParent() != null) tg = tg.getParent();
 		this.rootThreadGroup = tg;
-		this.activeThreadsByPriorities = new int[NativeThread.JAVA_PRIORITY_RANGE];
-		this.waitingThreadsByPriorities = new int[NativeThread.JAVA_PRIORITY_RANGE];
 		throttledPacketSendAverage =
 			new BootstrappingDecayingRunningAverage(0, 0, Long.MAX_VALUE, 100, null);
 		throttledPacketSendAverageRT =
@@ -1763,11 +1759,11 @@ public class NodeStats implements Persistable, BlockTimeCallback {
 	}
 
 	public int[] getActiveThreadsByPriority() {
-		return activeThreadsByPriorities;
+		return node.executor.runningThreads();
 	}
 
 	public int[] getWaitingThreadsByPriority() {
-		return waitingThreadsByPriorities;
+		return node.executor.waitingThreads();
 	}
 
 	public int getThreadLimit() {