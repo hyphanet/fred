c4d8cf8780e6fff94905e281505c27c0360f4c54
Matthew Toseland <toad@amphibian.dyndns.org>
2012-04-15 01:33:50 +0100

    Move MemoryChecker

--- src/freenet/node/stats/MemoryChecker.java
+++ src/freenet/node/stats/MemoryChecker.java
@@ -1,7 +1,7 @@
 /* This code is part of Freenet. It is distributed under the GNU General
  * Public License, version 2 (or at your option any later version). See
  * http://www.gnu.org/ for further details of the GPL. */
-package freenet.node;
+package freenet.node.stats;
 
 import freenet.support.Logger;
 import freenet.support.OOMHandler;
