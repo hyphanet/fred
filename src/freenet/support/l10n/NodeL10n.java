f4213b849d60c1f5336e38ce9d51b653ba4e07dc
Matthew Toseland <toad@amphibian.dyndns.org>
2012-05-10 21:38:59 +0100

    Really fix l10n

--- src/freenet/support/l10n/NodeL10n.java
+++ src/freenet/support/l10n/NodeL10n.java
@@ -37,8 +37,8 @@ public class NodeL10n {
 	 * @see LANGUAGE.mapToLanguage(String)
 	 */
 	public NodeL10n(final LANGUAGE lang, File overrideDir) {
-		NodeL10n.b = new BaseL10n("freenet/support/l10n/", "freenet.support.l10n.${lang}.properties",
-		  overrideDir.getPath()+File.separator+"freenet.support.l10n.${lang}.override.properties", lang);
+		NodeL10n.b = new BaseL10n("freenet/support/l10n/", "freenet.l10n.${lang}.properties",
+		  overrideDir.getPath()+File.separator+"freenet.l10n.${lang}.override.properties", lang);
 	}
 
 	/**
