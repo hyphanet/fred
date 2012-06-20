6a01c22159d197105d4994b695a5221c5f752bd9
Steve Dougherty <steve@asksteved.com>
2012-06-20 11:27:41 -0400

    Fix IllegalMonitorStateException on delay.

--- src/freenet/node/MHProbe.java
+++ src/freenet/node/MHProbe.java
@@ -529,7 +529,7 @@ public class MHProbe implements ByteCounter {
 		try {
 			long wait = WAIT_MAX;
 			while (wait >= WAIT_MAX) wait = (long)(-Math.log(node.random.nextDouble()) * WAIT_BASE / Math.E);
-			wait(wait);
+			Thread.sleep(wait);
 		} catch (InterruptedException e) {
 			if (logDEBUG) Logger.debug(MHProbe.class, "Interrupted while waiting before sending response.", e);
 		}
