b69aa0bf646db5554ad81d5bc9f381dc270a1f7f
Steve Dougherty <steve@asksteved.com>
2012-06-21 14:46:50 -0400

    Throw if a counter is too high or too low.

--- src/freenet/node/MHProbe.java
+++ src/freenet/node/MHProbe.java
@@ -76,8 +76,29 @@ public class MHProbe implements ByteCounter {
 	private class Counter {
 		private byte c = 0;
 
-		public void increment() { c++; }
-		public void decrement() { c--; }
+		public void increment() {
+			c++;
+			if (c > MAX_ACCEPTED) {
+				/*
+				 * The counter should never be incremented above the maximum, as an increment should
+				 * only happen after it has been confirmed to be below the limit. If this happens, it
+				 * indicates a concurrency problem or logic error.
+				 */
+				throw new IllegalStateException("Number of accepted probes exceeds the maximum.");
+			}
+		}
+		public void decrement() {
+			c--;
+			if (c < 0) {
+				/*
+				 * The counter should never be decremented lower than zero, as a decrement should always
+				 * be paired with an increment before it, and if a counter reaches zero it should be
+				 * removed to avoid memory leaks. If this happens, it indicates a concurrency problem or
+				 * logic error.
+				 */
+				throw new IllegalStateException("Number of accepted probes is negative.");
+			}
+		}
 		public byte value() { return c; }
 	}
 
