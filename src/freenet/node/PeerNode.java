5cd8c62ac82d1e819134e88391325350a1328f5b
Steve Dougherty <steve@asksteved.com>
2012-06-26 17:34:43 -0400

    Change getDegree() to synchronized.
    synchronized syncrhonizes on (this), as do all writes.

--- src/freenet/node/PeerNode.java
+++ src/freenet/node/PeerNode.java
@@ -1832,7 +1832,7 @@ public abstract class PeerNode implements USKRetrieverCallback, BasePeerNode {
 	 * Determines the degree of the peer via the locations of its peers it provides.
 	 * @return The number of peers this peer reports having, or 0 if this peer does not provide that information.
 	 */
-	public int getDegree() {
+	public synchronized int getDegree() {
 		if (currentPeersLocation == null) return 0;
 		return currentPeersLocation.length;
 	}
