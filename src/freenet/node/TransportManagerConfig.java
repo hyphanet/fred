549faf271c8eb5810c91c453d74b592b2bcab16d
chetanhosmani <chetanhosmani0@gmail.com>
2012-07-12 01:13:40 +0530

    Add configuration files
    The TransportManagerConfig is used to setup the TransportManager.
    They store addresses and enabled transports.
    
    TransportConfig is used to initialise a transport instance from a factory class.

---
+++ src/freenet/node/TransportManagerConfig.java
@@ -0,0 +1,40 @@
+package freenet.node;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import freenet.node.TransportManager.TransportMode;
+import freenet.pluginmanager.TransportPluginConfigurationException;
+import freenet.support.SimpleFieldSet;
+
+/**
+ * TransportManagerConfig keeps a track of all configurations and ports fred binds to.
+ * For now its sole purpose is to track transport plugin bindings in the transportConfigMap field.
+ * @author chetan
+ *
+ */
+public class TransportManagerConfig {
+	
+	public final TransportMode transportMode;
+	
+	public HashMap<String, SimpleFieldSet> transportConfigMap = new HashMap<String, SimpleFieldSet> ();
+	
+	public HashMap<String, Boolean> enabledTransports = new HashMap<String, Boolean> ();
+	
+	public TransportManagerConfig(TransportMode transportMode) {
+		this.transportMode = transportMode;
+	}
+	
+	public synchronized void addTransportConfig(String transportName, SimpleFieldSet transportConfig) {
+		transportConfigMap.put(transportName, transportConfig);
+	}
+	
+	public synchronized SimpleFieldSet getTransportConfig(String transportName) throws TransportPluginConfigurationException {
+		return transportConfigMap.get(transportName);
+	}
+	
+	public void writeConfigData() throws IOException {
+		
+	}
+
+}
