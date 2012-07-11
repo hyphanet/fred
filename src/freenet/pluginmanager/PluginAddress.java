a99b0b15b781e03b95c690e81ab099abd420dd53
chetanhosmani <chetanhosmani0@gmail.com>
2012-07-12 00:39:32 +0530

    Interface design for transport addresses
    This interface has been designed considering a very abstract address.
    Although it has been kept in mind that most transports will be IP based.
    Some methods will be supported by IP based transports only.

---
+++ src/freenet/pluginmanager/PluginAddress.java
@@ -0,0 +1,62 @@
+package freenet.pluginmanager;
+
+import freenet.io.comm.FreenetInetAddress;
+
+public interface PluginAddress{
+	
+	/** 
+	 * An address that can be stored, and used later on for a noderef. 
+	 * The plugin should be able to rebuild it from the String.
+	 * @return
+	 */
+	public String toStringAddress();
+	
+	public String toString();
+	
+	public boolean laxEquals(Object o);
+	
+	/**
+	 * Make sure if we have no information then we call it null.
+	 * For e.g. if no IP address, port number or host name is present then it should equate it to null.
+	 * Check class Peer for IP based implementation.
+	 * @param o
+	 * @return
+	 */
+	public boolean equals(Object o);
+	
+	public boolean strictEquals(Object o);
+	
+	/**
+	 * Byte representation of the physical location. We need this for authentication. 
+	 * So it should be constant for one object. Otherwise JFK may fail.
+	 * For IP based addresses it should return the same InetAddress in bytes.
+	 * @return
+	 */
+	public byte[] getBytes();
+	
+	/**
+	 * Get only the physical location, excluding the transport specific/ user specific data.
+	 * For e.g. in case of Inet based addresses, it ignores the port number.
+	 * The implementation may not apply in all cases.
+	 * @return
+	 */
+	public PluginAddress getPhysicalAddress();
+	
+	public void updateHostname() throws UnsupportedOperationException;
+	
+	/**
+	 * Drop the host name if it a IP based transport or else if applicable.
+	 * @throws UnsupportedOperationException
+	 */
+	public void dropHostName() throws UnsupportedOperationException;
+	
+	/**
+	 * If the address uses Inet based IP addresses.
+	 * @return
+	 * @throws UnsupportedOperationException
+	 */
+	public FreenetInetAddress getFreenetAddress() throws UnsupportedOperationException;
+	
+	public int getPortNumber() throws UnsupportedOperationException;
+	
+}
