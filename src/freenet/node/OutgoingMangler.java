147f81c88b4b0241f4321b5eaa65fe72f43978b2
chetanhosmani <chetanhosmani0@gmail.com>
2012-07-18 02:53:36 +0530

    Move methods from OutgoingPacketMangler to OutgoingMangler
    We want to support streams in the future. Move these common methods.

--- src/freenet/node/OutgoingMangler.java
+++ src/freenet/node/OutgoingMangler.java
@@ -1,5 +1,50 @@
 package freenet.node;
 
+import freenet.io.AddressTracker.Status;
+import freenet.io.comm.FreenetInetAddress;
+import freenet.io.comm.PeerContext;
+import freenet.pluginmanager.TransportPlugin;
+
 public interface OutgoingMangler {
+	
+	/**
+	 * Send a handshake, if possible, to the node.
+	 * @param pn
+	 */
+	public void sendHandshake(PeerNode pn, boolean notRegistered);
+	
+	/**
+	 * Is a peer disconnected?
+	 */
+	public boolean isDisconnected(PeerContext context);
+	
+	public TransportPlugin getTransport();
+	
+	/**
+	 * Port forwarding status.
+	 * @return A status code from AddressTracker. FIXME make this more generic when we need to.
+	 */
+	public Status getConnectivityStatus();
+	
+	/**
+	 * Is there any reason not to allow this connection? E.g. limits on the number of nodes on
+	 * a specific IP address?
+	 */
+	public boolean allowConnection(PeerNode node, FreenetInetAddress addr);
+	
+	/**
+	 * If the lower level code detects the port forwarding is broken, it will call this method.
+	 */
+	public void setPortForwardingBroken();
+	
+	/**
+	 * Get our compressed noderef
+	 */
+	public byte[] getCompressedNoderef();
+	
+	/**
+	 * Always allow local addresses?
+	 */
+	public boolean alwaysAllowLocalAddresses();
 
 }
--- src/freenet/node/OutgoingPacketMangler.java
+++ src/freenet/node/OutgoingPacketMangler.java
@@ -76,17 +76,6 @@ public interface OutgoingPacketMangler extends OutgoingMangler {
 			PacketSequenceException, WouldBlockException;
 
 	/**
-	 * Send a handshake, if possible, to the node.
-	 * @param pn
-	 */
-	public void sendHandshake(PeerNode pn, boolean notRegistered);
-
-	/**
-	 * Is a peer disconnected?
-	 */
-	public boolean isDisconnected(PeerContext context);
-	
-	/**
 	 * List of supported negotiation types in preference order (best last)
 	 */
 	public int[] supportedNegTypes(boolean forPublic);
@@ -111,30 +100,5 @@ public interface OutgoingPacketMangler extends OutgoingMangler {
 	 */
 	public Peer[] getPrimaryIPAddress();
 
-	/**
-	 * Get our compressed noderef
-	 */
-	public byte[] getCompressedNoderef();
-	
-	/**
-	 * Always allow local addresses?
-	 */
-	public boolean alwaysAllowLocalAddresses();
 
-	/**
-	 * Port forwarding status.
-	 * @return A status code from AddressTracker. FIXME make this more generic when we need to.
-	 */
-	public Status getConnectivityStatus();
-
-	/**
-	 * Is there any reason not to allow this connection? E.g. limits on the number of nodes on
-	 * a specific IP address?
-	 */
-	public boolean allowConnection(PeerNode node, FreenetInetAddress addr);
-
-	/**
-	 * If the lower level code detects the port forwarding is broken, it will call this method.
-	 */
-	public void setPortForwardingBroken();
 }
