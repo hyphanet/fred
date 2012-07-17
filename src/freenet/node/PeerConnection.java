42502100b4ebf39ade63c936bc93026989709d0c
chetanhosmani <chetanhosmani0@gmail.com>
2012-07-18 02:32:52 +0530

    Remove PeerPacketConnection and PeerStreamConnection
    No point having separate versions for now.
    PeerTransport will handle most of the code.

--- src/freenet/node/PeerConnection.java
+++ src/freenet/node/PeerConnection.java
@@ -3,6 +3,7 @@ package freenet.node;
 import java.util.Vector;
 
 import freenet.pluginmanager.PluginAddress;
+import freenet.pluginmanager.TransportPlugin;
 /**
  * Base object of PeerPacketConnection and PeerStreamConnection. Fields common to both are present.
  * @author chetan
@@ -10,7 +11,9 @@ import freenet.pluginmanager.PluginAddress;
  */
 public class PeerConnection {
 	
-	protected final String transportName; 
+	protected final String transportName;
+	
+	protected final TransportPlugin transportPlugin;
 	
 	/** Every connection can belong to only one peernode. */
 	protected final PeerNode pn;
@@ -18,65 +21,23 @@ public class PeerConnection {
 	/** The peer it connects to */
 	protected PluginAddress detectedTransportAddress;
 	
+	/** How much data did we send with the current tracker ? */
+	public long totalBytesExchangedWithCurrentTracker = 0;
+	
 	/** List of keys for every connection. Multiple setups might complete simultaneously.
 	 * This will also be used to replace current, previous and unverified to make it more generic.
 	 */
-	private Vector<SessionKey> keys = new Vector<SessionKey> ();
+	public Vector<SessionKey> keys = new Vector<SessionKey> ();
 	
-	private SessionKey currentTracker = null;
-	private SessionKey previousTracker = null;
-	private SessionKey unverifiedTracker = null;
+	public SessionKey currentTracker = null;
+	public SessionKey previousTracker = null;
+	public SessionKey unverifiedTracker = null;
 	
-	public PeerConnection(String transportName, PeerNode pn){
-		this.transportName = transportName;
+	public PeerConnection(TransportPlugin transportPlugin, PeerNode pn, PluginAddress detectedTransportAddress){
+		this.transportPlugin = transportPlugin;
+		this.transportName = transportPlugin.transportName;
 		this.pn = pn;
-	}
-	
-	/*
-	 * Custom implementation with several keys can be provided here.
-	 */
-	public synchronized void addKey(SessionKey key){
-		keys.add(key);
-	}
-	
-	public synchronized SessionKey setKey(SessionKey key, int position){
-		return keys.set(position, key);
-	}
-	
-	public synchronized Vector<SessionKey> getKeys(){
-		return keys;
-	}
-	
-	public int getKeysSize(){
-		return keys.size();
-	}
-	
-	/*
-	 * The old logic can be employed here.
-	 */
-
-	public synchronized SessionKey getCurrentKeyTracker() {
-		return currentTracker;
-	}
-	
-	public synchronized SessionKey getPreviousKeyTracker() {
-		return previousTracker;
-	}
-	
-	public synchronized SessionKey getUnverifiedKeyTracker() {
-		return unverifiedTracker;
-	}
-	
-	public synchronized void setCurrentKeyTracker(SessionKey currentTracker) {
-		this.currentTracker = currentTracker;
-	}
-	
-	public synchronized void setPreviousKeyTracker(SessionKey previousTracker) {
-		this.previousTracker = previousTracker;
-	}
-	
-	public synchronized void setUnverifiedKeyTracker(SessionKey unverifiedTracker) {
-		this.unverifiedTracker = unverifiedTracker;
+		this.detectedTransportAddress = detectedTransportAddress;
 	}
 	
 }
