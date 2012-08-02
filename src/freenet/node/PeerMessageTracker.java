632b430ba06d799333aa3c1e6078fb9815817874
chetanhosmani <chetanhosmani0@gmail.com>
2012-08-02 19:53:19 +0530

    Add simple message tracker which will be used per PeerNode.
    Nothing concrete. A simple idea.

--- src/freenet/node/PeerMessageTracker.java
+++ src/freenet/node/PeerMessageTracker.java
@@ -1,10 +1,37 @@
 package freenet.node;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import freenet.io.comm.DMT;
+
 /**
  * This class has been created to isolate the Message tracking portions of code in NewPacketFormat
  * @author chetan
  *
  */
 public class PeerMessageTracker {
+	
+	/** 
+	 * The actual buffer of outgoing messages that have not yet been acked.
+	 * LOCKING: Protected by sendBufferLock. 
+	 */
+	private final ArrayList<HashMap<Integer, MessageWrapper>> startedByPrio;
+	
+	public final PeerNode pn;
+	
+	public PeerMessageTracker(PeerNode pn) {
+		
+		this.pn = pn;
+		
+		startedByPrio = new ArrayList<HashMap<Integer, MessageWrapper>>(DMT.NUM_PRIORITIES);
+		for(int i = 0; i < DMT.NUM_PRIORITIES; i++) {
+			startedByPrio.add(new HashMap<Integer, MessageWrapper>());
+		}
+	}
+	
+	public MessageFragment getMessageFragment(int messageLength) {
+		return null;
+	}
 
 }
