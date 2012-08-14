b8f19fa804c95942bb9abd5b190c21e4acbaffac
Steve Dougherty <steve@asksteved.com>
2012-08-14 16:02:20 -0400

    Specify invalid HopsToLive value in error.

--- src/freenet/node/fcp/ProbeRequest.java
+++ src/freenet/node/fcp/ProbeRequest.java
@@ -56,7 +56,7 @@ public class ProbeRequest extends FCPMessage {
 			throw new MessageInvalidException(ProtocolErrorMessage.INVALID_MESSAGE, "Unrecognized parse probe type \"" + fs.get(TYPE) + "\": " + e, null, false);
 		} catch (FSParseException e) {
 			//Getting a String from a SimpleFieldSet does not throw - it can at worst return null.
-			throw new MessageInvalidException(ProtocolErrorMessage.INVALID_MESSAGE, "Unable to parse hopsToLive: " + e, null, false);
+			throw new MessageInvalidException(ProtocolErrorMessage.INVALID_MESSAGE, "Unable to parse hopsToLive \"" + fs.get(HTL) + "\": " + e, null, false);
 		}
 	}
 
