hyphanet/fred

commit 78719d6250aefbc58e86409e96aacbabed67011d

revert overzealous conversion of char->byte and add encodeUTF8/decodeUTF8 methods to make encoding well-defined and explicit

src/freenet/node/fcp/PeerNote.java
@@ -13,26 +13,26 @@ import freenet.support.SimpleFieldSet;
 
 public class PeerNote extends FCPMessage {
 	static final String name = "PeerNote";
-	
+
 	final String noteText;
 	final int peerNoteType;
 	final String nodeIdentifier;
 	final String identifier;
-	
+
 	public PeerNote(String nodeIdentifier, String noteText, int peerNoteType, String identifier) {
 		this.nodeIdentifier = nodeIdentifier;
 		this.noteText = noteText;
 		this.peerNoteType = peerNoteType;
 		this.identifier = identifier;
 	}
-	
+
 	@Override
 	public SimpleFieldSet getFieldSet() {
 		SimpleFieldSet fs = new SimpleFieldSet(true);
 		fs.putSingle("NodeIdentifier", nodeIdentifier);
 		fs.put("PeerNoteType", peerNoteType);
 		try {
-			fs.putSingle("NoteText", Base64.encode(noteText.getBytes("UTF-8"), true));
+			fs.putSingle("NoteText", Base64.encodeUTF8(noteText, true));
 		} catch (UnsupportedEncodingException e) {
 			throw new Error("Impossible: JVM doesn't support UTF-8: " + e, e);
 		}

src/freenet/node/DarknetPeerNode.java
@@ -92,7 +92,7 @@ public class DarknetPeerNode extends PeerNode {
 	private LinkedHashSet<Integer> queuedToSendN2NMExtraPeerDataFileNumbers;
 
 	private FRIEND_TRUST trustLevel;
-	
+
 	private FRIEND_VISIBILITY ourVisibility;
 	private FRIEND_VISIBILITY theirVisibility;
 
@@ -111,16 +111,16 @@ public class DarknetPeerNode extends PeerNode {
 		}
 
 	}
-	
+
 	public enum FRIEND_VISIBILITY {
 		YES((short)0), // Visible
 		NAME_ONLY((short)1), // Only the name is visible, but other friends can ask for a connection
 		NO((short)2); // Not visible to our other friends at all
-		
-		/** The codes are persistent and used to communicate between nodes, so they must not change. 
+
+		/** The codes are persistent and used to communicate between nodes, so they must not change.
 		 * Which is why we are not using ordinal(). */
 		final short code;
-		
+
 		FRIEND_VISIBILITY(short code) {
 			this.code = code;
 		}
@@ -138,7 +138,7 @@ public class DarknetPeerNode extends PeerNode {
 			return null;
 		}
 	}
-	
+
 	/**
 	 * Create a darknet PeerNode from a SimpleFieldSet
 	 * @param fs The SimpleFieldSet to parse
@@ -183,7 +183,7 @@ public class DarknetPeerNode extends PeerNode {
 				theirVisibility = FRIEND_VISIBILITY.valueOf(s);
 			} else {
 				theirVisibility = FRIEND_VISIBILITY.NO;
-			} 
+			}
 		} else {
 			if(trust == null) throw new IllegalArgumentException();
 			trustLevel = trust;
@@ -571,7 +571,7 @@ public class DarknetPeerNode extends PeerNode {
 			if(peerNoteType == Node.PEER_NOTE_TYPE_PRIVATE_DARKNET_COMMENT) {
 				synchronized(privateDarknetComment) {
 				  	try {
-						privateDarknetComment = new String(Base64.decode(fs.get("privateDarknetComment")));
+						privateDarknetComment = Base64.decodeUTF8(fs.get("privateDarknetComment"));
 					} catch (IllegalBase64Exception e) {
 						Logger.error(this, "Bad Base64 encoding when decoding a private darknet comment SimpleFieldSet", e);
 						return false;
@@ -816,11 +816,7 @@ public class DarknetPeerNode extends PeerNode {
 		}
 		SimpleFieldSet fs = new SimpleFieldSet(true);
 		fs.put("peerNoteType", Node.PEER_NOTE_TYPE_PRIVATE_DARKNET_COMMENT);
-		try {
-			fs.putSingle("privateDarknetComment", Base64.encode(comment.getBytes("UTF-8")));
-		} catch (UnsupportedEncodingException e) {
-			throw new Error("Impossible: JVM doesn't support UTF-8: " + e, e);
-		}
+		fs.putSingle("privateDarknetComment", Base64.encodeUTF8(comment));
 		if(localFileNumber == -1) {
 			localFileNumber = writeNewExtraPeerDataFile(fs, Node.EXTRA_PEER_DATA_TYPE_PEER_NOTE);
 			synchronized(privateDarknetComment) {
@@ -923,11 +919,10 @@ public class DarknetPeerNode extends PeerNode {
 			String s = fs.get("comment");
 			if(s != null) {
 				try {
-					s = new String(Base64.decode(s), "UTF-8");
-				} catch (UnsupportedEncodingException e) {
-					throw new Error("Impossible: JVM doesn't support UTF-8: " + e, e);
+					s = Base64.decodeUTF8(s);
 				} catch (IllegalBase64Exception e) {
 					// Maybe it wasn't encoded? FIXME remove
+					Logger.error(this, "Bad Base64 encoding when decoding a private darknet comment SimpleFieldSet", e);
 				}
 			}
 			comment = s;
@@ -938,11 +933,7 @@ public class DarknetPeerNode extends PeerNode {
 			fs.put("uid", uid);
 			fs.putSingle("filename", filename);
 			fs.putSingle("metadata.contentType", mimeType);
-			try {
-				fs.putSingle("comment", Base64.encode(comment.getBytes("UTF-8")));
-			} catch (UnsupportedEncodingException e) {
-				throw new Error("Impossible: JVM doesn't support UTF-8: " + e, e);
-			}
+			fs.putSingle("comment", Base64.encodeUTF8(comment));
 			fs.put("size", size);
 		}
 
@@ -1362,12 +1353,7 @@ public class DarknetPeerNode extends PeerNode {
 		fs.put("composedTime", now);
 		fs.put("hasAnActivelink", hasAnActiveLink);
 		if(description != null)
-			try {
-				fs.putSingle("Description", Base64.encode(description.getBytes("UTF-8")));
-			} catch (UnsupportedEncodingException e) {
-				throw new Error("Impossible: JVM doesn't support UTF-8: " + e, e);
-
-			}
+			fs.putSingle("Description", Base64.encodeUTF8(description));
 		fs.put("type", Node.N2N_TEXT_MESSAGE_TYPE_BOOKMARK);
 		sendNodeToNodeMessage(fs, Node.N2N_MESSAGE_TYPE_FPROXY, true, now, true);
 		setPeerNodeStatus(System.currentTimeMillis());
@@ -1379,12 +1365,8 @@ public class DarknetPeerNode extends PeerNode {
 		SimpleFieldSet fs = new SimpleFieldSet(true);
 		fs.putSingle("URI", URI.toString());
 		fs.put("composedTime", now);
-		if(description != null)
-			try {
-				fs.putSingle("Description", Base64.encode(description.getBytes("UTF-8")));
-			} catch (UnsupportedEncodingException e) {
-				throw new Error("Impossible: JVM doesn't support UTF-8: " + e, e);
-
+		if(description != null) {
+			fs.putSingle("Description", Base64.encodeUTF8(description));
 		}
 		fs.put("type", Node.N2N_TEXT_MESSAGE_TYPE_DOWNLOAD);
 		sendNodeToNodeMessage(fs, Node.N2N_MESSAGE_TYPE_FPROXY, true, now, true);
@@ -1396,15 +1378,11 @@ public class DarknetPeerNode extends PeerNode {
 		long now = System.currentTimeMillis();
 		SimpleFieldSet fs = new SimpleFieldSet(true);
 		fs.put("type", Node.N2N_TEXT_MESSAGE_TYPE_USERALERT);
-		try {
-			fs.putSingle("text", Base64.encode(message.getBytes("UTF-8")));
-			fs.put("composedTime", now);
-			sendNodeToNodeMessage(fs, Node.N2N_MESSAGE_TYPE_FPROXY, true, now, true);
-			this.setPeerNodeStatus(System.currentTimeMillis());
-			return getPeerNodeStatus();
-		} catch (UnsupportedEncodingException e) {
-			throw new Error("Impossible: JVM doesn't support UTF-8: " + e, e);
-		}
+		fs.putSingle("text", Base64.encodeUTF8(message));
+		fs.put("composedTime", now);
+		sendNodeToNodeMessage(fs, Node.N2N_MESSAGE_TYPE_FPROXY, true, now, true);
+		this.setPeerNodeStatus(System.currentTimeMillis());
+		return getPeerNodeStatus();
 	}
 
 	public int sendFileOfferAccepted(long uid) {
@@ -1477,7 +1455,7 @@ public class DarknetPeerNode extends PeerNode {
 		long sentTime = fs.getLong("sentTime", -1);
 		long receivedTime = fs.getLong("receivedTime", -1);
 	  	try {
-			text = new String(Base64.decode(fs.get("text")));
+			text = Base64.decodeUTF8(fs.get("text"));
 		} catch (IllegalBase64Exception e) {
 			Logger.error(this, "Bad Base64 encoding when decoding a N2NTM SimpleFieldSet", e);
 			return;
@@ -1597,7 +1575,7 @@ public class DarknetPeerNode extends PeerNode {
 		long receivedTime = fs.getLong("receivedTime", -1);
 		try {
 			if(fs.get("Description") != null)
-				description = new String(Base64.decode(fs.get("Description")));
+				description = Base64.decodeUTF8(fs.get("Description"));
 			uri = new FreenetURI(fs.get("URI"));
 		} catch (MalformedURLException e) {
 			Logger.error(this, "Malformed URI in N2NTM Bookmark Feed message");
@@ -1618,7 +1596,7 @@ public class DarknetPeerNode extends PeerNode {
 		long receivedTime = fs.getLong("receivedTime", -1);
 		try {
 			if(fs.get("Description") != null)
-				description = new String(Base64.decode(fs.get("Description")));
+				description = Base64.decodeUTF8(fs.get("Description"));
 			uri = new FreenetURI(fs.get("URI"));
 		} catch (MalformedURLException e) {
 			Logger.error(this, "Malformed URI in N2NTM File Feed message");
@@ -1748,7 +1726,7 @@ public class DarknetPeerNode extends PeerNode {
 		node.peers.writePeersDarknetUrgent();
 	}
 
-	/** FIXME This should be the worse of our visibility for the peer and that which the peer has told us. 
+	/** FIXME This should be the worse of our visibility for the peer and that which the peer has told us.
 	 * I.e. visibility is reciprocal. */
 	public synchronized FRIEND_VISIBILITY getVisibility() {
 		// ourVisibility can't be null.
@@ -1772,7 +1750,7 @@ public class DarknetPeerNode extends PeerNode {
 			Logger.normal(this, "Disconnected while sending visibility update");
 		}
 	}
-	
+
 	private void sendVisibility() throws NotConnectedException {
 		sendAsync(DMT.createFNPVisibility(getOurVisibility().code), null, node.nodeStats.initialMessagesCtr);
 	}
@@ -1794,14 +1772,14 @@ public class DarknetPeerNode extends PeerNode {
 		if(theirVisibility == null) return FRIEND_VISIBILITY.NO;
 		return theirVisibility;
 	}
-	
+
 	@Override
 	boolean dontKeepFullFieldSet() {
 		return false;
 	}
-	
+
 	private boolean sendingFullNoderef;
-	
+
 	public void sendFullNoderef() {
 		synchronized(this) {
 			if(sendingFullNoderef) return; // DoS????
@@ -1857,7 +1835,7 @@ public class DarknetPeerNode extends PeerNode {
 						}
 					}
 				}
-				
+
 			});
 		} catch (RuntimeException e) {
 			synchronized(this) {
@@ -1873,7 +1851,7 @@ public class DarknetPeerNode extends PeerNode {
 	}
 
 	private boolean receivingFullNoderef;
-	
+
 	public void handleFullNoderef(Message m) {
 		if(this.dontKeepFullFieldSet()) return;
 		long uid = m.getLong(DMT.UID);
@@ -1938,7 +1916,7 @@ public class DarknetPeerNode extends PeerNode {
 						}
 					}
 				}
-			});				
+			});
 		} catch (RuntimeException e) {
 			synchronized(this) {
 				receivingFullNoderef = false;

src/freenet/node/fcp/ModifyPeerNote.java
@@ -17,10 +17,10 @@ import freenet.support.SimpleFieldSet;
 public class ModifyPeerNote extends FCPMessage {
 
 	static final String NAME = "ModifyPeerNote";
-	
+
 	final SimpleFieldSet fs;
 	final String identifier;
-	
+
 	public ModifyPeerNote(SimpleFieldSet fs) {
 		this.fs = fs;
 		identifier = fs.get("Identifier");
@@ -69,7 +69,7 @@ public class ModifyPeerNote extends FCPMessage {
 		String noteText;
 		// **FIXME** this should be generalized for multiple peer notes per peer, after PeerNode is similarly generalized
 		try {
-			noteText = new String(Base64.decode(encodedNoteText));
+			noteText = Base64.decodeUTF8(encodedNoteText);
 		} catch (IllegalBase64Exception e) {
 			Logger.error(this, "Bad Base64 encoding when decoding a FCP-received private darknet comment SimpleFieldSet", e);
 			return;

src/freenet/support/Base64.java
@@ -2,6 +2,7 @@ package freenet.support;
 
 import java.nio.charset.Charset;
 
+
 /**
  * This class provides encoding of byte arrays into Base64-encoded strings,
  * and decoding the other way.
@@ -19,11 +20,11 @@ import java.nio.charset.Charset;
  */
 public class Base64
 {
-	static final Charset UTF8 = Charset.forName("UTF-8");
+  static final Charset UTF8 = Charset.forName("UTF-8");
 
-  private static byte[] base64Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~-".getBytes(UTF8);
+  private static char[] base64Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~-".toCharArray();
 
-  private static byte[] base64StandardAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".getBytes(UTF8);
+  private static char[] base64StandardAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".toCharArray();
 
   /**
    * A reverse lookup table to convert base64 letters back into the
@@ -31,12 +32,12 @@ public class Base64
    */
   private static byte[] base64Reverse;
   private static byte[] base64StandardReverse;
-  
+
    // Populate the base64Reverse lookup table from the base64Alphabet table.
   static {
     base64Reverse = new byte[128];
     base64StandardReverse = new byte[base64Reverse.length];
-    
+
       // Set all entries to 0xFF, which means that that particular letter
       // is not a legal base64 letter.
     for (int i = 0; i < base64Reverse.length; i++) {
@@ -60,28 +61,48 @@ public class Base64
   /* FIXME: Figure out where this function is used and maybe remove it if its not
    * used. Its old javadoc which has been here for a while fools the user into believing
    * that the format is standard compliant */
-  
+
   /**
    * Caller should specify equalsPad=true if they want a standards compliant padding,
    * but not standard compliant encoding.
    */
   public static String encode(byte[] in, boolean equalsPad) {
-	  return encode(in, equalsPad, base64Alphabet);
+    return encode(in, equalsPad, base64Alphabet);
   }
-  
+
+  /**
+   * Convenience method to encode a string, in our shortened format.
+   *
+   * Please use this to encode a string, rather than trying to encode the string
+   * yourself using the 0-arg String.getBytes() which is not deterministic.
+   */
+  public static String encodeUTF8(String in) {
+    return encodeUTF8(in, false);
+  }
+
+  /**
+   * Convenience method to encode a string.
+   *
+   * Please use this to encode a string, rather than trying to encode the string
+   * yourself using the 0-arg String.getBytes() which is not deterministic.
+   */
+  public static String encodeUTF8(String in, boolean equalsPad) {
+    return encode(in.getBytes(UTF8), equalsPad, base64Alphabet);
+  }
+
   /**
    * Standard compliant encoding.
    */
   public static String encodeStandard(byte[] in) {
-	  return encode(in, true, base64StandardAlphabet);
+    return encode(in, true, base64StandardAlphabet);
   }
-  
+
   /**
    * Caller should specify equalsPad=true if they want a standards compliant encoding.
    */
-  private static String encode(byte[] in, boolean equalsPad, byte[] alphabet)
+  private static String encode(byte[] in, boolean equalsPad, char[] alphabet)
   {
-    byte[] out = new byte[((in.length+2)/3)*4];
+    char[] out = new char[((in.length+2)/3)*4];
     int rem = in.length%3;
     int o = 0;
     for (int i = 0; i < in.length;) {
@@ -104,23 +125,33 @@ public class Base64
     if (equalsPad)
       while (outLen < out.length)
         out[outLen++] = '=';
-    return new String(out, 0, outLen, UTF8);
+    return new String(out, 0, outLen);
   }
 
   /**
    * Handles the standards-compliant padding (padded with '=' signs) as well as our
    * shortened form.
- * @throws IllegalBase64Exception 
+   * @throws IllegalBase64Exception
    */
   public static byte[] decode(String inStr) throws IllegalBase64Exception {
-	  return decode(inStr, base64Reverse);
+    return decode(inStr, base64Reverse);
   }
-  
+
+  /**
+   * Convenience method to decode into a string, in our shortened format.
+   *
+   * Please use this to decode into a string, rather than trying to decode the
+   * string yourself using new String(bytes[]) which is not deterministic.
+   */
+  public static String decodeUTF8(String inStr) throws IllegalBase64Exception {
+    return new String(decode(inStr), UTF8);
+  }
+
   /**
    * Handles the standards-compliant base64 encoding.
    */
   public static byte[] decodeStandard(String inStr) throws IllegalBase64Exception {
-	  return decode(inStr, base64StandardReverse);
+    return decode(inStr, base64StandardReverse);
   }
 
   /**
@@ -131,7 +162,7 @@ public class Base64
     throws IllegalBase64Exception
   {
     try {
-      byte[] in = inStr.getBytes(UTF8);
+      char[] in = inStr.toCharArray();
       int inLength = in.length;
 
         // Strip trailing equals signs.
@@ -165,7 +196,7 @@ public class Base64
         int outVal = (in1 << 18) | (in2 << 12) | (in3 << 6) | in4;
         out[o] = (byte) (outVal>>16);
         out[o+1] = (byte) (outVal>>8);
-        out[o+2] = (byte) outVal; 
+        out[o+2] = (byte) outVal;
         i += 4;
         o += 3;
       }

test/freenet/support/Base64Test.java
@@ -23,7 +23,7 @@ import junit.framework.TestCase;
 
 /**
  * Test case for {@link freenet.support.Base64} class.
- * 
+ *
  * @author Alberto Bacchelli &lt;sback@freenetproject.org&gt;
  */
 public class Base64Test extends TestCase {
@@ -48,11 +48,11 @@ public class Base64Test extends TestCase {
 	 * (see http://en.wikipedia.org/wiki/Base_64 as reference)
 	 * to verify if it encode works correctly.
 	 */
-	public void testEncode() {
-		byte[] aByteArrayToEncode = toEncode.getBytes();
+	public void testEncode() throws Exception {
+		byte[] aByteArrayToEncode = toEncode.getBytes("UTF-8");
 		assertEquals(Base64.encode(aByteArrayToEncode), toDecode);
 	}
-	
+
 	/**
 	 * Test the decode(String) method
 	 * against a well-known example
@@ -68,8 +68,8 @@ public class Base64Test extends TestCase {
 	 * Test the encodeStandard(byte[]) method
 	 * This is the same as encode() from generator/js/src/freenet/client/tools/Base64.java
 	 */
-	public void testEncodeStandard() {
-		byte[] aByteArrayToEncode = testSample.getBytes();
+	public void testEncodeStandard() throws Exception {
+		byte[] aByteArrayToEncode = testSample.getBytes("UTF-8");
 		assertEquals(Base64.encodeStandard(aByteArrayToEncode), testSampleStandardEncoding);
 	}
 
@@ -78,7 +78,7 @@ public class Base64Test extends TestCase {
 	 * This is the same as decode() from generator/js/src/freenet/client/tools/Base64.java
 	 */
 	public void testDecodeStandard() throws Exception {
-		String decodedString = new String(Base64.decodeStandard(testSampleStandardEncoding));
+		String decodedString = new String(Base64.decodeStandard(testSampleStandardEncoding), "UTF-8");
 		assertEquals(decodedString, testSample);
 	}
 
@@ -92,24 +92,24 @@ public class Base64Test extends TestCase {
 	public void testEncodeDecode() {
 		byte[] bytesDecoded;
 		byte[] bytesToEncode = new byte[5];
-		
+
 		//byte upper bound
 		bytesToEncode[0] = 127;
 		bytesToEncode[1] = 64;
 		bytesToEncode[2] = 0;
 		bytesToEncode[3] = -64;
 		//byte lower bound
-		bytesToEncode[4] = -128;	
-		
+		bytesToEncode[4] = -128;
+
 		String aBase64EncodedString = Base64.encode(bytesToEncode);
-		
+
 		try {
 			bytesDecoded = Base64.decode(aBase64EncodedString);
-			assertTrue(Arrays.equals(bytesToEncode,bytesDecoded)); } 
+			assertTrue(Arrays.equals(bytesToEncode,bytesDecoded)); }
 		catch (IllegalBase64Exception aException) {
 			fail("Not expected exception thrown : " + aException.getMessage()); }
 	}
-	
+
 	/**
 	 * Test the encode(String,boolean)
 	 * method to verify if the padding
@@ -118,13 +118,13 @@ public class Base64Test extends TestCase {
 	public void testEncodePadding() {
 		byte[][] methodBytesArray = {
 				//three byte Array -> no padding char expected
-				{4,4,4},		
+				{4,4,4},
 				//two byte Array -> one padding char expected
-				{4,4},		
-				//one byte Array -> two padding-chars expected	
-				{4}};		
+				{4,4},
+				//one byte Array -> two padding-chars expected
+				{4}};
 		String encoded;
-		
+
 		for (int i = 0; i<methodBytesArray.length; i++) {
 			encoded = Base64.encode(methodBytesArray[i],true);
 			if (i == 0)
@@ -134,7 +134,7 @@ public class Base64Test extends TestCase {
 				assertEquals(encoded.indexOf('='),encoded.length()-i);
 		}
 	}
-	
+
 	/**
 	 * Test if the decode(String) method
 	 * raise correctly an exception when
@@ -150,11 +150,11 @@ public class Base64Test extends TestCase {
 		catch (IllegalBase64Exception exception) {
 			assertSame("illegal Base64 character",exception.getMessage()); }
 	}
-	
+
 	/**
 	 * Test if the decode(String) method
 	 * raise correctly an exception when
-	 * providing a string with a 
+	 * providing a string with a
 	 * wrong Base64 length.
 	 * (as we can consider not-padded strings too,
 	 *  the only wrong lengths are the ones
@@ -169,10 +169,10 @@ public class Base64Test extends TestCase {
 		catch (IllegalBase64Exception exception) {
 			assertSame("illegal Base64 length",exception.getMessage()); }
 	}
-	
+
 	/**
 	 * Random test
-	 * 
+	 *
 	 * @throws IllegalBase64Exception
 	 */
 	public void testRandom() throws IllegalBase64Exception {

commit 8d7a3d13aaf5b5352ccdb095efdf3be69bac3c0e

Fix fail-compile-bug introduced in 78719d6250aefbc58e86409e96aacbabed67011d

src/freenet/node/fcp/PeerNote.java
@@ -31,11 +31,7 @@ public class PeerNote extends FCPMessage {
 		SimpleFieldSet fs = new SimpleFieldSet(true);
 		fs.putSingle("NodeIdentifier", nodeIdentifier);
 		fs.put("PeerNoteType", peerNoteType);
-		try {
-			fs.putSingle("NoteText", Base64.encodeUTF8(noteText, true));
-		} catch (UnsupportedEncodingException e) {
-			throw new Error("Impossible: JVM doesn't support UTF-8: " + e, e);
-		}
+		fs.putSingle("NoteText", Base64.encodeUTF8(noteText, true));
 		if(identifier != null)
 			fs.putSingle("Identifier", identifier);
 		return fs;