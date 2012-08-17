395d9374b7b7c585e3afd34b89da4068c947e2e6
Steve Dougherty <steve@asksteved.com>
2012-08-17 19:27:19 -0400

    Fix documentation error.
    Errors no longer use strings - they use codes.

--- src/freenet/node/probe/Error.java
+++ src/freenet/node/probe/Error.java
@@ -15,7 +15,7 @@ public enum Error {
 	TIMEOUT((byte) 2),
 	/**
 	 * An error occurred, but a node did not recognize the error used.
-	 * If this occurs locally, it will be specified along with the description string containing the remote error.
+	 * If this occurs locally, it will be specified along with the unrecognized code.
 	 */
 	UNKNOWN((byte) 3),
 	/**
