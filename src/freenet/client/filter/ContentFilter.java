cddb819c283ebdc5723392a6aaf3e2262cd9e3a1
Matthew Toseland <toad@amphibian.dyndns.org>
2012-08-17 17:35:11 +0100

    Allow image/x-png for png. Seen in the wild by TheSeeker.

--- src/freenet/client/filter/ContentFilter.java
+++ src/freenet/client/filter/ContentFilter.java
@@ -71,7 +71,7 @@ public class ContentFilter {
 				l10n("imageJpegWriteAdvice"), false, null, null, false));
 		
 		// PNG - has a filter
-		register(new MIMEType("image/png", "png", new String[0], new String[0],
+		register(new MIMEType("image/png", "png", new String[] { "image/x-png" }, new String[0],
 				true, false, new PNGFilter(true, true, true), null, false, false, false, false, true, false,
 				l10n("imagePngReadAdvice"),
 				l10n("imagePngWriteAdvice"), false, null, null, false));
