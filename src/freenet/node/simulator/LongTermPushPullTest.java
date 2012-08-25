9a4f62ed71ab30c5fd1a7205f3ce741b1e902a72
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2012-08-25 14:06:14 +0200

    Don’t call deprecated methods.

--- src/freenet/node/simulator/LongTermPushPullTest.java
+++ src/freenet/node/simulator/LongTermPushPullTest.java
@@ -150,7 +150,7 @@ public class LongTermPushPullTest {
 			// PUSH N+1 BLOCKS
 			for (int i = 0; i <= MAX_N; i++) {
 				Bucket data = randomData(node);
-				HighLevelSimpleClient client = node.clientCore.makeClient((short) 0);
+				HighLevelSimpleClient client = node.clientCore.makeClient((short) 0, false, false);
 				FreenetURI uri = new FreenetURI("KSK@" + uid + "-" + dateFormat.format(today.getTime()) + "-" + i);
 				System.out.println("PUSHING " + uri);
 				client.addEventHook(new ClientEventListener() {
@@ -207,7 +207,7 @@ public class LongTermPushPullTest {
 
 			// PULL N+1 BLOCKS
 			for (int i = 0; i <= MAX_N; i++) {
-				HighLevelSimpleClient client = node2.clientCore.makeClient((short) 0);
+				HighLevelSimpleClient client = node2.clientCore.makeClient((short) 0, false, false);
 				Calendar targetDate = (Calendar) today.clone();
 				targetDate.add(Calendar.DAY_OF_MONTH, -((1 << i) - 1));
 
