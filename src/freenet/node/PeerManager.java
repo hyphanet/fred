155a3696a801aced33d5666de917e1788dc5ca97
Matthew Toseland <toad@amphibian.dyndns.org>
2012-08-29 17:52:33 +0100

    Imports.

--- src/freenet/node/PeerManager.java
+++ src/freenet/node/PeerManager.java
@@ -15,7 +15,6 @@ import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
