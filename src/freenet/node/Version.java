f81b9688cfcce8db5e77fcf10082de38e91133fa
Matthew Toseland <toad@amphibian.dyndns.org>
2012-09-12 10:50:51 +0100

    Build 1412, mandatory on the 18th of September.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,17 +50,17 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1411;
+	private static final int buildNumber = 1412;
 
 	/** Oldest build of Fred we will talk to */
-	private static final int oldLastGoodBuild = 1409;
-	private static final int newLastGoodBuild = 1411;
+	private static final int oldLastGoodBuild = 1411;
+	private static final int newLastGoodBuild = 1412;
 	static final long transitionTime;
 
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2012, Calendar.SEPTEMBER, 5, 0, 0, 0 );
+		_cal.set( 2012, Calendar.SEPTEMBER, 18, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 
