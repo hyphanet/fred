d3e7b1385d50b7a09d645ad1afd271d5ca7298d0
Matthew Toseland <toad@amphibian.dyndns.org>
2012-09-25 17:06:35 +0100

    More tests.

--- test/freenet/crypt/CTRBlockCipherTest.java
+++ test/freenet/crypt/CTRBlockCipherTest.java
@@ -9,6 +9,7 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.ShortBufferException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -16,15 +17,16 @@ import junit.framework.TestCase;
 
 import freenet.crypt.ciphers.Rijndael;
 import freenet.support.HexUtil;
+import freenet.support.math.MersenneTwister;
 
 public class CTRBlockCipherTest extends TestCase {
 
 	/** Whether to assume JCA is available, and non-crippled. */
 	public static final boolean TEST_JCA = true;
-	
-	// FIXME test decryptability.
 
-	// FIXME test against JCE if available.
+	private MersenneTwister mt = new MersenneTwister(1634);
+
+	// FIXME test decryptability.
 
 	byte[] NIST_128_ENCRYPT_KEY = HexUtil
 			.hexToBytes("2b7e151628aed2a6abf7158809cf4f3c");
@@ -116,7 +118,10 @@ public class CTRBlockCipherTest extends TestCase {
 					+ "2b0930daa23de94ce87017ba2d84988d"
 					+ "dfc9c58db67aada613c2dd08457941a6");
 
-	public void testNIST() throws UnsupportedCipherException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+	public void testNIST() throws UnsupportedCipherException,
+			NoSuchAlgorithmException, NoSuchPaddingException,
+			InvalidKeyException, InvalidAlgorithmParameterException,
+			IllegalBlockSizeException, BadPaddingException {
 		// CTR mode test vectors.
 		// AES128
 		checkNIST(128, NIST_128_ENCRYPT_KEY, NIST_128_ENCRYPT_IV,
@@ -135,18 +140,43 @@ public class CTRBlockCipherTest extends TestCase {
 				NIST_256_DECRYPT_PLAINTEXT, NIST_256_DECRYPT_CIPHERTEXT);
 	}
 
+	public void testNISTRandomLength() throws UnsupportedCipherException,
+			NoSuchAlgorithmException, NoSuchPaddingException,
+			InvalidKeyException, InvalidAlgorithmParameterException,
+			IllegalBlockSizeException, BadPaddingException,
+			ShortBufferException {
+		// CTR mode test vectors.
+		// AES128
+		checkNISTRandomLength(128, NIST_128_ENCRYPT_KEY, NIST_128_ENCRYPT_IV,
+				NIST_128_ENCRYPT_PLAINTEXT, NIST_128_ENCRYPT_CIPHERTEXT);
+		checkNISTRandomLength(128, NIST_128_DECRYPT_KEY, NIST_128_DECRYPT_IV,
+				NIST_128_DECRYPT_PLAINTEXT, NIST_128_DECRYPT_CIPHERTEXT);
+		// AES192
+		checkNISTRandomLength(192, NIST_192_ENCRYPT_KEY, NIST_192_ENCRYPT_IV,
+				NIST_192_ENCRYPT_PLAINTEXT, NIST_192_ENCRYPT_CIPHERTEXT);
+		checkNISTRandomLength(192, NIST_192_DECRYPT_KEY, NIST_192_DECRYPT_IV,
+				NIST_192_DECRYPT_PLAINTEXT, NIST_192_DECRYPT_CIPHERTEXT);
+		// AES256
+		checkNISTRandomLength(256, NIST_256_ENCRYPT_KEY, NIST_256_ENCRYPT_IV,
+				NIST_256_ENCRYPT_PLAINTEXT, NIST_256_ENCRYPT_CIPHERTEXT);
+		checkNISTRandomLength(256, NIST_256_DECRYPT_KEY, NIST_256_DECRYPT_IV,
+				NIST_256_DECRYPT_PLAINTEXT, NIST_256_DECRYPT_CIPHERTEXT);
+	}
+
 	private void checkNIST(int bits, byte[] key, byte[] iv, byte[] plaintext,
-			byte[] ciphertext) throws UnsupportedCipherException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+			byte[] ciphertext) throws UnsupportedCipherException,
+			NoSuchAlgorithmException, NoSuchPaddingException,
+			InvalidKeyException, InvalidAlgorithmParameterException,
+			IllegalBlockSizeException, BadPaddingException {
 		// First test it with JCA.
-		if(TEST_JCA) {
-			SecretKeySpec k = 
-				new SecretKeySpec(key, "AES");
+		if (TEST_JCA) {
+			SecretKeySpec k = new SecretKeySpec(key, "AES");
 			Cipher c = Cipher.getInstance("AES/CTR/NOPADDING");
 			c.init(Cipher.ENCRYPT_MODE, k, new IvParameterSpec(iv));
 			byte[] output = c.doFinal(plaintext);
 			assertTrue(Arrays.equals(output, ciphertext));
 		}
-		
+
 		Rijndael cipher = new Rijndael(bits, 128);
 		cipher.initialize(key);
 		CTRBlockCipher ctr = new CTRBlockCipher(cipher);
@@ -154,6 +184,124 @@ public class CTRBlockCipherTest extends TestCase {
 		byte[] output = new byte[plaintext.length];
 		ctr.processBytes(plaintext, 0, plaintext.length, output, 0);
 		assertTrue(Arrays.equals(output, ciphertext));
+	}
+
+	private void checkNISTRandomLength(int bits, byte[] key, byte[] iv,
+			byte[] plaintext, byte[] ciphertext)
+			throws UnsupportedCipherException, NoSuchAlgorithmException,
+			NoSuchPaddingException, InvalidKeyException,
+			InvalidAlgorithmParameterException, IllegalBlockSizeException,
+			BadPaddingException, ShortBufferException {
+		for (int i = 0; i < 1024; i++) {
+			// First test it with JCA.
+			long seed = mt.nextLong();
+			if (TEST_JCA) {
+				SecretKeySpec k = new SecretKeySpec(key, "AES");
+				Cipher c = Cipher.getInstance("AES/CTR/NOPADDING");
+				c.init(Cipher.ENCRYPT_MODE, k, new IvParameterSpec(iv));
+				MersenneTwister random = new MersenneTwister(seed);
+				byte[] output = new byte[plaintext.length];
+				int inputPtr = 0;
+				int outputPtr = 0;
+				// Odd API designed for block ciphers etc.
+				// For CTR it should be able to return immediately each time.
+				while (inputPtr < plaintext.length) {
+					int max = plaintext.length - inputPtr;
+					int count = (max == 1) ? 1 : (random.nextInt(max - 1) + 1);
+					int moved = c.update(plaintext, inputPtr, count, output,
+							outputPtr);
+					outputPtr += moved;
+					inputPtr += moved;
+				}
+				c.doFinal(plaintext, 0, plaintext.length - outputPtr, output,
+						outputPtr);
+				assertTrue(Arrays.equals(output, ciphertext));
+			}
+
+			Rijndael cipher = new Rijndael(bits, 128);
+			cipher.initialize(key);
+			CTRBlockCipher ctr = new CTRBlockCipher(cipher);
+			ctr.init(iv);
+			byte[] output = new byte[plaintext.length];
+			MersenneTwister random = new MersenneTwister(seed);
+			int ptr = 0;
+			while (ptr < plaintext.length) {
+				int max = plaintext.length - ptr;
+				int count = (max == 1) ? 1 : (random.nextInt(max - 1) + 1);
+				ctr.processBytes(plaintext, ptr, count, output, ptr);
+				ptr += count;
+			}
+			assertTrue(Arrays.equals(output, ciphertext));
+		}
+	}
+	
+	public void testRandomJCA() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+		for(int i=0;i<1024;i++) {
+			byte[] plaintext = new byte[mt.nextInt(4096)+1];
+			byte[] key = new byte[32];
+			byte[] iv = new byte[16];
+			mt.nextBytes(plaintext);
+			mt.nextBytes(key);
+			mt.nextBytes(iv);
+			SecretKeySpec k = new SecretKeySpec(key, "AES");
+			Cipher c = Cipher.getInstance("AES/CTR/NOPADDING");
+			c.init(Cipher.ENCRYPT_MODE, k, new IvParameterSpec(iv));
+			byte[] output = c.doFinal(plaintext);
+			c = Cipher.getInstance("AES/CTR/NOPADDING");
+			c.init(Cipher.DECRYPT_MODE, k, new IvParameterSpec(iv));
+			byte[] decrypted = c.doFinal(output);
+			assertTrue(Arrays.equals(decrypted, plaintext));
+		}
+	}
+	
+	public void testRandom() throws UnsupportedCipherException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+		for(int i=0;i<1024;i++) {
+			byte[] plaintext = new byte[mt.nextInt(4096)+1];
+			byte[] key = new byte[32];
+			byte[] iv = new byte[16];
+			mt.nextBytes(plaintext);
+			mt.nextBytes(key);
+			mt.nextBytes(iv);
+			// First encrypt as a block.
+			Rijndael cipher = new Rijndael(256, 128);
+			cipher.initialize(key);
+			CTRBlockCipher ctr = new CTRBlockCipher(cipher);
+			ctr.init(iv);
+			byte[] ciphertext = new byte[plaintext.length];
+			ctr.processBytes(plaintext, 0, plaintext.length, ciphertext, 0);
+			// Now decrypt.
+			ctr = new CTRBlockCipher(cipher);
+			ctr.init(iv);
+			byte[] finalPlaintext = new byte[plaintext.length];
+			ctr.processBytes(ciphertext, 0, ciphertext.length, finalPlaintext, 0);
+			assertTrue(Arrays.equals(finalPlaintext, plaintext));
+			if(TEST_JCA) {
+				SecretKeySpec k = new SecretKeySpec(key, "AES");
+				Cipher c = Cipher.getInstance("AES/CTR/NOPADDING");
+				c.init(Cipher.ENCRYPT_MODE, k, new IvParameterSpec(iv));
+				byte[] output = c.doFinal(plaintext);
+				assertTrue(Arrays.equals(output, ciphertext));
+				c = Cipher.getInstance("AES/CTR/NOPADDING");
+				c.init(Cipher.DECRYPT_MODE, k, new IvParameterSpec(iv));
+				byte[] decrypted = c.doFinal(output);
+				assertTrue(Arrays.equals(decrypted, plaintext));
+			}
+			// Now encrypt again, in random pieces.
+			cipher.initialize(key);
+			ctr = new CTRBlockCipher(cipher);
+			ctr.init(iv);
+			byte[] output = new byte[plaintext.length];
+			MersenneTwister random = new MersenneTwister(mt.nextLong());
+			int ptr = 0;
+			while (ptr < plaintext.length) {
+				int max = plaintext.length - ptr;
+				int count = (max == 1) ? 1 : (random.nextInt(max - 1) + 1);
+				ctr.processBytes(plaintext, ptr, count, output, ptr);
+				ptr += count;
+			}
+			assertTrue(Arrays.equals(output, ciphertext));
+			
+		}
 	}
 
 }
