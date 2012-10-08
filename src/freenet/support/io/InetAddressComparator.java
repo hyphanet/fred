df1a4796a7778b5fa25cd853982663ef80d5b32a
Matthew Toseland <toad@amphibian.dyndns.org>
2012-10-08 17:07:04 +0100

    Don't need to check types.

--- src/freenet/support/io/InetAddressComparator.java
+++ src/freenet/support/io/InetAddressComparator.java
@@ -20,11 +20,6 @@ public class InetAddressComparator implements Comparator<InetAddress> {
 		// By hash code first. Works really fast for IPv4.
 		if(a > b) return 1;
 		else if(b > a) return -1;
-		// IPv6 > IPv4.
-		if(arg0 instanceof Inet4Address && arg1 instanceof Inet6Address)
-			return -1;
-		if(arg0 instanceof Inet6Address && arg1 instanceof Inet4Address)
-			return 1;
 		return Fields.compareBytes(arg0.getAddress(), arg1.getAddress());
 		// Hostnames in InetAddress are merely cached, equals() only operates on the byte[].
 	}
