hyphanet/fred

commit 2b54036d9702d46524dd87c394eefb46d05f9168

Open input streams to resources in local Jar files using the same file descriptor. This allows to unlock Jar files on JarClassLoader.close() and successfully delete them on Windows.

src/freenet/support/JarClassLoader.java
@@ -31,6 +31,7 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 import java.util.jar.Attributes.Name;
+import java.util.zip.ZipEntry;
 
 import freenet.support.io.FileUtil;
 
@@ -150,7 +151,7 @@ public class JarClassLoader extends ClassLoader implements Closeable {
 	 */
 	@Override
 	protected URL findResource(String name) {
-		/* compatibility code. remove when all plugins are fixed. */
+		/* FIXME compatibility code. remove when all plugins are fixed. */
 		if (name.startsWith("/")) {
 			name = name.substring(1);
 		}
@@ -163,6 +164,42 @@ public class JarClassLoader extends ClassLoader implements Closeable {
 		} catch (MalformedURLException e) {
 		}
 		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see java.lang.ClassLoader#getResourceAsStream(java.lang.String)
+	 */
+	@Override
+	public InputStream getResourceAsStream(String name) {
+		URL url = getResource(name);
+		if (url == null)
+			return null;
+
+		// If the resource is not from our jar, return it as normal
+		if (!url.toString().equals(findResource(name).toString()))
+			try {
+				return url.openStream();
+			} catch (IOException e) {
+				return null;
+			}
+
+		// If the resource is from our jar, open InputStream explicitly from the jar
+		// so that we can close() all opened streams later and let the jar file
+		// to be deleted on Windows
+
+		/* FIXME compatibility code. remove when all plugins are fixed. */
+		if (name.startsWith("/")) {
+			name = name.substring(1);
+		}
+
+		ZipEntry entry = tempJarFile.getEntry(name);
+		try {
+			return entry != null ? tempJarFile.getInputStream(entry) : null;
+		} catch (IOException e) {
+			return null;
+		}
 	}
 
 	/**

commit 02fba89cec7e56e81947ef4e4679e8480ef91fa8

Maybe fix bug introduced in 2b54036d9702d46524dd87c394eefb46d05f9168 which broke WoT.

src/freenet/support/JarClassLoader.java
@@ -171,12 +171,24 @@ public class JarClassLoader extends ClassLoader implements Closeable {
 	}
 
 	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see java.lang.ClassLoader#getResourceAsStream(java.lang.String)
+	 * Load a resource from this jar or from the parent ClassLoader.
 	 */
 	@Override
 	public InputStream getResourceAsStream(String name) {
+		InputStream is = getResourceAsStreamInner(name);
+		if(is != null)
+			return is;
+		else
+			return super.getResourceAsStream(name);
+	}
+
+	/** Only checks the jar, but opens the stream using ZipEntry's, so when
+	 * tempJarFile is closed, so are all the streams, hence we can delete 
+	 * the jar on Windows.
+	 * @return An InputStream from the jar if the name refers to a resource
+	 * within this jar file. 
+	 */
+	public InputStream getResourceAsStreamInner(String name) {
 		if(logMINOR) Logger.minor(this, "Requested resource: " + name, new Exception("debug"));
 		URL url = getResource(name);
 		if (url == null)