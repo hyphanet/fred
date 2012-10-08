6d8b992f38b7f9b86a7917bb4a8868f77203ac15
Matthew Toseland <toad@amphibian.dyndns.org>
2012-10-08 17:12:55 +0100

    More changes to InetAddressComparator, and document it.

--- src/freenet/support/io/InetAddressComparator.java
+++ src/freenet/support/io/InetAddressComparator.java
@@ -5,6 +5,11 @@ import java.util.Comparator;
 
 import freenet.support.Fields;
 
+/** Fast non-lexical Comparator for IP addresses for cases where an 
+ * attacker might forge IP addresses to try to exhaust a hashtable, so we 
+ * need to use a TreeMap, but we don't actually care whether similar IPs 
+ * are close together.
+ * @author toad */
 public class InetAddressComparator implements Comparator<InetAddress> {
 
 	public final static InetAddressComparator COMPARATOR =
@@ -18,7 +23,14 @@ public class InetAddressComparator implements Comparator<InetAddress> {
 		// By hash code first. Works really fast for IPv4.
 		if(a > b) return 1;
 		else if(b > a) return -1;
-		return Fields.compareBytes(arg0.getAddress(), arg1.getAddress());
+		byte[] bytes0 = arg0.getAddress();
+		byte[] bytes1 = arg1.getAddress();
+		// Fields.compareBytes doesn't go first by length, so check it here.
+		if(bytes0.length > bytes1.length)
+			return 1; // IPv6 > IPv4.
+		if(bytes1.length > bytes0.length)
+			return -1; // IPv4 > IPv6.
+		return Fields.compareBytes(bytes0, bytes1);
 		// Hostnames in InetAddress are merely cached, equals() only operates on the byte[].
 	}
 
