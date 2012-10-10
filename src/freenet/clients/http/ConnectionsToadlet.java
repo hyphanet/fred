fae436c2f5d2cef5fa2de8b1a1685513e157402c
Matthew Toseland <toad@amphibian.dyndns.org>
2012-10-10 11:56:39 +0100

    Only insert the flag <img> if we have the flag. We don't have all of them.

--- src/freenet/clients/http/ConnectionsToadlet.java
+++ src/freenet/clients/http/ConnectionsToadlet.java
@@ -900,8 +900,11 @@ public abstract class ConnectionsToadlet extends Toadlet {
 			String addr = peerNodeStatus.getPeerAddressNumerical();
 			if(addr != null && !addr.contains(":")) {
 				Country country = ipc.locateIP(addr);
-				if(country != null)
-					locationChild.addChild("img", new String[] { "src", "title" }, new String[] { "/static/icon/flags/"+country.toString().toLowerCase()+".png", country.getName()});
+				if(country != null) {
+					String flagPath = "/static/icon/flags/"+country.toString().toLowerCase()+".png";
+					if(StaticToadlet.haveFile(flagPath))
+						locationChild.addChild("img", new String[] { "src", "title" }, new String[] { flagPath, country.getName()});
+				}
 			}
 			locationChild.addChild("#", ((peerNodeStatus.getPeerAddress() != null) ? (peerNodeStatus.getPeerAddress() + ':' + peerNodeStatus.getPeerPort()) : (l10n("unknownAddress"))) + pingTime);
 		}
