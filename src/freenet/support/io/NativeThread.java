032e0c12122340002be9417a0e399ea3c543a45d
Matthew Toseland <toad@amphibian.dyndns.org>
2012-10-15 21:39:57 +0100

    Remove ancient check

--- src/freenet/support/io/NativeThread.java
+++ src/freenet/support/io/NativeThread.java
@@ -66,7 +66,7 @@ public class NativeThread extends Thread {
 	static {
 		Logger.minor(NativeThread.class, "Running init()");
 		// Loading the NativeThread library isn't useful on macos
-		boolean maybeLoadNative = ("Linux".equalsIgnoreCase(System.getProperty("os.name"))) && (NodeStarter.extBuildNumber > 18);
+		boolean maybeLoadNative = ("Linux".equalsIgnoreCase(System.getProperty("os.name")));
 		Logger.debug(NativeThread.class, "Run init(): should loadNative="+maybeLoadNative);
 		if(maybeLoadNative && LibraryLoader.loadNative("/freenet/support/io/", "NativeThread")) {
 			NATIVE_PRIORITY_BASE = getLinuxPriority();
