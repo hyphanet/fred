9992943857b66cfb29b806c3c74fce9893b40177
Matthew Toseland <toad@amphibian.dyndns.org>
2012-10-23 17:45:44 +0100

    Only check for order for CLASSPATH too

--- src/freenet/node/updater/MainJarDependenciesChecker.java
+++ src/freenet/node/updater/MainJarDependenciesChecker.java
@@ -387,18 +387,20 @@ outer:	for(String propName : props.stringPropertyNames()) {
 			}
 			
 			int order = 0;
-			
-			s = props.getProperty("order");
-			if(s != null) {
-				try {
-					// Order is an optional field.
-					// For most stuff we don't care.
-					// But if it's present it must be correct!
-					order = Integer.parseInt(s);
-				} catch (NumberFormatException e) {
-					System.err.println("Unable to update to build "+build+": dependencies.properties broken: Broken order for "+baseName+" : \""+s+"\"");
-					broken = true;
-					continue;
+
+			if(type == DEPENDENCY_TYPE.CLASSPATH) {
+				s = props.getProperty("order");
+				if(s != null) {
+					try {
+						// Order is an optional field.
+						// For most stuff we don't care.
+						// But if it's present it must be correct!
+						order = Integer.parseInt(s);
+					} catch (NumberFormatException e) {
+						System.err.println("Unable to update to build "+build+": dependencies.properties broken: Broken order for "+baseName+" : \""+s+"\"");
+						broken = true;
+						continue;
+					}
 				}
 			}
 			
