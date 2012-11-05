289a29a9a657b20d629e864625d23637d7a42735
Matthew Toseland <toad@amphibian.dyndns.org>
2012-11-05 12:13:06 +0000

    Trivial

--- src/freenet/clients/http/QueueToadlet.java
+++ src/freenet/clients/http/QueueToadlet.java
@@ -1239,8 +1239,8 @@ public class QueueToadlet extends Toadlet implements RequestCompletionCallback, 
 						completedDownloadToDisk.add(download);
 				} else if(download.hasFinished()) {
 					int failureCode = download.getFailureCode();
+					String mimeType = download.getMIMEType();
 					if(failureCode == FetchException.CONTENT_VALIDATION_UNKNOWN_MIME) {
-						String mimeType = download.getMIMEType();
 						mimeType = ContentFilter.stripMIMEType(mimeType);
 						LinkedList<DownloadRequestStatus> list = failedUnknownMIMEType.get(mimeType);
 						if(list == null) {
@@ -1249,7 +1249,6 @@ public class QueueToadlet extends Toadlet implements RequestCompletionCallback, 
 						}
 						list.add(download);
 					} else if(failureCode == FetchException.CONTENT_VALIDATION_BAD_MIME) {
-						String mimeType = download.getMIMEType();
 						mimeType = ContentFilter.stripMIMEType(mimeType);
 						MIMEType type = ContentFilter.getMIMEType(mimeType);
 						LinkedList<DownloadRequestStatus> list;
