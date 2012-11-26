1a09409b26a354f7f4583a8912a727bbd47e074a
Eleriseth <Eleriseth@WPECGLtYbVi8Rl6Y7Vzl2Lvd2EUVW99v3yNV3IWROG8.fms>
2012-11-26 17:54:24 +0000

    PeerNode.completedAnnounce was completely broken [ this is the actual bug fix, not the cleanup ]
    and we got off with that only because shouldAcceptAnnounce was broken too.

--- src/freenet/node/PeerNode.java
+++ src/freenet/node/PeerNode.java
@@ -3921,6 +3921,9 @@ public abstract class PeerNode implements USKRetrieverCallback, BasePeerNode, Pe
 		for(int i=0;i<runningAnnounceUIDs.length;i++) {
 			long l = runningAnnounceUIDs[i];
 			if(l == uid) continue;
+			if(x == newList.length)
+				// uid was not found in runningAnnounceUIDs
+				return false;
 			newList[x++] = l;
 		}
 		if(x < runningAnnounceUIDs.length) {
