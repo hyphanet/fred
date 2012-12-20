0efd2ceabd69d8d14d9a2bd16a82baa1106bbc6c
Matthew Toseland <toad@amphibian.dyndns.org>
2012-12-20 16:56:20 +0000

    Fix bug #5578.

--- src/freenet/keys/FreenetURI.java
+++ src/freenet/keys/FreenetURI.java
@@ -969,12 +969,14 @@ public class FreenetURI implements Cloneable, Comparable<FreenetURI> {
 		if(keyType != null && (keyType.equals("KSK") || keyType.equals("SSK") || keyType.equals("USK"))) {
 			if(logMINOR)
 				Logger.minor(this, "Adding docName: " + docName);
-			if(docName == null)
-				// Really this shouldn't be possible...
-				throw new NullPointerException();
-			names.add(docName);
-			if(keyType.equals("USK"))
-				names.add(Long.toString(suggestedEdition));
+			if(docName != null) {
+				names.add(docName);
+				if(keyType.equals("USK"))
+					names.add(Long.toString(suggestedEdition));
+			} else if(!keyType.equals("SSK")) {
+				// "SSK@" is legal for an upload.
+				throw new IllegalStateException("No docName for key of type "+keyType);
+			}
 		}
 		if(metaStr != null)
 			for(int i = 0; i < metaStr.length; i++) {
