hyphanet/fred

commit a62d4e130d543c14c203fa12600d1ecfb0c9fc8c

Remove obsolete unused statistics and debugging code

src/freenet/node/Node.java
@@ -5299,11 +5299,6 @@ public class Node implements TimeSkewDetectorCallback {
 	}
 
 
-	private long completeInsertsStored;
-	private long completeInsertsOldStore;
-	private long completeInsertsTotal;
-	private long completeInsertsNotStoredWouldHaveStored;	// DEBUGGING: should be 0 but can be nonzero if e.g. a request originates from a backed off node; should be very low in any case; FIXME remove eventually
-
 	/** Should we commit the block to the store rather than the cache?
 	 *
 	 * <p>We used to check whether we are a sink by checking whether any peer has
@@ -5331,60 +5326,26 @@ public class Node implements TimeSkewDetectorCallback {
     	double target = key.toNormalizedDouble();
     	double myDist = Location.distance(myLoc, target);
 
-    	boolean wouldHaveStored = !peers.isCloserLocation(target, MIN_UPTIME_STORE_KEY);
-
     	// First, calculate whether we would have stored it using the old formula.
-		if(wouldHaveStored)
-			completeInsertsOldStore++;
-
     	if(logMINOR) Logger.minor(this, "Should store for "+key+" ?");
     	// Don't sink store if any of the nodes we routed to, or our predecessor, is both high-uptime and closer to the target than we are.
     	if(source != null && !source.isLowUptime()) {
     		if(Location.distance(source, target) < myDist) {
     	    	if(logMINOR) Logger.minor(this, "Not storing because source is closer to target for "+key+" : "+source);
-    	    	synchronized(this) {
-    	    		completeInsertsTotal++;
-    	    		if(wouldHaveStored) {
-    	    			if(logMINOR) Logger.minor(this, "Would have stored but haven't stored");
-    	    			completeInsertsNotStoredWouldHaveStored++;
-    	    		}
-    	    	}
-    			return false;
     		}
     	}
     	for(PeerNode pn : routedTo) {
     		if(Location.distance(pn, target) < myDist && !pn.isLowUptime()) {
     	    	if(logMINOR) Logger.minor(this, "Not storing because peer "+pn+" is closer to target for "+key+" his loc "+pn.getLocation()+" my loc "+myLoc+" target is "+target);
-    	    	synchronized(this) {
-    	    		completeInsertsTotal++;
-    	    		if(wouldHaveStored) {
-    	    			if(logMINOR) Logger.minor(this, "Would have stored but haven't stored");
-    	    			completeInsertsNotStoredWouldHaveStored++;
-    	    		}
-    	    	}
     			return false;
     		} else {
     			if(logMINOR) Logger.minor(this, "Should store maybe, peer "+pn+" loc = "+pn.getLocation()+" my loc is "+myLoc+" target is "+target+" low uptime is "+pn.isLowUptime());
     		}
     	}
-    	synchronized(this) {
-    		completeInsertsStored++;
-    		completeInsertsTotal++;
-    	}
     	if(logMINOR) Logger.minor(this, "Should store returning true for "+key+" target="+target+" myLoc="+myLoc+" peers: "+routedTo.length);
     	return true;
 	}
 
-
-	private final DecimalFormat fix3p3pct = new DecimalFormat("##0.000%");
-
-	public synchronized void drawStoreStats(HTMLNode infobox) {
-		if (completeInsertsTotal != 0) {
-			infobox.addChild("p", "Stored inserts: "+completeInsertsStored+" of "+completeInsertsTotal+" ("+fix3p3pct.format((completeInsertsStored*1.0)/completeInsertsTotal)+")");
-			infobox.addChild("p", "Would have stored: "+completeInsertsOldStore+" of "+completeInsertsTotal+" ("+fix3p3pct.format((completeInsertsOldStore*1.0)/completeInsertsTotal)+")");
-			infobox.addChild("p", "Would have stored but wasn't stored: "+completeInsertsNotStoredWouldHaveStored+" of "+completeInsertsTotal+" ("+fix3p3pct.format((completeInsertsNotStoredWouldHaveStored*1.0)/completeInsertsTotal)+")");
-		}
-	}
 
 	public boolean getWriteLocalToDatastore() {
 		return writeLocalToDatastore;

src/freenet/node/PeerManager.java
@@ -896,81 +896,6 @@ public class PeerManager {
 		return getRandomPeer(null);
 	}
 
-	public double closestPeerLocation(double loc, double ignoreLoc, int minUptimePercent) {
-		PeerNode[] peers;
-		synchronized(this) {
-			peers = connectedPeers;
-		}
-		double bestDiff = 1.0;
-		double bestLoc = Double.MAX_VALUE;
-		boolean foundOne = false;
-		for(int i = 0; i < peers.length; i++) {
-			PeerNode p = peers[i];
-			if(!p.isRoutable())
-				continue;
-			if(p.outputLoadTracker(true).getLastIncomingLoadStats() == null) {
-				if(logMINOR)
-					Logger.minor(this, "Skipping (no load stats RT): "+p.getPeer());
-				continue;
-			}
-			if(p.outputLoadTracker(false).getLastIncomingLoadStats() == null) {
-				if(logMINOR)
-					Logger.minor(this, "Skipping (no load stats bulk): "+p.getPeer());
-				continue;
-			}
-			if(p.isRoutingBackedOffEither()) {
-				if(logMINOR) Logger.minor(this, "Skipping (backoff): "+p+" loc "+p.getLocation());
-				continue;
-			}
-			if(p.getUptime() < minUptimePercent)
-				continue;
-			double peerloc = p.getLocation();
-			if(Math.abs(peerloc - ignoreLoc) < Double.MIN_VALUE * 2)
-				continue;
-			double diff = Location.distance(peerloc, loc);
-			if(diff < bestDiff) {
-				foundOne = true;
-				bestDiff = diff;
-				if(logMINOR) Logger.minor(this, "Found best loc "+peerloc+" from "+p+" diff = "+diff);
-				bestLoc = peerloc;
-			}
-		}
-		if(!foundOne)
-			if(logMINOR)
-				Logger.minor(this, "closerPeerLocation() not found, trying backed off nodes...");
-			for(int i = 0; i < peers.length; i++) {
-				PeerNode p = peers[i];
-				if(!p.isRoutable())
-					continue;
-				if(p.getUptime() < minUptimePercent)
-					continue;
-				// Ignore backoff state
-				double peerloc = p.getLocation();
-				if(Math.abs(peerloc - ignoreLoc) < Double.MIN_VALUE * 2)
-					continue;
-				double diff = Location.distance(peerloc, loc);
-				if(diff < bestDiff) {
-					foundOne = true;
-					bestDiff = diff;
-					if(logMINOR) Logger.minor(this, "Found best loc "+peerloc+" from "+p+" (second round) diff="+diff);
-					bestLoc = peerloc;
-				}
-			}
-		return bestLoc;
-	}
-
-	public boolean isCloserLocation(double loc, int minUptimePercent) {
-		double nodeLoc = node.lm.getLocation();
-		double nodeDist = Location.distance(nodeLoc, loc);
-		if(logMINOR) Logger.minor(this, "My loc is "+nodeLoc+" my dist is "+nodeDist+" target is "+loc);
-		double closest = closestPeerLocation(loc, nodeLoc, minUptimePercent);
-		if(closest > 1.0)
-			// No peers found
-			return false;
-		double closestDist = Location.distance(closest, loc);
-		return closestDist < nodeDist;
-	}
-
 	public PeerNode closerPeer(PeerNode pn, Set<PeerNode> routedTo, double loc, boolean ignoreSelf, boolean calculateMisrouting,
 	        int minVersion, List<Double> addUnpickedLocsTo, Key key, short outgoingHTL, int ignoreBackoffUnder, boolean isLocal, boolean realTime, boolean excludeMandatoryBackoff) {
 		return closerPeer(pn, routedTo, loc, ignoreSelf, calculateMisrouting, minVersion, addUnpickedLocsTo, 2.0, key, outgoingHTL, ignoreBackoffUnder, isLocal, realTime, null, false, System.currentTimeMillis(), excludeMandatoryBackoff);

commit b82ea08634e9b0377747b8413f5ffd3cb12895cb

Node.shouldStoreDeep: restore accidentally removed return statement
Regression by a62d4e130d543c14c203fa12600d1ecfb0c9fc8c

src/freenet/node/Node.java
@@ -5335,6 +5335,7 @@ public class Node implements TimeSkewDetectorCallback {
     		if(Location.distance(source, target) < myDist) {
     	    	if(logMINOR) Logger.minor(this, "Not storing because source is closer to target for "+key+" : "+source);
     		}
+			return false;
     	}
     	for(PeerNode pn : routedTo) {
     		if(Location.distance(pn, target) < myDist && !pn.isLowUptime()) {