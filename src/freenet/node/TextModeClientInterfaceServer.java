9709f5193591ed47eca5af7c32edd9386891ab4d
Matthew Toseland <toad@amphibian.dyndns.org>
2013-02-15 18:20:11 +0000

    Argh, sorry, fix build for previous commit  (was incomplete)

--- src/freenet/node/TextModeClientInterfaceServer.java
+++ src/freenet/node/TextModeClientInterfaceServer.java
@@ -87,7 +87,7 @@ public class TextModeClientInterfaceServer implements Runnable {
 		if(direct) {
 	        HighLevelSimpleClient client = core.makeClient(RequestStarter.INTERACTIVE_PRIORITY_CLASS, true, false);
 			TextModeClientInterface directTMCI =
-				new TextModeClientInterface(node, client, core.getDownloadsDir(), System.in, System.out);
+				new TextModeClientInterface(node, core, client, core.getDownloadsDir(), System.in, System.out);
 			node.executor.execute(directTMCI, "Direct text mode interface");
 			core.setDirectTMCI(directTMCI);
 		}
