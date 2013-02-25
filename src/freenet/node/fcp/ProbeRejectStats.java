93914ff5915bf7276ac82faa0c7d309a5f028cdc
Matthew Toseland <toad@amphibian.dyndns.org>
2013-02-25 23:03:00 +0000

    Fix broken FCP probe message.

--- src/freenet/node/fcp/ProbeRejectStats.java
+++ src/freenet/node/fcp/ProbeRejectStats.java
@@ -7,7 +7,7 @@ public class ProbeRejectStats extends FCPResponse {
 		fs.put(BULK_CHK_REQUEST_REJECTS, (int)stats[0]);
 		fs.put(BULK_SSK_REQUEST_REJECTS, (int)stats[1]);
 		fs.put(BULK_CHK_INSERT_REJECTS, (int)stats[2]);
-		fs.put(BULK_CHK_INSERT_REJECTS, (int)stats[3]);
+		fs.put(BULK_SSK_INSERT_REJECTS, (int)stats[3]);
 	}
 
 	@Override
