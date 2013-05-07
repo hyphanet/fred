hyphanet/fred

commit 524c45e327ec1553540d24583752c1278271d932

db4o-proof Eleriseth's last commit: activate and deactivate cb, like in the rest of the function.

src/freenet/client/async/USKInserter.java
@@ -163,7 +163,14 @@ public class USKInserter implements ClientPutState, USKFetcherCallback, PutCompl
 	private void insertSucceeded(ObjectContainer container, ClientContext context, long edition) {
 		if(ctx.ignoreUSKDatehints) {
 			if(logMINOR) Logger.minor(this, "Inserted to edition "+edition);
+			boolean cbActive = true;
+			if(!container.ext().isActive(cb)) {
+				cbActive = false;
+				container.activate(cb, 1);
+			}
 			cb.onSuccess(this, container, context);
+			if(!cbActive)
+				container.deactivate(cb, 1);
 			return;
 		}
 		if(logMINOR) Logger.minor(this, "Inserted to edition "+edition+" - inserting USK date hints...");

commit 366fa57b61c3c592ed4c68aa66e1fcd889378668

Fix NPE with FCP/ClientPut/IgnoreUSKDatehints
Regression by 524c45e327ec1553540d24583752c1278271d932 and 74c35d81b11ac34bbcc3c9b6b0f62a8d4cd6ad41

(freenet.support.PooledExecutor$MyThread, RequestStarter$SenderThread for freenet.client.async.TransientChosenBlock@225b7221(5585), ERROR): Caught java.lang.NullPointerException running job freenet.support.PooledExecutor$Job@358da448
java.lang.NullPointerException
        at freenet.client.async.USKInserter.insertSucceeded(USKInserter.java:167)
        at freenet.client.async.USKInserter.onSuccess(USKInserter.java:296)
        at freenet.client.async.SingleBlockInserter.onSuccess(SingleBlockInserter.java:461)
        at freenet.client.async.TransientChosenBlock.onInsertSuccess(TransientChosenBlock.java:48)
        at freenet.client.async.SingleBlockInserter$MySendableRequestSender.send(SingleBlockInserter.java:612)
        at freenet.client.async.ChosenBlock.send(ChosenBlock.java:70)
        at freenet.node.RequestStarter$SenderThread.run(RequestStarter.java:270)
        at freenet.support.PooledExecutor$MyThread.innerRun(PooledExecutor.java:247)
        at freenet.support.PooledExecutor$MyThread.realRun(PooledExecutor.java:187)
        at freenet.support.io.NativeThread.run(NativeThread.java:129)

Reported-by: TheSeeker@irc

src/freenet/client/async/USKInserter.java
@@ -164,7 +164,7 @@ public class USKInserter implements ClientPutState, USKFetcherCallback, PutCompl
 		if(ctx.ignoreUSKDatehints) {
 			if(logMINOR) Logger.minor(this, "Inserted to edition "+edition);
 			boolean cbActive = true;
-			if(!container.ext().isActive(cb)) {
+			if(persistent && !container.ext().isActive(cb)) {
 				cbActive = false;
 				container.activate(cb, 1);
 			}