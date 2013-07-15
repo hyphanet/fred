8cdae079ce425b7dd7ac954e1de47621ca1e1121
Matthew Toseland <toad@amphibian.dyndns.org>
2013-07-15 19:35:19 +0100

    Separate form and separate infobox for filter preview on Upload a File page, too messy/complex UI otherwise.

--- src/freenet/clients/http/QueueToadlet.java
+++ src/freenet/clients/http/QueueToadlet.java
@@ -858,13 +858,6 @@ public class QueueToadlet extends Toadlet implements RequestCompletionCallback, 
 						}
 				}
 				return;
-            // Filter File button on upload page
-            } else if (request.isPartSet("filter-upload")) {
-            	try {
-            		throw new RedirectException(ContentFilterToadlet.PATH);
-            	} catch (URISyntaxException e) {
-            		Logger.error(this, "Invalid path for content filter toadlet", e);
-            	}
 			} else if (request.isPartSet("recommend_request")) {
 				PageNode page = ctx.getPageMaker().getPageNode(l10n("recommendAFileToFriends"), ctx);
 				HTMLNode pageNode = page.outer;
