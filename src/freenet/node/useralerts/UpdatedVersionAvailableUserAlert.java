f6d33e126759f0eb6649465c27fa162917c2b56b
Matthew Toseland <toad@amphibian.dyndns.org>
2013-07-24 16:31:21 +0100

    Include the formPassword here anyway.

--- src/freenet/node/useralerts/UpdatedVersionAvailableUserAlert.java
+++ src/freenet/node/useralerts/UpdatedVersionAvailableUserAlert.java
@@ -86,9 +86,9 @@ public class UpdatedVersionAvailableUserAlert extends AbstractUserAlert {
 		
 		alertNode.addChild("#", ut.firstBit);
 		
-		// No need for formPassword, since we want to show the confirmation page anyway.
 		if(ut.formText != null) {
 			alertNode.addChild("form", new String[] { "action", "method" }, new String[] { "/", "post" }).addChild("input", new String[] { "type", "name", "value" }, new String[] { "submit", "update", ut.formText });
+			alertNode.addChild("input", new String[] { "type", "name", "value" }, new String[] { "hidden", "formPassword", updater.node.clientCore.formPassword });
 		}
 		
 		updater.renderProgress(alertNode);
