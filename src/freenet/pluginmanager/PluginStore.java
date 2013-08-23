a26d5303a87180799de9ec33c3899e10947ceb68
Matthew Toseland <toad@amphibian.dyndns.org>
2013-08-23 23:28:38 +0100

    Add exportStoreAsSFS() and new PluginStore(SFS)

--- src/freenet/pluginmanager/PluginStore.java
+++ src/freenet/pluginmanager/PluginStore.java
@@ -4,12 +4,18 @@
 package freenet.pluginmanager;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import com.db4o.Db4o;
 import com.db4o.ObjectContainer;
 import com.db4o.ObjectSet;
 import com.db4o.config.Configuration;
 import com.db4o.io.MemoryIoAdapter;
+
+import freenet.node.FSParseException;
+import freenet.support.Base64;
+import freenet.support.IllegalBase64Exception;
+import freenet.support.SimpleFieldSet;
 
 /**
  * This is a PluginStore. Plugins can use that to store all kinds of primary
@@ -30,7 +36,11 @@ public class PluginStore {
 	public final HashMap<String, byte[]> bytesArrays = new HashMap<String, byte[]>();
 	public final HashMap<String, String> strings = new HashMap<String, String>();
 	public final HashMap<String, String[]> stringsArrays = new HashMap<String, String[]>();
-
+	
+	public PluginStore() {
+	    // Default constructor. See below for constructor from SFS.
+	}
+	
 	public byte[] exportStore() {
 		Configuration conf = Db4o.newConfiguration();
 		MemoryIoAdapter mia = new MemoryIoAdapter();
@@ -61,4 +71,130 @@ public class PluginStore {
 			return null;
 		}
 	}
+	
+	public SimpleFieldSet exportStoreAsSFS() {
+	    SimpleFieldSet fs = new SimpleFieldSet(true);
+	    for(Map.Entry<String, PluginStore> entry : subStores.entrySet()) {
+	        fs.put("substore."+encode(entry.getKey()), entry.getValue().exportStoreAsSFS());
+	    }
+	    for(Map.Entry<String, Long> entry : longs.entrySet()) {
+	        fs.put("long."+encode(entry.getKey()), entry.getValue());
+	    }
+	    for(Map.Entry<String, long[]> entry : longsArrays.entrySet()) {
+	        fs.put("longs."+encode(entry.getKey()), entry.getValue());
+	    }
+	    for(Map.Entry<String, Integer> entry : integers.entrySet()) {
+	        fs.put("integer."+encode(entry.getKey()), entry.getValue());
+	    }
+	    for(Map.Entry<String, int[]> entry : integersArrays.entrySet()) {
+	        fs.put("integers."+encode(entry.getKey()), entry.getValue());
+	    }
+	    for(Map.Entry<String, Short> entry : shorts.entrySet()) {
+	        fs.put("short."+encode(entry.getKey()), entry.getValue());
+	    }
+	    for(Map.Entry<String, short[]> entry : shortsArrays.entrySet()) {
+	        fs.put("shorts."+encode(entry.getKey()), entry.getValue());
+	    }
+	    for(Map.Entry<String, Byte> entry : bytes.entrySet()) {
+	        fs.put("byte."+encode(entry.getKey()), entry.getValue());
+	    }
+	    for(Map.Entry<String, byte[]> entry : bytesArrays.entrySet()) {
+	        fs.put("bytes."+encode(entry.getKey()), entry.getValue());
+	    }
+	    for(Map.Entry<String, String> entry : strings.entrySet()) {
+	        fs.putSingle("string."+encode(entry.getKey()), entry.getValue());
+	    }
+	    for(Map.Entry<String, String[]> entry : stringsArrays.entrySet()) {
+	        fs.putEncoded("strings."+encode(entry.getKey()), entry.getValue());
+	    }
+	    return fs;
+	}
+	
+    public PluginStore(SimpleFieldSet sfs) throws IllegalBase64Exception, FSParseException {
+        SimpleFieldSet group = sfs.subset("substore");
+        if(group != null) {
+            for(Map.Entry<String, SimpleFieldSet> entry : group.directSubsets().entrySet()) {
+                subStores.put(decode(entry.getKey()), new PluginStore(entry.getValue()));
+            }
+        }
+        group = sfs.subset("long");
+        if(group != null) {
+            for(String s : group.directKeys()) {
+                longs.put(decode(s), group.getLong(s));
+            }
+        }
+        group = sfs.subset("longs");
+        if(group != null) {
+            for(String s : group.directKeys()) {
+                longsArrays.put(decode(s), group.getLongArray(s));
+            }
+        }
+        group = sfs.subset("integer");
+        if(group != null) {
+            for(String s : group.directKeys()) {
+                integers.put(decode(s), group.getInt(s));
+            }
+        }
+        group = sfs.subset("integers");
+        if(group != null) {
+            for(String s : group.directKeys()) {
+                integersArrays.put(decode(s), group.getIntArray(s));
+            }
+        }
+        group = sfs.subset("short");
+        if(group != null) {
+            for(String s : group.directKeys()) {
+                shorts.put(decode(s), group.getShort(s));
+            }
+        }
+        group = sfs.subset("shorts");
+        if(group != null) {
+            for(String s : group.directKeys()) {
+                shortsArrays.put(decode(s), group.getShortArray(s));
+            }
+        }
+        group = sfs.subset("boolean");
+        if(group != null) {
+            for(String s : group.directKeys()) {
+                booleans.put(decode(s), group.getBoolean(s));
+            }
+        }
+        group = sfs.subset("booleans");
+        if(group != null) {
+            for(String s : group.directKeys()) {
+                booleansArrays.put(decode(s), group.getBooleanArray(s));
+            }
+        }
+        group = sfs.subset("byte");
+        if(group != null) {
+            for(String s : group.directKeys()) {
+                bytes.put(decode(s), group.getByte(s));
+            }
+        }
+        group = sfs.subset("bytes");
+        if(group != null) {
+            for(String s : group.directKeys()) {
+                bytesArrays.put(decode(s), group.getByteArray(s));
+            }
+        }
+        group = sfs.subset("string");
+        if(group != null) {
+            strings.putAll(group.directKeyValues());
+        }
+        group = sfs.subset("strings");
+        if(group != null) {
+            for(String s : group.directKeys()) {
+                stringsArrays.put(decode(s), group.getAllEncoded(s));
+            }
+        }
+    }
+
+	private static final String encode(String s) {
+	    return Base64.encodeUTF8(s);
+	}
+	
+	private static final String decode(String s) throws IllegalBase64Exception {
+	    return Base64.decodeUTF8(s);
+	}
+
 }
