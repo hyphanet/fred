6e3fbda1acf5c2e86658c9747db602437b47b7f9
Matthew Toseland <toad@amphibian.dyndns.org>
2013-09-02 13:22:42 +0100

    Imports.

--- src/freenet/node/updater/MainJarDependenciesChecker.java
+++ src/freenet/node/updater/MainJarDependenciesChecker.java
@@ -25,7 +25,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 import java.util.TreeSet;
