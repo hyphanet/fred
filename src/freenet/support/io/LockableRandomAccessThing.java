10512f527af548672e9be4f6833c50f15db53f50
Matthew Toseland <toad@amphibian.dyndns.org>
2013-09-04 16:29:27 +0100

    lock() can throw

--- src/freenet/support/io/LockableRandomAccessThing.java
+++ src/freenet/support/io/LockableRandomAccessThing.java
@@ -1,5 +1,7 @@
 package freenet.support.io;
 
+import java.io.IOException;
+
 /** A RandomAccessThing which allows you to lock it open for a brief period to indicate that you are
  * using it and it would be a bad idea to close the pooled fd. Locking the RAF open does not provide
  * any concurrency guarantees but the implementation must guarantee to do the right thing, either
@@ -9,7 +11,7 @@ package freenet.support.io;
 public interface LockableRandomAccessThing extends RandomAccessThing {
 	
     /** Keep the RAF open. Does not prevent others from writing to it. */
-	public RAFLock lock();
+	public RAFLock lock() throws IOException;
 	
 }
 
