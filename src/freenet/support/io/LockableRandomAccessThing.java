e51329445cb85d408bc69923b24096d5da3ac0b0
Matthew Toseland <toad@amphibian.dyndns.org>
2013-09-04 15:27:11 +0100

    Javadocs.

--- src/freenet/support/io/LockableRandomAccessThing.java
+++ src/freenet/support/io/LockableRandomAccessThing.java
@@ -1,11 +1,14 @@
 package freenet.support.io;
 
 /** A RandomAccessThing which allows you to lock it open for a brief period to indicate that you are
- * using it and it would be a bad idea to close the pooled fd.
+ * using it and it would be a bad idea to close the pooled fd. Locking the RAF open does not provide
+ * any concurrency guarantees but the implementation must guarantee to do the right thing, either
+ * using a mutex or supporting concurrent writes.
  * @author toad
  */
 public interface LockableRandomAccessThing extends RandomAccessThing {
 	
+    /** Keep the RAF open. Does not prevent others from writing to it. */
 	public void lock();
 	
 	public void unlock();
