7652eac036d86fbc456ee1c1b558a72e437347fa
xor-freenet <xor@freenetproject.org>
2013-09-23 22:44:46 +0200

    Remove "m" prefix of member variables: - The prefix is the style convention in WOT/FT, but not in fred, so it can be removed.

--- src/freenet/support/RandomGrabHashSet.java
+++ src/freenet/support/RandomGrabHashSet.java
@@ -21,22 +21,22 @@ public final class RandomGrabHashSet<E> {
     /**
      * Random generator used as backend for {@link #getRandom()}.
      */
-    private final Random mRandom; 
+    private final Random random; 
     
     /**
      * Stores all elements.
      * Allows {@link #getRandom()} to execute in O(1).
      */
-    private final ArrayList<E> mData = new ArrayList<E>();
+    private final ArrayList<E> data = new ArrayList<E>();
     
     /**
-     * Tells which slot each element resides in {{@link #mData}}.
+     * Tells which slot each element resides in {{@link #data}}.
      * This allows {@link #contains(Object)} and {@link #remove(Object)} to execute in amortised O(1).
      */
-    private final HashMap<E, Integer> mIndex = new HashMap<E, Integer>();
+    private final HashMap<E, Integer> index = new HashMap<E, Integer>();
     
-    public RandomGrabHashSet(final Random random) {
-        mRandom = random;
+    public RandomGrabHashSet(final Random myRandom) {
+        random = myRandom;
     }
     
     
@@ -45,11 +45,11 @@ public final class RandomGrabHashSet<E> {
      * Add assert(indexIsValid()) to any functions which modify stuff.
      */
     protected boolean indexIsValid() {
-        if(mIndex.size() != mData.size())
+        if(index.size() != data.size())
             return false;
         
-        for(Entry<E, Integer> entry : mIndex.entrySet()) {
-            if(!mData.get(entry.getValue()).equals(entry.getKey()))
+        for(Entry<E, Integer> entry : index.entrySet()) {
+            if(!data.get(entry.getValue()).equals(entry.getKey()))
                 return false;
         }
         
@@ -63,45 +63,45 @@ public final class RandomGrabHashSet<E> {
         if(contains(item))
             throw new IllegalArgumentException("Element exists already: " + item);
         
-        mData.add(item);
-        mIndex.put(item, mData.size()-1);
+        data.add(item);
+        index.put(item, data.size()-1);
         
-        assert(mIndex.size() == mData.size());
-        assert(mData.get(mIndex.get(item)) == item);
+        assert(index.size() == data.size());
+        assert(data.get(index.get(item)) == item);
     }
     
     public boolean contains(final E item) {
-        return mIndex.containsKey(item);
+        return index.containsKey(item);
     }
     
     public void remove(final E toRemove) {
-        final Integer indexOfRemovedItem = mIndex.remove(toRemove) ;
+        final Integer indexOfRemovedItem = index.remove(toRemove) ;
         if(indexOfRemovedItem == null)
             throw new NoSuchElementException();
         
-        assert(mData.get(indexOfRemovedItem).equals(toRemove));
+        assert(data.get(indexOfRemovedItem).equals(toRemove));
         
         // We cannot use ArrayList.remove() because it would shift all following elements.
         // Instead of that, we replace the now-empty slot with the last element
-        final int indexOfLastItem = mData.size()-1;
-        final E lastItem = mData.remove(indexOfLastItem);
+        final int indexOfLastItem = data.size()-1;
+        final E lastItem = data.remove(indexOfLastItem);
         if(indexOfRemovedItem != indexOfLastItem) {
-            mData.set(indexOfRemovedItem, lastItem);
-            mIndex.put(lastItem, indexOfRemovedItem);
+            data.set(indexOfRemovedItem, lastItem);
+            index.put(lastItem, indexOfRemovedItem);
         }
         
-        assert(mIndex.size() == mData.size());
-        assert(mIndex.get(toRemove) == null);
-        assert(lastItem.equals(toRemove) || mData.get(mIndex.get(lastItem)).equals(lastItem));
+        assert(index.size() == data.size());
+        assert(index.get(toRemove) == null);
+        assert(lastItem.equals(toRemove) || data.get(index.get(lastItem)).equals(lastItem));
     }
     
     public int size() {
-        assert(mData.size() == mIndex.size());
-        return mData.size();
+        assert(data.size() == index.size());
+        return data.size();
     }
     
     public E getRandom() {
-        return mData.get(mRandom.nextInt(mData.size()));
+        return data.get(random.nextInt(data.size()));
     }
 
 }
