04fca372c52577e2ea9daa16aa72dcc22c30a479
xor-freenet <xor@freenetproject.org>
2013-09-23 22:40:41 +0200

    Rename mArray to mData. Thanks Bombe!

--- src/freenet/support/RandomGrabHashSet.java
+++ src/freenet/support/RandomGrabHashSet.java
@@ -27,10 +27,10 @@ public final class RandomGrabHashSet<E> {
      * Stores all elements.
      * Allows {@link #getRandom()} to execute in O(1).
      */
-    private final ArrayList<E> mArray = new ArrayList<E>();
+    private final ArrayList<E> mData = new ArrayList<E>();
     
     /**
-     * Tells which slot each element resides in {{@link #mArray}}.
+     * Tells which slot each element resides in {{@link #mData}}.
      * This allows {@link #contains(Object)} and {@link #remove(Object)} to execute in amortised O(1).
      */
     private final HashMap<E, Integer> mIndex = new HashMap<E, Integer>();
@@ -45,11 +45,11 @@ public final class RandomGrabHashSet<E> {
      * Add assert(indexIsValid()) to any functions which modify stuff.
      */
     protected boolean indexIsValid() {
-        if(mIndex.size() != mArray.size())
+        if(mIndex.size() != mData.size())
             return false;
         
         for(Entry<E, Integer> entry : mIndex.entrySet()) {
-            if(!mArray.get(entry.getValue()).equals(entry.getKey()))
+            if(!mData.get(entry.getValue()).equals(entry.getKey()))
                 return false;
         }
         
@@ -63,11 +63,11 @@ public final class RandomGrabHashSet<E> {
         if(contains(item))
             throw new IllegalArgumentException("Element exists already: " + item);
         
-        mArray.add(item);
-        mIndex.put(item, mArray.size()-1);
+        mData.add(item);
+        mIndex.put(item, mData.size()-1);
         
-        assert(mIndex.size() == mArray.size());
-        assert(mArray.get(mIndex.get(item)) == item);
+        assert(mIndex.size() == mData.size());
+        assert(mData.get(mIndex.get(item)) == item);
     }
     
     public boolean contains(final E item) {
@@ -79,29 +79,29 @@ public final class RandomGrabHashSet<E> {
         if(indexOfRemovedItem == null)
             throw new NoSuchElementException();
         
-        assert(mArray.get(indexOfRemovedItem).equals(toRemove));
+        assert(mData.get(indexOfRemovedItem).equals(toRemove));
         
         // We cannot use ArrayList.remove() because it would shift all following elements.
         // Instead of that, we replace the now-empty slot with the last element
-        final int indexOfLastItem = mArray.size()-1;
-        final E lastItem = mArray.remove(indexOfLastItem);
+        final int indexOfLastItem = mData.size()-1;
+        final E lastItem = mData.remove(indexOfLastItem);
         if(indexOfRemovedItem != indexOfLastItem) {
-            mArray.set(indexOfRemovedItem, lastItem);
+            mData.set(indexOfRemovedItem, lastItem);
             mIndex.put(lastItem, indexOfRemovedItem);
         }
         
-        assert(mIndex.size() == mArray.size());
+        assert(mIndex.size() == mData.size());
         assert(mIndex.get(toRemove) == null);
-        assert(lastItem.equals(toRemove) || mArray.get(mIndex.get(lastItem)).equals(lastItem));
+        assert(lastItem.equals(toRemove) || mData.get(mIndex.get(lastItem)).equals(lastItem));
     }
     
     public int size() {
-        assert(mArray.size() == mIndex.size());
-        return mArray.size();
+        assert(mData.size() == mIndex.size());
+        return mData.size();
     }
     
     public E getRandom() {
-        return mArray.get(mRandom.nextInt(mArray.size()));
+        return mData.get(mRandom.nextInt(mData.size()));
     }
 
 }
