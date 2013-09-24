hyphanet/fred

commit 5456e6401726900052b9e0f8a219798862fdaf86

Check length, this should pick up some subtle bugs.

src/freenet/node/NPFPacket.java
@@ -390,6 +390,8 @@ class NPFPacket {
 			}
 		}
 
+        assert(offset == length);
+
 		if(offset < buf.length) {
 			//More room, so add padding
 			Util.randomBytes(paddingGen, buf, offset, buf.length - offset);

commit 64541ebebd9683fbbafe48c1324b28c31757a40d

Fix bogus assertion I introduced in 5456e6401726900052b9e0f8a219798862fdaf86

src/freenet/node/NPFPacket.java
@@ -257,6 +257,7 @@ class NPFPacket {
 	}
 
 	public int toBytes(byte[] buf, int offset, Random paddingGen) {
+	    int origOffset = offset;
 		buf[offset] = (byte) (sequenceNumber >>> 24);
 		buf[offset + 1] = (byte) (sequenceNumber >>> 16);
 		buf[offset + 2] = (byte) (sequenceNumber >>> 8);
@@ -392,7 +393,7 @@ class NPFPacket {
 			}
 		}
 
-        assert(offset == length);
+        assert(offset - origOffset == length);
 
 		if(offset < buf.length) {
 			//More room, so add padding