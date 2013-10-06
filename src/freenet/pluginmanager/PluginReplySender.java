b9df63833c297e2eb8673b1f0a2dfc6582a53f75
xor-freenet <xor@freenetproject.org>
2013-10-06 05:52:29 +0200

    Add draft of synchronous FCP calls between plugins: - Currently only supports synchronous sending from FCP "server" to client - Necessary so WOT's event notifications can tell whether an event has actually arrived at the client. Also, there are lots of plugins which emulate synchronous communication so it is a good idea to implement it inside the node. - This commit only implements send for direct communication, i.e. for plugins running in the same node. - A real FCP implementation will and must follow, otherwise it won't compile.

--- src/freenet/pluginmanager/PluginReplySender.java
+++ src/freenet/pluginmanager/PluginReplySender.java
@@ -3,6 +3,7 @@
  * http://www.gnu.org/ for further details of the GPL. */
 package freenet.pluginmanager;
 
+import freenet.node.fcp.FCPCallFailedException;
 import freenet.support.SimpleFieldSet;
 import freenet.support.api.Bucket;
 import freenet.support.io.ArrayBucket;
@@ -38,4 +39,12 @@ public abstract class PluginReplySender {
 	
 	public abstract void send(SimpleFieldSet params, Bucket bucket) throws PluginNotFoundException;
 
+	/**
+	 * In opposite to send, this waits for the client to process the reply.
+	 * This allows it to throw {@link Throwable} if sending the reply to the client failed.
+	 * In case of {@link PluginReplySenderDirect}, the {@link Throwable} will even be the {@link Throwable} which the client's
+	 * {@link FredPluginTalker#onReply(String, String, SimpleFieldSet, Bucket) threw.
+	 * @throws FCPCallFailedException If the processing of the message failed at the client.
+	 */
+    public abstract void sendSynchronous(SimpleFieldSet params, Bucket bucket) throws FCPCallFailedException;
 }
--- src/freenet/pluginmanager/PluginReplySenderDirect.java
+++ src/freenet/pluginmanager/PluginReplySenderDirect.java
@@ -3,7 +3,12 @@
  * http://www.gnu.org/ for further details of the GPL. */
 package freenet.pluginmanager;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import freenet.node.Node;
+import freenet.node.fcp.FCPCallFailedException;
 import freenet.support.Logger;
 import freenet.support.SimpleFieldSet;
 import freenet.support.api.Bucket;
@@ -41,4 +46,42 @@ public class PluginReplySenderDirect extends PluginReplySender {
 		}, "FCPPlugin reply runner for " + pluginname);
 	}
 
+    @Override
+    public void sendSynchronous(final SimpleFieldSet params, final Bucket bucket) throws FCPCallFailedException {
+        final Lock lock = new ReentrantLock();
+        final Condition condition = lock.newCondition();
+
+        try {
+            lock.lock();
+            
+            class Runner implements Runnable {
+                volatile boolean finished = false;
+                Throwable throwable = null;
+                
+                @Override
+                public void run() {
+                    lock.lock();
+                    try {
+                        target.onReply(pluginname, identifier, params, bucket);
+                    } catch (Throwable t) {
+                        throwable = t; 
+                    } finally {
+                        finished = true;
+                        condition.signal();
+                        lock.unlock();
+                    }
+                }
+            };
+            
+            final Runner runner = new Runner();
+            node.executor.execute(runner, "FCPPlugin reply runner for " + pluginname);
+            while(!runner.finished)
+                condition.awaitUninterruptibly(); // May wake up spuriously, which is why we check the runne.finished boolean
+            
+            if(runner.throwable != null)
+              throw new FCPCallFailedException(runner.throwable);
+        } finally {
+            lock.unlock();
+        }
+    }
 }
