ac8bafce3426b0f4da981914d6589d8ca827b4d3
Matthias Treydte <mt@waldheinz.de>
2014-05-14 20:36:11 +0200

    Download junit to lib folder.

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ set -e
 # build freenet-ext.jar
 ant -f contrib/freenet-ext/build.xml
 
-# grab some build dependencies
+# grab some more dependencies
 wget http://bouncycastle.org/download/bcprov-jdk15on-150.jar -O lib/bcprov.jar
+wget 'http://search.maven.org/remotecontent?filepath=junit/junit/4.11/junit-4.11.jar' -O lib/junit.jar
 
 # build Fred
 ant -f build-clean.xml
