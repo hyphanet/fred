95dce576e13f15d4a436ff2fa797f2d46b909654
Matthias Treydte <mt@waldheinz.de>
2014-05-14 20:40:45 +0200

    Downgrade to Bouncy Castle 1.49 because of the OCB IV fuckup.

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ set -e
 ant -f contrib/freenet-ext/build.xml
 
 # grab some more dependencies
-wget http://bouncycastle.org/download/bcprov-jdk15on-150.jar -O lib/bcprov.jar
+wget http://bouncycastle.org/download/bcprov-jdk15on-149.jar -O lib/bcprov.jar
 wget 'http://search.maven.org/remotecontent?filepath=junit/junit/4.11/junit-4.11.jar' -O lib/junit.jar
 
 # build Fred
