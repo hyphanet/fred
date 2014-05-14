d22460c5096cf9dfd2b2a980ef292ef0e6ec7d99
Matthias Treydte <mt@waldheinz.de>
2014-05-14 20:20:16 +0200

    Use locally built contrib.

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 
+# we want to abort on first error
+set -e
+
 # build freenet-ext.jar
 ant -f contrib/freenet-ext/build.xml
+
+# grab some build dependencies
+wget http://bouncycastle.org/download/bcprov-jdk15on-150.jar -O lib/bcprov.jar
+
+# build Fred
+ant -f build-clean.xml
 
