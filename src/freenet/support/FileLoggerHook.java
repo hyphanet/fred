ba71188f7f7eef3bb49f1d0d1568e5c0f85c8644
Amorphous-Serendipity <Amorphous-Serendipity@users.noreply.github.com>
2014-05-14 18:45:57 -0400

    PULL FIXES – Pull Cleanup
    Fixed typos and made recommended changes.

--- src/freenet/support/FileLoggerHook.java
+++ src/freenet/support/FileLoggerHook.java
@@ -529,7 +529,7 @@ public class FileLoggerHook extends LoggerHook implements Closeable {
 	
 	static {
 		try {
-			BOM = (String.valueOf((char)0xFEFF)).getBytes(ENCODING);
+			BOM = "\uFEFF".getBytes(ENCODING);
 		} catch (UnsupportedEncodingException e) {
 			throw new Error(e);
 		}
