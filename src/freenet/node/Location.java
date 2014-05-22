e2336d3fab2cfd60414c15190827a5c1f29240a7
Bert Massop <bert.massop@gmail.com>
2014-05-22 14:35:12 +0200

    Rewrite of the Location class.
    This fixes bugs:
    * Location.equals(0.0, 1.0) returned false
    * Location.distance(invalidLoc, invalidLoc, true) did not return 0.0
    
    Further changes:
    * Reuse change() (e.g. simpleDistance() is just abs(change()))
    * Reuse isValid() instead of explicit bounds checks
    * Simplify change()
    * Simplify normalize()
    * Use LOCATION_INVALID (= -1.0) instead of magic numbers
    * Add just a bit more documentation

--- src/freenet/node/Location.java
+++ src/freenet/node/Location.java
@@ -13,21 +13,29 @@ import freenet.support.Logger;
  */
 public class Location {
 
+	public static final double LOCATION_INVALID = -1.0;
+
+	/**
+	 * Parses a location string.
+	 * Returns LOCATION_INVALID for all invalid locations and on parse errors.
+	 */
 	public static double getLocation(String init) {
 		try {
-			if(init == null) return -1;
+			if (init == null) {
+				return LOCATION_INVALID;
+			}
 			double d = Double.parseDouble(init);
-			if(d < 0.0 || d > 1.0) return -1;
+			if (!isValid(d)) {
+				return LOCATION_INVALID;
+			}
 			return d;
 		} catch (NumberFormatException e) {
-			return -1;
+			return LOCATION_INVALID;
 		}
 	}
 	
-	static double distance(PeerNode p, double loc) {
-		double d = distance(p.getLocation(), loc);
-		return d;
-		//return d * p.getBias();
+	public static double distance(PeerNode p, double loc) {
+		return distance(p.getLocation(), loc);
 	}
 
 	/**
@@ -38,73 +46,82 @@ public class Location {
 		return distance(a, b, false);
 	}
 
+	/**
+	 * Distance between two locations.
+	 * If allowCrazy is false, passing any location outside [0.0, 1.0] will result in an exception.
+	 * If allowCrazy is true, invalid locations are considered to be at 2.0, and the result is
+	 * calculated accordingly.
+	 */
 	public static double distance(double a, double b, boolean allowCrazy) {
-	    // Circular keyspace
-		if(!allowCrazy) {
-			if((a < 0.0 || a > 1.0)||(b < 0.0 || b > 1.0)) {
-		    	Logger.error(PeerManager.class, "Invalid Location ! a = "+a +" b = "+ b + "Please report this bug!", new Exception("error"));
-		    	throw new NullPointerException();
+		if (!allowCrazy) {
+			if (!isValid(a) || !isValid(b)) {
+				Logger.error(PeerManager.class, "Invalid Location ! a = " + a + " b = " + b +
+						"Please report this bug!", new Exception("error"));
+				throw new NullPointerException();
 			}
 			return simpleDistance(a, b);	
 		} else {
-			if(a < 0.0 || a > 1.0) a = 2.0;
-			if(b < 0.0 || b > 1.0) b = 2.0;
-			if(a > 1.0 && b > 2.0)
-				return 0.0; // Both are out of range so both are equal
-			if(a > 1.0)
-				return a - b;
-			if(b > 1.0)
-				return b - a;
+			if (!isValid(a) && !isValid(b)) {
+				return 0.0; // Both are out of range so both are equal.
+			}
+			if (!isValid(a)) {
+				return 2.0 - b;
+			}
+			if (!isValid(b)) {
+				return 2.0 - a;
+			}
+			// Both values are valid.
 			return simpleDistance(a, b);
 		}
 	}
 
+	/**
+	 * Distance between two locations without bounds check.
+	 * The behaviour is undefined for locations outside [0.0, 1.0].
+	 */
 	private static double simpleDistance(double a, double b) {
-		if (a > b) return Math.min (a - b, 1.0 - a + b);
-		else return Math.min (b - a, 1.0 - b + a);
+		return Math.abs(change(a, b));
 	}
 
-	/** Patterned after distance( double, double ), but also retuns the direction of the change (positive/negative). When given two values on opposite ends of the keyspace, it will return +0.5 */
+	/**
+	 * Distance between two locations, including direction of the change (positive/negative).
+	 * When given two values on opposite ends of the keyspace, it will return +0.5.
+	 * The behaviour is undefined for locations outside [0.0, 1.0].
+	 */
 	public static double change(double from, double to) {
-		if (to > from) {
-			double directDifference = to - from;
-			double oppositeDifference = 1.0 - to + from;
-			if (directDifference <= oppositeDifference) {
-				return directDifference;
-			} else {
-				return -oppositeDifference;
-			}
-		} else {
-			// It's going the other way, return: -1*change(to, from) except the edge case of 0.5;
-			double directDifference = from - to;
-			double oppositeDifference = 1.0 - from + to;
-			if (directDifference < oppositeDifference) {
-				return -directDifference;
-			} else {
-				return oppositeDifference;
-			}
+		double change = to - from;
+		if (change > 0.5) {
+			return change - 1.0;
 		}
+		if (change <= -0.5) {
+			return change + 1.0;
+		}
+		return change;
 	}
 	
 	/**
-	 * Given an arbitrary double (not bound to [0.0, 1.0]) return the normalized double [0.0, 1.0] which would result in simple
-	 * wrapping/overflowing. e.g. normalize(1.0+0.5)==0.5, normalize(0.5-1.0)==0.5, normalize({0.0,1.0})=={0.0,1.0}
+	 * Given an arbitrary double (not bound to [0.0, 1.0)) return the normalized double [0.0, 1.0) which would result in simple
+	 * wrapping/overflowing. e.g. normalize(0.3+1.0)==0.3, normalize(0.3-1.0)==0.3, normalize(x)==x with x in [0.0, 1.0)
 	 * @bug: if given double has wrapped too many times, the return value may be not be precise.
 	 */
 	public static double normalize(double rough) {
-		int intPart=(int)rough;
-		double maybeNegative=rough-intPart;
-		//note: maybeNegative is bound between (-1.0, 1.0)
-		if (maybeNegative < 0)
-			return 1.0+maybeNegative;
-		return maybeNegative;
-	}
-	
-	public static boolean equals(double newLoc, double currentLocation) {
-		return Math.abs(newLoc - currentLocation) < Double.MIN_VALUE * 2;
+		double normal = rough % 1.0;
+		if (normal < 0) {
+			return 1.0 + normal;
+		}
+		return normal;
+	}	
+
+	/**
+	 * Tests for equality of two locations.
+	 * Locations are considered equal if their distance is (almost) zero, e.g. equals(0.0, 1.0) is true.
+	 */
+	public static boolean equals(double a, double b) {
+		return simpleDistance(a, b) < Double.MIN_VALUE * 2;
 	}
 
 	public static boolean isValid(double loc) {
 		return loc >= 0.0 && loc <= 1.0;
 	}
 }
+
