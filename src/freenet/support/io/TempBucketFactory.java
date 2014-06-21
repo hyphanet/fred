hyphanet/fred

commit 2bc9e69fd07e40cc3717b8c1721e787c47657081

Don’t throw an exception if already closed.

src/freenet/support/io/TempBucketFactory.java
@@ -210,7 +210,9 @@ public class TempBucketFactory implements BucketFactory {
 			}
 			
 			private void _maybeMigrateRamBucket(long futureSize) throws IOException {
-				if(closed) throw new IOException("Already closed");
+				if (closed) {
+					return;
+				}
 				if(isRAMBucket()) {
 					boolean shouldMigrate = false;
 					boolean isOversized = false;

commit ceb74bbee2dd52031a7ccb18bc06856a6194770a

write() must check closed, or it will NPE here, but in any case it must throw IOException if already closed. Fixed regression in 2bc9e69fd07e40cc3717b8c1721e787c47657081.

src/freenet/support/io/TempBucketFactory.java
@@ -239,6 +239,7 @@ public class TempBucketFactory implements BucketFactory {
 			public final void write(int b) throws IOException {
 				synchronized(TempBucket.this) {
 					long futureSize = currentSize + 1;
+					if(closed) throw new IOException("Already closed"); // Must throw if already closed
 					_maybeMigrateRamBucket(futureSize);
 					os.write(b);
 					currentSize = futureSize;
@@ -251,6 +252,7 @@ public class TempBucketFactory implements BucketFactory {
 			public final void write(byte b[], int off, int len) throws IOException {
 				synchronized(TempBucket.this) {
 					long futureSize = currentSize + len;
+                    if(closed) throw new IOException("Already closed"); // Must throw if already closed
 					_maybeMigrateRamBucket(futureSize);
 					os.write(b, off, len);
 					currentSize = futureSize;
@@ -263,7 +265,7 @@ public class TempBucketFactory implements BucketFactory {
 			public final void flush() throws IOException {
 				synchronized(TempBucket.this) {
 					_maybeMigrateRamBucket(currentSize);
-					if(!closed)
+					if(!closed) // Like close().
 						os.flush();
 				}
 			}
@@ -271,7 +273,7 @@ public class TempBucketFactory implements BucketFactory {
 			@Override
 			public final void close() throws IOException {
 				synchronized(TempBucket.this) {
-					if(closed) return;
+					if(closed) return; // Silently return if already closed (convention with AutoCloseable).
 					_maybeMigrateRamBucket(currentSize);
 					os.flush();
 					os.close();