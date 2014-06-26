60b367dcf493b33b461608cdb2e6be27ca6c4718
Matthew Toseland <toad@amphibian.dyndns.org>
2014-06-26 13:02:46 +0100

    Test locking.

--- test/freenet/support/io/PooledRandomAccessFileWrapperTest.java
+++ test/freenet/support/io/PooledRandomAccessFileWrapperTest.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Random;
 
+import freenet.support.io.LockableRandomAccessThing.RAFLock;
+
 public class PooledRandomAccessFileWrapperTest extends BaseRandomAccessThingTest {
 
     private static final int[] TEST_LIST = new int[] { 0, 1, 32, 64, 32768, 1024*1024, 1024*1024+1 };
@@ -58,6 +60,155 @@ public class PooledRandomAccessFileWrapperTest extends BaseRandomAccessThingTest
         b.free();
     }
 
+    /** Test that locking and unlocking do something */
+    public void testLock() throws IOException {
+        int sz = 1024;
+        PooledRandomAccessFileWrapper.setMaxFDs(1);
+        assertEquals(PooledRandomAccessFileWrapper.getOpenFDs(), 0);
+        assertEquals(PooledRandomAccessFileWrapper.getClosableFDs(), 0);
+        PooledRandomAccessFileWrapper a = construct(sz);
+        PooledRandomAccessFileWrapper b = construct(sz);
+        assertEquals(PooledRandomAccessFileWrapper.getOpenFDs(), 1);
+        assertEquals(PooledRandomAccessFileWrapper.getClosableFDs(), 1);
+        assertFalse(a.isLocked());
+        assertFalse(b.isLocked());
+        RAFLock lock = a.lockOpen();
+        try {
+            assertTrue(a.isLocked());
+            assertFalse(b.isLocked());
+            assertEquals(PooledRandomAccessFileWrapper.getOpenFDs(), 1);
+            assertEquals(PooledRandomAccessFileWrapper.getClosableFDs(), 0);
+        } finally {
+            lock.unlock();
+            assertFalse(a.isLocked());
+            assertEquals(PooledRandomAccessFileWrapper.getOpenFDs(), 1);
+            assertEquals(PooledRandomAccessFileWrapper.getClosableFDs(), 1);
+        }
+        a.close();
+        b.close();
+        assertEquals(PooledRandomAccessFileWrapper.getOpenFDs(), 0);
+        assertEquals(PooledRandomAccessFileWrapper.getClosableFDs(), 0);
+        a.free();
+        b.free();
+    }
+    
+    /** Test that locking enforces limits and blocks when appropriate. 
+     * @throws InterruptedException */
+    public void testLockBlocking() throws IOException, InterruptedException {
+        int sz = 1024;
+        PooledRandomAccessFileWrapper.setMaxFDs(1);
+        assertEquals(PooledRandomAccessFileWrapper.getOpenFDs(), 0);
+        final PooledRandomAccessFileWrapper a = construct(sz);
+        final PooledRandomAccessFileWrapper b = construct(sz);
+        assertEquals(PooledRandomAccessFileWrapper.getOpenFDs(), 1);
+        assertFalse(a.isLocked());
+        assertFalse(b.isLocked());
+        RAFLock lock = a.lockOpen();
+        assertTrue(a.isOpen());
+        assertEquals(PooledRandomAccessFileWrapper.getOpenFDs(), 1);
+        // Now try to lock on a second thread.
+        // It should wait until the first thread unlocks.
+        class Status {
+            boolean hasStarted;
+            boolean hasLocked;
+            boolean canFinish;
+            boolean hasFinished;
+            boolean success;
+        }
+        final Status s = new Status();
+        Runnable r = new Runnable() {
+            
+            @Override
+            public void run() {
+                synchronized(s) {
+                    s.hasStarted = true;
+                    s.notify();
+                }
+                try {
+                    RAFLock lock = b.lockOpen();
+                    synchronized(s) {
+                        s.hasLocked = true;
+                        s.notify();
+                    }
+                    synchronized(s) {
+                        while(!s.canFinish)
+                            try {
+                                s.wait();
+                            } catch (InterruptedException e) {
+                                // Ignore.
+                            }
+                    }
+                    lock.unlock();
+                    synchronized(s) {
+                        s.success = true;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    fail("Caught IOException trying to lock: "+e);
+                } finally {
+                    synchronized(s) {
+                        s.hasFinished = true;
+                        s.notify();
+                    }
+                }
+            }
+            
+        };
+        new Thread(r).start();
+        // Wait for it to start.
+        synchronized(s) {
+            while(!s.hasStarted) {
+                s.wait();
+            }
+            assertFalse(s.hasLocked);
+            assertFalse(s.hasFinished);
+        }
+        assertEquals(PooledRandomAccessFileWrapper.getOpenFDs(), 1);
+        assertTrue(a.isOpen());
+        assertFalse(b.isOpen());
+        // Wait while holding lock, to give it some time to progress if it's buggy.
+        Thread.sleep(100);
+        synchronized(s) {
+            assertFalse(s.hasLocked);
+            assertFalse(s.hasFinished);
+        }
+        assertEquals(PooledRandomAccessFileWrapper.getOpenFDs(), 1);
+        assertTrue(a.isOpen());
+        assertFalse(b.isOpen());
+        // Now release lock.
+        lock.unlock();
+        // Wait for it to proceed.
+        synchronized(s) {
+            while(!(s.hasLocked || s.hasFinished))
+                s.wait();
+            assertTrue(s.hasLocked);
+        }
+        assertFalse(a.isOpen());
+        assertTrue(b.isOpen());
+        assertTrue(b.isLocked());
+        assertEquals(PooledRandomAccessFileWrapper.getOpenFDs(), 1);
+        
+        // Now let it proceed.
+        synchronized(s) {
+            s.canFinish = true;
+            s.notifyAll();
+            while(!s.hasFinished) {
+                s.wait();
+            }
+            assertTrue(s.success);
+        }
+        assertFalse(a.isLocked());
+        assertFalse(b.isLocked());
+        assertEquals(PooledRandomAccessFileWrapper.getClosableFDs(), 1);
+        assertEquals(PooledRandomAccessFileWrapper.getOpenFDs(), 1);
+        a.close();
+        assertEquals(PooledRandomAccessFileWrapper.getOpenFDs(), 1);
+        b.close();
+        assertEquals(PooledRandomAccessFileWrapper.getOpenFDs(), 0);
+        a.free();
+        b.free();
+    }
+    
     // FIXME more tests???
     
 }
