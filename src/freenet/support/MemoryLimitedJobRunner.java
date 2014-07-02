7d0921c88ef1fcf9cd87115a7fd1e05cb5f32759
Matthew Toseland <toad@amphibian.dyndns.org>
2014-07-02 16:57:31 +0100

    Move MemoryLimitedJob to a separate file for easier importing.

--- src/freenet/support/MemoryLimitedJobRunner.java
+++ src/freenet/support/MemoryLimitedJobRunner.java
@@ -99,25 +99,6 @@ public class MemoryLimitedJobRunner {
         }
 
     }
-    
-    public static abstract class MemoryLimitedJob {
-        
-        final long initialAllocation;
-        
-        public MemoryLimitedJob(long initial) {
-            this.initialAllocation = initial;
-        }
-        
-        /** Does not affect queueing, only the priority of the thread when it is run. */
-        public abstract int getPriority();
-        
-        /** Start the job.
-         * @param chunk The chunk of the scarce resource that has been allocated for this job. Can
-         * be released but not added to. Initial size is equal to initialAllocation(). 
-         * @return True to free up all resources. Otherwise the job must release() asynchronously. */
-        public abstract boolean start(MemoryLimitedChunk chunk);
-        
-    }
 
 }
 
