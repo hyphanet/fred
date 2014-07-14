8f6d5589f62e0c14088a8e4ecc190073ab6b93d9
unixninja92 <charlesvt@gmail.com>
2014-07-14 14:01:19 -0400

    Added Documentation to KeyPairType, and made constructors private.

--- src/freenet/crypt/KeyPairType.java
+++ src/freenet/crypt/KeyPairType.java
@@ -5,24 +5,42 @@ package freenet.crypt;
 
 import java.security.spec.ECGenParameterSpec;
 
+/**
+ * Keeps track of curve names and specs for EC based algorithms available to Freenet. 
+ * Also includes DSA for legacy support of old network content. 
+ * @author unixninja92
+ *
+ */
 public enum KeyPairType {
+	/**
+	 * @deprecated DSA should only be used for legacy support of old network
+	 * content. Replaced by {@link #ECP256}
+	 */
 	@Deprecated
 	DSA(),
 	ECP256("EC", "secp256r1"),
 	ECP384("EC", "secp384r1"),
-	ECP512("EC",  "secp521r1");
+	ECP521("EC",  "secp521r1");
 
 	public final String alg;
 	public final String specName;
 	public final ECGenParameterSpec spec;
 
-	KeyPairType(){
+	/**
+	 * Creates the DSA enum value. 
+	 */
+	private KeyPairType(){
 		alg = name();
 		specName = alg;
 		spec = null;
 	}
 
-	KeyPairType(String alg, String specName){
+	/**
+	 * Creates EC enum values and creates ECGenparameterSpecs for them. 
+	 * @param alg What algorithm KeyPairGenerators should use
+	 * @param specName The elliptic curve to use. 
+	 */
+	private KeyPairType(String alg, String specName){
 		this.alg = alg;
 		this.specName = specName;
 		spec = new ECGenParameterSpec(specName);
