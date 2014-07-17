0d4bfd28afb886d3c3f65c3fbeb6f50cffe9b09d
Matthew Toseland <toad@amphibian.dyndns.org>
2014-07-17 14:38:12 +0100

    Add some async tests and FIXMEs.

--- test/freenet/support/MemoryLimitedJobRunnerTest.java
+++ test/freenet/support/MemoryLimitedJobRunnerTest.java
@@ -171,6 +171,129 @@ public class MemoryLimitedJobRunnerTest extends TestCase {
         return true;
     }
 
-    // FIXME test asynchronous jobs.
+    class AsynchronousJob extends MemoryLimitedJob {
+        
+        private boolean canStart;
+        private boolean isStarted;
+        private boolean canFinish;
+        private boolean isFinished;
+        private final Object completionSemaphore;
+        
+        AsynchronousJob(long size, boolean canStart, Object semaphore) {
+            super(size);
+            this.canStart = canStart;
+            canFinish = false;
+            completionSemaphore = semaphore;
+        }
 
+        @Override
+        public int getPriority() {
+            return NativeThread.NORM_PRIORITY;
+        }
+
+        @Override
+        public boolean start(final MemoryLimitedChunk chunk) {
+            waitForCanStart();
+            synchronized(completionSemaphore) {
+                isStarted = true;
+                completionSemaphore.notifyAll();
+            }
+            Thread t = new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    waitForCanFinish();
+                    synchronized(completionSemaphore) {
+                        isFinished = true;
+                        completionSemaphore.notifyAll();
+                    }
+                    chunk.release();
+                }
+                
+            });
+            t.start();
+            return false;
+        }
+
+        private synchronized void waitForCanFinish() {
+            while(!canFinish)
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    // Ignore.
+                }
+        }
+
+        private synchronized void waitForCanStart() {
+            while(!canStart)
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    // Ignore.
+                }
+        }
+
+        public boolean isFinished() {
+            synchronized(completionSemaphore) {
+                return isFinished;
+            }
+        }
+        
+        public boolean isStarted() {
+            synchronized(completionSemaphore) {
+                return isStarted;
+            }
+        }
+        
+        public synchronized void setCanStart() {
+            if(canStart) throw new IllegalStateException();
+            canStart = true;
+            notify();
+        }
+        
+        public synchronized void setCanFinish() {
+            if(canFinish) throw new IllegalStateException();
+            canFinish = true;
+            notify();
+        }
+        
+    }
+    
+    public void testAsyncQueueingSmallDelayed() throws InterruptedException {
+        innerTestAsyncQueueingSmallDelayed(1, 10, 20, false);
+        innerTestAsyncQueueingSmallDelayed(1, 1024, 1024*2, false);
+    }
+    
+    public void testAsyncQueueingManySmallDelayed() throws InterruptedException {
+        innerTestAsyncQueueingSmallDelayed(1, 10, 10, false);
+        innerTestAsyncQueueingSmallDelayed(1, 20, 10, false);
+        innerTestAsyncQueueingSmallDelayed(1, 1024, 1024, false);
+        innerTestAsyncQueueingSmallDelayed(1, 2048, 1024, false);
+        innerTestAsyncQueueingSmallDelayed(1, 20, 1, false);
+    }
+    
+    private void innerTestAsyncQueueingSmallDelayed(int JOB_SIZE, int JOB_COUNT, int JOB_LIMIT,
+            boolean startLive) throws InterruptedException {
+        SynchronousJob[] jobs = new SynchronousJob[JOB_COUNT];
+        final Object completion = new Object();
+        for(int i=0;i<jobs.length;i++) jobs[i] = new SynchronousJob(JOB_SIZE, startLive, completion);
+        MemoryLimitedJobRunner runner = new MemoryLimitedJobRunner(JOB_LIMIT, executor);
+        for(SynchronousJob job : jobs)
+            runner.queueJob(job);
+        Thread.sleep(100);
+        assertTrue(noneFinished(jobs));
+        if(!startLive) {
+            for(SynchronousJob job : jobs)
+                job.setCanStart();
+        }
+        if(JOB_COUNT <= JOB_LIMIT)
+            waitForAllStarted(jobs, completion);
+        for(SynchronousJob job : jobs)
+            job.setCanFinish();
+        waitForAllFinished(jobs, completion);
+    }
+
+    // FIXME Test that it goes back to exactly 0? Probably need some arbitrary unreliable delays...? :(
+    
+    // FIXME Test that it sticks to the limits?
 }
