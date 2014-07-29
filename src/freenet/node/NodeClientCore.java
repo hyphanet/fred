f96bfe6bfe668373df407802a9a55380d5b38ac6
Matthew Toseland <toad@amphibian.dyndns.org>
2014-07-29 20:49:36 +0100

    Always use the old splitfile code on <256MB of memory (because the new FEC and old FEC both use memory, so using both means losing 8MB).

--- src/freenet/node/NodeClientCore.java
+++ src/freenet/node/NodeClientCore.java
@@ -517,22 +517,27 @@ public class NodeClientCore implements Persistable, DBJobRunner, ExecutorIdleCal
 
 		});
 		alwaysCommit = nodeConfig.getBoolean("alwaysCommit");
-		nodeConfig.register("useNewSplitfileCodeTransient", true, sortOrder++, true, false, "NodeClientCore.useNewSplitfileCodeTransient", "NodeClientCore.useNewSplitfileCodeTransient",
-		        new BooleanCallback() {
-
-                    @Override
-                    public Boolean get() {
-                        return HighLevelSimpleClientImpl.USE_NEW_SPLITFILE_CODE_TRANSIENT;
-                    }
-
-                    @Override
-                    public void set(Boolean val) throws InvalidConfigValueException,
-                            NodeNeedRestartException {
-                        HighLevelSimpleClientImpl.USE_NEW_SPLITFILE_CODE_TRANSIENT = val;
-                    }
-		    
-		});
-		HighLevelSimpleClientImpl.USE_NEW_SPLITFILE_CODE_TRANSIENT = nodeConfig.getBoolean("useNewSplitfileCodeTransient");
+		if(maxMemory >= 256) {
+		    nodeConfig.register("useNewSplitfileCodeTransient", true, sortOrder++, true, false, "NodeClientCore.useNewSplitfileCodeTransient", "NodeClientCore.useNewSplitfileCodeTransient",
+		            new BooleanCallback() {
+		        
+		        @Override
+		        public Boolean get() {
+		            return HighLevelSimpleClientImpl.USE_NEW_SPLITFILE_CODE_TRANSIENT;
+		        }
+		        
+		        @Override
+		        public void set(Boolean val) throws InvalidConfigValueException,
+		        NodeNeedRestartException {
+		            HighLevelSimpleClientImpl.USE_NEW_SPLITFILE_CODE_TRANSIENT = val;
+		        }
+		        
+		    });
+		    HighLevelSimpleClientImpl.USE_NEW_SPLITFILE_CODE_TRANSIENT = nodeConfig.getBoolean("useNewSplitfileCodeTransient");
+		} else {
+		    System.err.println("Disabling testing new splitfile code because memory limit is less than 256MB: "+maxMemory);
+		    HighLevelSimpleClientImpl.USE_NEW_SPLITFILE_CODE_TRANSIENT = false;
+		}
 	}
 
 	private void initUSK(ObjectContainer container) {
