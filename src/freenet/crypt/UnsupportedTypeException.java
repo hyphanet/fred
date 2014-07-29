89315965c2fe56ae407f6ff4f5fbb613e57b6789
unixninja92 <charlesvt@gmail.com>
2014-07-29 13:27:37 -0400

    Fixed indentation of UnsupportedTypeException

--- src/freenet/crypt/UnsupportedTypeException.java
+++ src/freenet/crypt/UnsupportedTypeException.java
@@ -10,11 +10,11 @@ package freenet.crypt;
  *
  */
 public class UnsupportedTypeException extends IllegalArgumentException {
-	private static final long serialVersionUID = -1;
+    private static final long serialVersionUID = -1;
     public UnsupportedTypeException(Enum<?> type, String s) {
-    	super("Unsupported "+type.getDeclaringClass().getName()+" "+type.name()+" used. "+s);
+        super("Unsupported "+type.getDeclaringClass().getName()+" "+type.name()+" used. "+s);
     }
     public UnsupportedTypeException(Enum<?> type){
-    	this(type, "");
+        this(type, "");
     }
 }
