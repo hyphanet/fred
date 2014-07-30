79eec0770aed14034375ae66b08169eb531fe305
Matthew Toseland <toad@amphibian.dyndns.org>
2014-07-30 14:07:39 +0100

    Add checksum tool, will be used for using HMAC's rather than CRC's when encrypted, also tidies a bit. PLEASE REVIEW: Are there standard tools for this? What about unixninja's work?

---
+++ src/freenet/crypt/CRCChecksumChecker.java
@@ -0,0 +1,42 @@
+package freenet.crypt;
+
+import java.io.OutputStream;
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
+
+import freenet.support.Fields;
+
+public class CRCChecksumChecker extends ChecksumChecker {
+
+    @Override
+    public int checksumLength() {
+        return 4;
+    }
+
+    @Override
+    public OutputStream checksumWriter(OutputStream os) {
+        return new ChecksumOutputStream(os, new CRC32(), true);
+    }
+
+    @Override
+    public boolean checkChecksum(byte[] data, int offset, int length, byte[] checksum) {
+        if(checksum.length != 4) throw new IllegalArgumentException();
+        CRC32 crc = new CRC32();
+        crc.update(data, offset, length);
+        int computed = (int)crc.getValue();
+        int stored = Fields.bytesToInt(checksum);
+        return computed == stored;
+    }
+
+    @Override
+    public byte[] appendChecksum(byte[] data) {
+        byte[] output = new byte[data.length+4];
+        System.arraycopy(data, 0, output, 0, data.length);
+        Checksum crc = new CRC32();
+        crc.update(data, 0, data.length);
+        byte[] checksum = Fields.intToBytes((int)crc.getValue());
+        System.arraycopy(checksum, 0, output, data.length, 4);
+        return output;
+    }
+
+}
---
+++ src/freenet/crypt/ChecksumChecker.java
@@ -0,0 +1,24 @@
+package freenet.crypt;
+
+import java.io.OutputStream;
+
+/** Simple utility to check and write checksums. */
+public abstract class ChecksumChecker {
+    
+    /** Get the length of the checksum */
+    public abstract int checksumLength();
+    
+    /** Get an OutputStream that will write a checksum when closed. */
+    public abstract OutputStream checksumWriter(OutputStream os);
+    
+    public abstract byte[] appendChecksum(byte[] data);
+    
+    /** Verify a checksum or throw */
+    public void verifyChecksum(byte[] data, int offset, int length, byte[] checksum) throws ChecksumFailedException {
+        if(!checkChecksum(data, offset, length, checksum)) throw new ChecksumFailedException();
+    }
+    
+    /** Verify a checksum or report */
+    public abstract boolean checkChecksum(byte[] data, int offset, int length, byte[] checksum);
+
+}
