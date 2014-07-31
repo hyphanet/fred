e26af7ab94e12bfd751c3811eb74dc7d8747ec69
Matthew Toseland <toad@amphibian.dyndns.org>
2014-08-01 00:11:04 +0100

    Serialize creation of RAFs so that the disk space checks work, and to avoid fragmentation. Add FIXME, this should be per-filesystem.

--- src/freenet/support/io/DiskSpaceCheckingRandomAccessThingFactory.java
+++ src/freenet/support/io/DiskSpaceCheckingRandomAccessThingFactory.java
@@ -2,12 +2,19 @@ package freenet.support.io;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class DiskSpaceCheckingRandomAccessThingFactory implements LockableRandomAccessThingFactory {
 
     private final LockableRandomAccessThingFactory underlying;
     private final File dir;
     private volatile long minDiskSpace;
+    
+    /** LOCKING: We synchronize throughout the whole operation to prevent fragmentation and to have
+     * an accurate free space estimate. FIXME ideally this would be per-filesystem. It might be 
+     * possible to get that information from Java (1.7) via java.nio.file. */
+    private static final Lock lock = new ReentrantLock(true);
     
     public DiskSpaceCheckingRandomAccessThingFactory(LockableRandomAccessThingFactory underlying, 
             File dir, long minDiskSpace) {
@@ -25,24 +32,32 @@ public class DiskSpaceCheckingRandomAccessThingFactory implements LockableRandom
         
     }
 
-    // LOCKING: We synchronize throughout the whole operation to prevent fragmentation and to have
-    // an accurate free space estimate.
     
     @Override
-    public synchronized LockableRandomAccessThing makeRAF(long size) throws IOException {
-        if(dir.getFreeSpace() > size + minDiskSpace)
-            return underlying.makeRAF(size);
-        else
-            throw new InsufficientSpaceException();
+    public LockableRandomAccessThing makeRAF(long size) throws IOException {
+        lock.lock();
+        try {
+            if(dir.getFreeSpace() > size + minDiskSpace)
+                return underlying.makeRAF(size);
+            else
+                throw new InsufficientSpaceException();
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
     public synchronized LockableRandomAccessThing makeRAF(byte[] initialContents, int offset, int size)
             throws IOException {
-        if(dir.getFreeSpace() > size + minDiskSpace)
-            return underlying.makeRAF(size);
-        else
-            throw new InsufficientSpaceException();
+        lock.lock();
+        try {
+            if(dir.getFreeSpace() > size + minDiskSpace)
+                return underlying.makeRAF(size);
+            else
+                throw new InsufficientSpaceException();
+        } finally {
+            lock.unlock();
+        }
     }
 
 }
