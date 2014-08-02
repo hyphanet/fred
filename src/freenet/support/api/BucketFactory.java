6579b269246b35de279ff2caf7b6baded2af528e
Matthew Toseland <toad@amphibian.dyndns.org>
2014-08-02 13:43:19 +0100

    Another common "don't know".

--- src/freenet/support/api/BucketFactory.java
+++ src/freenet/support/api/BucketFactory.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public interface BucketFactory {
 	/**
 	 * Create a bucket.
-	 * @param size The maximum size of the data, or -1 if we don't know.
+	 * @param size The maximum size of the data, or -1 or Long.MAX_VALUE if we don't know.
 	 * Some buckets will throw IOException if you go over this length.
 	 * @return
 	 * @throws IOException
