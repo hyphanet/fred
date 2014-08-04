171db0b26f638ffb27b3f7e2c7a68f5e927bacb1
Matthew Toseland <toad@amphibian.dyndns.org>
2014-08-04 18:07:40 +0100

    Explain.

--- src/freenet/client/async/WantsCooldownCallback.java
+++ src/freenet/client/async/WantsCooldownCallback.java
@@ -2,7 +2,8 @@ package freenet.client.async;
 
 import com.db4o.ObjectContainer;
 
-/** Interface for requests that require a callback when they go into cooldown. */
+/** Interface for requests that require a callback when they go into cooldown.
+ * Called object should schedule a job on the jobRunner if it is persistent and will change things. */
 public interface WantsCooldownCallback {
 
 	/** The request has gone into cooldown for some period. */
