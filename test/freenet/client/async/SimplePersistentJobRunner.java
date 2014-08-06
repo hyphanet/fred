f52dad4217402262838a05a82c8e5ad3a1eea86c
Matthew Toseland <toad@amphibian.dyndns.org>
2014-08-06 22:28:58 +0100

    Fix tests, missing file.

---
+++ test/freenet/client/async/SimplePersistentJobRunner.java
@@ -0,0 +1,42 @@
+package freenet.client.async;
+
+import freenet.support.Executor;
+
+public class SimplePersistentJobRunner implements PersistentJobRunner {
+
+    final Executor exec;
+    final ClientContext context;
+    
+    public SimplePersistentJobRunner(Executor exec, ClientContext context) {
+        this.exec = exec;
+        this.context = context;
+    }
+
+    @Override
+    public void queue(final PersistentJob job, int threadPriority) {
+        exec.execute(new Runnable() {
+
+            @Override
+            public void run() {
+                job.run(context);
+            }
+            
+        });
+    }
+
+    @Override
+    public void queueLowOrDrop(PersistentJob job) {
+        queue(job, 0);
+    }
+
+    @Override
+    public void setCommitThisTransaction() {
+        // Ignore.
+    }
+
+    @Override
+    public boolean hasStarted() {
+        return true;
+    }
+
+}
