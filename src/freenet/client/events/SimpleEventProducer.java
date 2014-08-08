4841b86ba485c97dcbdffab8ac8d6d0f84c6a068
Matthew Toseland <toad@amphibian.dyndns.org>
2014-08-08 11:58:14 +0100

    Indent.

--- src/freenet/client/events/SimpleEventProducer.java
+++ src/freenet/client/events/SimpleEventProducer.java
@@ -2,6 +2,7 @@
  * Public License, version 2 (or at your option any later version). See
  * http://www.gnu.org/ for further details of the GPL. */
 package freenet.client.events;
+
 import java.io.Serializable;
 import java.util.Enumeration;
 import java.util.NoSuchElementException;
@@ -13,7 +14,7 @@ import freenet.support.Logger;
 /**
  * Event handeling for clients. SimpleEventProducer is a simple
  * ClientEventProducer implementation that can be used for others.
- *
+ * 
  * @author oskar
  **/
 public class SimpleEventProducer implements ClientEventProducer, Serializable {
@@ -23,67 +24,64 @@ public class SimpleEventProducer implements ClientEventProducer, Serializable {
 
     /**
      * Create a new SimpleEventProducer
-     *
+     * 
      **/
     public SimpleEventProducer() {
-	listeners = new Vector<ClientEventListener>();
+        listeners = new Vector<ClientEventListener>();
     }
-    
+
     /** Create a new SimpleEventProducer with the given listeners. */
     public SimpleEventProducer(ClientEventListener[] cela) {
-	this();
-	for (int i = 0 ; i < cela.length ; i++)
-	    addEventListener(cela[i]);
+        this();
+        for (int i = 0; i < cela.length; i++)
+            addEventListener(cela[i]);
     }
-    
-	@Override
+
+    @Override
     public void addEventListener(ClientEventListener cel) {
-	if(cel != null)
-	    listeners.addElement(cel);
-	else
-	    throw new IllegalArgumentException("Adding a null listener!");
+        if (cel != null)
+            listeners.addElement(cel);
+        else
+            throw new IllegalArgumentException("Adding a null listener!");
     }
-    
-	@Override
+
+    @Override
     public boolean removeEventListener(ClientEventListener cel) {
-	boolean b = listeners.removeElement(cel);
-	listeners.trimToSize();
-	return b;
+        boolean b = listeners.removeElement(cel);
+        listeners.trimToSize();
+        return b;
     }
 
     /**
      * Sends the ClientEvent to all registered listeners of this object.
      **/
-	@Override
+    @Override
     public void produceEvent(ClientEvent ce, ClientContext context) {
-	for (Enumeration<ClientEventListener> e = listeners.elements() ; 
-	     e.hasMoreElements();) {
+        for (Enumeration<ClientEventListener> e = listeners.elements(); e.hasMoreElements();) {
             try {
-            	ClientEventListener cel = e.nextElement();
+                ClientEventListener cel = e.nextElement();
                 cel.receive(ce, context);
             } catch (NoSuchElementException ne) {
-		Logger.normal(this, "Concurrent modification in "+
-				"produceEvent!: "+this);
-	    } catch (Exception ue) {
+                Logger.normal(this, "Concurrent modification in " + "produceEvent!: " + this);
+            } catch (Exception ue) {
                 System.err.println("---Unexpected Exception------------------");
                 ue.printStackTrace();
                 System.err.println("-----------------------------------------");
             }
-	}
+        }
     }
-    
+
     /** Returns the listeners as an array. */
     public ClientEventListener[] getEventListeners() {
-	ClientEventListener[] ret =
-	    new ClientEventListener[listeners.size()];
-	listeners.copyInto(ret);
-	return ret;
+        ClientEventListener[] ret = new ClientEventListener[listeners.size()];
+        listeners.copyInto(ret);
+        return ret;
     }
 
     /** Adds all listeners in the given array. */
     public void addEventListeners(ClientEventListener[] cela) {
-	for (int i = 0 ; i < cela.length ; i++)
-	    addEventListener(cela[i]);
+        for (int i = 0; i < cela.length; i++)
+            addEventListener(cela[i]);
     }
 
 }
