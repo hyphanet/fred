f8f732047c27ec5450073275a6cace74a8b6c4b9
xor-freenet <xor@freenetproject.org>
2014-08-08 18:22:19 +0200

    Add draft of interface FredPluginFCPServer to replace FredPluginFCP - Deprecates FredPluginFCP

---
+++ src/freenet/pluginmanager/FredPluginFCPServer.java
@@ -0,0 +1,28 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
+package freenet.pluginmanager;
+
+import freenet.node.fcp.FCPPluginClient;
+import freenet.support.SimpleFieldSet;
+import freenet.support.api.Bucket;
+
+/**
+ * Plugins which provide FCP services to clients must implement this interface.
+ * 
+ * @see FCPPluginClient A client will be represented as this class to the server plugin. Its JavaDoc also contains a nice overview of the code path which 
+ *                      FCP plugin messages flow through.
+ * @author xor (xor@freenetproject.org)
+ */
+public interface FredPluginFCPServer {
+    
+    public static enum ClientPermissions {
+        ACCESS_DIRECT,
+        ACCESS_FCP_RESTRICTED,
+        ACCESS_FCP_FULL
+    };
+    
+
+    void handleFCPPluginClientMessage(FCPPluginClient client, SimpleFieldSet params, Bucket data, ClientPermissions permissions);
+
+}
