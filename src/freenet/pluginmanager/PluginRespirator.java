beb5c1bb5b4415e89ec63a34d5747893a6c51ac6
xor-freenet <xor@freenetproject.org>
2014-08-13 17:33:33 +0200

    PluginRespirator: Add client-side API connectToOtherPlugin() for connecting to a plugin with the new plugin FCP stuff. - Deprecate function getPluginTalker() because the new API was designed to replace that. - The name was chosen to be "connect*()" instead of "get*()" because connecting is a more generic and widely used term for communication between software. - The name was chosen to be "connectToOtherPlugin()" instead of "connectToPlugin()" due to semantics of the containing class: PluginRespirator is an object which is handed to a plugin which is loaded into the node to keep the plugin supplied with all stuff it needs. So connectToPlugin() would sound like the plugin which owns the respirator is connecting to itself. - The function FCPServer.registerFCPPluginClient() which it will need is not present yet, so compiling will fail. Add a TODO of implementing it nervertheless to describe what it should do. - The new unrelated imports are because someone forgot to organize the imports when adding JavaDoc to other functions.

--- src/freenet/pluginmanager/PluginRespirator.java
+++ src/freenet/pluginmanager/PluginRespirator.java
@@ -15,8 +15,11 @@ import freenet.config.SubConfig;
 import freenet.node.Node;
 import freenet.node.NodeClientCore;
 import freenet.node.RequestStarter;
+import freenet.node.fcp.FCPPluginClient;
 import freenet.support.HTMLNode;
 import freenet.support.URIPreEncoder;
+import freenet.support.api.HTTPRequest;
+import freenet.support.plugins.helpers1.WebInterfaceToadlet;
 
 public class PluginRespirator {
 	private static final ArrayList<SessionManager> sessionManagers = new ArrayList<SessionManager>(4);
@@ -94,10 +97,48 @@ public class PluginRespirator {
 		return formNode;
 	}
 	
-	/** Get a PluginTalker so you can talk with other plugins. */
+	/**
+	 * Get a PluginTalker so you can talk with other plugins.
+	 * 
+	 * @deprecated Use {@link #connecToOtherPlugin(String, FredPluginFCPClient)} instead.
+	 */
+	@Deprecated
 	public PluginTalker getPluginTalker(FredPluginTalker fpt, String pluginname, String identifier) throws PluginNotFoundException {
 		return new PluginTalker(fpt, node, pluginname, identifier);
 	}
+	
+	/**
+	 * <p>Creates a FCP connection with another plugin running in the same node.</p>
+	 * 
+	 * <p>While you are formally connecting via FCP, there is no actual network connection being created. The FCP messages are passed-through directly as Java
+	 * objects. Therefore, this mechanism should be somewhat efficient.</p>
+	 * 
+	 * <p>(Plugins should communicate via FCP instead of passing objects of their own Java classes even if they are running within the same node because this
+	 * encourages implementation of FCP servers, which in turn allows people to write alternative user interfaces for plugins.<br/>
+	 * Also, this will allow future changes to the node to make it able to run each plugin within its own node and only connect them via real networked FC
+	 * connections. This could be used for load balancing of plugins across multiple machines, CPU usage monitoring and other nice stuff.)</p>
+	 * 
+	 * @param pluginName The name of the main class of the plugin - that is the class which implements {@link FredPlugin}.
+	 *                   See {@link PluginManager#getPluginInfoByClassName(String)}.
+	 * @param messageHandler An object of your plugin which implements the {@link FredPluginFCPClient} interface. Its purpose is to handle FCP messages which
+	 *                       the remote plugin sends back to your plugin.
+	 * @return A {@link FCPPluginClient} representing the connection. You are encouraged to keep it in memory and use it for as long as you need it. Notice 
+	 *         especially that keeping it in memory won't block unloading of the remote plugin. If the remote plugin is unloaded, the send-functions will fail.
+	 *         Then you have to create a fresh connection with this function.
+	 */
+    public FCPPluginClient connecToOtherPlugin(String pluginName, FredPluginFCPClient messageHandler) throws PluginNotFoundException {
+        FCPPluginClient client = new FCPPluginClient(pluginName, node.getPluginManager().getPluginFCPServer(pluginName), messageHandler);
+        // TODO FIXME: Certain plugins, notably WOT, might want to initiate communication with the client when certain events happen.
+        // They need something to store in their database to reference the client, and that would be the ID. So we need to register clients by ID and allow
+        // getting them by ID.
+        // So please implement the following function. It should put the client into a table which allows querying it by its ID.
+        // This should come together with a function FCPServer.getFCPPluginClientByID().
+        // Maybe the usage of the FCPPluginClient constructor should also be moved to the register* function. Or add a static FCPPluginClient.construct...()
+        // function which deals with both the construction and registering at the server. (This should maybe also be done for the secondary FCPPluginClient
+        // constructor.)
+        node.clientCore.getFCPServer().registerFCPPluginClient(client);
+        return client;
+    }
 
 	/** Get the ToadletContainer, which manages HTTP. You can then register
 	 * toadlets on it, which allow integrating your plugin into the main
