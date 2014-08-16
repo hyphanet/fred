f3b43f7ef14212b66e022249c1a8eebe7f072f4a
unixninja92 <charlesvt@gmail.com>
2014-08-16 13:28:51 -0400

    Documented MasterSecret

--- src/freenet/crypt/MasterSecret.java
+++ src/freenet/crypt/MasterSecret.java
@@ -8,14 +8,28 @@ import javax.crypto.spec.IvParameterSpec;
 
 import freenet.support.Logger;
 
+/**
+ * MasterSecret is a serializable secret used to derive various keys and ivs for local storage in 
+ * Freenet.
+ * @author unixninja92
+ *
+ */
 public final class MasterSecret implements Serializable{
     private static final long serialVersionUID = -8411217325990445764L;
     private final SecretKey masterKey;
     
+    /**
+     * Creates a new MasterSecret. 
+     */
     public MasterSecret(){
         masterKey = KeyGenUtils.genSecretKey(KeyType.HMACSHA512);
     }
     
+    /**
+     * Derives a SecretKey of the specified type from the MasterSecret. 
+     * @param type The type of key to derive
+     * @return The derived key
+     */
     public SecretKey deriveKey(KeyType type){
         try {
             return KeyGenUtils.deriveSecretKey(masterKey, getClass(), type.name()+" key", type);
@@ -25,6 +39,11 @@ public final class MasterSecret implements Serializable{
         return null;
     }
     
+    /**
+     * Derives a IvParameterSpec of the specified type from the MasterSecret. 
+     * @param type The type of iv to derive
+     * @return The derived iv
+     */
     public IvParameterSpec deriveIv(KeyType type){
         try {
             return KeyGenUtils.deriveIvParameterSpec(masterKey, getClass(), type.name()+" iv", 
