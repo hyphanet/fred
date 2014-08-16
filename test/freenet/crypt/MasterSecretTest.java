b47e300b17ac85a8c347d1ef77052ac070ed0cd5
unixninja92 <charlesvt@gmail.com>
2014-08-16 13:22:16 -0400

    Wrote unit tests for MasterSecret

--- test/freenet/crypt/MasterSecretTest.java
+++ test/freenet/crypt/MasterSecretTest.java
@@ -2,18 +2,60 @@ package freenet.crypt;
 
 import static org.junit.Assert.*;
 
+import java.security.InvalidKeyException;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+
 import org.junit.Test;
 
 public class MasterSecretTest {
-
+    private final static KeyType[] types = KeyType.values();
+    
     @Test
     public void testDeriveKey() {
-        fail("Not yet implemented");
+        MasterSecret secret = new MasterSecret();
+        for(KeyType type: types){
+            assertNotNull(secret.deriveKey(type));
+        }
     }
 
     @Test
+    public void testDeriveKeyLength() {
+        MasterSecret secret = new MasterSecret();
+        for(KeyType type: types){
+            assertEquals(secret.deriveKey(type).getEncoded().length, type.keySize >> 3);
+        }
+    }
+    
+
+    @Test (expected = NullPointerException.class)
+    public void testDeriveKeyNullInput() {
+        MasterSecret secret = new MasterSecret();
+        secret.deriveKey(null);
+    }
+    
+    @Test
     public void testDeriveIv() {
-        fail("Not yet implemented");
+        MasterSecret secret = new MasterSecret();
+        for(KeyType type: types){
+            assertNotNull(secret.deriveIv(type));
+        }
+    }
+
+    @Test
+    public void testDeriveIvLength() {
+        MasterSecret secret = new MasterSecret();
+        for(KeyType type: types){
+            assertEquals(secret.deriveIv(type).getIV().length, type.ivSize >> 3);
+        }
+    }
+    
+
+    @Test (expected = NullPointerException.class)
+    public void testDeriveIvNullInput() {
+        MasterSecret secret = new MasterSecret();
+        secret.deriveIv(null);
     }
 
 }
