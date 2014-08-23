76b6006c74e53203866960a8e23cc672c83b7c9c
xor-freenet <xor@freenetproject.org>
2014-08-23 21:30:45 +0200

    FCPPluginClientTracker: Trim class visibility

--- src/freenet/node/fcp/FCPPluginClientTracker.java
+++ src/freenet/node/fcp/FCPPluginClientTracker.java
@@ -36,7 +36,7 @@ import freenet.support.io.NativeThread;
  * 
  * @author xor (xor@freenetproject.org)
  */
-public final class FCPPluginClientTracker extends NativeThread {
+final class FCPPluginClientTracker extends NativeThread {
     
     /**
      * Backend table of {@link WeakReference}s to known clients. Monitored by a {@link ReferenceQueue} to automatically remove entries for clients which have
