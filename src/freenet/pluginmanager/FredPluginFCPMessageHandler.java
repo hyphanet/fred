4a085ab4ed555fbb19c22bfc2a9beccab20e2ffa
xor-freenet <xor@freenetproject.org>
2014-08-26 17:36:26 +0200

    FredPluginFCPMessageHandler: Move function from old interface to this one

--- src/freenet/pluginmanager/FredPluginFCPMessageHandler.java
+++ src/freenet/pluginmanager/FredPluginFCPMessageHandler.java
@@ -35,9 +35,40 @@ public interface FredPluginFCPMessageHandler {
      * @see FredPluginFCPMessageHandler The parent interface FredPluginFCPMessageHandler provides an overview.
      */
     public interface ServerSideFCPMessageHandler extends FredPluginFCPMessageHandler {
-        
+    
+    public static enum ClientPermissions {
+        /** The client plugin is running within the same node as the server plugin. TODO FIXME: Is there any reason not to assume {@link #ACCESS_FCP_FULL}? */
+        ACCESS_DIRECT,
+        ACCESS_FCP_RESTRICTED,
+        ACCESS_FCP_FULL
+    };
+    
+    /**
+     * <p>Is called to handle messages from your clients.<br/>
+     * <b>Must not</b> block for very long and thus must only do small amounts of processing.<br/>
+     * You <b>must not</b> keep a hard reference to the passed {@link FCPPluginClient} object outside of the scope of this function:
+     * This would prevent client plugins from being unloaded.</p>
+     * 
+     * <p>If you ...<br/>
+     * - Need a long time to compute a reply.<br/>
+     * - Want send messages to the client after having exited this function; maybe even triggered by events at your plugin, not by client messages.<br/>
+     * Then you should:<br/>
+     * - Obtain the ID of the client via {@link FCPPluginClient#getID()}, store it, and exit this message handling function.<br/>
+     * - Compute your reply in another thread.</br>
+     * - Once you're ready to send the reply, use {@link PluginRespirator#getPluginClientByID(java.util.UUID)} to obtain the client.<br/>
+     * </p>
+     *  
+     * @param client The client which sent the message. To be used for sending back a reply.<br/>
+     *               You <b>must not</b> keep a hard reference to this object outside of the scope of this function: This would prevent client plugins from
+     *               being unloaded. See the head of the documentation of this function for an explanation of how to store a pointer to a certain client.
+     * @param permissions Permissions of the client. FIXME: Maybe move this to {@link FCPPluginClient}. If it is moved, still mention it in the JavaDoc here.
+     * @param messageIdentifier The identifier of the client message as specified by the client. Must be passed through when sending a reply.
+     * @param parameters Part 1 of client message: Human-readable parameters. Shall be small amount of data.
+     * @param data Part 2 of client message: Non-human readable, large size bulk data. Can be null.
+     */
+    void handleFCPPluginClientMessage(FCPPluginClient client, ClientPermissions permissions, String messageIdentifier, SimpleFieldSet parameters, Bucket data);
     }
-       
+
     /**
      * Client plugins which connect to a FCP server plugin must implement this interface.<br/>
      * The purpose of this interface is to provide a message handling function for processing messages received from the server.
