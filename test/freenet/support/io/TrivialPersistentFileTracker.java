f5082ab58477028feeab4d4f3f8df173f302d61c
Matthew Toseland <toad@amphibian.dyndns.org>
2014-08-27 13:59:07 +0100

    Missing file for 84e78990755ccd8f46b579371704e45635d414fe

---
+++ test/freenet/support/io/TrivialPersistentFileTracker.java
@@ -0,0 +1,35 @@
+package freenet.support.io;
+
+import java.io.File;
+
+public class TrivialPersistentFileTracker implements PersistentFileTracker {
+    
+    final File dir;
+    final FilenameGenerator fg;
+
+    public TrivialPersistentFileTracker(File dir, FilenameGenerator fg) {
+        this.dir = dir;
+        this.fg = fg;
+    }
+
+    @Override
+    public void register(File file) {
+        // Ignore.
+    }
+
+    @Override
+    public void delayedFreeBucket(DelayedFreeBucket bucket) {
+        bucket.free();
+    }
+
+    @Override
+    public File getDir() {
+        return dir;
+    }
+
+    @Override
+    public FilenameGenerator getGenerator() {
+        return fg;
+    }
+
+}
