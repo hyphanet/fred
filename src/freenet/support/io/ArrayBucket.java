7f988327abcdcb8530660bbeb76a4a908c307506
Matthew Toseland <toad@amphibian.dyndns.org>
2014-08-27 17:34:10 +0100

    Prevent use of an ArrayBucket after using toRandomAccessThing()

--- src/freenet/support/io/ArrayBucket.java
+++ src/freenet/support/io/ArrayBucket.java
@@ -127,6 +127,8 @@ public class ArrayBucket implements Bucket, Serializable, RandomAccessBucket {
 
     @Override
     public LockableRandomAccessThing toRandomAccessThing() {
+        freed = true;
+        data = null;
         return new ByteArrayRandomAccessThing(data);
     }
 }
