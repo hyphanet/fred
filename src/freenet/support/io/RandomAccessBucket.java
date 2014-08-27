be0a27d688f09dbd6b10762d3d583378d9fe9f24
Matthew Toseland <toad@amphibian.dyndns.org>
2014-08-27 13:52:42 +0100

    New interface RandomAccessBucket will be needed for compression etc.

---
+++ src/freenet/support/io/RandomAccessBucket.java
@@ -0,0 +1,15 @@
+package freenet.support.io;
+
+import freenet.support.api.Bucket;
+
+/** A Bucket which can be converted to a LockableRandomAccessThing without copying. Mostly we need 
+ * this where the size of something we will later use as a RandomAccessThing is uncertain. It 
+ * provides a separate object because the API's are incompatible; in particular, the size of a 
+ * RandomAccessThing is fixed (and this is mostly a good thing). */
+public interface RandomAccessBucket extends Bucket {
+    
+    /** Convert the Bucket to a LockableRandomAccessThing. Must be efficient, i.e. will not copy 
+     * the data. Freeing the Bucket is unnecessary if you free the LockableRandomAccessThing. */
+    public LockableRandomAccessThing toRandomAccessThing();
+
+}
