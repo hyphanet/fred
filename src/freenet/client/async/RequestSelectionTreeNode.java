e45682f24375bdc8a74956bcaa75e684af87fb01
Matthew Toseland <toad@amphibian.dyndns.org>
2014-08-30 15:03:11 +0100

    FIXME

--- src/freenet/client/async/RequestSelectionTreeNode.java
+++ src/freenet/client/async/RequestSelectionTreeNode.java
@@ -1,6 +1,9 @@
 package freenet.client.async;
 
-/** Something that can have a CooldownCacheItem: Anything in the request selection tree. */
+/** Something that can have a CooldownCacheItem: Anything in the request selection tree, including
+ * the actual requests at the bottom (but ClientRequestSelector isn't actually a 
+ * RequestSelectionTreeNode at the moment, the root is the priorities in the array on 
+ * ClientRequestSelector; FIXME consistency with RGA.root etc). */
 public interface RequestSelectionTreeNode {
     
     /** Return the parent RequestSelectionTreeNode or null if it's not in the tree or is the root (e.g.
