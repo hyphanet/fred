036f8f8deed79177dca51133dad770659211948a
Matthew Toseland <toad@amphibian.dyndns.org>
2014-09-07 14:08:33 +0100

    Imports

--- src/freenet/support/io/DelayedFreeBucket.java
+++ src/freenet/support/io/DelayedFreeBucket.java
@@ -17,7 +17,6 @@ import freenet.support.LogThresholdCallback;
 import freenet.support.Logger;
 import freenet.support.Logger.LogLevel;
 import freenet.support.api.Bucket;
-import freenet.support.api.BucketFactory;
 
 public class DelayedFreeBucket implements Bucket, Serializable, DelayedFree {
 
