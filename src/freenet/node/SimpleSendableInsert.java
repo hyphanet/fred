hyphanet/fred

commit 3928bb243e01b1076771f9a3a98064258b03ec08

Store the SendableRequestItemKey's directly, we don't need the SendableInsert.

src/freenet/node/SimpleSendableInsert.java
@@ -159,7 +159,7 @@ public class SimpleSendableInsert extends SendableInsert {
 
 	@Override
 	public synchronized SendableRequestItem chooseKey(KeysFetchingLocally keys, ClientContext context) {
-		if(keys.hasInsert(this, NullSendableRequestItem.nullItem))
+		if(keys.hasInsert(NullSendableRequestItem.nullItem))
 			return null;
 		if(finished) return null;
 		else

src/freenet/client/async/ClientRequestScheduler.java
@@ -368,7 +368,7 @@ public class ClientRequestScheduler implements RequestScheduler {
 
 	@Override
 	public void removeRunningInsert(SendableInsert insert, SendableRequestItemKey token) {
-		selector.removeRunningInsert(insert, token);
+		selector.removeRunningInsert(token);
 		// Must remove here, because blocks selection and therefore creates cooldown cache entries.
 		insert.clearWakeupTime(clientContext);
 	}
@@ -438,7 +438,7 @@ public class ClientRequestScheduler implements RequestScheduler {
 	
 	@Override
 	public boolean addRunningInsert(SendableInsert insert, SendableRequestItemKey token) {
-		return selector.addRunningInsert(insert, token);
+		return selector.addRunningInsert(token);
 	}
 	
 	@Override

src/freenet/client/async/ClientRequestSelector.java
@@ -89,7 +89,7 @@ public class ClientRequestSelector implements KeysFetchingLocally {
 			recentSuccesses = new ArrayDeque<BaseSendableGet>();
 		} else {
 			keysFetching = null;
-			runningInserts = new HashSet<RunningInsert>();
+			runningInserts = new HashSet<SendableRequestItemKey>();
 			recentSuccesses = null;
 		}
 		priorities = new SectoredRandomGrabArray[RequestStarter.NUMBER_OF_PRIORITY_CLASSES];
@@ -120,31 +120,7 @@ public class ClientRequestSelector implements KeysFetchingLocally {
 	
 	private transient HashMap<Key, WeakReference<BaseSendableGet>[]> transientRequestsWaitingForKeysFetching;
 	
-	private static class RunningInsert {
-		
-		final SendableInsert insert;
-		final SendableRequestItemKey token;
-		
-		RunningInsert(SendableInsert i, SendableRequestItemKey t) {
-			insert = i;
-			token = t;
-		}
-		
-		@Override
-		public int hashCode() {
-			return insert.hashCode() ^ token.hashCode();
-		}
-		
-		@Override
-		public boolean equals(Object o) {
-			if(!(o instanceof RunningInsert)) return false;
-			RunningInsert r = (RunningInsert) o;
-			return r.insert == insert && (r.token == token || r.token.equals(token));
-		}
-		
-	}
-	
-	private transient final HashSet<RunningInsert> runningInserts;
+	private transient final HashSet<SendableRequestItemKey> runningInserts;
 	
 	/** Choose a priority to start requests from.
 	 * @return The priority chosen or the time at which a priority will have requests to send.
@@ -558,34 +534,31 @@ outer:	for(;choosenPriorityClass <= maxPrio;choosenPriorityClass++) {
 	}
 
 	@Override
-	public boolean hasInsert(SendableInsert insert, SendableRequestItemKey token) {
-		RunningInsert tmp = new RunningInsert(insert, token);
+	public boolean hasInsert(SendableRequestItemKey token) {
 		synchronized(runningInserts) {
-			return runningInserts.contains(tmp);
+			return runningInserts.contains(token);
 		}
 	}
 
-	public boolean addRunningInsert(SendableInsert insert, SendableRequestItemKey token) {
-		RunningInsert tmp = new RunningInsert(insert, token);
+	public boolean addRunningInsert(SendableRequestItemKey token) {
 		synchronized(runningInserts) {
-			boolean retval = runningInserts.add(tmp);
+			boolean retval = runningInserts.add(token);
 			if(!retval) {
 			    // This shouldn't happen often, because the chooseBlock()'s should check for it...
-				Logger.error(this, "Already in runningInserts: "+insert+" : "+token);
+				Logger.error(this, "Already in runningInserts: "+token);
 			} else {
 				if(logMINOR)
-					Logger.minor(this, "Added to runningInserts: "+insert+" : "+token);
+					Logger.minor(this, "Added to runningInserts: "+token);
 			}
 			return retval;
 		}
 	}
 	
-	public void removeRunningInsert(SendableInsert insert, SendableRequestItemKey token) {
-		RunningInsert tmp = new RunningInsert(insert, token);
+	public void removeRunningInsert(SendableRequestItemKey token) {
 		if(logMINOR)
-			Logger.minor(this, "Removing from runningInserts: "+insert+" : "+token);
+			Logger.minor(this, "Removing from runningInserts: "+token);
 		synchronized(runningInserts) {
-			runningInserts.remove(tmp);
+			runningInserts.remove(token);
 		}
 	}
 

src/freenet/client/async/SingleBlockInserter.java
@@ -605,7 +605,7 @@ public class SingleBlockInserter extends SendableInsert implements ClientPutStat
 			synchronized(this) {
 				if(finished) return null;
 				key = new BlockItemKey(this, hashCode());
-				if(ignored.hasInsert(this, key))
+				if(ignored.hasInsert(key))
 				    return null;
 				return getBlockItem(key, context);
 			}
@@ -621,7 +621,7 @@ public class SingleBlockInserter extends SendableInsert implements ClientPutStat
 	    synchronized(this) {
 	        if(finished) return -1;
             BlockItemKey key = new BlockItemKey(this, hashCode());
-            if(keysFetching.hasInsert(this, key))
+            if(keysFetching.hasInsert(key))
                 return Long.MAX_VALUE;
             return 0;
 	    }

src/freenet/client/async/SplitFileInserterSegmentBlockChooser.java
@@ -46,8 +46,7 @@ public class SplitFileInserterSegmentBlockChooser extends SimpleBlockChooser {
     
     protected boolean checkValid(int chosen) {
         if(!super.checkValid(chosen)) return false;
-        return !keysFetching.hasInsert(segment.parent.getSendableInsert(), 
-                new BlockInsert(segment, chosen));
+        return !keysFetching.hasInsert(new BlockInsert(segment, chosen));
     }
 
     /** Handle an RNF if the n-consecutive-RNFs-count-as-success hack is enabled.

src/freenet/node/KeysFetchingLocally.java
@@ -26,6 +26,6 @@ public interface KeysFetchingLocally {
 	 * Is this request:token pair being executed? FIXME this should be tracked by the inserter 
 	 * itself.
 	 */
-	public boolean hasInsert(SendableInsert insert, SendableRequestItemKey token);
+	public boolean hasInsert(SendableRequestItemKey token);
 
 }

src/freenet/node/SendableRequestItemKey.java
@@ -5,7 +5,8 @@ package freenet.node;
  * transient inserts are running. @see SendableRequestItem.getKey(). If that method just returns 
  * the request, the default equals() and hashCode() should be fine, however if you have a separate
  * key class for checking quickly whether something is queued you will need real equals() and 
- * hashCode() methods. */
+ * hashCode() methods. Should be globally unique, i.e. should not equal the keys for other 
+ * requests, so don't just use e.g. an integer on its own. */
 public interface SendableRequestItemKey {
 	
 	/** You must implement this! */

test/freenet/client/async/SplitFileFetcherStorageTest.java
@@ -869,7 +869,7 @@ public class SplitFileFetcherStorageTest extends TestCase {
         }
 
         @Override
-        public boolean hasInsert(SendableInsert insert, SendableRequestItemKey token) {
+        public boolean hasInsert(SendableRequestItemKey token) {
             return false;
         }
 

commit c120124db502fa5546fcbb390211638f17241627

Fix SimpleSendableInsert regression in 3928bb243e01b1076771f9a3a98064258b03ec08

src/freenet/node/SimpleSendableInsert.java
@@ -156,14 +156,48 @@ public class SimpleSendableInsert extends SendableInsert {
 		if(finished) return 0;
 		return 1;
 	}
+	
+	// FIXME share with SingleBlockInserter???
+	private static class MySendableRequestItem implements SendableRequestItem, SendableRequestItemKey {
+	    
+	    final SimpleSendableInsert parent;
+
+        public MySendableRequestItem(SimpleSendableInsert parent) {
+            this.parent = parent;
+        }
+
+        @Override
+        public void dump() {
+            // Ignore.
+        }
+
+        @Override
+        public SendableRequestItemKey getKey() {
+            return this;
+        }
+        
+        @Override
+        public boolean equals(Object o) {
+            if(o instanceof MySendableRequestItem) {
+                return ((MySendableRequestItem)o).parent == parent;
+            } else return false;
+        }
+        
+        @Override
+        public int hashCode() {
+            return parent.hashCode();
+        }
+	    
+	}
 
 	@Override
 	public synchronized SendableRequestItem chooseKey(KeysFetchingLocally keys, ClientContext context) {
-		if(keys.hasInsert(NullSendableRequestItem.nullItem))
+	    MySendableRequestItem mine = new MySendableRequestItem(this);
+		if(keys.hasInsert(mine))
 			return null;
 		if(finished) return null;
 		else
-			return NullSendableRequestItem.nullItem;
+			return mine;
 	}
 
 	@Override