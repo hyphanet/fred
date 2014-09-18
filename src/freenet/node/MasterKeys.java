5672cbea609e3b682be4e08830d55485844c7b8a
Matthew Toseland <toad@amphibian.dyndns.org>
2014-09-18 16:41:24 +0100

    Remove duplicate writing code

--- src/freenet/node/MasterKeys.java
+++ src/freenet/node/MasterKeys.java
@@ -6,7 +6,6 @@ import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.io.UnsupportedEncodingException;
@@ -143,67 +142,15 @@ public class MasterKeys {
 			}
 		}
 		System.err.println("Creating new master keys file");
-		// If we are still here, we need to create a new master keys file
-		// FIXME try{}, move declarations of sensitive data out, clear() in finally {}
-		byte[] clientCacheKey = new byte[32];
-		hardRandom.nextBytes(clientCacheKey);
-		byte[] databaseKey = new byte[32];
-		hardRandom.nextBytes(databaseKey);
-		byte[] tempfilesMasterSecret = new byte[64];
-		hardRandom.nextBytes(tempfilesMasterSecret);
-		byte[] iv = new byte[32];
-		hardRandom.nextBytes(iv);
-		byte[] salt = new byte[32];
-		hardRandom.nextBytes(salt);
-		FileOutputStream fos = new FileOutputStream(masterKeysFile);
-		long flags = 0;
-		byte[] flagBytes = Fields.longToBytes(flags);
-		byte[] data = new byte[flagBytes.length + clientCacheKey.length + databaseKey.length + tempfilesMasterSecret.length + HASH_LENGTH];
-		int offset = 0;
-		System.arraycopy(flagBytes, 0, data, offset, flagBytes.length);
-		offset += flagBytes.length;
-		System.arraycopy(clientCacheKey, 0, data, offset, clientCacheKey.length);
-		offset += clientCacheKey.length;
-		System.arraycopy(databaseKey, 0, data, offset, databaseKey.length);
-		offset += databaseKey.length;
-		System.arraycopy(tempfilesMasterSecret, 0, data, offset, tempfilesMasterSecret.length);
-		offset += tempfilesMasterSecret.length;
-		MessageDigest md = SHA256.getMessageDigest();
-		md.update(data, 0, offset);
-		byte[] hash = md.digest();
-		System.arraycopy(hash, 0, data, offset, HASH_LENGTH);
-		offset += HASH_LENGTH;
-		/* assert(offset == data.length); */
-
-//		System.err.println("Flag bytes: "+HexUtil.bytesToHex(flagBytes));
-//		System.err.println("Client cache key: "+HexUtil.bytesToHex(clientCacheKey));
-//		System.err.println("Hash: "+HexUtil.bytesToHex(clientCacheKey));
-//		System.err.println("Data: "+HexUtil.bytesToHex(data));
-
-		byte[] pwd = password.getBytes("UTF-8");
-		md.update(pwd);
-		md.update(salt);
-		byte[] outerKey = md.digest();
-		SHA256.returnMessageDigest(md); md = null;
-//		System.err.println("Outer key: "+HexUtil.bytesToHex(outerKey));
-		BlockCipher cipher;
-		try {
-			cipher = new Rijndael(256, 256);
-		} catch (UnsupportedCipherException e) {
-			// Impossible
-			throw new Error(e);
-		}
-		cipher.initialize(outerKey);
-		PCFBMode pcfb = PCFBMode.create(cipher, iv);
-		pcfb.blockEncipher(data, 0, data.length);
-//		System.err.println("Encrypted data: "+HexUtil.bytesToHex(data));
-		fos.write(salt);
-		fos.write(iv);
-		fos.write(data);
-		fos.close();
-		clear(data);
-		clear(hash);
-		return new MasterKeys(clientCacheKey, databaseKey, tempfilesMasterSecret, flags);
+        byte[] clientCacheKey = new byte[32];
+        hardRandom.nextBytes(clientCacheKey);
+        byte[] databaseKey = new byte[32];
+        hardRandom.nextBytes(databaseKey);
+        byte[] tempfilesMasterSecret = new byte[64];
+        hardRandom.nextBytes(tempfilesMasterSecret);
+		MasterKeys ret = new MasterKeys(clientCacheKey, databaseKey, tempfilesMasterSecret, 0);
+		ret.write(masterKeysFile, password, hardRandom);
+		return ret;
 	}
 
 	public static void clear(byte[] buf) {
@@ -213,6 +160,10 @@ public class MasterKeys {
 
 	public void changePassword(File masterKeysFile, String newPassword, RandomSource hardRandom) throws IOException {
 		System.err.println("Writing new master.keys file");
+		write(masterKeysFile, newPassword, hardRandom);
+	}
+	
+	private void write(File masterKeysFile, String newPassword, RandomSource hardRandom) throws IOException {
 		// Write it to a byte[], check size, then replace in-place atomically
 
 		// New IV, new salt, same client cache key, same database key
