6ee6f3a3fb3911cdd9bb10d0bdbcc7b63ef00cdc
Matthew Toseland <toad@amphibian.dyndns.org>
2014-09-23 20:10:10 +0100

    Document an important API glitch

--- src/freenet/support/api/RandomAccessBucket.java
+++ src/freenet/support/api/RandomAccessBucket.java
@@ -6,7 +6,12 @@ import java.io.IOException;
 /** A Bucket which can be converted to a LockableRandomAccessBuffer without copying. Mostly we need 
  * this where the size of something we will later use as a RandomAccessBuffer is uncertain. It 
  * provides a separate object because the API's are incompatible; in particular, the size of a 
- * RandomAccessBuffer is fixed (and this is mostly a good thing). */
+ * RandomAccessBuffer is fixed (and this is mostly a good thing). 
+ * 
+ * FINALIZERS: Persistent RandomAccessBucket's should never free on finalize. Transient RABs can
+ * free on finalize, but must ensure that this only happens if both the Bucket and the RAB are no
+ * longer reachable.
+ */
 public interface RandomAccessBucket extends Bucket {
     
     /** Convert the Bucket to a LockableRandomAccessBuffer. Must be efficient, i.e. will not copy 
