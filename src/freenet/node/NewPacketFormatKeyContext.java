hyphanet/fred

commit 5cf880586de442233aa87d58d254112c00b15551

Packet handling: simple efficiency improvements
PacketThrottle:
* add method to report multiple lost packets, so we can call that
  instead of iterating over notifyOfPacketLost. This replaces the
  original notifyOfPacketLost that is now no longer used

NewPacketFormatKeyContext:
* reduce the operations inside a synchronized(){} block to a bare
  minimum
* only keep track of the sent times of *lost* packets: if a packet is
  not lost, we still have a SentPacket that keeps track of its own sent
  time (this also reduces the amount of work in ack() for the most
  common code path)
* reduce bounds on packets to track sent time of from 16384 to 128 (we
  don't expect that many *lost* packets on short notice, and if we do,
  not having a super-accurate RTT estimate is the least of our
  problems)
* improve ack() CPU efficiency by using SentTimeCache instead of
  SentTimes (we're no longer concerned about memory given the above, so
  optimize for speed)
* use for(item : collection) wherever applicable for readability
* cleanup modified code to conform to the coding standards

SentTimeCache:
* this class replaces SentTimes
* uses a LinkedHashMap with O(1) lookup instead of a linear search
* uses more memory than SentTimes did, but we shouldn't care anymore

SentTimes:
* removed since it's highly inefficient by design and is no longer used

SentTimeCacheTest:
* new unit test for SentTimeCache

src/freenet/node/NewPacketFormatKeyContext.java
@@ -9,7 +9,7 @@ import freenet.io.xfer.PacketThrottle;
 import freenet.node.NewPacketFormat.SentPacket;
 import freenet.support.LogThresholdCallback;
 import freenet.support.Logger;
-import freenet.support.SentTimes;
+import freenet.support.SentTimeCache;
 import freenet.support.Logger.LogLevel;
 
 /** NewPacketFormat's context for each SessionKey. Specifically, packet numbers are unique
@@ -31,12 +31,11 @@ public class NewPacketFormatKeyContext {
 	
 	private final TreeMap<Integer, Long> acks = new TreeMap<Integer, Long>();
 	private final HashMap<Integer, SentPacket> sentPackets = new HashMap<Integer, SentPacket>();
-	/** Keep this many sent times even if the packets are not acked, so we can compute an
-	 * accurate round trip time if they are acked after we had decided they were lost. */
-	private static final int MAX_SENT_TIMES = 16384;
-	/** This must be memory efficient. Given we have one per peer, a TreeMap would be way
-	 * too big. */
-	private final SentTimes sentTimes = new SentTimes(MAX_SENT_TIMES);
+	/** Keep this many sent times for lost packets, so we can compute an accurate round trip time if
+	 * they are acked after we had decided they were lost. */
+	private static final int MAX_LOST_SENT_TIMES = 128;
+	/** We add all lost packets sequence numbers and the corresponding sent time to this cache. */
+	private final SentTimeCache lostSentTimes = new SentTimeCache(MAX_LOST_SENT_TIMES);
 	
 	private final Object sequenceNumberLock = new Object();
 	
@@ -104,32 +103,32 @@ public class NewPacketFormatKeyContext {
 		}
 	}
 
-	/** One of our outgoing packets has been acknowledged.
-	 * @return False if we have already acked the packet */
+	/** One of our outgoing packets has been acknowledged. */
 	public void ack(int ack, BasePeerNode pn, SessionKey key) {
 		long rtt;
 		int maxSize;
 		boolean lostBeforeAcked = false;
 		boolean validAck = false;
+		long ackReceived = System.currentTimeMillis();
+		if(logDEBUG) Logger.debug(this, "Acknowledging packet "+ack+" from "+pn);
+		SentPacket sent;
 		synchronized(sentPackets) {
-			if(logDEBUG) Logger.debug(this, "Acknowledging packet "+ack+" from "+pn);
-			SentPacket sent = sentPackets.remove(ack);
-			if(sent != null) {
-				rtt = sent.acked(key);
-				maxSize = (maxSeenInFlight * 2) + 10;
-				sentTimes.removeTime(ack);
-				validAck = true;
+			sent = sentPackets.remove(ack);
+		}
+		if(sent != null) {
+			rtt = sent.acked(key);
+			maxSize = (maxSeenInFlight * 2) + 10;
+			validAck = true;
+		} else {
+			if(logDEBUG) Logger.debug(this, "Already acked or lost "+ack);
+			lostBeforeAcked = true;
+			long packetSent = lostSentTimes.queryAndRemove(ack);
+			if(packetSent < 0) {
+				if(logDEBUG) Logger.debug(this, "No time for "+ack+" - maybe acked twice?");
+				return;
 			} else {
-				if(logDEBUG) Logger.debug(this, "Already acked or lost "+ack);
-				lostBeforeAcked = true;
-				long l = sentTimes.removeTime(ack);
-				if(l < 0) {
-					if(logDEBUG) Logger.debug(this, "No time for "+ack+" - maybe acked twice?");
-					return;
-				} else {
-					rtt = System.currentTimeMillis() - l;
-					maxSize = (maxSeenInFlight * 2) + 10;
-				}
+				rtt = ackReceived - packetSent;
+				maxSize = (maxSeenInFlight * 2) + 10;
 			}
 		}
 		
@@ -139,7 +138,7 @@ public class NewPacketFormatKeyContext {
 			pn.reportPing(rt);
 			throttle = pn.getThrottle();
 			if(validAck)
-				pn.receivedAck(System.currentTimeMillis());
+				pn.receivedAck(ackReceived);
 		}
 		if(throttle != null) {
 			throttle.setRoundTripTime(rt);
@@ -226,32 +225,33 @@ public class NewPacketFormatKeyContext {
 	}
 
 	public void sent(SentPacket sentPacket, int seqNum, int length) {
+	    sentPacket.sent(length);
+	    int inFlight;
 		synchronized(sentPackets) {
-			if(!sentPacket.messages.isEmpty()) {
-				sentTimes.add(seqNum, System.currentTimeMillis());
-			}
-			sentPacket.sent(length);
 			sentPackets.put(seqNum, sentPacket);
-			int inFlight = sentPackets.size();
-			if(inFlight > maxSeenInFlight) {
-				maxSeenInFlight = inFlight;
-				if(logDEBUG) Logger.debug(this, "Max seen in flight new record: "+maxSeenInFlight+" for "+this);
+			inFlight = sentPackets.size();
+		}
+		if(inFlight > maxSeenInFlight) {
+			maxSeenInFlight = inFlight;
+			if (logDEBUG) {
+			    Logger.debug(this, "Max seen in flight new record: " + maxSeenInFlight +
+			                       " for " + this);
 			}
 		}
 	}
 	
 	public long timeCheckForLostPackets(double averageRTT) {
 		long timeCheck = Long.MAX_VALUE;
+		// Because MIN_RTT_FOR_RETRANSMIT > MAX_ACK_DELAY, and because averageRTT() includes the
+		// actual ack delay, we don't need to add it on here.
+		double avgRtt = Math.max(MIN_RTT_FOR_RETRANSMIT, averageRTT);
+		long maxDelay = (long)(avgRtt + MAX_ACK_DELAY * 1.1);
 		synchronized(sentPackets) {
-			// Because MIN_RTT_FOR_RETRANSMIT > MAX_ACK_DELAY, and because averageRTT() includes the actual ack delay, we don't need to add it on here.
-			double avgRtt = Math.max(MIN_RTT_FOR_RETRANSMIT, averageRTT);
-			
-			Iterator<Map.Entry<Integer, SentPacket>> it = sentPackets.entrySet().iterator();
-			while(it.hasNext()) {
-				Map.Entry<Integer, SentPacket> e = it.next();
-				SentPacket s = e.getValue();
-				long t = (long) (s.getSentTime() + (avgRtt + MAX_ACK_DELAY * 1.1));
-				if(t < timeCheck) timeCheck = t;
+			for (SentPacket s : sentPackets.values()) {
+				long t = s.getSentTime() + maxDelay;
+				if (t < timeCheck) {
+				    timeCheck = t;
+			    }
 			}
 		}
 		return timeCheck;
@@ -261,35 +261,44 @@ public class NewPacketFormatKeyContext {
 		//Mark packets as lost
 		int bigLostCount = 0;
 		int count = 0;
+		
+		// Because MIN_RTT_FOR_RETRANSMIT > MAX_ACK_DELAY, and because averageRTT() includes the
+		// actual ack delay, we don't need to add it on here.
+		double avgRtt = Math.max(MIN_RTT_FOR_RETRANSMIT, averageRTT);
+		long maxDelay = (long)(avgRtt + MAX_ACK_DELAY * 1.1);
+		long threshold = curTime - maxDelay;
+		
 		synchronized(sentPackets) {
-			// Because MIN_RTT_FOR_RETRANSMIT > MAX_ACK_DELAY, and because averageRTT() includes the actual ack delay, we don't need to add it on here.
-			double avgRtt = Math.max(MIN_RTT_FOR_RETRANSMIT, averageRTT);
-
 			Iterator<Map.Entry<Integer, SentPacket>> it = sentPackets.entrySet().iterator();
 			while(it.hasNext()) {
 				Map.Entry<Integer, SentPacket> e = it.next();
 				SentPacket s = e.getValue();
-				if(s.getSentTime() < (curTime - (avgRtt + MAX_ACK_DELAY * 1.1))) {
-					if(logMINOR) {
+				if (s.getSentTime() < threshold) {
+					if (logMINOR) {
 						Logger.minor(this, "Assuming packet " + e.getKey() + " has been lost. "
 						                + "Delay " + (curTime - s.getSentTime()) + "ms, "
-						                + "threshold " + (avgRtt + MAX_ACK_DELAY * 1.1) + "ms");
+						                + "threshold " + threshold + "ms");
 					}
-					s.lost();
+					// Store the packet sentTime in our lost sent times cache, so we can calculate
+					// RTT if an ack may surface later on.
+					if(!s.messages.isEmpty()) {
+				        lostSentTimes.report(e.getKey(), s.getSentTime());
+			        }
+			        // Mark the packet as lost and remove it from our active packets.
+			        s.lost();
 					it.remove();
 					bigLostCount++;
-				} else
+				} else {
 					count++;
+				}
 			}
-			if(count > 0 && logMINOR)
-				Logger.minor(this, ""+count+" packets in flight with threshold "+(avgRtt + MAX_ACK_DELAY * 1.1) + "ms");
 		}
+		if(count > 0 && logMINOR)
+			Logger.minor(this, "" + count + " packets in flight with threshold " + maxDelay + "ms");
 		if(bigLostCount != 0 && pn != null) {
 			PacketThrottle throttle = pn.getThrottle();
 			if(throttle != null) {
-				for(int i=0;i<bigLostCount;i++) {
-					throttle.notifyOfPacketLost();
-				}
+				throttle.notifyOfPacketsLost(bigLostCount);
 			}
 			pn.backoffOnResend();
 		}
@@ -308,14 +317,11 @@ public class NewPacketFormatKeyContext {
 
 	public void disconnected() {
 		synchronized(sentPackets) {
-			Iterator<Map.Entry<Integer, SentPacket>> it = sentPackets.entrySet().iterator();
-			while(it.hasNext()) {
-				Map.Entry<Integer, SentPacket> e = it.next();
-				SentPacket s = e.getValue();
+			for (SentPacket s: sentPackets.values()) {
 				s.lost();
 			}
 			sentPackets.clear();
 		}
 	}
-
 }
+

src/freenet/io/xfer/PacketThrottle.java
@@ -59,14 +59,20 @@ public class PacketThrottle {
 		if(logMINOR) Logger.minor(this, "Set round trip time to "+rtt+" on "+this);
 	}
 
-    public synchronized void notifyOfPacketLost() {
-		_droppedPackets++;
-		_totalPackets++;
-		_windowSize *= PACKET_DROP_DECREASE_MULTIPLE;
-		if(_windowSize < 1.0F) _windowSize = 1.0F;
-		slowStart = false;
-		if(logMINOR)
-			Logger.minor(this, "notifyOfPacketLost(): "+this);
+    public synchronized void notifyOfPacketsLost(int numPackets) {
+        if (numPackets <= 0) {
+            throw new IllegalArgumentException("Reported loss is zero or negative");
+        }
+        _droppedPackets += numPackets;
+        _totalPackets += numPackets;
+        _windowSize *= Math.pow(PACKET_DROP_DECREASE_MULTIPLE, numPackets);
+        if (_windowSize < 1.0F) {
+            _windowSize = 1.0F;
+        }
+        slowStart = false;
+        if (logMINOR) {
+            Logger.minor(this, "notifyOfPacketsLost(): " + this);
+        }
     }
 
     /**

src/freenet/support/SentTimeCache.java
@@ -1,0 +1,105 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
+package freenet.support;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Keeps track of times at which sequence numbers were reported as sent, with bounded capacity.
+ * Cached times at which {@link #sent(int)} was invoked are dropped on a first-in first-out basis.
+ *
+ * @author bertm
+ */
+public class SentTimeCache {
+
+    /**
+     * LinkedHashMap with int keys, long values, that has bounded capacity.
+     */
+    private class BoundedSentTimeMap extends LinkedHashMap<Integer, Long> {
+        private static final long serialVersionUID = 0;
+        private final int maxSize;
+        
+        /**
+         * Constructs a map with the given maximum (and initial) size.
+         */
+        BoundedSentTimeMap(int maxSize) {
+            super(maxSize);
+            if (maxSize <= 0) {
+                throw new IllegalArgumentException("Negative or zero maxSize");
+            }
+            this.maxSize = maxSize;
+        }
+        
+        /**
+         * Automatically maintains the maximum size by returning true if the capacity is exceeded,
+         * indicating that the eldest entry must be removed.
+         * @see LinkedHashMap#removeEldestEntry(Map.Entry)
+         */
+        protected boolean removeEldestEntry(Map.Entry<Integer, Long> eldest) {
+            return size() > maxSize;
+        }
+    }
+    
+    /**
+     * The inner cache.
+     */
+    private BoundedSentTimeMap cache;
+    
+    /**
+     * Constructs a sent time cache with the given maximal capacity.
+     */
+    public SentTimeCache(int maxSize) {
+        cache = new BoundedSentTimeMap(maxSize);
+    }
+    
+    /**
+     * Reports the given sequence number as being sent at the given time. If the cache is at full
+     * capacity, this will lead to the oldest entry being dropped. If the sequence number was
+     * already in this cache, the given time will be associated with the sequence number, but the 
+     * order in which sequence numbers are dropped from the cache will not be affected.
+     * @param seqnum the sequence number
+     * @param time the sent time in milliseconds
+     */
+    public synchronized void report(int seqnum, long time) {
+        cache.put(seqnum, time);
+    }
+    
+    /**
+     * Convenience wrapper for {@link #report(int, long)}.
+     * Reports the given sequence number as being sent right now. If the cache is at full capacity,
+     * this will lead to the oldest entry being dropped. If the sequence number was already in this
+     * cache, the current time will be associated with the sequence number, but the order in which
+     * sequence numbers are dropped from the cache will not be affected.
+     * @param seqnum the sequence number
+     * @see SentTimeCache#report(int, long)
+     */
+    public synchronized void sent(int seqnum) {
+        long time = System.currentTimeMillis();
+        report(seqnum, time);
+    }
+    
+    /**
+     * Queries the sent time for the given sequence number and removes it from the cache.
+     * @param seqnum the sequence number
+     * @returns The time at which the sequence number was reported to {@link #sent(int)}, in
+     * milliseconds, or a negative value if the sequence number was not in this cache.
+     */
+    public synchronized long queryAndRemove(int seqnum) {
+        Long ret = cache.remove(seqnum);
+        if (ret == null) {
+            return -1;
+        }
+        return ret;
+    }
+
+    /**
+     * Queries the number of items currently held by this cache. This method is not thread-safe.
+     * @return The number of items in this cache.
+     */
+    int size() {
+        return cache.size();
+    }
+}
+

src/freenet/support/SentTimes.java
@@ -1,43 +1,0 @@
-package freenet.support;
-
-/**
- * This is not a CPU-efficient structure! However, it IS memory efficient. Using a TreeMap
- * for the same job would use an unacceptable amount of memory. Binary searching might be
- * a possibility, but the amount of copying data around that would be involved might 
- * negate the savings. A btree would be completely excessive. Consider the callers' actual
- * usage carefully when optimising.
- * @author toad
- */
-public class SentTimes {
-	
-	private final int[] seqnums;
-	private final long[] times;
-	private int ptr;
-	
-	public SentTimes(int size) {
-		seqnums = new int[size];
-		times = new long[size];
-		ptr = 0;
-	}
-	
-	public synchronized void add(int seqnum, long time) {
-		if(time < 0) throw new IllegalArgumentException();
-		seqnums[ptr] = seqnum;
-		times[ptr] = time;
-		ptr++;
-		if(ptr == seqnums.length) ptr = 0;
-	}
-	
-	public synchronized long removeTime(int seqnum) {
-		for(int i=0;i<seqnums.length;i++) {
-			if(seqnums[i] == seqnum && times[i] > 0) {
-				long t = times[i];
-				seqnums[i] = 0;
-				times[i] = -1;
-				return t;
-			}
-		}
-		return -1;
-	}
-
-}

test/freenet/support/SentTimeCacheTest.java
@@ -1,0 +1,88 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
+package freenet.support;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests for {@link SentTimeCache}.
+ *
+ * @author bertm
+ */
+public class SentTimeCacheTest extends TestCase {
+    private static final int CACHE_SIZE = 32;
+
+    /**
+     * Tests if the cache adheres to its given maximum capacity.
+     */
+    public void testMaxSize() {
+        SentTimeCache c = newCache();
+        fillWithSequence(c);
+        c.sent(CACHE_SIZE + 1);
+        assertEquals(c.size(), CACHE_SIZE);
+    }
+
+    /**
+     * Tests the correctness of simple {@link SentTimeCache#queryAndRemove(int)} use.
+     */
+    public void testQueryAndRemove() {
+        SentTimeCache c = newCache();
+        fillWithSequence(c);
+
+        // Test if all entries are reported back correctly.
+        for (int n = 1; n <= CACHE_SIZE; n++) {
+            long t = c.queryAndRemove(n);
+            // The returned time is correct.
+            assertEquals(t, n);
+            // The entry is removed from the cache.
+            assertEquals(c.size(), CACHE_SIZE - n);
+        }
+    }
+
+    /**
+     * Tests the FIFO characteristics of the cache.
+     */
+    public void testFifo() {
+        SentTimeCache c = newCache();
+        fillWithSequence(c);
+        // Test if old entries are pushed out.
+        for (int n = 1; n <= CACHE_SIZE; n++) {
+            // Push the oldest entry out by reporting a new one.
+            c.report(n + CACHE_SIZE, n + CACHE_SIZE);
+            // Try to fetch the entry that should have been removed.
+            long t = c.queryAndRemove(n);
+            // The query was not successful.
+            assertTrue(t < 0);
+            // Nothing was removed from from the cache.
+            assertEquals(c.size(), CACHE_SIZE);
+        }
+        // Test if the newly inserted entries are kept.
+        for (int n = 1; n <= CACHE_SIZE; n++) {
+            long t = c.queryAndRemove(n + CACHE_SIZE);
+            assertEquals(t, n + CACHE_SIZE);
+        }
+    }
+
+    /**
+     * Constructs a new cache of size {@link #CACHE_SIZE} and asserts its emptiness.
+     * @return The cache.
+     */
+    private SentTimeCache newCache() {
+        SentTimeCache c = new SentTimeCache(CACHE_SIZE);
+        assertEquals(c.size(), 0);
+        return c;
+    }
+
+    /**
+     * Fills the cache with sequence [1..{@link #CACHE_SIZE}] ({@code seqnum} equal to {@code
+     * time}), asserting it is at full capacity before returning.
+     * @param c the cache
+     */
+    private void fillWithSequence(SentTimeCache c) {
+        for (int n = 1; n <= CACHE_SIZE; n++) {
+            c.report(n, n);
+        }
+        assertEquals(c.size(), CACHE_SIZE);
+    }
+}

commit 5d6ffa5937c4c0de22271087ac57040f73501703

Fix racing
regression by 5cf880586de442233aa87d58d254112c00b15551

src/freenet/node/NewPacketFormatKeyContext.java
@@ -114,10 +114,10 @@ public class NewPacketFormatKeyContext {
 		SentPacket sent;
 		synchronized(sentPackets) {
 			sent = sentPackets.remove(ack);
+			maxSize = (maxSeenInFlight * 2) + 10;
 		}
 		if(sent != null) {
 			rtt = sent.acked(key);
-			maxSize = (maxSeenInFlight * 2) + 10;
 			validAck = true;
 		} else {
 			if(logDEBUG) Logger.debug(this, "Already acked or lost "+ack);
@@ -128,7 +128,6 @@ public class NewPacketFormatKeyContext {
 				return;
 			} else {
 				rtt = ackReceived - packetSent;
-				maxSize = (maxSeenInFlight * 2) + 10;
 			}
 		}
 		
@@ -226,16 +225,15 @@ public class NewPacketFormatKeyContext {
 
 	public void sent(SentPacket sentPacket, int seqNum, int length) {
 	    sentPacket.sent(length);
-	    int inFlight;
 		synchronized(sentPackets) {
 			sentPackets.put(seqNum, sentPacket);
-			inFlight = sentPackets.size();
-		}
-		if(inFlight > maxSeenInFlight) {
-			maxSeenInFlight = inFlight;
-			if (logDEBUG) {
-			    Logger.debug(this, "Max seen in flight new record: " + maxSeenInFlight +
-			                       " for " + this);
+			int inFlight = sentPackets.size();
+			if(inFlight > maxSeenInFlight) {
+				maxSeenInFlight = inFlight;
+				if (logDEBUG) {
+					Logger.debug(this, "Max seen in flight new record: " + maxSeenInFlight +
+							" for " + this);
+				}
 			}
 		}
 	}