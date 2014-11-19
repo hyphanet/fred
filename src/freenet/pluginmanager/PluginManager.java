hyphanet/fred

commit 869f62559a06b2f72c9224a2179001dd551ea442

Start the plugins off-thread. Don't let them block the rest of startup. If they are loaded from Freenet, they could take a long time to load, especially if they also block IP detection!

src/freenet/pluginmanager/PluginManager.java
@@ -234,8 +234,16 @@ public class PluginManager {
 
 	public void start(Config config) {
 		if(toStart != null)
-			for(String name : toStart)
-				startPluginAuto(name, false);
+			for(final String name : toStart) {
+			    core.getExecutor().execute(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        startPluginAuto(name, false);
+                    }
+			        
+			    });
+			}
 		synchronized(pluginWrappers) {
 			started = true;
 			toStart = null;

commit c2d413232adaabdfe9475b4aa3b5ae133b1cb29e

PluginManager: fix plugin configuration corruption
This fixes a regression introduced by
869f62559a06b2f72c9224a2179001dd551ea442 which corrupts plugin
configuration by prompting getConfigLoadString() to access plugin
startup information before startup is complete.

src/freenet/pluginmanager/PluginManager.java
@@ -25,6 +25,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.concurrent.Semaphore;
 import java.util.jar.Attributes;
 import java.util.jar.JarException;
 import java.util.jar.JarFile;
@@ -231,21 +232,36 @@ public class PluginManager {
 	private String[] toStart;
 
 	public void start(Config config) {
-		if(toStart != null)
+		if (toStart == null) {
+			synchronized (pluginWrappers) {
+				started = true;
+				return;
+			}
+		}
+
+		final Semaphore startingPlugins = new Semaphore(0);
 			for(final String name : toStart) {
 			    core.getExecutor().execute(new Runnable() {
 
                     @Override
                     public void run() {
                         startPluginAuto(name, false);
+                        startingPlugins.release();
                     }
-			        
+
 			    });
 			}
+
+		core.getExecutor().execute(new Runnable() {
+			@Override
+			public void run() {
+				startingPlugins.acquireUninterruptibly(toStart.length);
 		synchronized(pluginWrappers) {
 			started = true;
 			toStart = null;
 		}
+			}
+		});
 	}
 
 	public void stop(long maxWaitTime) {