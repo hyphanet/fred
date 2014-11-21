hyphanet/fred

commit 7fd10aa55995e2dcd2f996ef18987d21948eef6c

Fix leak in SlashdotStore and add logging

src/freenet/store/SlashdotStore.java
@@ -229,12 +229,20 @@ public class SlashdotStore<T extends StorableBlock> implements FreenetStore<T> {
 	}
 	
 	protected void purgeOldData(ByteArrayWrapper key, DiskBlock addFirst) {
+	    if(logDEBUG) Logger.minor(this, "Dumping old data from "+this+(addFirst == null ? "" : " and adding "+addFirst.data));
 		List<DiskBlock> blocks = null;
+		DiskBlock oldBlock;
 		synchronized(this) {
 			long now = System.currentTimeMillis();
 			if(addFirst != null) {
 				addFirst.lastAccessed = now;
-				blocksByRoutingKey.push(key, addFirst);
+				oldBlock = blocksByRoutingKey.push(key, addFirst);
+				if(oldBlock != null) {
+				    Logger.warning(this, "Replacing "+oldBlock+" with "+addFirst+" for "+key);
+	                if(blocks == null) blocks = new ArrayList<DiskBlock>();
+	                if(logDEBUG) Logger.minor(this, "Will dump "+oldBlock);
+	                blocks.add(oldBlock);
+				}
 				writes++;
 			}
 			while(true) {
@@ -242,6 +250,7 @@ public class SlashdotStore<T extends StorableBlock> implements FreenetStore<T> {
 				DiskBlock block = blocksByRoutingKey.peekValue();
 				if(now - block.lastAccessed < maxLifetime && blocksByRoutingKey.size() < maxKeys) break;
 				if(blocks == null) blocks = new ArrayList<DiskBlock>();
+				if(logDEBUG) Logger.minor(this, "Will dump "+block);
 				blocks.add(block);
 				blocksByRoutingKey.popValue();
 			}

src/freenet/support/LRUMap.java
@@ -64,14 +64,16 @@ public class LRUMap<K, V> {
      *       recently used position, but doesn't add
      *       a duplicate entry in the queue.
      */
-    public final synchronized void push(K key, V value) {
+    public final synchronized V push(K key, V value) {
     	if(key == null)
     		throw new NullPointerException();
+    	V old = null;
         QItem<K,V> insert = hash.get(key);
         if (insert == null) {
             insert = new QItem<K, V>(key, value);
             hash.put(key,insert);
         } else {
+            old = insert.value;
         	insert.value = value;
             list.remove(insert);
         }
@@ -79,6 +81,7 @@ public class LRUMap<K, V> {
         	Logger.minor(this, "Pushed "+insert+" ( "+key+ ' ' +value+" )");
 
         list.unshift(insert);
+        return old;
     } 
 
     /**

commit cd02c0aa675629716ad327e52f00751f61b56ddb

Remove unnecessary logging
regression by 7fd10aa55995e2dcd2f996ef18987d21948eef6c

src/freenet/store/SlashdotStore.java
@@ -229,7 +229,6 @@ public class SlashdotStore<T extends StorableBlock> implements FreenetStore<T> {
 	}
 	
 	protected void purgeOldData(ByteArrayWrapper key, DiskBlock addFirst) {
-	    if(logDEBUG) Logger.minor(this, "Dumping old data from "+this+(addFirst == null ? "" : " and adding "+addFirst.data));
 		List<DiskBlock> blocks = null;
 		DiskBlock oldBlock;
 		synchronized(this) {
@@ -238,9 +237,7 @@ public class SlashdotStore<T extends StorableBlock> implements FreenetStore<T> {
 				addFirst.lastAccessed = now;
 				oldBlock = blocksByRoutingKey.push(key, addFirst);
 				if(oldBlock != null) {
-				    Logger.warning(this, "Replacing "+oldBlock+" with "+addFirst+" for "+key);
 	                if(blocks == null) blocks = new ArrayList<DiskBlock>();
-	                if(logDEBUG) Logger.minor(this, "Will dump "+oldBlock);
 	                blocks.add(oldBlock);
 				}
 				writes++;
@@ -250,7 +247,6 @@ public class SlashdotStore<T extends StorableBlock> implements FreenetStore<T> {
 				DiskBlock block = blocksByRoutingKey.peekValue();
 				if(now - block.lastAccessed < maxLifetime && blocksByRoutingKey.size() < maxKeys) break;
 				if(blocks == null) blocks = new ArrayList<DiskBlock>();
-				if(logDEBUG) Logger.minor(this, "Will dump "+block);
 				blocks.add(block);
 				blocksByRoutingKey.popValue();
 			}