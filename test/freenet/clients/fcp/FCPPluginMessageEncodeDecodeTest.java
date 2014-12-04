7176c35d2a69e82931cab6274fd88fbc7edf7a77
xor-freenet <xor@freenetproject.org>
2014-12-04 09:09:33 +0100

    Post-merge fixes of merging purge-db4o
    Move class FCPPluginMessageEncodeDecodeTest from package
    freenet.node.fcp to  freenet.clients.fcp as the related classes were
    moved like that as well  in purge-db4o.
    
    This is part of a series of commits to fix compiler issues as a result
    of the merge 85be221b19882802806ca12614e96ad156f466d6.
    The series is not complete yet, the last commit will tell its the last.

--- test/freenet/clients/fcp/FCPPluginMessageEncodeDecodeTest.java
+++ test/freenet/clients/fcp/FCPPluginMessageEncodeDecodeTest.java
@@ -1,7 +1,7 @@
 /* This code is part of Freenet. It is distributed under the GNU General
  * Public License, version 2 (or at your option any later version). See
  * http://www.gnu.org/ for further details of the GPL. */
-package freenet.node.fcp;
+package freenet.clients.fcp;
 
 import java.io.IOException;
 import java.util.ArrayList;
