hyphanet/fred

commit fb1413b974eebc7b2ddd7243b9982790ed7204c1

Only round-robin for urgent messages. Just use a list for non-urgent ones. Move them to urgent if they're not sent in a reasonable time.

src/freenet/node/PeerMessageQueue.java
@@ -3,6 +3,7 @@ package freenet.node;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Map;
 
 import freenet.io.comm.DMT;
@@ -64,36 +65,52 @@ public class PeerMessageQueue {
 		 * duplicate keys. */
 		ArrayList<Items> itemsWithID;
 		Map<Long, Items> itemsByID;
+		/** Non-urgent messages. Same order as in Items, so stuff to send first is at
+		 * the beginning. */
+		LinkedList<MessageItem> itemsNonUrgent;
 		// Construct structures lazily, we're protected by the overall synchronized.
 
 		/** Add a new message, to the end of the lists, i.e. in first-in-first-out order,
 		 * which will wait for the existing messages to be sent first. */
 		public void addLast(MessageItem item) {
-			long id = item.getID();
-			Items list;
-			if(itemsByID == null) {
-				itemsByID = new HashMap<Long, Items>();
-				itemsWithID = new ArrayList<Items>();
-				list = new Items(id);
-				itemsWithID.add(list);
-				itemsByID.put(id, list);
-			} else {
-				list = itemsByID.get(id);
-				if(list == null) {
-					list = new Items(id);
-					// In order to ensure fairness, we add it at the beginning.
-					// This method is typically called by sendAsync().
-					// If there are later items they are probably block transfers that are
-					// already in progress; it is fairer to send the new item first.
-					itemsWithID.add(0, list);
-					itemsByID.put(id, list);
-				}
+			if(itemsNonUrgent == null)
+				itemsNonUrgent = new LinkedList<MessageItem>();
+			itemsNonUrgent.addLast(item);
+		}
+		
+		private void moveToUrgent(long now) {
+			ListIterator<MessageItem> it = itemsNonUrgent.listIterator();
+			while(it.hasNext()) {
+				MessageItem item = it.next();
+				if(item.submitted + PacketSender.MAX_COALESCING_DELAY >= now) {
+					// Move to urgent list
+					long id = item.getID();
+					Items list;
+					if(itemsByID == null) {
+						itemsByID = new HashMap<Long, Items>();
+						itemsWithID = new ArrayList<Items>();
+						list = new Items(id);
+						itemsWithID.add(list);
+						itemsByID.put(id, list);
+					} else {
+						list = itemsByID.get(id);
+						if(list == null) {
+							list = new Items(id);
+							// In order to ensure fairness, we add it at the beginning.
+							// This method is typically called by sendAsync().
+							// If there are later items they are probably block transfers that are
+							// already in progress; it is fairer to send the new item first.
+							itemsWithID.add(0, list);
+							itemsByID.put(id, list);
+						}
+					}
+					list.addLast(item);
+				} else return;
 			}
-			list.addLast(item);
 		}
 
 		/** Add a new message to the beginning i.e. send it as soon as possible (e.g. if
-		 * we tried to send it and failed). */
+		 * we tried to send it and failed); it is assumed to already be urgent. */
 		public void addFirst(MessageItem item) {
 			long id = item.getID();
 			Items list;
@@ -161,6 +178,42 @@ public class PeerMessageQueue {
 			}
 			return length;
 		}
+		
+		private int addNonUrgentMessages(int size, int minSize, int maxSize, long now, ArrayList<MessageItem> messages) {
+			assert(size >= 0);
+			assert(minSize >= 0);
+			assert(maxSize >= minSize);
+			if(size < 0) size = -size; // FIXME remove extra paranoia
+			if(itemsNonUrgent == null) return size;
+			for(ListIterator<MessageItem> items = itemsNonUrgent.listIterator();items.hasNext();) {
+				MessageItem item = items.next();
+				int thisSize = item.getLength();
+				boolean oversize = false;
+				if(size + 2 + thisSize > maxSize) {
+					if(size == minSize) {
+						// Won't fit regardless, send it on its own.
+						oversize = true;
+					} else {
+						// Send what we have so far.
+						return -size;
+					}
+				}
+				size += 2 + thisSize;
+				items.remove();
+				messages.add(item);
+				if(itemsByID != null) {
+					long id = item.getID();
+					Items tracker = itemsByID.get(id);
+					if(tracker != null) {
+						// Demote the corresponding tracker to maintain round-robin.
+						itemsWithID.remove(tracker);
+						itemsWithID.add(tracker);
+					}
+				}
+				if(oversize) return size;
+			}
+			return size;
+		}
 
 		/**
 		 * Add messages to <code>messages</code> until there are no more
@@ -179,7 +232,7 @@ public class PeerMessageQueue {
 		 * @return the size of <code>messages</code>, multiplied by -1 if there were
 		 * messages that didn't fit
 		 */
-		private int addMessages(int size, int minSize, int maxSize, long now, ArrayList<MessageItem> messages, boolean isUrgent) {
+		private int addUrgentMessages(int size, int minSize, int maxSize, long now, ArrayList<MessageItem> messages) {
 			assert(size >= 0);
 			assert(minSize >= 0);
 			assert(maxSize >= minSize);
@@ -208,12 +261,6 @@ public class PeerMessageQueue {
 						continue;
 					}
 					MessageItem item = list.items.getFirst();
-					if(isUrgent && item.submitted + PacketSender.MAX_COALESCING_DELAY > now) {
-						// Skip non-urgent item, try the rest.
-						skipped++;
-						continue;
-					}
-					
 					int thisSize = item.getLength();
 					boolean oversize = false;
 					if(size + 2 + thisSize > maxSize) {
@@ -250,30 +297,23 @@ public class PeerMessageQueue {
 		 * @return
 		 */
 		int addPriorityMessages(int size, int minSize, int maxSize, long now, ArrayList<MessageItem> messages, MutableBoolean incomplete) {
-			// Urgent messages first.
-			size = innerAddMessages(size, minSize, maxSize, now, messages, incomplete, true);
-			if(incomplete.value) return size;
-			// If no more urgent messages, try to add some non-urgent messages too.
-			size = innerAddMessages(size, minSize, maxSize, now, messages, incomplete, false);
-			return size;
-		}
-
-		private int innerAddMessages(int size, int minSize, int maxSize,
-				long now, ArrayList<MessageItem> messages,
-				MutableBoolean incomplete,
-				boolean urgentOnly) {
-			while(true) {
-				synchronized(PeerMessageQueue.this) {
-					size = addMessages(size, minSize, maxSize, now, messages, urgentOnly);
-				}
+			synchronized(PeerMessageQueue.this) {
+				// Urgent messages first.
+				moveToUrgent(now);
+				size = addUrgentMessages(size, minSize, maxSize, now, messages);
 				if(size < 0) {
-					incomplete.value = true;
 					size = -size;
+					incomplete.value = true;
+					return size;
 				}
-				boolean recall = false;
-				if(!recall) break;
+				// If no more urgent messages, try to add some non-urgent messages too.
+				size = addNonUrgentMessages(size, minSize, maxSize, now, messages);
+				if(size < 0) {
+					size = -size;
+					incomplete.value = true;
+				}
+				return size;
 			}
-			return size;
 		}
 
 		public void clear() {

commit bf4b2c1e0fe027daad4e9e6965db984566882a5b

Fix PeerMessageQueue.getMessageQueueLengthBytes()
getMessageQueueLengthBytes() did not count messages on
pq.itemsNonUrgent
Regression by fb1413b974eebc7b2ddd7243b9982790ed7204c1

src/freenet/node/PeerMessageQueue.java
@@ -757,13 +757,21 @@ public class PeerMessageQueue {
 	}
 
 	public synchronized long getMessageQueueLengthBytes() {
+		if (cachedQueueSize>=0 && cachedQueueSizeIsComplete)
+			return cachedQueueSize;
 		long x = 0;
 		for(PrioQueue pq : queuesByPriority) {
+			if(pq.itemsNonUrgent != null) {
+				for(MessageItem item : pq.itemsNonUrgent)
+					x += item.getLength() + MESSAGE_OVERHEAD;
+			}
 			if(pq.nonEmptyItemsWithID != null)
 				for(PrioQueue.Items q : pq.nonEmptyItemsWithID)
 					for(MessageItem it : q.items)
 						x += it.getLength() + MESSAGE_OVERHEAD;
 		}
+		cachedQueueSize = x > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int)x;
+		cachedQueueSizeIsComplete = true;
 		return x;
 	}
 