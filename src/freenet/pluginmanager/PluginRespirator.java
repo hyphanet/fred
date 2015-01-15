95649212fba83ffadc9488b408b550c51d70b448
xor-freenet <xor@freenetproject.org>
2015-01-15 18:01:05 +0100

    PluginRespirator.connectToOtherPlugin(): Improve JavaDoc readability
    The "by implementing" sounded like it was refering to how connecting
    works, but it actually meant to refer how a FCP server is implemented.

--- src/freenet/pluginmanager/PluginRespirator.java
+++ src/freenet/pluginmanager/PluginRespirator.java
@@ -111,8 +111,8 @@ public class PluginRespirator {
 	}
 
     /**
-     * <p>Creates a FCP client connection with another plugin which is a FCP server by implementing
-     * the {@link FredPluginFCPMessageHandler.ServerSideFCPMessageHandler} interface.<br>
+     * <p>Creates a FCP client connection with another plugin which is a FCP server (= which
+     * implements interface {@link FredPluginFCPMessageHandler.ServerSideFCPMessageHandler}).<br>
      * Currently, the remote plugin must run in the same node, but the fact that FCP is used lays
      * foundation for a future implementation to allow you to connect to other plugins by network,
      * no matter where they are running.</p>
