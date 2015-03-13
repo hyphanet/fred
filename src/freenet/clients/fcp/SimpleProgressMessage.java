b11b8152140a0d3c6821c9d4f2eefca68b5b1d51
xor-freenet <xor@freenetproject.org>
2015-03-13 11:10:29 +0100

    FCP SimpleProgressMessage: Propagate Date latestSuccess & latestFailure
    This adds the date the request last succeeded or failed in fetching a
    block to the FCP message, as a Unix timestamp.
    
    It also adds getters for internal fred use, since the following commit
    will need those to propagate the information from the contained
    SplitfileProgressEvent to constructors of RequestStatus objects.

--- src/freenet/clients/fcp/SimpleProgressMessage.java
+++ src/freenet/clients/fcp/SimpleProgressMessage.java
@@ -3,7 +3,7 @@
  * http://www.gnu.org/ for further details of the GPL. */
 package freenet.clients.fcp;
 
-import java.io.Serializable;
+import java.util.Date;
 
 import freenet.client.events.SplitfileProgressEvent;
 import freenet.node.Node;
@@ -35,7 +35,9 @@ public class SimpleProgressMessage extends FCPMessage {
 		fs.put("Required", event.minSuccessfulBlocks);
 		fs.put("Failed", event.failedBlocks);
 		fs.put("FatallyFailed", event.fatallyFailedBlocks);
+		fs.put("LatestFailure", event.latestFailure != null ? event.latestFailure.getTime() : 0);
 		fs.put("Succeeded",event.succeedBlocks);
+        fs.put("LatestSuccess", event.latestSuccess != null ? event.latestSuccess.getTime() : 0);
 		fs.put("FinalizedTotal", event.finalizedTotal);
 		if(event.minSuccessFetchBlocks != 0)
 			fs.put("MinSuccessFetchBlocks", event.minSuccessFetchBlocks);
@@ -70,6 +72,11 @@ public class SimpleProgressMessage extends FCPMessage {
 		return event.succeedBlocks;
 	}
 	
+	public Date getLatestSuccess() {
+        // clone() because Date is mutable
+	    return event.latestSuccess != null ? (Date)event.latestSuccess.clone() : null;
+	}
+	
 	public double getFailedBlocks(){
 		return event.failedBlocks;
 	}
@@ -77,6 +84,11 @@ public class SimpleProgressMessage extends FCPMessage {
 	public double getFatalyFailedBlocks(){
 		return event.fatallyFailedBlocks;
 	}
+	
+    public Date getLatestFailure() {
+        // clone() because Date is mutable
+        return event.latestFailure != null ? (Date)event.latestFailure.clone() : null;
+    }
 
 	public boolean isTotalFinalized() {
 		return event.finalizedTotal;
