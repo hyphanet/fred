hyphanet/fred

commit 0baad95141fd4e896d856a3c96be051d55e54ede

Factor out CompatibilityAnalyser to a separate class. Some changes to how ClientGet handles events.

src/freenet/client/async/CompatibilityAnalyser.java
@@ -1,0 +1,77 @@
+package freenet.client.async;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+import freenet.client.InsertContext;
+import freenet.client.InsertContext.CompatibilityMode;
+import freenet.support.Logger;
+
+public class CompatibilityAnalyser implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    long min;
+    long max;
+    byte[] cryptoKey;
+    boolean dontCompress;
+    boolean definitive;
+    
+    public CompatibilityAnalyser() {
+        this.min = InsertContext.CompatibilityMode.COMPAT_UNKNOWN.ordinal();
+        this.max = InsertContext.CompatibilityMode.COMPAT_UNKNOWN.ordinal();
+    }
+    
+    CompatibilityAnalyser(long min, long max, byte[] cryptoKey, boolean dontCompress, boolean definitive) {
+        this.min = min;
+        this.max = max;
+        this.cryptoKey = cryptoKey;
+        this.dontCompress = dontCompress;
+        this.definitive = definitive;
+    }
+    
+    public void merge(long min, long max, byte[] cryptoKey, boolean dontCompress, boolean definitive) {
+        if(this.definitive) {
+            Logger.warning(this, "merge() after definitive", new Exception("debug"));
+            return;
+        }
+        if(definitive) this.definitive = true;
+        if(!dontCompress) this.dontCompress = false;
+        if(min > this.min) this.min = min;
+        if(max < this.max || this.max == InsertContext.CompatibilityMode.COMPAT_UNKNOWN.ordinal()) this.max = max;
+        if(this.cryptoKey == null) {
+            this.cryptoKey = cryptoKey;
+        } else if(cryptoKey != null && !Arrays.equals(this.cryptoKey, cryptoKey)) {
+            Logger.error(this, "Two different crypto keys!");
+            this.cryptoKey = null;
+        }
+    }
+
+    public CompatibilityMode min() {
+        return InsertContext.CompatibilityMode.values()[(int)min];
+    }
+    
+    public CompatibilityMode max() {
+        return InsertContext.CompatibilityMode.values()[(int)min];
+    }
+
+    public byte[] getCryptoKey() {
+        return cryptoKey;
+    }
+
+    public boolean dontCompress() {
+        return dontCompress;
+    }
+
+    public boolean definitive() {
+        return definitive;
+    }
+
+    public InsertContext.CompatibilityMode[] getModes() {
+        return new InsertContext.CompatibilityMode[] {
+                InsertContext.CompatibilityMode.values()[(int)min],
+                InsertContext.CompatibilityMode.values()[(int)max]
+        };
+    }
+    
+
+}

src/freenet/clients/fcp/ClientGet.java
@@ -18,6 +18,7 @@ import freenet.client.async.ClientContext;
 import freenet.client.async.ClientGetCallback;
 import freenet.client.async.ClientGetter;
 import freenet.client.async.ClientRequester;
+import freenet.client.async.CompatibilityAnalyser;
 import freenet.client.async.PersistenceDisabledException;
 import freenet.client.async.PersistentClientCallback;
 import freenet.client.async.PersistentJob;
@@ -90,9 +91,8 @@ public class ClientGet extends ClientRequest implements ClientGetCallback, Clien
 	private transient boolean sentToNetwork;
 	/** Current compatibility mode. This is updated over time as the request progresses, and can be
 	 * used e.g. to reinsert the file. This is NOT transient, as the ClientGetter does not retain 
-	 * this information. FIXME consider moving this to ClientGetter (it's not really part of what
-	 * ClientGetter is for though). */
-	private CompatibilityMode compatMessage;
+	 * this information. */
+	private CompatibilityAnalyser compatMode;
 	private transient ExpectedHashes expectedHashes;
 
         private static volatile boolean logMINOR;
@@ -129,6 +129,7 @@ public class ClientGet extends ClientRequest implements ClientGetCallback, Clien
 		fctx.maxOutputLength = maxOutputLength;
 		fctx.maxTempLength = maxOutputLength;
 		fctx.canWriteClientCache = writeToClientCache;
+		compatMode = new CompatibilityAnalyser();
 		// FIXME fctx.ignoreUSKDatehints = ignoreUSKDatehints;
 		Bucket ret = null;
 		this.returnType = returnType;
@@ -178,6 +179,7 @@ public class ClientGet extends ClientRequest implements ClientGetCallback, Clien
 		fctx.canWriteClientCache = message.writeToClientCache;
 		fctx.filterData = message.filterData;
 		fctx.ignoreUSKDatehints = message.ignoreUSKDatehints;
+		compatMode = new CompatibilityAnalyser();
 
 		if(message.allowedMIMETypes != null) {
 			fctx.allowedMIMETypes = new HashSet<String>();
@@ -381,19 +383,6 @@ public class ClientGet extends ClientRequest implements ClientGetCallback, Clien
 			}
 		} else if(msg instanceof SendingToNetworkMessage) {
 			sentToNetwork = true;
-		} else if(msg instanceof CompatibilityMode) {
-			CompatibilityMode compat = (CompatibilityMode)msg;
-			if(compatMessage != null) {
-				compatMessage.merge(compat.min, compat.max, compat.cryptoKey, compat.dontCompress, compat.definitive);
-			} else {
-				compatMessage = compat;
-			}
-			if(client != null) {
-				RequestStatusCache cache = client.getRequestStatusCache();
-				if(cache != null) {
-					cache.updateDetectedCompatModes(identifier, compat.getModes(), compat.cryptoKey, compat.dontCompress);
-				}
-			}
 		} else if(msg instanceof ExpectedHashes) {
 			if(expectedHashes != null) {
 				Logger.error(this, "Got a new ExpectedHashes", new Exception("debug"));
@@ -420,15 +409,19 @@ public class ClientGet extends ClientRequest implements ClientGetCallback, Clien
 			// Do nothing, it's not persistent.
 		} else
 			assert(false);
-		if(persistenceType == PERSIST_CONNECTION && handler == null)
-			handler = origHandler.outputHandler;
-		if(handler != null)
-			handler.queue(msg);
-		else
-			client.queueClientRequestMessage(msg, verbosityMask);
+		queueProgressMessageInner(msg, handler, verbosityMask);
 	}
 
-	@Override
+	private void queueProgressMessageInner(FCPMessage msg, FCPConnectionOutputHandler handler, int verbosityMask) {
+	    if(persistenceType == PERSIST_CONNECTION && handler == null)
+	        handler = origHandler.outputHandler;
+	    if(handler != null)
+	        handler.queue(msg);
+	    else
+	        client.queueClientRequestMessage(msg, verbosityMask);
+    }
+
+    @Override
 	public void sendPendingMessages(FCPConnectionOutputHandler handler, boolean includePersistentRequest, boolean includeData, boolean onlyData) {
 		if(!onlyData) {
 			if(includePersistentRequest) {
@@ -452,9 +445,7 @@ public class ClientGet extends ClientRequest implements ClientGetCallback, Clien
 		    trySendAllDataMessage(handler);
 		}
 		
-		if(compatMessage != null) {
-			handler.queue(compatMessage);
-		}
+		handler.queue(new CompatibilityMode(identifier, global, compatMode));
 		
 		if(expectedHashes != null) {
 			handler.queue(expectedHashes);
@@ -535,6 +526,9 @@ public class ClientGet extends ClientRequest implements ClientGetCallback, Clien
 		if(finished) return;
 		final FCPMessage progress;
 		final int verbosityMask;
+		// FIXME we are doing this backwards.
+		// FIXME update the internal state via a handle* method, possibly running a job.
+		// FIXME then check verbosityMask when sending messages.
 		if(ce instanceof SplitfileProgressEvent) {
 			verbosityMask = ClientGet.VERBOSITY_SPLITFILE_PROGRESS;
 			if((verbosity & verbosityMask) == 0)
@@ -548,11 +542,8 @@ public class ClientGet extends ClientRequest implements ClientGetCallback, Clien
 				return;
 			progress = new SendingToNetworkMessage(identifier, global);
 		} else if(ce instanceof SplitfileCompatibilityModeEvent) {
-			verbosityMask = ClientGet.VERBOSITY_COMPATIBILITY_MODE;
-			if((verbosity & verbosityMask) == 0)
-				return;
-			SplitfileCompatibilityModeEvent event = (SplitfileCompatibilityModeEvent)ce;
-			progress = new CompatibilityMode(identifier, global, event.minCompatibilityMode, event.maxCompatibilityMode, event.splitfileCryptoKey, event.dontCompress, event.bottomLayer);
+		    handleCompatibilityMode((SplitfileCompatibilityModeEvent)ce, context);
+		    return;
 		} else if(ce instanceof ExpectedHashesEvent) {
 			verbosityMask = ClientGet.VERBOSITY_EXPECTED_HASHES;
 			if((verbosity & verbosityMask) == 0)
@@ -597,8 +588,40 @@ public class ClientGet extends ClientRequest implements ClientGetCallback, Clien
 			trySendProgress(progress, verbosityMask, null);
 		}
 	}
+	
+	private void handleCompatibilityMode(final SplitfileCompatibilityModeEvent ce, ClientContext context) {
+	    if(persistenceType == PERSIST_FOREVER && context.jobRunner.hasStarted()) {
+	        try {
+	            context.jobRunner.queue(new PersistentJob() {
+	                
+	                @Override
+	                public boolean run(ClientContext context) {
+	                    innerHandleCompatibilityMode(ce, context);
+	                    return false;
+	                }
+	                
+	            }, NativeThread.HIGH_PRIORITY);
+	        } catch (PersistenceDisabledException e) {
+	            // Not much we can do
+	        }
+	    } else {
+	        innerHandleCompatibilityMode(ce, context);
+	    }
+	}
 
-	@Override
+	private void innerHandleCompatibilityMode(SplitfileCompatibilityModeEvent ce, ClientContext context) {
+	    compatMode.merge(ce.minCompatibilityMode, ce.maxCompatibilityMode, ce.splitfileCryptoKey, ce.dontCompress, ce.bottomLayer);
+	    if(client != null) {
+	        RequestStatusCache cache = client.getRequestStatusCache();
+	        if(cache != null) {
+	            cache.updateDetectedCompatModes(identifier, compatMode.getModes(), compatMode.getCryptoKey(), compatMode.dontCompress());
+	        }
+	    }
+	    if((verbosity & VERBOSITY_COMPATIBILITY_MODE) != 0)
+	        queueProgressMessageInner(new CompatibilityMode(identifier, global, compatMode), null, VERBOSITY_COMPATIBILITY_MODE);
+    }
+
+    @Override
 	protected ClientRequester getClientRequest() {
 		return getter;
 	}
@@ -697,22 +720,15 @@ public class ClientGet extends ClientRequest implements ClientGetCallback, Clien
 	}
 	
 	public InsertContext.CompatibilityMode[] getCompatibilityMode() {
-		if(compatMessage != null) {
-			return compatMessage.getModes();
-		} else
-			return new InsertContext.CompatibilityMode[] { InsertContext.CompatibilityMode.COMPAT_UNKNOWN, InsertContext.CompatibilityMode.COMPAT_UNKNOWN };
+	    return compatMode.getModes();
 	}
 	
 	public boolean getDontCompress() {
-		if(compatMessage == null) return false;
-		return compatMessage.dontCompress;
+		return compatMode.dontCompress();
 	}
 	
 	public byte[] getOverriddenSplitfileCryptoKey() {
-		if(compatMessage != null) {
-			return compatMessage.cryptoKey;
-		} else
-			return null;
+	    return compatMode.getCryptoKey();
 	}
 
 	@Override
@@ -792,7 +808,7 @@ public class ClientGet extends ClientRequest implements ClientGetCallback, Clien
 				redirect = getFailedMessage.redirectURI;
 			this.getFailedMessage = null;
 			this.progressPending = null;
-			compatMessage = null;
+			compatMode = new CompatibilityAnalyser();
 			expectedHashes = null;
 			started = false;
 			if(disableFilterData)

src/freenet/clients/fcp/CompatibilityMode.java
@@ -1,71 +1,39 @@
 package freenet.clients.fcp;
 
-import java.io.Serializable;
-import java.util.Arrays;
-
 import freenet.client.InsertContext;
+import freenet.client.async.CompatibilityAnalyser;
 import freenet.node.Node;
 import freenet.support.HexUtil;
-import freenet.support.Logger;
 import freenet.support.SimpleFieldSet;
 
-public class CompatibilityMode extends FCPMessage implements Serializable {
+public class CompatibilityMode extends FCPMessage {
+    
+    public CompatibilityMode(String identifier, boolean global, CompatibilityAnalyser compat) {
+        this.identifier = identifier;
+        this.global = global;
+        this.compat = compat;
+    }
 	
     private static final long serialVersionUID = 1L;
+    
+    private final CompatibilityAnalyser compat;
     final String identifier;
-	long min;
-	long max;
-	final boolean global;
-	byte[] cryptoKey;
-	boolean dontCompress;
-	boolean definitive;
-	
-	CompatibilityMode(String id, boolean global, long min, long max, byte[] cryptoKey, boolean dontCompress, boolean definitive) {
-		this.identifier = id;
-		this.global = global;
-		this.min = min;
-		this.max = max;
-		this.cryptoKey = cryptoKey;
-		this.dontCompress = dontCompress;
-		this.definitive = definitive;
-	}
-	
-	protected CompatibilityMode() {
-	    // For serialization.
-	    identifier = null;
-	    global = false;
-	}
-	
-	void merge(long min, long max, byte[] cryptoKey, boolean dontCompress, boolean definitive) {
-		if(this.definitive) {
-			Logger.warning(this, "merge() after definitive", new Exception("debug"));
-			return;
-		}
-		if(definitive) this.definitive = true;
-		if(!dontCompress) this.dontCompress = false;
-		if(min > this.min) this.min = min;
-		if(max < this.max || this.max == InsertContext.CompatibilityMode.COMPAT_UNKNOWN.ordinal()) this.max = max;
-		if(this.cryptoKey == null) {
-			this.cryptoKey = cryptoKey;
-		} else if(cryptoKey != null && !Arrays.equals(this.cryptoKey, cryptoKey)) {
-			Logger.error(this, "Two different crypto keys!");
-			this.cryptoKey = null;
-		}
-	}
-	
+    final boolean global;
+    
 	@Override
 	public SimpleFieldSet getFieldSet() {
 		SimpleFieldSet fs = new SimpleFieldSet(false);
-		fs.putOverwrite("Min", InsertContext.CompatibilityMode.values()[(int)min].name());
-		fs.putOverwrite("Max", InsertContext.CompatibilityMode.values()[(int)max].name());
-		fs.put("Min.Number", min);
-		fs.put("Max.Number", max);
+		fs.putOverwrite("Min", compat.min().name());
+		fs.putOverwrite("Max", compat.max().name());
+		fs.put("Min.Number", compat.min().ordinal());
+		fs.put("Max.Number", compat.max().ordinal());
 		fs.putOverwrite("Identifier", identifier);
 		fs.put("Global", global);
+		byte[] cryptoKey = compat.getCryptoKey();
 		if(cryptoKey != null)
 			fs.putOverwrite("SplitfileCryptoKey", HexUtil.bytesToHex(cryptoKey));
-		fs.put("DontCompress", dontCompress);
-		fs.put("Definitive", definitive);
+		fs.put("DontCompress", compat.dontCompress());
+		fs.put("Definitive", compat.definitive());
 		return fs;
 	}
 	
@@ -80,10 +48,7 @@ public class CompatibilityMode extends FCPMessage implements Serializable {
 	}
 
 	public InsertContext.CompatibilityMode[] getModes() {
-		return new InsertContext.CompatibilityMode[] {
-				InsertContext.CompatibilityMode.values()[(int)min],
-				InsertContext.CompatibilityMode.values()[(int)max]
-		};
+	    return compat.getModes();
 	}
 
 }

commit 5e9a59fce7388e02dcecca6e3f3ddd13ed0a7df7

CompatibilityAnalyser: fix dontCompress always being false
This regression was introduced in
0baad95141fd4e896d856a3c96be051d55e54ede.

Reviewed-By: Eleriseth <Eleriseth@WPECGLtYbVi8Rl6Y7Vzl2Lvd2EUVW99v3yNV3IWROG8.fms>

src/freenet/client/async/CompatibilityAnalyser.java
@@ -23,6 +23,7 @@ public class CompatibilityAnalyser implements Serializable {
     public CompatibilityAnalyser() {
         this.min = CompatibilityMode.COMPAT_UNKNOWN;
         this.max = CompatibilityMode.COMPAT_UNKNOWN;
+        this.dontCompress = true;
     }
     
     public void merge(CompatibilityMode min, CompatibilityMode max, byte[] cryptoKey, boolean dontCompress, boolean definitive) {