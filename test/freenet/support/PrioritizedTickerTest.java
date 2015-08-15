2e5dd16dfdbdd510ed01a912d077b25d33c521b3
Matthew Toseland <matthew@toselandcs.co.uk>
2015-08-15 21:34:50 +0100

    Add test for remove()

--- test/freenet/support/PrioritizedTickerTest.java
+++ test/freenet/support/PrioritizedTickerTest.java
@@ -1,5 +1,6 @@
 package freenet.support;
 
+import junit.framework.AssertionFailedError;
 import junit.framework.TestCase;
 
 public class PrioritizedTickerTest extends TestCase {
@@ -29,6 +30,17 @@ public class PrioritizedTickerTest extends TestCase {
 		
 	};
 	
+    Runnable simpleRunnable2 = new Runnable() {
+        
+        @Override
+        public void run() {
+            synchronized(PrioritizedTickerTest.this) {
+                runCount+=10;
+            }
+        }
+        
+    };
+    
 	public void testSimple() throws InterruptedException {
 		synchronized(PrioritizedTickerTest.this) {
 			runCount = 0;
@@ -54,6 +66,71 @@ public class PrioritizedTickerTest extends TestCase {
 			assert(runCount == 2);
 		}
 	}
+
+    public void testRemove() throws InterruptedException {
+        synchronized(PrioritizedTickerTest.this) {
+            runCount = 0;
+        }
+        assert(ticker.queuedJobs() == 0);
+        ticker.queueTimedJob(simpleRunnable, 5);
+        ticker.removeQueuedJob(simpleRunnable);
+        Thread.sleep(50);
+        synchronized(PrioritizedTickerTest.this) {
+            assert(runCount == 0);
+        }
+        assert(ticker.queuedJobs() == 0);
+        Thread.sleep(100);
+        synchronized(PrioritizedTickerTest.this) {
+            assert(runCount == 0);
+        }
+        ticker.queueTimedJob(simpleRunnable, 100);
+        assert(ticker.queuedJobs() == 1);
+        Thread.sleep(10);
+        ticker.removeQueuedJob(simpleRunnable);
+        assert(ticker.queuedJobs() == 0);
+        Thread.sleep(200);
+        assert(ticker.queuedJobs() == 0);
+        synchronized(PrioritizedTickerTest.this) {
+            assert(runCount == 0);
+        }
+        try {
+            ticker.removeQueuedJob(simpleRunnable);
+            fail();
+        } catch (AssertionFailedError e) {
+            // Ok.
+        }
+        boolean testedBothInSameMillisecond = false;
+        do {
+            // Need to get them in the same millisecond. :(
+            ticker.queueTimedJob(simpleRunnable, 50);
+            ticker.queueTimedJob(simpleRunnable2, 50);
+            int count = ticker.queuedJobs();
+            assert(count == 1 || count == 2);
+            if(count == 1) testedBothInSameMillisecond = true;
+            ticker.removeQueuedJob(simpleRunnable);
+            assert(ticker.queuedJobs() == 1);
+            try {
+                ticker.removeQueuedJob(simpleRunnable);
+                fail();
+            } catch (AssertionFailedError e) {
+                // Ok.
+            }
+            assert(ticker.queuedJobs() == 1);
+            ticker.removeQueuedJob(simpleRunnable2);
+            try {
+                ticker.removeQueuedJob(simpleRunnable2);
+                fail();
+            } catch (AssertionFailedError e) {
+                // Ok.
+            }
+            assert(ticker.queuedJobs() == 0);
+            Thread.sleep(100);
+            assert(ticker.queuedJobs() == 0);
+            synchronized(PrioritizedTickerTest.this) {
+                assert(runCount == 0);
+            }
+        } while(!testedBothInSameMillisecond);
+    }
 
 	public void testDeduping() throws InterruptedException {
 		if(!TestProperty.EXTENSIVE) return; // FIXME unreliable test, only run on -Dtest.extensive=true
