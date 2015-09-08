ccb3c6e0af8a76a1ff6ba7d53e764c610f47ccd8
Konstantinos Togias <info@ktogias.gr>
2015-09-08 14:44:04 +0300

    Added symetrical notice in ClientGetCallback. I have not implemented any code to  check event receiving from a get callback, thus I don not know what other event s appart from SplitfileProgressEvent are generated during the fetch process.

--- src/freenet/client/async/ClientGetCallback.java
+++ src/freenet/client/async/ClientGetCallback.java
@@ -8,6 +8,15 @@ import freenet.client.FetchResult;
 
 /** Internal callback interface for requests. Methods are called on the database thread if the request
  * is persistent, otherwise on whatever thread completed the request (therefore with a null container).
+ * 
+ * <br/><br/>
+ * <i>NOTICE: This interface provides onSuccess and onFailure methods for getting 
+ * notified about the progress of the fetch process. <br/>
+ * If you want to get more detailed progress notifications you have to also implement {@link ClientEventListener} 
+ * interface and subscribe your callback to the {@link ClientEventProducer} of the getter's
+ * {@link FetchContext} using event producer's addEventListener method. <br/>
+ * This way your callback will be able to receive {@link SplitfileProgressEvent} and other events 
+ * during the fetch process.</i>
  */
 public interface ClientGetCallback extends ClientBaseCallback {
 	/** Called on successful fetch. Caller should schedule a job on the Ticker
@@ -16,5 +25,5 @@ public interface ClientGetCallback extends ClientBaseCallback {
 
 	/** Called on failed/canceled fetch. Caller should schedule a job on the Ticker
 	 * or Executor (on the ClientContext) if it needs to do much work. */
-	public void onFailure(FetchException e, ClientGetter state);
+	public void onSuccess(FetchException e, ClientGetter state);
 }
