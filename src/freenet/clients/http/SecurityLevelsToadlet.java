8a31546b7b9be275375e99e597be494a0a76261f
Matthew Toseland <matthew@toselandcs.co.uk>
2015-12-26 12:42:18 +0000

    Add localised link to WP (REVIEW)
    The appropriate link for FDE may vary depending on the locale. Having
    said that, this may increase the need for reviewing l10n strings? TODO
    Discuss!

--- src/freenet/clients/http/SecurityLevelsToadlet.java
+++ src/freenet/clients/http/SecurityLevelsToadlet.java
@@ -534,7 +534,7 @@ public class SecurityLevelsToadlet extends Toadlet {
 		NodeL10n.getBase().addL10nSubstitution(seclevelGroup.addChild("p").addChild("i"), "SecurityLevels.physicalThreatLevelFDE",
 		        new String[]{"bold", "link"},
 		        new HTMLNode[]{HTMLNode.STRONG,
-		                HTMLNode.linkInNewWindow(ExternalLinkToadlet.escape("http://www.truecrypt.org/"))});
+		                HTMLNode.linkInNewWindow(ExternalLinkToadlet.escape(l10nSec("physicalThreatLevelFDELink")))});
 		HTMLNode swapWarning = seclevelGroup.addChild("p").addChild("i");
 		OperatingSystem os = FileUtil.detectedOS;
 		swapWarning.addChild("#", NodeL10n.getBase().getString("SecurityLevels.physicalThreatLevelSwapfile",
