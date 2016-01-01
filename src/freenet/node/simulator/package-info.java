eb32a2a6daad363318ecfd6ad163395fa75785c1
Matthew Toseland <matthew@toselandcs.co.uk>
2016-01-01 22:48:05 +0000

    Move simulator instructions into package-info

--- src/freenet/node/simulator/package-info.java
+++ src/freenet/node/simulator/package-info.java
@@ -3,5 +3,11 @@
  * nodes in the same JVM to test some key feature (routing, ULPR propagation
  * etc), or one or more real nodes connect to the real network and do some 
  * tests e.g. bootstrapping, inserting and fetching data.
+ * 
+ * To run simulations, do something like:
+ * 
+ * java -cp freenet.jar:freenet-ext.jar freenet.node.simulator.RealNodeProbeTest
+ * 
+ * On Windows the classpath separator is ; instead of :.
  */
 package freenet.node.simulator;
--- src/freenet/node/simulator/readme.txt
+++
@@ -1,5 +0,0 @@
-To run simulations, do something like:
-
-java -cp freenet.jar:freenet-ext.jar freenet.node.simulator.RealNodeProbeTest
-
-On Windows the classpath separator is ; instead of :.
