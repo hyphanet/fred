8b7276daa6186a4533fc5c9518c6d034d5f5be84
Matthew Toseland <matthew@toselandcs.co.uk>
2016-01-01 22:49:06 +0000

    Formatting.

--- src/freenet/node/simulator/package-info.java
+++ src/freenet/node/simulator/package-info.java
@@ -1,13 +1,13 @@
 /**
- * Simulators and test code using the real Freenet code: Either multiple 
+ * <p>Simulators and test code using the real Freenet code: Either multiple 
  * nodes in the same JVM to test some key feature (routing, ULPR propagation
  * etc), or one or more real nodes connect to the real network and do some 
- * tests e.g. bootstrapping, inserting and fetching data.
+ * tests e.g. bootstrapping, inserting and fetching data.</p>
  * 
- * To run simulations, do something like:
+ * <p>To run simulations, do something like:</p>
  * 
- * java -cp freenet.jar:freenet-ext.jar freenet.node.simulator.RealNodeProbeTest
+ * <p><pre>java -cp freenet.jar:freenet-ext.jar freenet.node.simulator.RealNodeProbeTest</pre></p>
  * 
- * On Windows the classpath separator is ; instead of :.
+ * <p>On Windows the classpath separator is ; instead of :.</p>
  */
 package freenet.node.simulator;
