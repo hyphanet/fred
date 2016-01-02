a2f3bc80226b49856c18a314fd7b55f45a9d570d
Matthew Toseland <matthew@toselandcs.co.uk>
2016-01-02 19:32:13 +0000

    Simplify
    Conflicts:
    	src/freenet/node/PeerManager.java

--- src/freenet/node/PeerManager.java
+++ src/freenet/node/PeerManager.java
@@ -243,6 +243,7 @@ public class PeerManager {
 				// Read a single NodePeer
 				SimpleFieldSet fs;
 				fs = new SimpleFieldSet(br, false, true);
+				boolean broken = true;
 				try {
 					PeerNode pn = PeerNode.create(fs, node, crypto, opennet, this);
 					if(oldOpennetPeers) {
@@ -252,29 +253,22 @@ public class PeerManager {
 					        opennet.addOldOpennetNode((OpennetPeerNode)pn);
 					} else
 						addPeer(pn, true, false);
-					gotSome = true;
 				} catch(FSParseException e2) {
 					Logger.error(this, "Could not parse peer: " + e2 + '\n' + fs.toString(), e2);
 					System.err.println("Cannot parse a friend from the peers file: "+e2);
-					someBroken = true;
-					continue;
 				} catch(PeerParseException e2) {
 					Logger.error(this, "Could not parse peer: " + e2 + '\n' + fs.toString(), e2);
 					System.err.println("Cannot parse a friend from the peers file: "+e2);
-					someBroken = true;
-					continue;
 				} catch(ReferenceSignatureVerificationException e2) {
 					Logger.error(this, "Could not parse peer: " + e2 + '\n' + fs.toString(), e2);
 					System.err.println("Cannot parse a friend from the peers file: "+e2);
-					someBroken = true;
-					continue;
 				} catch (RuntimeException e2) {
 					Logger.error(this, "Could not parse peer: " + e2 + '\n' + fs.toString(), e2);
 					System.err.println("Cannot parse a friend from the peers file: "+e2);
-					someBroken = true;
-					continue;
 					// FIXME tell the user???
 				}
+				if(broken) someBroken = true;
+				else gotSome = true;
 			}
 		} catch(EOFException e) {
 			// End of file, fine
