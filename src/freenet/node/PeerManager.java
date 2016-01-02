eb399504aa8acc9069675a9a41f81168223d8a51
Matthew Toseland <matthew@toselandcs.co.uk>
2016-01-02 19:30:08 +0000

    Log after saving the .broken file

--- src/freenet/node/PeerManager.java
+++ src/freenet/node/PeerManager.java
@@ -305,15 +305,6 @@ public class PeerManager {
 		} catch(IOException e3) {
 			Logger.error(this, "Ignoring " + e3 + " caught reading " + peersFile, e3);
 		}
-		if(!droppedOldPeers.isEmpty()) {
-            try {
-                node.clientCore.alerts.register(droppedOldPeers);
-                Logger.error(this, droppedOldPeers.getText());
-		    } catch (Throwable t) {
-		        // Startup MUST complete, don't let client layer problems kill it.
-		        Logger.error(this, "Caught error telling user about dropped peers", t);
-		    }
-		}
 		if(someBroken) {
 			try {
 				brokenPeersFile.delete();
@@ -326,6 +317,15 @@ public class PeerManager {
 			} catch (IOException e) {
 				System.err.println("Unable to copy broken peers file.");
 			}
+		}
+		if(!droppedOldPeers.isEmpty()) {
+		    try {
+		        node.clientCore.alerts.register(droppedOldPeers);
+		        Logger.error(this, droppedOldPeers.getText());
+		    } catch (Throwable t) {
+		        // Startup MUST complete, don't let client layer problems kill it.
+		        Logger.error(this, "Caught error telling user about dropped peers", t);
+		    }
 		}
 		return !someBroken;
 	}
