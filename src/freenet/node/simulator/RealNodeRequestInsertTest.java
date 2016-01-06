9452f80dfdffaaa6eaa79ea7d87cb87f30949aa6
Matthew Toseland <matthew@toselandcs.co.uk>
2016-01-06 12:15:05 +0000

    Logging

--- src/freenet/node/simulator/RealNodeRequestInsertTest.java
+++ src/freenet/node/simulator/RealNodeRequestInsertTest.java
@@ -106,9 +106,10 @@ public class RealNodeRequestInsertTest extends RealNodeRoutingTest {
         }
         wd.mkdir();
         //NOTE: globalTestInit returns in ignored random source
-        String logDetails = "freenet.node.Request:MINOR,freenet.node.CHK:MINOR,freenet.node.SSK:MINOR," +
-        		"freenet.io.comm.MessageCore:MINOR,freenet.node.Peer:MINOR,freenet.node.Node:MINOR";
-        NodeStarter.globalTestInit(new File(name), false, LogLevel.NORMAL, logDetails, true, 
+        //String logDetails = "freenet.node.Request:MINOR,freenet.node.CHK:MINOR,freenet.node.SSK:MINOR," +
+        //		"freenet.io.comm.MessageCore:MINOR,freenet.node.Peer:MINOR,freenet.node.Node:MINOR";
+        String logDetails = "";
+        NodeStarter.globalTestInit(new File(name), false, LogLevel.ERROR, logDetails, true, 
                 BYPASS_TRANSPORT_LAYER, null);
         System.out.println("Insert/retrieve test");
         System.out.println();
