hyphanet/fred

commit 5d21c855655c1f974a8e9333d74c1d564224bf4c

Parse everything in NodeDispatcher so we check everything *before* we check the UIDs or accept the request.

src/freenet/node/AnnounceSender.java
@@ -42,7 +42,9 @@ public class AnnounceSender implements PrioRunnable, ByteCounter {
 	private final long uid;
 	private final OpennetManager om;
 	private final Node node;
-	private Message msg;
+	private final long xferUID;
+	private final int noderefLength;
+	private final int paddedLength;
 	private byte[] noderefBuf;
 	private short htl;
 	private double target;
@@ -50,15 +52,16 @@ public class AnnounceSender implements PrioRunnable, ByteCounter {
 	private final PeerNode onlyNode;
 	private int forwardedRefs;
 
-	public AnnounceSender(Message m, long uid, PeerNode source, OpennetManager om, Node node) {
+	public AnnounceSender(double target, short htl, long uid, PeerNode source, OpennetManager om, Node node, long xferUID, int noderefLength, int paddedLength) {
 		this.source = source;
 		this.uid = uid;
-		this.msg = m;
 		this.om = om;
 		this.node = node;
 		this.onlyNode = null;
-		htl = (short) Math.min(m.getShort(DMT.HTL), node.maxHTL());
-		target = m.getDouble(DMT.TARGET_LOCATION); // FIXME validate
+		this.htl = htl;
+		this.xferUID = xferUID;
+		this.paddedLength = paddedLength;
+		this.noderefLength = noderefLength;
 		cb = null;
 	}
 
@@ -67,7 +70,6 @@ public class AnnounceSender implements PrioRunnable, ByteCounter {
 		this.uid = node.random.nextLong();
 		// Prevent it being routed back to us.
 		node.completed(uid);
-		msg = null;
 		this.om = om;
 		this.node = node;
 		this.htl = node.maxHTL();
@@ -75,6 +77,9 @@ public class AnnounceSender implements PrioRunnable, ByteCounter {
 		this.cb = cb;
 		this.onlyNode = onlyNode;
 		noderefBuf = om.crypto.myCompressedFullRef();
+		this.xferUID = 0;
+		this.paddedLength = 0;
+		this.noderefLength = 0;
 	}
 
 	@Override
@@ -510,9 +515,6 @@ public class AnnounceSender implements PrioRunnable, ByteCounter {
 	 * @return True unless the noderef is bogus.
 	 */
 	private boolean transferNoderef() {
-		long xferUID = msg.getLong(DMT.TRANSFER_UID);
-		int noderefLength = msg.getInt(DMT.NODEREF_LENGTH);
-		int paddedLength = msg.getInt(DMT.PADDED_LENGTH);
 		noderefBuf = OpennetManager.innerWaitForOpennetNoderef(xferUID, paddedLength, noderefLength, source, false, uid, true, this, node);
 		if(noderefBuf == null) {
 			return false;

src/freenet/node/NodeDispatcher.java
@@ -27,6 +27,7 @@ import freenet.support.Fields;
 import freenet.support.LogThresholdCallback;
 import freenet.support.Logger;
 import freenet.support.ShortBuffer;
+import freenet.support.SizeUtil;
 import freenet.support.Logger.LogLevel;
 import freenet.support.io.NativeThread;
 
@@ -617,6 +618,26 @@ public class NodeDispatcher implements Dispatcher, Runnable {
 	
 	private boolean handleAnnounceRequest(Message m, PeerNode source) {
 		long uid = m.getLong(DMT.UID);
+		double target = m.getDouble(DMT.TARGET_LOCATION); // FIXME validate
+		short htl = (short) Math.min(m.getShort(DMT.HTL), node.maxHTL());
+		long xferUID = m.getLong(DMT.TRANSFER_UID);
+		int noderefLength = m.getInt(DMT.NODEREF_LENGTH);
+		int paddedLength = m.getInt(DMT.PADDED_LENGTH);
+
+		// SECURITY: Must check it is a valid message before checking recentlyCompleted.
+		// Otherwise can do fun tricks like probing recentlyCompleted!
+		if(target < 0.0 || target >= 1.0 || htl <= 0 || 
+				paddedLength < 0 || paddedLength > OpennetManager.MAX_OPENNET_NODEREF_LENGTH ||
+				noderefLength > paddedLength) {
+			Message msg = DMT.createFNPRejectedOverload(uid, true, false, false);
+			try {
+				source.sendAsync(msg, null, node.nodeStats.announceByteCounter);
+			} catch (NotConnectedException e) {
+				// Ok
+			}
+			return true;
+		}
+		
 		OpennetManager om = node.getOpennet();
 		if(om == null || !source.canAcceptAnnouncements()) {
 			if(om != null && source instanceof SeedClientPeerNode)
@@ -669,7 +690,7 @@ public class NodeDispatcher implements Dispatcher, Runnable {
 					return true;
 				}
 			}
-			AnnounceSender sender = new AnnounceSender(m, uid, source, om, node);
+			AnnounceSender sender = new AnnounceSender(target, htl, uid, source, om, node, xferUID, noderefLength, paddedLength);
 			node.executor.execute(sender, "Announcement sender for "+uid);
 			success = true;
 			return true;

src/freenet/node/OpennetManager.java
@@ -1086,19 +1086,6 @@ public class OpennetManager {
 	}
 
 	static byte[] innerWaitForOpennetNoderef(long xferUID, int paddedLength, int realLength, PeerNode source, boolean isReply, long uid, boolean sendReject, ByteCounter ctr, Node node) {
-		if (paddedLength > OpennetManager.MAX_OPENNET_NODEREF_LENGTH) {
-			Logger.error(OpennetManager.class, "Noderef too big: "+SizeUtil.formatSize(paddedLength)
-					+" real length "+SizeUtil.formatSize(realLength));
-			if(sendReject) rejectRef(uid, source, DMT.NODEREF_REJECTED_TOO_BIG, ctr);
-			return null;
-		}
-		if (realLength > paddedLength) {
-			Logger.error(OpennetManager.class, "Real length larger than padded length: "
-					+ SizeUtil.formatSize(paddedLength)
-					+ " real length "+SizeUtil.formatSize(realLength));
-			if(sendReject) rejectRef(uid, source, DMT.NODEREF_REJECTED_REAL_BIGGER_THAN_PADDED, ctr);
-			return null;
-		}
 		byte[] buf = new byte[paddedLength];
 		ByteArrayRandomAccessThing raf = new ByteArrayRandomAccessThing(buf);
 		PartiallyReceivedBulk prb = new PartiallyReceivedBulk(node.usm, buf.length, Node.PACKET_SIZE, raf, false);

commit 71a788164a896d6e5b6af0dfe0f35da5a6927633

Add missing assignment for Opennet announce location
This regression was introduced in 5d21c855655c1f974a8e9333d74c1d564224bf4c
which was released in build01412 on 2012-09-12.

src/freenet/node/AnnounceSender.java
@@ -47,7 +47,7 @@ public class AnnounceSender implements PrioRunnable, ByteCounter {
 	private final int paddedLength;
 	private byte[] noderefBuf;
 	private short htl;
-	private double target;
+	private final double target;
 	private final AnnouncementCallback cb;
 	private final PeerNode onlyNode;
 	private int forwardedRefs;
@@ -62,6 +62,7 @@ public class AnnounceSender implements PrioRunnable, ByteCounter {
 		this.xferUID = xferUID;
 		this.paddedLength = paddedLength;
 		this.noderefLength = noderefLength;
+		this.target = target;
 		this.cb = cb;
 	}
 