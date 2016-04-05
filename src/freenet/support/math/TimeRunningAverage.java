7b36ca9ca4981791a6c7895acd4e1ef02873b6c8
Matthew Toseland <matthew@toselandcs.co.uk>
2016-04-05 16:52:13 +0100

    Add new class TimeRunningAverage

---
+++ src/freenet/support/math/TimeRunningAverage.java
@@ -0,0 +1,65 @@
+package freenet.support.math;
+
+/** Average weighted by the number of milliseconds until the next report. */
+public class TimeRunningAverage implements RunningAverage, Cloneable {
+    
+    public TimeRunningAverage clone() {
+        try {
+            return (TimeRunningAverage) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new Error(e); // Impossible
+        }
+    }
+
+    /** Time at which the last report was made. 0 if no reports so far. */
+    long lastReportTime;
+    /** Value of the last report. */
+    double lastReportValue;
+    /** Start time */
+    long firstReportTime;
+    /** Total time * reported value from the first report time to the last report time, hence not 
+     * including the lastReportValue. */
+    double totalTimeTimesValues;
+    int reports;
+    
+    @Override
+    public synchronized double currentValue() {
+        if(firstReportTime == 0) return Double.NaN; // No reports;
+        long now = System.currentTimeMillis();
+        double newTotal = totalTimeTimesValues + (now - lastReportTime) * lastReportValue;
+        return newTotal / (now - firstReportTime);
+    }
+
+    @Override
+    public void report(double d) {
+        long now = System.currentTimeMillis();
+        if(firstReportTime == 0) {
+            // First report.
+            lastReportTime = now;
+            firstReportTime = now;
+            lastReportValue = d;
+        } else {
+            assert(lastReportTime < now);
+            totalTimeTimesValues += (now - lastReportTime) * lastReportValue;
+            lastReportTime = now;
+            lastReportValue = d;
+        }
+        reports++;
+    }
+
+    @Override
+    public void report(long d) {
+        report((double)d);
+    }
+
+    @Override
+    public double valueIfReported(double r) {
+        return currentValue();
+    }
+
+    @Override
+    public long countReports() {
+        return reports;
+    }
+
+}
