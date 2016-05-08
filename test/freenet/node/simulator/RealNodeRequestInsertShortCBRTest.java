b65bd3524d91e0804487c0f33d05d2c91a601118
Matthew Toseland <matthew@toselandcs.co.uk>
2016-05-08 11:17:39 +0100

    Increase bwlimits and only run for extensive tests

---
+++ test/freenet/node/simulator/RealNodeRequestInsertShortCBRTest.java
@@ -0,0 +1,30 @@
+package freenet.node.simulator;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import freenet.io.comm.PeerParseException;
+import freenet.io.comm.ReferenceSignatureVerificationException;
+import freenet.keys.CHKEncodeException;
+import freenet.keys.KeyDecodeException;
+import freenet.keys.SSKEncodeException;
+import freenet.node.FSParseException;
+import freenet.node.NodeInitException;
+import freenet.node.NodeStarter.TestingVMBypass;
+import freenet.node.simulator.RealNodeRequestInsertTester.ExitException;
+import freenet.node.simulator.RealNodeTester.SimulatorOverloadedException;
+import freenet.support.TestProperty;
+import freenet.support.LoggerHook.InvalidThresholdException;
+import freenet.support.compress.InvalidCompressionCodecException;
+
+public class RealNodeRequestInsertShortCBRTest extends RealNodeRequestInsertShortTestBase {
+
+    @Test
+    public void testSmallNetwork() throws CHKEncodeException, SSKEncodeException, FSParseException, PeerParseException, InvalidThresholdException, NodeInitException, ReferenceSignatureVerificationException, InterruptedException, SimulatorOverloadedException, InvalidCompressionCodecException, IOException, KeyDecodeException, ExitException {
+        if(!TestProperty.EXTENSIVE) return;
+        RealNodeRequestInsertTester.CBR_BWLIMIT = 32*1024;
+        testSmallNetwork(TestingVMBypass.CBR_QUEUE_BYPASS);
+    }
+    
+}
