3e4b67fc0426d6945acaffa76a7ea90944d990c1
Steve Dougherty <steve@asksteved.com>
2016-05-21 18:54:23 -0400

    Build 1473
    See tag for changelog.

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -50,18 +50,18 @@ public class Version {
 	public static final String protocolVersion = "1.0";
 
 	/** The build number of the current revision */
-	private static final int buildNumber = 1472;
+	private static final int buildNumber = 1473;
 
 	/** Oldest build of fred we will talk to *before* _cal */
-	private static final int oldLastGoodBuild = 1465;
+	private static final int oldLastGoodBuild = 1466;
 	/** Oldest build of fred we will talk to *after* _cal */
-	private static final int newLastGoodBuild = 1466;
+	private static final int newLastGoodBuild = 1472;
 	static final long transitionTime;
 
 	static {
 		final Calendar _cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		// year, month - 1 (or constant), day, hour, minute, second
-		_cal.set( 2014, Calendar.NOVEMBER, 16, 0, 0, 0 );
+		_cal.set( 2016, Calendar.JUNE, 5, 0, 0, 0 );
 		transitionTime = _cal.getTimeInMillis();
 	}
 
