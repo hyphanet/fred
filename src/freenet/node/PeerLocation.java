24b457621fb6d9694164538d7ddfc0a479eefbe2
Bert Massop <bert.massop@gmail.com>
2016-06-21 22:27:59 +0200

    Reduce algorithmic complexity of FOAF routing

--- src/freenet/node/PeerLocation.java
+++ src/freenet/node/PeerLocation.java
@@ -3,6 +3,7 @@ package freenet.node;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 import freenet.support.Logger;
 
@@ -106,4 +107,99 @@ public class PeerLocation {
 		return oldLoc;
 	}
 
+	/**
+	 * Finds the position of the first element in the sorted list greater than the given element,
+	 * or -1 if none.
+	 * This is a binary search of logarithmic complexity.
+	 */
+	private static int findFirstGreater(List<Double> elems, double x) {
+		int low = 0;
+		int high = elems.size();
+		int mid;
+		if (elems.get(high - 1) <= x) {
+			return -1;
+		}
+		while (low != high) {
+			mid = low + (high - low) / 2;
+			if (elems.get(mid) > x) {
+				high = mid;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return low;
+	}
+
+	/**
+	 * Finds the position of the closest location in the sorted list of locations.
+	 * This is a binary search of logarithmic complexity.
+	 */
+	static int findClosestLocation(List<Double> locs, double l) {
+		assert(locs.size() > 0);
+		if (locs.size() == 1) {
+			return 0;
+		}
+		final int firstGreater = findFirstGreater(locs, l);
+		final int left;
+		final int right;
+		if (firstGreater == -1 || firstGreater == 0) {
+			// All locations are greater, or all locations are smaller.
+			// Closest location must be either smallest or greatest location.
+			left = locs.size() - 1;
+			right = 0;
+		} else {
+			left = firstGreater - 1;
+			right = firstGreater;
+		}
+		if (Location.distance(l, locs.get(left)) <= Location.distance(l, locs.get(right))) {
+			return left;
+		}
+		return right;
+	}
+
+	/**
+	 * Finds the closest non-excluded peer in O(log n + m) time, where n is the number of peers and
+	 * m the number of exclusions.
+	 * @param exclude the set of locations to exclude, may be null
+	 * @return the closest non-excluded peer's location, or NaN if none is found
+	 */
+	public double getClosestPeerLocation(double l, Set<Double> exclude) {
+		List<Double> locs = getPeerLocations();
+		final int N = locs.size();
+		if (N == 0) {
+			return Double.NaN;
+		}
+		final int closest = findClosestLocation(locs, l);
+		if (exclude == null || !exclude.contains(closest)) {
+			return locs.get(closest);
+		}
+
+		int left = (closest - 1) % N;
+		int right = (closest + 1) % N;
+		double leftDist = Location.distance(l, locs.get(left));
+		double rightDist = Location.distance(l, locs.get(right));
+		// Iterate over at most m closest peers
+		while (left != right) {
+			if (leftDist <= rightDist) {
+				final double loc = locs.get(left);
+				if (!exclude.contains(loc)) {
+					return loc;
+				}
+				left = (left - 1) % N;
+				leftDist = Location.distance(l, locs.get(left));
+			} else {
+				final double loc = locs.get(right);
+				if (!exclude.contains(loc)) {
+					return loc;
+				}
+				right = (right + 1) % N;
+				rightDist = Location.distance(l, locs.get(right));
+			}
+		}
+		final double loc = locs.get(left);
+		if (!exclude.contains(loc)) {
+			return loc;
+		}
+		return Double.NaN;
+	}
 }
