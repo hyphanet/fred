99ab159cc468259d0d9499a82ee31aba689a2a31
Bert Massop <bert.massop@gmail.com>
2016-07-30 00:27:49 +0200

    Switch to non-outdated high-security SSL crypto

--- src/freenet/io/SSLNetworkInterface.java
+++ src/freenet/io/SSLNetworkInterface.java
@@ -58,8 +58,7 @@ public class SSLNetworkInterface extends NetworkInterface {
 		serverSocket.setWantClientAuth(false);
 
 		serverSocket.setEnabledCipherSuites(new String[] {
-		    "TLS_DHE_RSA_WITH_AES_256_CBC_SHA", // We want PFS (DHE)
-		    // "TLS_RSA_WITH_AES_256_CBC_SHA",
+		    "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
 		});
 
 		return serverSocket;
