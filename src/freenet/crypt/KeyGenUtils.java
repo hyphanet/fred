hyphanet/fred

commit abad64d133ed9a674d5f666f48db178a85652b9e

always use BouncyCastle in KeyGenUtils

src/freenet/crypt/KeyGenUtils.java
@@ -22,6 +22,8 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
 import freenet.node.NodeStarter;
 import freenet.support.Fields;
 
@@ -45,7 +47,7 @@ public final class KeyGenUtils {
             throw new UnsupportedTypeException(type);
         }
         try {
-            KeyPairGenerator kg = KeyPairGenerator.getInstance(type.alg);
+            KeyPairGenerator kg = KeyPairGenerator.getInstance(type.alg, new BouncyCastleProvider());
             kg.initialize(type.spec);
             return kg.generateKeyPair();
         } catch (NoSuchAlgorithmException e) {
@@ -66,7 +68,7 @@ public final class KeyGenUtils {
             throw new UnsupportedTypeException(type);
         }
         try {
-            KeyFactory kf = KeyFactory.getInstance(type.alg);
+            KeyFactory kf = KeyFactory.getInstance(type.alg, new BouncyCastleProvider());
             X509EncodedKeySpec xks = new X509EncodedKeySpec(pub);
             return kf.generatePublic(xks);
         } catch (NoSuchAlgorithmException e) {
@@ -121,7 +123,7 @@ public final class KeyGenUtils {
             throw new UnsupportedTypeException(type);
         }
         try {
-            KeyFactory kf = KeyFactory.getInstance(type.alg);
+            KeyFactory kf = KeyFactory.getInstance(type.alg, new BouncyCastleProvider());
 
             PublicKey pubK = getPublicKey(type, pub);
 
@@ -168,7 +170,7 @@ public final class KeyGenUtils {
      */
     public static SecretKey genSecretKey(KeyType type){
         try{
-            KeyGenerator kg = KeyGenerator.getInstance(type.alg);
+            KeyGenerator kg = KeyGenerator.getInstance(type.alg, new BouncyCastleProvider());
             kg.init(type.keySize);
             return kg.generateKey();
         } catch (NoSuchAlgorithmException e) {

test/freenet/crypt/KeyGenUtilsTest.java
@@ -3,7 +3,11 @@
  * http://www.gnu.org/ for further details of the GPL. */
 package freenet.crypt;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.security.GeneralSecurityException;
 import java.security.InvalidKeyException;
@@ -22,7 +26,6 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.Test;
 
 import freenet.support.HexUtil;
-import freenet.support.Logger;
 
 public class KeyGenUtilsTest {
     private static final int trueLength = 16;

commit fc041811b1e7227cbd8dfc831c2ed5dc5bb6083a

[PATCH] Fix memory leak with BouncyCastleProvider
Fix issue with BounceCastleProvider introduced in
abad64d133ed9a674d5f666f48db178a85652b9e causing node shutdown with OOM
error.

It was opted to use a static initialization method to setup provider
using the `static` initializer.

src/freenet/crypt/KeyGenUtils.java
@@ -36,6 +36,10 @@ import freenet.support.Fields;
  */
 public final class KeyGenUtils {
 
+    static {
+        java.security.Security.insertProviderAt(new BouncyCastleProvider(), 1);
+    }
+
     /**
      * Generates a public/private key pair formatted for the algorithm specified
      * and stores the keys in a KeyPair. Can not handle DSA keys.
@@ -47,7 +51,7 @@ public final class KeyGenUtils {
             throw new UnsupportedTypeException(type);
         }
         try {
-            KeyPairGenerator kg = KeyPairGenerator.getInstance(type.alg, new BouncyCastleProvider());
+            KeyPairGenerator kg = KeyPairGenerator.getInstance(type.alg);
             kg.initialize(type.spec);
             return kg.generateKeyPair();
         } catch (NoSuchAlgorithmException e) {
@@ -68,7 +72,7 @@ public final class KeyGenUtils {
             throw new UnsupportedTypeException(type);
         }
         try {
-            KeyFactory kf = KeyFactory.getInstance(type.alg, new BouncyCastleProvider());
+            KeyFactory kf = KeyFactory.getInstance(type.alg);
             X509EncodedKeySpec xks = new X509EncodedKeySpec(pub);
             return kf.generatePublic(xks);
         } catch (NoSuchAlgorithmException e) {
@@ -123,7 +127,7 @@ public final class KeyGenUtils {
             throw new UnsupportedTypeException(type);
         }
         try {
-            KeyFactory kf = KeyFactory.getInstance(type.alg, new BouncyCastleProvider());
+            KeyFactory kf = KeyFactory.getInstance(type.alg);
 
             PublicKey pubK = getPublicKey(type, pub);
 
@@ -170,7 +174,7 @@ public final class KeyGenUtils {
      */
     public static SecretKey genSecretKey(KeyType type){
         try{
-            KeyGenerator kg = KeyGenerator.getInstance(type.alg, new BouncyCastleProvider());
+            KeyGenerator kg = KeyGenerator.getInstance(type.alg);
             kg.init(type.keySize);
             return kg.generateKey();
         } catch (NoSuchAlgorithmException e) {