1154cf5498b8ea85671f5764b94cc2b39446b93d
Oleh Shklyar <oleh.shklyar@redwerk.com>
2019-07-19 15:28:54 +0300

    formatting firsttimewizard.js

--- src/freenet/clients/http/staticfiles/js/firsttimewizard.js
+++ src/freenet/clients/http/staticfiles/js/firsttimewizard.js
@@ -1,16 +1,19 @@
-var slideToggler, trigger,
+var slideToggler,
+    trigger,
     bind = function(fn, me) {
         return function() {
-        return fn.apply(me, arguments);
-    };
+            return fn.apply(me, arguments);
+        };
     },
     indexOf = [].indexOf || function(item) {
-        for (var i = 0, l = this.length; i < l; i++) {
-        if (i in this && this[i] === item) return i;
-    }
+        for (var i = 0, length = this.length; i < length; i++) {
+            if (i in this && this[i] === item) {
+                return i;
+            }
+        }
 
-    return -1;
-};
+        return -1;
+    };
 
 slideToggler = (function() {
     function slideToggler(el1) {
@@ -25,62 +28,83 @@ slideToggler = (function() {
 
     slideToggler.prototype.getHeight = function() {
         var clone;
+
         if (this.el.clientHeight > 10) {
             return this.el.clientHeight;
         }
+
         clone = this.el.cloneNode(true);
         clone.style.cssText = 'position: absolute; visibility: hidden; display: block;';
+
         this.el.parentNode.appendChild(clone);
         this.height = clone.clientHeight;
         this.el.parentNode.removeChild(clone);
+
         return this.height;
     };
 
     slideToggler.prototype.toggle = function(time) {
         var currHeight, disp, el, end, init, ref, repeat, start;
+
         el = this.el;
+
         if (!el) {
             return;
         }
+
         if (!(this.height > 0)) {
             this.height = this.getHeight();
         }
+
         if (time == null) {
             time = this.height;
         }
+
         currHeight = el.clientHeight * (getComputedStyle(el).display !== 'none');
         ref = currHeight > this.height / 2 ? [this.height, 0] : [0, this.height];
         start = ref[0];
         end = ref[1];
         disp = end - start;
+
         this.el.classList[end === 0 ? 'remove' : 'add']('open');
         this.el.style.cssText = "overflow: hidden; display: block; padding-top: 0; padding-bottom: 0";
+
         init = (new Date).getTime();
+
         repeat = function() {
-        var instance, ref1, repeatLoop, results, step;
-        instance = (new Date).getTime() - init;
-        step = start + disp * instance / time;
-        if (instance <= time) {
-            el.style.height = step + 'px';
-        } else {
-            el.style.cssText = "display: " + (end === 0 ? 'none' : 'block');
-        }
-        repeatLoop = requestAnimationFrame(repeat);
-        if (ref1 = Math.floor(step), indexOf.call((function() {
-            results = [];
-            for (var i = start; start <= end ? i <= end : i >= end; start <= end ? i++ : i--) {
-            results.push(i);
+            var instance, ref1, repeatLoop, results, step, isCancelAnimation;
+
+            instance = (new Date).getTime() - init;
+            step = start + disp * instance / time;
+
+            if (instance <= time) {
+                el.style.height = step + 'px';
+            } else {
+                el.style.cssText = "display: " + (end === 0 ? 'none' : 'block');
             }
-            return results;
-            }).apply(this), ref1) < 0) {
-            return cancelAnimationFrame(repeatLoop);
+
+            repeatLoop = requestAnimationFrame(repeat);
+            ref1 = Math.floor(step);
+            isCancelAnimation = indexOf.call((function() {
+                results = [];
+
+                for (var i = start; start <= end ? i <= end : i >= end; start <= end ? i++ : i--) {
+                    results.push(i);
+                }
+
+                return results;
+            }).apply(this), ref1);
+
+            if (isCancelAnimation < 0) {
+                return cancelAnimationFrame(repeatLoop);
             }
-        };
-        return repeat();
         };
 
-        return slideToggler; }
-)();
+        return repeat();
+    };
+
+    return slideToggler;
+})();
 
 function toggle(event, data, time) {
     var hide,
