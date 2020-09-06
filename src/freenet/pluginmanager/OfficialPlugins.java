7e79133fadf3e7032c96189e1043b5522616a49d
Arne Babenhauserheide <arne_bab@web.de>
2020-09-06 18:33:04 +0200

    add plugin UPnP2
    from commit da0ff1c0f2798505f713e2e8c1596832825ca948

--- src/freenet/pluginmanager/OfficialPlugins.java
+++ src/freenet/pluginmanager/OfficialPlugins.java
@@ -80,8 +80,11 @@ public class OfficialPlugins {
 					.essential()
 					.recommendedVersion(10007)
 					.minimumVersion(10003)
-					.loadedFrom(
-                        "CHK@ZiX8yeMHTUtNfJAgxpwH~jLRnnbb41BKEkAxOD~33tY,aBTvD3IoPKPLjnHOCNQ4-iRwqVED5kHgkmD4UhGdITk,AAMC--8/UPnP-10007.jar");
+					.loadedFrom("CHK@ZiX8yeMHTUtNfJAgxpwH~jLRnnbb41BKEkAxOD~33tY,aBTvD3IoPKPLjnHOCNQ4-iRwqVED5kHgkmD4UhGdITk,AAMC--8/UPnP-10007.jar");
+			addPlugin("UPnP2")
+					.inGroup("connectivity")
+					.loadedFrom("CHK@oFNunyhic~ug3lWas8Jabpwbt3heHhrFzHswN~GhPNc,j~2AHw~ZyZGNMuqW3zmukTJHysDg5lBTvrySerSPxkI,AAMC--8/freenet-UPnP2.jar")
+					.advanced();
 			addPlugin("XMLLibrarian")
 					.inGroup("index")
 					.minimumVersion(26)
