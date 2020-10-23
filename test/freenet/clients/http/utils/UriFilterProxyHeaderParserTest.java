3514c8b1c230424c497ce2ad2f03a91f107a20f3
Arne Babenhauserheide <arne_bab@web.de>
2020-10-23 21:45:37 +0200

    parametrized tests to named test methods

--- test/freenet/clients/http/utils/UriFilterProxyHeaderParserTest.java
+++ test/freenet/clients/http/utils/UriFilterProxyHeaderParserTest.java
@@ -1,83 +1,262 @@
 package freenet.clients.http.utils;
 
-import java.util.Arrays;
-import java.util.Collection;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-import junit.framework.TestCase;
 
 import freenet.support.MultiValueTable;
 
-@RunWith(Parameterized.class)
-public class UriFilterProxyHeaderParserTest extends TestCase {
+public class UriFilterProxyHeaderParserTest {
 
-  private final String fProxyPort;
-  private final String fProxyBindTo;
-  private final String uriScheme;
-  private final String uriHost;
-  private final MultiValueTable<String, String> headers;
-  private String resultUriPrefix;
-
-  @Parameterized.Parameters(
-      name = "{index}: portConfig=\"{0}\", bindTo=\"{1}\", uriScheme=\"{2}\", uriHost=\"{3}\", headers={4}, expected=\"{5}\"")
-  public static Collection constructorParametersAndResult () {
-    return Arrays.asList(new Object[][] {
-        // typical arguments
-        {"8888", "127.0.0.1", "", "", MultiValueTable.from(new String[] {}, new String[] {}), "http://127.0.0.1:8888"},
-        // empty arguments result in plain http:// prefix
-        {"", "", "", "", MultiValueTable.from(new String[] {}, new String[] {}), "http://127.0.0.1:8888"},
-        {"", "", null, null, MultiValueTable.from(new String[] {}, new String[] {}), "http://127.0.0.1:8888"},
-        // sanity checks for defaults
-        {"8888", "", "", "", MultiValueTable.from(new String[] {}, new String[] {}), "http://127.0.0.1:8888"},
-        {"", "127.0.0.1", "", "", MultiValueTable.from(new String[] {}, new String[] {}), "http://127.0.0.1:8888"},
-        // taking proxy values
-        {"8888", "127.0.0.1,foo", "", "", MultiValueTable.from(new String[] {"x-forwarded-host"}, new String[] {"foo"}), "http://foo"},
-        {"8888", "foo,127.0.0.1", "", "", MultiValueTable.from(new String[] {"x-forwarded-host"}, new String[] {"foo:8888"}), "http://foo:8888"},
-        {"8889", "127.0.0.1,foo", "", "", MultiValueTable.from(new String[] {"x-forwarded-host"}, new String[] {"foo:8889"}), "http://foo:8889"},
-        {"8888", "127.0.0.1", "", "", MultiValueTable.from(new String[] {"x-forwarded-proto"}, new String[] {"https"}), "https://127.0.0.1:8888"},
-        // taking uri values
-        {"8888", "127.0.0.1,foo", "", "foo", MultiValueTable.from(new String[] {"x-forwarded-host"}, new String[] {"foo"}), "http://foo"},
-        {"8888", "127.0.0.1,foo", "", "foo:8888", MultiValueTable.from(new String[] {"x-forwarded-host"}, new String[] {"foo:8888"}), "http://foo:8888"},
-        {"8889", "127.0.0.1,foo", "", "foo:8889", MultiValueTable.from(new String[] {"x-forwarded-host"}, new String[] {"foo:8889"}), "http://foo:8889"},
-        {"8888", "127.0.0.1", "https", "", MultiValueTable.from(new String[] {"x-forwarded-proto"}, new String[] {"https"}), "https://127.0.0.1:8888"},
-        // ignored header values
-        {"8888", "127.0.0.1", "", "", MultiValueTable.from(new String[] {"x-forwarded-host"}, new String[] {"foo"}), "http://127.0.0.1:8888"},
-        {"8888", "127.0.0.1", "", "", MultiValueTable.from(new String[] {"x-forwarded-proto"}, new String[] {"catchme"}), "http://127.0.0.1:8888"},
-        {"8888", "127.0.0.1", "", "foo:8888", MultiValueTable.from(new String[] {"x-forwarded-host"}, new String[] {"foo:8888"}), "http://127.0.0.1:8888"},
-        {"8889", "127.0.0.1", "", "", MultiValueTable.from(new String[] {"x-forwarded-host"}, new String[] {"foo:8888"}), "http://127.0.0.1:8889"},
-        // ignored uri values
-        // no such bindToHost
-        {"8888", "127.0.0.1", "", "foo", MultiValueTable.from(new String[] {"x-forwarded-host"}, new String[] {"foo"}), "http://127.0.0.1:8888"},
-        {"8888", "127.0.0.1", "", "foo:8888", MultiValueTable.from(new String[] {"x-forwarded-host"}, new String[] {"foo:8888"}), "http://127.0.0.1:8888"},
-        // port mismatch
-        {"8888", "127.0.0.1,foo", "", "foo:8889", MultiValueTable.from(new String[] {"x-forwarded-host"}, new String[] {"foo:8889"}), "http://127.0.0.1:8888"},
-        {"8888", "127.0.0.1", "https", "", MultiValueTable.from(new String[] {"x-forwarded-proto"}, new String[] {"https"}), "https://127.0.0.1:8888"},
-
-    });
+  // typical arguments
+  @Test
+  public void standardValuesParsedAsStandardPrefix() {
+    testUriPrefixMatchesExpected(
+        "8888",
+        "127.0.0.1",
+        "",
+        "",
+        MultiValueTable.from(new String[]{}, new String[]{}),
+        "http://127.0.0.1:8888");
   }
 
-  public UriFilterProxyHeaderParserTest(
+  // empty arguments result in plain http:// prefix
+  @Test
+  public void emptyArgumentsParsedAsStandardPrefix() {
+    testUriPrefixMatchesExpected(
+        "",
+        "",
+        "",
+        "",
+        MultiValueTable.from(new String[]{}, new String[]{}),
+        "http://127.0.0.1:8888");
+  }
+
+  @Test
+  public void nullArgumentsParsedAsStandardPrefix() {
+    testUriPrefixMatchesExpected(
+        "",
+        "",
+        null,
+        null,
+        MultiValueTable.from(new String[]{}, new String[]{}),
+        "http://127.0.0.1:8888");
+  }
+
+  // sanity checks for defaults
+  @Test
+  public void defaultPortParsedAsStandardPrefix() {
+    testUriPrefixMatchesExpected(
+        "8888",
+        "",
+        "",
+        "",
+        MultiValueTable.from(new String[]{}, new String[]{}),
+        "http://127.0.0.1:8888");
+  }
+
+  @Test
+  public void defaultIpParsedAsStandardPrefix() {
+    testUriPrefixMatchesExpected(
+        "",
+        "127.0.0.1",
+        "",
+        "",
+        MultiValueTable.from(new String[]{}, new String[]{}),
+        "http://127.0.0.1:8888");
+  }
+
+  // taking proxy values
+  @Test
+  public void allowedProxyHostIsUsedIfInSecondPositionOfBindTo() {
+    testUriPrefixMatchesExpected(
+        "8888",
+        "127.0.0.1,foo",
+        "",
+        "",
+        MultiValueTable.from(new String[]{ "x-forwarded-host" }, new String[]{ "foo" }),
+        "http://foo");
+  }
+
+  @Test
+  public void allowedProxyHostIsUsedIfInFirstPositionOfBindTo() {
+    testUriPrefixMatchesExpected(
+        "8888",
+        "foo,127.0.0.1",
+        "",
+        "",
+        MultiValueTable.from(new String[]{ "x-forwarded-host" }, new String[]{ "foo:8888" }),
+        "http://foo:8888");
+  }
+
+  @Test
+  public void allowedProxyHostWithNonstandardPortIsUsed() {
+    testUriPrefixMatchesExpected(
+        "8889",
+        "127.0.0.1,foo",
+        "",
+        "",
+        MultiValueTable.from(new String[]{ "x-forwarded-host" }, new String[]{ "foo:8889" }),
+        "http://foo:8889");
+  }
+
+  @Test
+  public void httpsProtocolFromProxyIsUsed() {
+    testUriPrefixMatchesExpected(
+        "8888",
+        "127.0.0.1",
+        "",
+        "",
+        MultiValueTable.from(new String[]{ "x-forwarded-proto" }, new String[]{ "https" }),
+        "https://127.0.0.1:8888");
+  }
+
+  // taking uri values
+  @Test
+  public void allowedUriHostIsUsed() {
+    testUriPrefixMatchesExpected(
+        "8888",
+        "127.0.0.1,foo",
+        "",
+        "foo",
+        MultiValueTable.from(new String[]{}, new String[]{}),
+        "http://foo");
+  }
+
+  @Test
+  public void allowedUriHostWithStandardPortAreUsed() {
+    testUriPrefixMatchesExpected(
+        "8888",
+        "127.0.0.1,foo",
+        "",
+        "foo:8888",
+        MultiValueTable.from(new String[]{}, new String[]{}),
+        "http://foo:8888");
+  }
+
+  @Test
+  public void allowedUriAndNonstandardPortAreUsed() {
+    testUriPrefixMatchesExpected(
+        "8889",
+        "127.0.0.1,foo",
+        "",
+        "foo:8889",
+        MultiValueTable.from(new String[]{}, new String[]{}),
+        "http://foo:8889");
+  }
+
+  @Test
+  public void httpsProtocolFromUriIsUsed() {
+    testUriPrefixMatchesExpected(
+        "8888",
+        "127.0.0.1",
+        "https",
+        "",
+        MultiValueTable.from(new String[]{}, new String[]{}),
+        "https://127.0.0.1:8888");
+  }
+
+  // ignored header values
+  @Test
+  public void disallowedProxyHostHeaderIsIgnored() {
+    testUriPrefixMatchesExpected(
+        "8888",
+        "127.0.0.1",
+        "",
+        "",
+        MultiValueTable.from(new String[]{ "x-forwarded-host" }, new String[]{ "foo" }),
+        "http://127.0.0.1:8888");
+  }
+
+  @Test
+  public void disallowedProxyProtocolIsIgnored() {
+    testUriPrefixMatchesExpected(
+        "8888",
+        "127.0.0.1",
+        "",
+        "",
+        MultiValueTable.from(new String[]{ "x-forwarded-proto" }, new String[]{ "catchme" }),
+        "http://127.0.0.1:8888");
+  }
+
+  @Test
+  public void disallowedProxyHostAndPortAreIgnored() {
+    testUriPrefixMatchesExpected(
+        "8888",
+        "127.0.0.1",
+        "",
+        "foo:8888",
+        MultiValueTable.from(new String[]{ "x-forwarded-host" }, new String[]{ "foo:8888" }),
+        "http://127.0.0.1:8888");
+  }
+
+  @Test
+  public void disallowedProxyWithStandardPortIsIgnoredIfPortIsNotStandardAndHostNotAllowed() {
+    testUriPrefixMatchesExpected(
+        "8889",
+        "127.0.0.1",
+        "",
+        "",
+        MultiValueTable.from(new String[]{ "x-forwarded-host" }, new String[]{ "foo:8888" }),
+        "http://127.0.0.1:8889");
+  }
+
+  @Test
+  public void disallowedProxyWithStandardPortIsIgnoredIfPortIsNotStandard() {
+    testUriPrefixMatchesExpected(
+        "8889",
+        "127.0.0.1",
+        "",
+        "",
+        MultiValueTable.from(
+            new String[]{ "x-forwarded-host" },
+            new String[]{ "127.0.0.1:8888" }),
+        "http://127.0.0.1:8889");
+  }
+
+  // ignored uri values
+  // no such bindToHost
+  @Test
+  public void disallowedUriHostIsIgnored() {
+    testUriPrefixMatchesExpected(
+        "8888",
+        "127.0.0.1",
+        "",
+        "foo",
+        MultiValueTable.from(new String[]{ "x-forwarded-host" }, new String[]{ "foo" }),
+        "http://127.0.0.1:8888");
+  }
+
+  @Test
+  public void disallowedUriHostWithStandardPortIsIgnored() {
+    testUriPrefixMatchesExpected(
+        "8888",
+        "127.0.0.1",
+        "",
+        "foo:8888",
+        MultiValueTable.from(new String[]{ "x-forwarded-host" }, new String[]{ "foo:8888" }),
+        "http://127.0.0.1:8888");
+  }
+
+  // port mismatch
+  @Test
+  public void disallowedUriWithAllowedHostButDisallowedPortIsIgnored() {
+    testUriPrefixMatchesExpected(
+        "8888",
+        "127.0.0.1,foo",
+        "",
+        "foo:8889",
+        MultiValueTable.from(new String[]{ "x-forwarded-host" }, new String[]{ "foo:8889" }),
+        "http://127.0.0.1:8888");
+  }
+
+  private void testUriPrefixMatchesExpected(
       String fProxyPort,
       String fProxyBindTo,
       String uriScheme,
       String uriHost,
       MultiValueTable<String, String> headers,
-      String resultUriPrefix
-  ) {
-    this.fProxyPort = fProxyPort;
-    this.fProxyBindTo = fProxyBindTo;
-    this.uriScheme = uriScheme;
-    this.uriHost = uriHost;
-    this.headers = headers;
-    this.resultUriPrefix = resultUriPrefix;
-  }
-
-
-  @Test
-  public void testUriPrefixMatchesExpected() {
+      String resultUriPrefix) {
     String schemeHostAndPort = new UriFilterProxyHeaderParser(
         fProxyPort,
         fProxyBindTo,
@@ -86,8 +265,16 @@ public class UriFilterProxyHeaderParserTest extends TestCase {
         headers)
         .getSchemeHostAndPort();
     assertTrue(
-        String.format("schemeHostAndPort %s does not match expected %s",
-            schemeHostAndPort, resultUriPrefix),
+        String.format(
+            "schemeHostAndPort %s does not match expected %s; portConfig=\"%s\", bindTo=\"%s\", uriScheme=\"%s\", uriHost=\"%s\", headers=%s, expected=\"%s\"",
+            schemeHostAndPort,
+            resultUriPrefix,
+            fProxyPort,
+            fProxyBindTo,
+            uriScheme,
+            uriHost,
+            headers,
+            resultUriPrefix),
         schemeHostAndPort.equals(resultUriPrefix));
 
   }
