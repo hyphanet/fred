17acba7c43e8215464d7355e7190821d48c10c7f
DC* <des@riseup.net>
2021-03-28 22:31:01 +0100

    m3u compability for mobile devices

--- src/freenet/clients/http/ToadletContextImpl.java
+++ src/freenet/clients/http/ToadletContextImpl.java
@@ -465,7 +465,7 @@ public class ToadletContextImpl implements ToadletContext {
 	private static String generateRestrictedScriptSrc() {
 		// TODO: auto-generate these hashes from the path to the source file
 		String[] allowedScriptHashes = new String[] {
-				"sha256-emGXuxNdTQP2ylJOeGhLCKYFO+1g/2u6FtPSzMKQ06A=" // freenet/clients/http/staticfiles/js/m3u-player.js
+				"sha256-kglOvopjCg9Et+Z7jBUnNbj1Rk9KWnmda4XnYrbYxsE=" // freenet/clients/http/staticfiles/js/m3u-player.js
 		};
 		if (allowedScriptHashes.length == 0) {
 			return "'none'";
