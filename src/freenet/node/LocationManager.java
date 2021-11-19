3f506940c9d097a494654a26e6acb4226e6929e7
Arne Babenhauserheide <arne_bab@web.de>
2021-11-19 22:06:07 +0100

    remove unnecessary import

--- src/freenet/node/LocationManager.java
+++ src/freenet/node/LocationManager.java
@@ -37,8 +37,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TimeZone;
 
-import sun.util.resources.TimeZoneNames;
-
 import freenet.client.FetchException;
 import freenet.client.FetchResult;
 import freenet.client.HighLevelSimpleClient;
