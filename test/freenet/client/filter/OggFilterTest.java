f4565a98dd61e294ba68a523718d52c7ff09de5e
Arne Babenhauserheide <arne_bab@web.de>
2021-11-20 18:35:13 +0100

    add output of filtered file to the OggFilterTest

--- test/freenet/client/filter/OggFilterTest.java
+++ test/freenet/client/filter/OggFilterTest.java
@@ -2,8 +2,13 @@ package freenet.client.filter;
 
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Arrays;
+
+import org.apache.commons.compress.utils.IOUtils;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
@@ -32,6 +37,31 @@ public class OggFilterTest extends TestCase {
 			filter.readFilter(input, output, null, null, null, null);
 		} catch(DataFilterException e) {}
 		Assert.assertTrue(Arrays.equals(new byte[]{}, output.toByteArray()));
+		input.close();
+		output.close();
+	}
+
+    /** the purpose of this test is to create the testoutputFile so you can check it with a video player. */
+	public void testFilterFfmpegEncodedVideoSegment() throws IOException, DataFilterException {
+        String testoutputFile = getClass().getResource(
+	 			"./ogg/36C3_-_opening--cc-by--c3voc--fem-ags-opensuse--ccc--filtered-testoutput.ogv")
+					.getFile();
+		DataInputStream inputFileUnchanged = new DataInputStream(getClass().getResourceAsStream(
+				"./ogg/36C3_-_opening--cc-by--c3voc--fem-ags-opensuse--ccc--filtered.ogv"));
+		ByteArrayOutputStream unchangedData = new ByteArrayOutputStream();
+		IOUtils.copy(inputFileUnchanged, unchangedData);
+		DataInputStream inputFileToParse = new DataInputStream(getClass().getResourceAsStream(
+				"./ogg/36C3_-_opening--cc-by--c3voc--fem-ags-opensuse--ccc--orig.ogv"));
+		DataInputStream input = inputFileToParse;
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		try {
+			filter.readFilter(input, output, null, null, null, null);
+			FileOutputStream newFileStream = new FileOutputStream(testoutputFile);
+			System.out.println(testoutputFile);
+			output.writeTo(newFileStream);
+			newFileStream.close();
+		} catch(DataFilterException e) {}
+		Assert.assertTrue(Arrays.equals(unchangedData.toByteArray(), output.toByteArray()));
 		input.close();
 		output.close();
 	}
