fd97762059872773f1aedaa57c23d5cee6fee898
Arne Babenhauserheide <arne_bab@web.de>
2022-03-20 16:27:53 +0100

    bandwidth: parse bit suffix correctly

--- src/freenet/support/Fields.java
+++ src/freenet/support/Fields.java
@@ -706,7 +706,7 @@ public abstract class Fields {
 		final String lower = limit.toLowerCase();
 		for(String ending :
 			new String[] {
-				"/s", "/sec", "/second", "bps", NodeL10n.getBase().getString("FirstTimeWizardToadlet.bandwidthPerSecond").toLowerCase()
+				"/s", "/sec", "/second", "ps", NodeL10n.getBase().getString("FirstTimeWizardToadlet.bandwidthPerSecond").toLowerCase()
 			}) {
 			if(lower.endsWith(ending)) {
 				return limit.substring(0, limit.length() - ending.length());
@@ -732,11 +732,16 @@ public abstract class Fields {
 
 	/**
 	 * Parse a human-readable string possibly including SI and ICE units into an integer.
+     * 
+     * If it is a size (suffix b für bits or B for bytes), the size is returned as bytes.
+     * 8b = 1, 8B = 8.
 	 * @throws NumberFormatException
 	 *             if the string is not parseable
 	 */
 	public static int parseInt(String s) throws NumberFormatException {
-		s = s.replaceFirst("(i)*B$", "");
+        boolean isSizeInBits = s.endsWith("b");
+        // strip bit/byte suffix
+        s = s.replaceFirst((isSizeInBits ? "(i)*b$" : "(i)*B$"), "");
 		int res = 1;
 		int x = s.length() - 1;
 		int idx;
@@ -750,7 +755,7 @@ public abstract class Fields {
 			res = Integer.MAX_VALUE;
 			throw new NumberFormatException(e.getMessage());
 		}
-		return res;
+		return isSizeInBits ? res / 8 : res;
 	}
 
 	/**
