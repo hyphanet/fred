584ffc6be0aab543c2315e746571eedf64bf373f
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2023-09-28 00:44:56 +0200

    ✨ Add output stream to fix other output streams
    To be more specific, this FilterOutputStream can be used to “fix” a
    GzipOutputStream for continued use in Freenet.
    
    Prior to Java 17, the “OS” field in the GZIP header was set to 0; Java 17
    changed this to 0xff, to match the spec, which is lovely but breaks Freenet
    (because it changes hashes calculated for keys).
    
    By replacing a single byte in the GZIP-compressed output stream, everything can
    be made as it was.

---
+++ test/freenet/support/compress/SingleOffsetReplacingOutputStreamTest.java
@@ -0,0 +1,93 @@
+package freenet.support.compress;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Random;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class SingleOffsetReplacingOutputStreamTest {
+
+	@Test
+	public void allBytesBeforeTheOffsetAreUnchanged() throws IOException {
+		filterOutputStreamWithOffsetAndReplacementWritingByteArrays(32768, 1, 65536);
+	}
+
+	@Test
+	public void byteAtOffsetZeroCanBeReplaced() throws IOException {
+		filterOutputStreamWithOffsetAndReplacementWritingByteArrays(10, 1, 0);
+	}
+
+	@Test
+	public void byteAtOffsetOneCanBeReplaced() throws IOException {
+		filterOutputStreamWithOffsetAndReplacementWritingByteArrays(10, 1, 1);
+	}
+
+	@Test
+	public void byteAtEndOfBufferCanBeReplaced() throws IOException {
+		filterOutputStreamWithOffsetAndReplacementWritingByteArrays(10, 1, 9);
+	}
+
+	@Test
+	public void byteAtBeginningOfSecondBufferCanBeReplaced() throws IOException {
+		filterOutputStreamWithOffsetAndReplacementWritingByteArrays(10, 2, 10);
+	}
+
+	@Test
+	public void byteInTheMiddleOfSecondBufferCanBeReplaced() throws IOException {
+		filterOutputStreamWithOffsetAndReplacementWritingByteArrays(10, 3, 15);
+	}
+
+	@Test
+	public void byteAtOffsetZeroCanBeReplacedWhenWritingSingleBytes() throws IOException {
+		filterOutputStreamWithOffsetAndReplacementWritingSingleBytes(4096, 16, 0);
+	}
+
+	@Test
+	public void byteInMiddleOfStreamBeReplacedWhenWritingSingleBytes() throws IOException {
+		filterOutputStreamWithOffsetAndReplacementWritingSingleBytes(8192, 8, 12345);
+	}
+
+	@Test
+	public void byteAtEndOfStreamCanBeReplacedWhenWritingSingleBytes() throws IOException {
+		filterOutputStreamWithOffsetAndReplacementWritingSingleBytes(16384, 4, 65535);
+	}
+
+	private void filterOutputStreamWithOffsetAndReplacementWritingSingleBytes(int blockSize, int numberOfBlocks, int replacementOffset) throws IOException {
+		filterOutputStreamWithOffsetAndReplacement(blockSize, numberOfBlocks, replacementOffset, (buffer, length, repetitions, outputStream) -> {
+			for (int index = 0; index < length * repetitions; index++) {
+				outputStream.write(buffer[index % length]);
+			}
+		});
+	}
+
+	private void filterOutputStreamWithOffsetAndReplacementWritingByteArrays(int blockSize, int numberOfBlocks, int replacementOffset) throws IOException {
+		filterOutputStreamWithOffsetAndReplacement(blockSize, numberOfBlocks, replacementOffset, (buffer, length, repetitions, outputStream) -> {
+			for (int blockIndex = 0; blockIndex < repetitions; blockIndex++) {
+				outputStream.write(buffer, 0, length);
+			}
+		});
+	}
+
+	private void filterOutputStreamWithOffsetAndReplacement(int blockSize, int numberOfBlocks, int replacementOffset, BufferToOutputStreamCopierStrategy bufferToOutputStreamCopierStrategy) throws IOException {
+		byte[] bufferToWrite = new byte[blockSize];
+		new Random().nextBytes(bufferToWrite);
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		SingleOffsetReplacingOutputStream outputStream = new SingleOffsetReplacingOutputStream(byteArrayOutputStream, replacementOffset, (byte) (bufferToWrite[replacementOffset % blockSize] ^ 0xff));
+		bufferToOutputStreamCopierStrategy.copyBufferToOutput(bufferToWrite, blockSize, numberOfBlocks, outputStream);
+		byte[] writtenBuffer = byteArrayOutputStream.toByteArray();
+		for (int offset = 0; offset < blockSize * numberOfBlocks; offset++) {
+			assertThat("offset " + offset, writtenBuffer[offset], equalTo((byte) (bufferToWrite[offset % blockSize] ^ ((offset == replacementOffset) ? 0xff : 0x00))));
+		}
+	}
+
+	@FunctionalInterface
+	private interface BufferToOutputStreamCopierStrategy {
+		void copyBufferToOutput(byte[] buffer, int length, int repetitions, OutputStream outputStream) throws IOException;
+	}
+
+}
