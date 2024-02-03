5fc35f94a57ffef68a56b13ea3bb8163af704d99
Juiceman <juiceman69@gmail.com>
2024-02-02 22:42:24 -0500

    Convert tabs to spaces
    Going folder by folder to make review easier

--- src/freenet/crypt/KeyAgreementSchemeContext.java
+++ src/freenet/crypt/KeyAgreementSchemeContext.java
@@ -5,23 +5,23 @@ package freenet.crypt;
 
 public abstract class KeyAgreementSchemeContext {
 
-	protected long lastUsedTime;
+    protected long lastUsedTime;
 
     /** ECDSA signature. Used by negType 9+. */
     public byte[] ecdsaSig;
     /** A timestamp: when was the context created ? */
     public final long lifetime = System.currentTimeMillis();
 
-	/**
-	* @return The time at which this object was last used.
-	*/
-	public synchronized long lastUsedTime() {
-		return lastUsedTime;
-	}
-	  
+    /**
+    * @return The time at which this object was last used.
+    */
+    public synchronized long lastUsedTime() {
+        return lastUsedTime;
+    }
+      
     public void setECDSASignature(byte[] sig) {
         this.ecdsaSig = sig;
     }
 
-	public abstract byte[] getPublicKeyNetworkFormat();
+    public abstract byte[] getPublicKeyNetworkFormat();
 }
