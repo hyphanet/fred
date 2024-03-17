82cc9045619d6c79b8dd24ffe120aeae68aa33e1
Arne Babenhauserheide <arne_bab@web.de>
2024-03-17 11:17:41 +0100

    whitespace change reversal

--- test/freenet/client/filter/ContentFilterTest.java
+++ test/freenet/client/filter/ContentFilterTest.java
@@ -197,12 +197,12 @@ public class ContentFilterTest {
         assertEquals(SPAN_WITH_STYLE, htmlFilter(SPAN_WITH_STYLE));
         assertEquals(HTML5_TAGS, htmlFilter(HTML5_TAGS));
 
-		assertEquals(BASE_HREF, htmlFilter(BASE_HREF));
-		assertEquals(DELETED_BASE_HREF, htmlFilter(BAD_BASE_HREF));
-		assertEquals(DELETED_BASE_HREF, htmlFilter(BAD_BASE_HREF2));
-		assertEquals(DELETED_BASE_HREF, htmlFilter(BAD_BASE_HREF3));
-		assertEquals(DELETED_BASE_HREF, htmlFilter(BAD_BASE_HREF4));
-		assertEquals(DELETED_BASE_HREF, htmlFilter(BAD_BASE_HREF5));
+        assertEquals(BASE_HREF, htmlFilter(BASE_HREF));
+        assertEquals(DELETED_BASE_HREF, htmlFilter(BAD_BASE_HREF));
+        assertEquals(DELETED_BASE_HREF, htmlFilter(BAD_BASE_HREF2));
+        assertEquals(DELETED_BASE_HREF, htmlFilter(BAD_BASE_HREF3));
+        assertEquals(DELETED_BASE_HREF, htmlFilter(BAD_BASE_HREF4));
+        assertEquals(DELETED_BASE_HREF, htmlFilter(BAD_BASE_HREF5));
 
 		// m3u filter is added when there is a video or audio tag
 		for (String content : HTML_MEDIA_TAG_COMBINATIONS) {
