0b520d1a1e6b524397ccec0aa94854d03c1129a8
torusrxxx <torusrxxx@users.noreply.github.com>
2024-11-13 11:20:22 +0800

    Add dominant-baseline

--- src/freenet/client/filter/CSSTokenizerFilter.java
+++ src/freenet/client/filter/CSSTokenizerFilter.java
@@ -206,6 +206,7 @@ class CSSTokenizerFilter {
 		allelementVerifiers.add("cursor");
 		allelementVerifiers.add("direction");
 		allelementVerifiers.add("display");
+		allelementVerifiers.add("dominant-baseline");
 		allelementVerifiers.add("elevation");
 		allelementVerifiers.add("empty-cells");
 		allelementVerifiers.add("flex");
@@ -1001,6 +1002,11 @@ class CSSTokenizerFilter {
 
 			auxilaryVerifiers[140] = new CSSPropertyVerifier(null, null, Arrays.asList("133b135b136"), null, true);
 			elementVerifiers.put(element,new CSSPropertyVerifier(null, null, Arrays.asList("131a132", "140<0,1>[1,3]", "137", "138", "139"), null, true));
+			allelementVerifiers.remove(element);
+		}
+		else if("dominant-baseline".equalsIgnoreCase(element))
+		{
+			elementVerifiers.put(element,new CSSPropertyVerifier(Arrays.asList("auto", "text-bottom", "alphabetic", "ideographic", "middle", "central", "mathematical", "hanging", "text-top"),ElementInfo.VISUALMEDIA));
 			allelementVerifiers.remove(element);
 		}
 		else if("elevation".equalsIgnoreCase(element))
