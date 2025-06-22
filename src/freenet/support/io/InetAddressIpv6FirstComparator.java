73170a0018cce65a3f82d01f1e39dcad195d2550
Arne Babenhauserheide <arne_bab@web.de>
2025-06-22 22:18:45 +0200

    Refactor: use thenComparing with prefer-approach. Thanks to bertm!

--- src/freenet/support/io/InetAddressIpv6FirstComparator.java
+++ src/freenet/support/io/InetAddressIpv6FirstComparator.java
@@ -3,8 +3,11 @@ package freenet.support.io;
 import static freenet.node.NodeStats.DEFAULT_MAX_PING_TIME;
 
 import java.io.IOException;
+import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.util.Comparator;
+import java.util.Objects;
+import java.util.function.Predicate;
 
 import freenet.support.Fields;
 import freenet.support.LRUCache;
@@ -17,64 +20,28 @@ public class InetAddressIpv6FirstComparator implements Comparator<InetAddress> {
 	// need a cache for reachability to avoid doing NlogN issReachable checks in worst case.
 	public LRUCache<Integer, Boolean> reachabilityCache = new LRUCache<>(1000, 300000);
 
+	private final Comparator<InetAddress> innerComparator = prefer(Objects::nonNull)
+			.thenComparing(preferNot(InetAddress::isAnyLocalAddress))
+			.thenComparing(preferNot(InetAddress::isLoopbackAddress))
+			.thenComparing(preferNot(InetAddress::isLinkLocalAddress))
+			.thenComparing(prefer(this::isReachableSiteLocalAddress))
+			.thenComparing(prefer(Inet6Address.class::isInstance))
+			.thenComparingInt(Objects::hashCode)
+			.thenComparing(InetAddress::getAddress, Fields::compareBytes);
+
+	private boolean isReachableSiteLocalAddress(InetAddress inetAddress) {
+		return inetAddress.isSiteLocalAddress() && isReachable(inetAddress);
+	}
+
 	public static final InetAddressIpv6FirstComparator COMPARATOR =
 		new InetAddressIpv6FirstComparator();
 
 	@Override
 	public int compare(InetAddress arg0, InetAddress arg1) {
-		if ((arg0 == null && arg1 == null)) return 0;
-		// prefer non-null over null
-		if (arg0 == null) return 1;
-		if (arg1 == null) return -1;
-		if(arg0.equals(arg1)) return 0;
-		// prefer everything to broadcast
-		if (!arg0.isAnyLocalAddress() && arg1.isAnyLocalAddress()) {
-			return -1;
-		} else if (arg0.isAnyLocalAddress() && !arg1.isAnyLocalAddress()) {
-			return 1;
+		if(Objects.equals(arg0, arg1)) {
+			return 0;
 		}
-		// prefer everything over loopback / localhost
-		if (!arg0.isLoopbackAddress() && arg1.isLoopbackAddress()) {
-			return -1;
-		} else if (arg0.isLoopbackAddress() && !arg1.isLoopbackAddress()) {
-			return 1;
-		}
-		// prefer LAN routable addresses over link-local addresses because most of these will not work
-		if (!arg0.isLinkLocalAddress() && arg1.isLinkLocalAddress()) {
-			return -1;
-		} else if (arg0.isLinkLocalAddress() && !arg1.isLinkLocalAddress()) {
-			return 1;
-		}
-		// prefer LAN addresses *if they are routable* over global addresses, because they should stay within VPNs
-		if (!arg0.isSiteLocalAddress() && arg1.isSiteLocalAddress()) {
-			// prefer reachable over unreachable addresses. This is usually a ping. TODO: This actually pings all advertised ip addresses. Is that OK? Do we need a maximum number of accepted addresses to prevent abuse as DDoS? 
-			boolean reachable = isReachable(arg1);
-			if (reachable) {
-				return 1;
-			}
-		} else if (arg0.isSiteLocalAddress() && !arg1.isSiteLocalAddress()) {
-			boolean reachable = isReachable(arg0);
-			if (reachable) {
-				return -1;
-			}
-		}
-		byte[] bytes0 = arg0.getAddress();
-		byte[] bytes1 = arg1.getAddress();
-		// prefer IPv6 over IPv4
-		if(bytes0.length > bytes1.length) {
-			return -1;
-		} else if(bytes1.length > bytes0.length) {
-			return 1;
-		}
-
-		// calculate hashCode only once; used multiple times later
-		int hashCode0 = arg0.hashCode();
-		int hashCode1 = arg1.hashCode();
-		// Sort by hash code as fallback. This is fast.
-		if(hashCode0 > hashCode1) return 1;
-		else if(hashCode1 > hashCode0) return -1;
-		return Fields.compareBytes(bytes0, bytes1);
-		// Hostnames in InetAddress are merely cached, equals() only operates on the byte[].
+		return innerComparator.compare(arg0, arg1);
 	}
 
 	private boolean isReachable(InetAddress inetAddress) {
@@ -89,6 +56,28 @@ public class InetAddressIpv6FirstComparator implements Comparator<InetAddress> {
 			reachabilityCache.put(hashCode, reachable);
 		}
 		return reachable;
+	}
+
+	// inverted prefer, because not(...) is only documented since Java 11
+	private Comparator<InetAddress> preferNot(Predicate<InetAddress> pred) {
+		return (arg0, arg1) -> -1 * predicateToCompare(pred, arg0, arg1);
+	}
+
+	private Comparator<InetAddress> prefer(Predicate<InetAddress> pred) {
+		return (arg0, arg1) -> predicateToCompare(pred, arg0, arg1);
+	}
+
+	private int predicateToCompare(Predicate<InetAddress> pred, InetAddress arg0, InetAddress arg1) {
+		if (pred.test(arg0) && pred.test(arg1)) {
+				return 0;
+			}
+			if (pred.test(arg0)) {
+				return -1;
+			}
+			if (pred.test(arg1)) {
+				return 1;
+			}
+			return 0;
 	}
 
 }
