6dae8040b20649fb0a6a96433de9846f6844f5c9
Arne Babenhauserheide <arne_bab@web.de>
2025-08-17 18:38:00 +0200

    Add copy-to-clipboard element for upload/download lists.

---
+++ src/freenet/clients/http/staticfiles/js/clipboard.js
@@ -0,0 +1,19 @@
+// @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
+function addCopyKeyFieldToFinishedTransfers() {
+  if (navigator && navigator.clipboard) {
+    var matching = document.getElementsByClassName("finished_key_link");
+    for (var matchingElement of matching) {
+      var key = matchingElement.dataset.key;
+      if (key) {
+        var toClipboard = document.createElement("span");
+        toClipboard.textContent = " ⎘";
+        toClipboard.classList.add("copy-to-clipboard-element");
+        toClipboard.setAttribute("title", "Copy to Clipboard");
+        toClipboard.onclick=() => navigator.clipboard.writeText(key);
+        matchingElement.parentElement.appendChild(toClipboard);
+      }
+    }
+  }
+}
+document.addEventListener("DOMContentLoaded", addCopyKeyFieldToFinishedTransfers);
+// @license-end
