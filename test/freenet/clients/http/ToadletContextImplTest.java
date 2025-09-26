fa38421100d758f0ae6525d2c6e755c62962c557
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2025-09-26 20:31:26 +0200

    ✅ Add tests for GET requests

--- test/freenet/clients/http/ToadletContextImplTest.java
+++ test/freenet/clients/http/ToadletContextImplTest.java
@@ -4,25 +4,47 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.URI;
 import java.util.Arrays;
 import org.junit.Test;
 
 import static freenet.test.HttpResponse.parse;
 import static freenet.test.HttpResponseMatchers.hasBody;
+import static freenet.test.HttpResponseMatchers.hasHeader;
 import static freenet.test.HttpResponseMatchers.hasStatus;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class ToadletContextImplTest {
+
+	@Test
+	public void emptyRequestWillCloseTheSocket() throws IOException {
+		setupInputStream("");
+		ToadletContextImpl.handle(socket, null, null, null, null);
+		verify(socket).close();
+	}
+
+	@Test
+	public void requestingHomepageWillReturnHomepage() throws Exception {
+		setupInputStream("GET / HTTP/1.0\r\n\r\n");
+		when(toadletContainer.findToadlet(any())).thenReturn(homepageToadlet);
+		ToadletContextImpl.handle(socket, toadletContainer, null, null, null);
+		assertThat(parse(outputStream.toByteArray()), contains(allOf(
+				hasStatus(200), hasBody(equalTo("OK\n".getBytes(UTF_8)))
+		)));
+	}
 
 	@Test
 	public void requestingNoOutputPageWillReturnStatus204() throws Exception {
@@ -30,6 +52,39 @@ public class ToadletContextImplTest {
 		when(toadletContainer.findToadlet(any())).thenReturn(noOutputToadlet);
 		ToadletContextImpl.handle(socket, toadletContainer, null, null, null);
 		assertThat(parse(outputStream.toByteArray()), contains(hasStatus(equalTo(204), equalTo("No Content"))));
+	}
+
+	@Test
+	public void redirectExceptionFromToadletResultsInRedirect() throws Exception {
+		setupInputStream("GET /redirect-toadlet HTTP/1.0\r\n\r\n");
+		Toadlet redirectingToadlet = mock(Toadlet.class, RETURNS_DEEP_STUBS);
+		when(redirectingToadlet.findSupportedMethods()).thenReturn("GET");
+		doThrow(new RedirectException("/new-location")).when(redirectingToadlet).handleMethodGET(any(), any(), any());
+		when(toadletContainer.findToadlet(new URI("/redirect-toadlet"))).thenReturn(redirectingToadlet);
+		when(toadletContainer.findToadlet(new URI("/new-location"))).thenReturn(homepageToadlet);
+		ToadletContextImpl.handle(socket, toadletContainer, null, null, null);
+		assertThat(parse(outputStream.toByteArray()), contains(allOf(hasStatus(200))));
+	}
+
+	@Test
+	public void sendingInvalidHttpRequestLineResultsInHttpStatus400() throws IOException {
+		setupInputStream("GET HTTP/1.0\r\n\r\n");
+		ToadletContextImpl.handle(socket, toadletContainer, null, null, null);
+		assertThat(parse(outputStream.toByteArray()), contains(hasStatus(equalTo(400), equalTo("Bad Request"))));
+	}
+
+	@Test
+	public void sendingInvalidHttpVersionResultsInHttpStatus400() throws IOException {
+		setupInputStream("GET / HTTP/123.0\r\n\r\n");
+		ToadletContextImpl.handle(socket, toadletContainer, null, null, null);
+		assertThat(parse(outputStream.toByteArray()), contains(hasStatus(equalTo(400), equalTo("Bad Request"))));
+	}
+
+	@Test
+	public void sendingInvalidUrlResultsInHttpStatus400() throws IOException {
+		setupInputStream("GET :invalid HTTP/1.0\r\n\r\n");
+		ToadletContextImpl.handle(socket, toadletContainer, null, null, null);
+		assertThat(parse(outputStream.toByteArray()), contains(hasStatus(equalTo(400), equalTo("Bad Request"))));
 	}
 
 	@Test
@@ -70,6 +125,50 @@ public class ToadletContextImplTest {
 		)));
 	}
 
+	@Test
+	public void sendingGetRequestWithContentLengthHeaderResultsInHttpStatus400() throws IOException {
+		setupInputStream("GET /too-much-content-length HTTP/1.0\r\nContent-Length: 1\r\n\r\n");
+		ToadletContextImpl.handle(socket, toadletContainer, null, null, null);
+		assertThat(parse(outputStream.toByteArray()), contains(hasStatus(equalTo(400), equalTo("Bad Request"))));
+	}
+
+	@Test
+	public void getRequestWithoutToadletResultsInHttpStatus404() throws Exception {
+		setupInputStream("GET /no-toadlet HTTP/1.1\r\n\r\n");
+		when(toadletContainer.findToadlet(any())).thenReturn(null);
+		ToadletContextImpl.handle(socket, toadletContainer, null, null, null);
+		assertThat(parse(outputStream.toByteArray()), contains(hasStatus(equalTo(404), equalTo("Not Found"))));
+	}
+
+	@Test
+	public void permanentRedirectExceptionWhenLocatingToadletWillResultInRedirect() throws Exception {
+		setupInputStream("GET /redirect-from-container HTTP/1.0\r\n\r\n");
+		when(toadletContainer.findToadlet(any())).thenThrow(new PermanentRedirectException(new URI("/new-location")));
+		ToadletContextImpl.handle(socket, toadletContainer, null, null, null);
+		assertThat(parse(outputStream.toByteArray()), contains(allOf(
+				hasStatus(equalTo(301), equalTo("Moved Permanently")),
+				hasHeader("Location", contains("/new-location"))
+		)));
+	}
+
+	@Test
+	public void requestWithFullAccessForwardsRequestToPageMakerForParsingAdvancedModeSwitches() throws Exception {
+		setupInputStream("GET / HTTP/1.0\r\n\r\n");
+		when(toadletContainer.isAllowedFullAccess(any())).thenReturn(true);
+		when(toadletContainer.findToadlet(any())).thenReturn(homepageToadlet);
+		ToadletContextImpl.handle(socket, toadletContainer, pageMaker, null, null);
+		verify(pageMaker).parseMode(any(), eq(toadletContainer));
+	}
+
+	@Test
+	public void requestWithoutFullAccessDoesNotForwardRequestToPageMakerForParsingAdvancedModeSwitches() throws Exception {
+		setupInputStream("GET / HTTP/1.0\r\n\r\n");
+		when(toadletContainer.isAllowedFullAccess(any())).thenReturn(false);
+		when(toadletContainer.findToadlet(any())).thenReturn(homepageToadlet);
+		ToadletContextImpl.handle(socket, toadletContainer, pageMaker, null, null);
+		verify(pageMaker, never()).parseMode(any(), eq(toadletContainer));
+	}
+
 	private void setupInputStream(String request) throws IOException {
 		when(socket.getInputStream()).thenReturn(new ByteArrayInputStream(request.getBytes(UTF_8)));
 	}
@@ -77,6 +176,7 @@ public class ToadletContextImplTest {
 	private final Socket socket = mock(Socket.class, RETURNS_DEEP_STUBS);
 	private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 	private final ToadletContainer toadletContainer = mock(ToadletContainer.class, RETURNS_DEEP_STUBS);
+	private final PageMaker pageMaker = mock(PageMaker.class, RETURNS_DEEP_STUBS);
 
 	private final Toadlet homepageToadlet = mock(Toadlet.class, RETURNS_DEEP_STUBS);
 	private final Toadlet noOutputToadlet = mock(Toadlet.class, RETURNS_DEEP_STUBS);
