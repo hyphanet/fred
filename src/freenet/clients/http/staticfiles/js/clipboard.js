34f8037a18927d7c01ee07ccac448a3575ef4def
Arne Babenhauserheide <arne_bab@web.de>
2026-04-25 17:41:52 +0200

    Ensure that addCopyKeyFieldToFinishedTransfers runs only once

--- src/freenet/clients/http/staticfiles/js/clipboard.js
+++ src/freenet/clients/http/staticfiles/js/clipboard.js
@@ -1,19 +1,22 @@
 // @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
-function addCopyKeyFieldToFinishedTransfers () {
-  if (navigator && navigator.clipboard) {
-    var matching = document.getElementsByClassName('finished_key_link');
-    for (var matchingElement of matching) {
-      var key = matchingElement.dataset.key;
-      if (key) {
-        var toClipboard = document.createElement('span');
-        toClipboard.textContent = ' ⎘';
-        toClipboard.classList.add('copy-to-clipboard-element');
-        toClipboard.setAttribute('title', 'Copy to Clipboard');
-        toClipboard.onclick = () => navigator.clipboard.writeText(key);
-        matchingElement.after(toClipboard);
+if (typeof(document.addCopyKeyFieldToFinishedTransfers) === 'undefined') {
+  function addCopyKeyFieldToFinishedTransfers () {
+    if (navigator && navigator.clipboard) {
+      var matching = document.getElementsByClassName('finished_key_link');
+      for (var matchingElement of matching) {
+        var key = matchingElement.dataset.key;
+        if (key) {
+          var toClipboard = document.createElement('span');
+          toClipboard.textContent = ' ⎘';
+          toClipboard.classList.add('copy-to-clipboard-element');
+          toClipboard.setAttribute('title', 'Copy to Clipboard');
+          toClipboard.onclick = () => navigator.clipboard.writeText(key);
+          matchingElement.after(toClipboard);
+        }
       }
     }
   }
+  document.addCopyKeyFieldToFinishedTransfers = addCopyKeyFieldToFinishedTransfers;
+  document.addEventListener('DOMContentLoaded', addCopyKeyFieldToFinishedTransfers);
 }
-document.addEventListener('DOMContentLoaded', addCopyKeyFieldToFinishedTransfers);
 // @license-end
