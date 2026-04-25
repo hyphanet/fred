3833a36b7986ba893cd68a2a3cabddf835e49f5a
Arne Babenhauserheide <arne_bab@web.de>
2026-04-25 18:15:00 +0200

    fix: use const to define the key data to avoid scoping problems
    Also dataset key renaming.

--- src/freenet/clients/http/staticfiles/js/clipboard.js
+++ src/freenet/clients/http/staticfiles/js/clipboard.js
@@ -4,14 +4,16 @@ if (typeof(document.addCopyKeyFieldToFinishedTransfers) === 'undefined') {
     if (navigator && navigator.clipboard) {
       var matching = document.getElementsByClassName('finished_key_link');
       for (var matchingElement of matching) {
-        var key = matchingElement.dataset.key;
-        if (key) {
+        // must use const to avoid the weird scoping of var replacing all text with the last element
+        // const for declaration is usable in IE11+.
+        const text = matchingElement.dataset.copytext;
+        if (text) {
           var toClipboard = document.createElement('span');
           // SVG image thanks to bertm!
           toClipboard.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="height: 1em; width: 1em; margin-inline-start: .5ex; vertical-align: middle;"><path stroke="currentColor" stroke-width="2" d="M 2 16 v -12 a 2 2 0 0 1 2 -2 h 12 M 9 7 h 10 a 2 2 0 0 1 2 2 v 10 a 2 2 0 0 1 -2 2 h -10 a 2 2 0 0 1 -2 -2 v -10 a 2 2 0 0 1 2 -2" fill="none"></path></svg>';
           toClipboard.classList.add('copy-to-clipboard-element');
-          toClipboard.setAttribute('title', matchingElement.dataset.title);
-          toClipboard.onclick = () => navigator.clipboard.writeText(key);
+          toClipboard.setAttribute('title', matchingElement.dataset.copytitle);
+          toClipboard.onclick = () => navigator.clipboard.writeText(text);
           matchingElement.after(toClipboard);
         }
       }
