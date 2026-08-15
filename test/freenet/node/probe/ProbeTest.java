a1f0432526283f38e81cfa9c8f31d95cae1c2b57
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2026-08-15 11:37:34 +0200

    Adopt cleaner probe test by bombe

--- test/freenet/node/probe/ProbeTest.java
+++ test/freenet/node/probe/ProbeTest.java
@@ -1,61 +1,65 @@
 package freenet.node.probe;
 
-import static org.junit.Assert.*;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mockito;
-
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
-
-import java.util.Timer;
-import java.util.TimerTask;
-
 import freenet.node.Node;
 import freenet.node.PeerNode;
-import freenet.node.SeedServerTestPeerNode;
+import java.util.Timer;
+import java.util.TimerTask;
+import org.junit.Test;
+
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class ProbeTest {
 
-  static class DirectlyRunningTimer extends Timer {
+  @Test
+  public void probeLocationIsRandomized() {
+    when(node.getLocation()).thenReturn(0.4);
+    when(node.getRandom().nextGaussian()).thenReturn(0.1);
+
+    probe.start((byte) 1, 12345L, Type.LOCATION, listener);
+    verify(listener).onLocation(0.4f + 0.1f * 0.01f / 2.0f);
+  }
+
+  @Test
+  public void overflowOnNodeLocationRandomizationIsHandledCorrectly() {
+    when(node.getLocation()).thenReturn(0.999);
+    when(node.getRandom().nextGaussian()).thenReturn(1.0);
+
+    probe.start((byte) 1, 12345L, Type.LOCATION, listener);
+    verify(listener).onLocation((float) ((0.999 + 1.0 * 0.01 / 2.0) - 1.0));
+  }
+
+  @Test
+  public void underflowOnNodeLocationRandomizationIsHandledCorrectly() {
+    when(node.getLocation()).thenReturn(0.001);
+    when(node.getRandom().nextGaussian()).thenReturn(-1.0);
+
+    probe.start((byte) 1, 12345L, Type.LOCATION, listener);
+    verify(listener).onLocation((float) ((0.001 - 1.0 * 0.01 / 2.0) + 1.0));
+  }
+
+  public ProbeTest() {
+    node = mock(Node.class, RETURNS_DEEP_STUBS);
+    when(node.getConnectedPeers()).thenReturn(new PeerNode[] {});
+    when(node.getConfig().get("node").getBoolean("probeLocation")).thenReturn(true);
+    when(node.getRandom().nextDouble()).thenReturn(0.5);
+    listener = mock(Listener.class, RETURNS_DEEP_STUBS);
+    probe = new Probe(node);
+    probe.setTimer(new DirectlyRunningTimer());
+  }
+
+  private static class DirectlyRunningTimer extends Timer {
+
     public void schedule(TimerTask task, long delay) {
       task.run();
     }
+
   }
 
-  /**
-   * Tagging class for the error.
-   */
-  static class DoesNotRandomizeLocationException extends IllegalStateException {
-  }
-  @Test
-  public void locationIsRandomized() {
-    // with a minimal node with static location, fake random and empty peer list
-    Node node = mock(Node.class, RETURNS_DEEP_STUBS);
-    when(node.getLocation()).thenReturn(0.5);
-    when(node.getConnectedPeers()).thenReturn(new PeerNode[]{});
-    when(node.getRandom().nextDouble()).thenReturn(0.5);
-    when(node.getRandom().nextGaussian()).thenReturn(0.1);
-    when(node.getConfig().get("node").getBoolean("probeLocation")).thenReturn(true);
+  private final Node node;
+  private final Listener listener;
+  private final Probe probe;
 
-    // and a listener that throws when receiving an invalid value
-    Listener listener = mock(Listener.class, RETURNS_DEEP_STUBS);
-    doThrow(DoesNotRandomizeLocationException.class).when(listener).onLocation(0.5f);
-
-    // and a probe that runs requests instantly
-    Probe probe = new Probe(node);
-    probe.setTimer(new DirectlyRunningTimer());
-
-    // check that the location is randomized (the listener does not throw)
-    probe.start((byte)1, 12345L, Type.LOCATION, listener);
-
-    // and that the listener does throw when the location is not randomized
-    when(node.getRandom().nextGaussian()).thenReturn(0.0);
-    assertThrows(DoesNotRandomizeLocationException.class,
-        () -> probe.start((byte)1, 12345L, Type.LOCATION, listener));
-  }
 }
