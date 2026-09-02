c767cc993d19eee23ae278a868dbdb802fc9e17e
Arne Babenhauserheide (freenet releases) <arne_bab@web.de>
2026-09-02 22:45:46 +0000

    build-offline.sh: propagate the error from the gradle invocation

--- build-offline.sh
+++ build-offline.sh
@@ -6,6 +6,10 @@ cp build.gradle build.gradle.orig
 sed -i '/com.android.tools.build/d' build.gradle
 
 ./gradlew jar --offline --no-daemon
+RES=$?
 
 mv gradle/wrapper/gradle-wrapper.properties.orig gradle/wrapper/gradle-wrapper.properties
 mv build.gradle.orig build.gradle
+
+# propagate the error from the gradle invocation
+test $RES -eq 0
